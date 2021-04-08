e8fbcdfaed003634fba5c1729901a0ba22af4013
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-04-08 09:01:23 +0300

    Show git commit hash in index page
    Paves the way for showing commit hash in production.

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,14 +52,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>{`Version: ${process.env.NEXT_PUBLIC_GIT_HASH}`}</p>
       </footer>
     </div>
   );
