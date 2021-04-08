24751908ae7402ec18b00bf70c3e4fa5a0288ce3
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-04-08 09:00:23 +0300

    Disable SSR
    We don't need SSR so lets disable it for now.
    https://dev.to/apkoponen/how-to-disable-server-side-rendering-ssr-in-next-js-1563

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+function SafeHydrate({ children }: { children: JSX.Element }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  );
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  );
 }
 
 // eslint-disable-next-line import/no-default-export
