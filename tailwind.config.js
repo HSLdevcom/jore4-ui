22e46e8ea77ff97d216e99bb8cdf7012ed086742
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-04-23 14:24:57 +0300

    Enable `!important` variants for tailwind, improve hover effects on navbar
    Usage of !important was needed in order to be able to override
    border-color property on styles in active NavLinks...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'], // Tailwind can tree-shake unused styles in production in files defined in these directories
   darkMode: false, // or 'media' or 'class'
@@ -10,7 +14,27 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      borderColor: ['important'],
+    },
   },
-  plugins: [],
+  plugins: [
+    // add support for 'important' variant which can be used by prefixing class with "!"
+    // e.g. "!border-white" turns "border-white" class into !important one.
+    // see: https://github.com/tailwindlabs/tailwindcss/issues/493#issuecomment-610907147
+    // !! NOTE: important variants have to be explicitly enabled for rule. List of
+    // rules: https://tailwindcss.com/docs/configuring-variants#default-variants-reference
+    plugin(({ addVariant }) => {
+      addVariant('important', ({ container }) => {
+        container.walkRules((rule) => {
+          // eslint-disable-next-line no-param-reassign
+          rule.selector = `.\\!${rule.selector.slice(1)}`;
+          rule.walkDecls((decl) => {
+            // eslint-disable-next-line no-param-reassign
+            decl.important = true;
+          });
+        });
+      });
+    }),
+  ],
 };
