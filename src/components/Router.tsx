ac96adbe1e71044e6c48f8d5589daea621cd0ce5
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-06-10 10:20:17 +0300

    Use relative import for local module

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { routes } from 'routes';
+import { routes } from '../routes';
 import { Navbar } from './navbar';
 
 export const Router: FunctionComponent = () => {
