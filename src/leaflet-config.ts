0279290b2bdc937adc842c6a78cfd469069c9283
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-06-23 10:09:01 +0300

    Avoid top-level await

--- src/leaflet-config.ts
+++ src/leaflet-config.ts
@@ -7,14 +7,15 @@ if (process.browser) {
   // next.js tries to import it on server and then we get "window is undefined"
   // errors.
 
-  // @ts-expect-error TS compiler isn't happy with top-level await nor .default import, but this seems to work anyway as we have enabled top level awaits in next.js config
-  const L = (await import('leaflet')).default;
+  (async () => {
+    const L = (await import('leaflet')).default;
 
-  // Tell leaflet where to find image assets
-  const DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-  });
+    // Tell leaflet where to find image assets
+    const DefaultIcon = L.icon({
+      iconUrl: icon,
+      shadowUrl: iconShadow,
+    });
 
-  L.Marker.prototype.options.icon = DefaultIcon;
+    L.Marker.prototype.options.icon = DefaultIcon;
+  })();
 }
