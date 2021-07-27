1c18edcbf18bde64ac497b8515ad324094dba257
Jörg Schummer <jorg.schummer@vincit.fi>
2021-07-27 15:50:48 +0300

    Hasura role-based authorization
    - proxy Hasura requests through the dev server to have the browser
      attach the session cookie to each request
    - the useQueryWithRole -hook is provided to request a specific role for
      a Hasura graphql query
    - the installed authRoleMiddleware will pick up the requested role and
      attach it to the graphql request as a request header
    
    Hasura can then be configured to use the jore4-auth webhook. Hasura will
    forward both the session cookie and the requested role header to the
    auth backend webhook, which will authorize the request if the user has a
    permission matching the specified requested role.

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,10 +1,52 @@
-import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  HttpLink,
+  InMemoryCache,
+  split,
+  useQuery,
+} from '@apollo/client';
+import { OperationVariables } from '@apollo/client/core';
 import { WebSocketLink } from '@apollo/client/link/ws';
+import { QueryHookOptions } from '@apollo/client/react/types/types';
 import { getMainDefinition } from '@apollo/client/utilities';
+import { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { DocumentNode } from 'graphql';
+
+const REQUESTED_HASURA_ROLE_HEADER = 'x-hasura-role';
+
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+export function useQueryWithRole<TData = any>(
+  query: DocumentNode | TypedDocumentNode<TData, OperationVariables>,
+  role: string,
+  options?: QueryHookOptions<TData>,
+) {
+  return useQuery(query, {
+    ...options,
+    variables: { ...options?.variables, role },
+  });
+}
+
+const authRoleMiddleware = new ApolloLink((operation, forward) => {
+  // add the requested authorization role to the headers if it is specified
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      ...(operation.variables.role && {
+        [REQUESTED_HASURA_ROLE_HEADER]: operation.variables.role,
+      }),
+    },
+  }));
+
+  return forward(operation);
+});
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:8080/v1/graphql',
+  uri: 'http://localhost:3000/api/hasura/v1/graphql',
 });
+
+const apolloLink = concat(authRoleMiddleware, httpLink);
 
 // because next.js might run this on server-side and websockets aren't
 // supported there, we have to check if we are on browser before
@@ -29,9 +71,9 @@ const link = process.browser
       },
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       wsLink!,
-      httpLink,
+      apolloLink,
     )
-  : httpLink;
+  : apolloLink;
 
 const cache = new InMemoryCache({
   typePolicies: {
