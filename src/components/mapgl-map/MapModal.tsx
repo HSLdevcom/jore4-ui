ccf179ebfca0b81c86b80324671a25bee02d225f
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-09-14 14:34:26 +0300

    Avoid map clipping in modal view

--- src/components/mapgl-map/MapModal.tsx
+++ src/components/mapgl-map/MapModal.tsx
@@ -13,7 +13,11 @@ export const MapModal: React.FC<Props> = ({ isOpen, onClose, className }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} className={className}>
       <MapHeader onClose={onClose} />
-      <Map />
+      {/* Setting height of map component dynamically seems to be tricky.
+      Current 90vh is close enough in most cases, but in small screens map
+      gets clipped by MapHeader and in big screens it might leave empyty space
+      below the map. */}
+      <Map height="90vh" />
     </Modal>
   );
 };
