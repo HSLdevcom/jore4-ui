e798c28394dd95d5672a5a19e2a134c6b6f47ca3
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-10-06 13:35:31 +0300

    Simplify route deletion and modifications
    - Prevent user from drawing more than one route even
      if user is faster than debounced callback
    - As we always have only one route, simplify delete route button
      so that user doesn't need to select route to be deleted anymore.

--- src/components/mapgl-map/DrawRouteLayer.tsx
+++ src/components/mapgl-map/DrawRouteLayer.tsx
@@ -1,8 +1,10 @@
 import composeRefs from '@seznam/compose-react-refs';
 import debounce from 'lodash/debounce';
 import React, {
+  useCallback,
   useContext,
   useImperativeHandle,
+  useMemo,
   useRef,
   useState,
 } from 'react';
@@ -44,19 +46,24 @@ const DrawRouteLayerComponent = (
 ): JSX.Element => {
   const { map } = useContext(MapContext);
   const editorRef = useRef<ExplicitAny>(null);
-
-  const [selectedFeatureIndex, setSelectedFeatureIndex] = useState<
-    number | null
-  >(null);
   const [hasRoute, setHasRoute] = useState(false);
 
-  useImperativeHandle(externalRef, () => ({
-    onDeleteSelectedRoute: () => {
-      editorRef.current.deleteFeatures(selectedFeatureIndex);
-      const routeId = String(selectedFeatureIndex);
+  const onDelete = useCallback(
+    (routeId: string) => {
+      editorRef.current.deleteFeatures(routeId);
       removeRoute(map, routeId);
-      setSelectedFeatureIndex(null);
       setHasRoute(false);
+    },
+    [map],
+  );
+
+  useImperativeHandle(externalRef, () => ({
+    onDeleteRoute: () => {
+      if (hasRoute) {
+        // currently user can draw only one route, so id of it will always be '0'
+        const routeId = '0';
+        onDelete(routeId);
+      }
     },
   }));
 
@@ -72,31 +79,35 @@ const DrawRouteLayerComponent = (
 
   const modeHandler = createModeHandler();
 
-  const onAddRoute = async (e: EditorCallback) => {
-    const addedFeatureIndex = e.data.length - 1;
-    const routeId = String(addedFeatureIndex);
-    const { coordinates } = e.data[addedFeatureIndex].geometry;
-    const res = await getRoute(coordinates);
-    if (res?.routes?.[0]?.geometry) {
-      setHasRoute(true);
-      addRoute(map, routeId, res.routes[0].geometry);
-    }
-  };
+  const onAddRoute = useCallback(
+    async (e: EditorCallback) => {
+      const addedFeatureIndex = e.data.length - 1;
+      const routeId = String(addedFeatureIndex);
+      const { coordinates } = e.data[addedFeatureIndex].geometry;
 
-  const onSelect = (e: EditorCallback) => {
-    if (e.selectedFeatureIndex >= 0) {
-      setSelectedFeatureIndex(e.selectedFeatureIndex);
-    }
-  };
+      const res = await getRoute(coordinates);
+      if (res?.routes?.[0]?.geometry) {
+        addRoute(map, routeId, res.routes[0].geometry);
+      } else {
+        // map matching backend didn't returned valid route. -> remove
+        // also drawn route. Maybe we should show notification to the user
+        // when this happens?
+        onDelete(routeId);
+      }
+    },
+    [map, onDelete],
+  );
+
+  const debouncedOnAddRoute = useMemo(() => debounce(onAddRoute, 500), [
+    onAddRoute,
+  ]);
 
   const onUpdate = (e: EditorCallback) => {
-    if (e.editType === 'addFeature') {
-      // user added new route
-      onAddRoute(e);
-    }
-    if (e.editType === 'movePosition' && selectedFeatureIndex !== undefined) {
-      // user edited existing route
-      onAddRoute(e);
+    if (e.editType === 'addFeature' || e.editType === 'movePosition') {
+      // user added new route or edited existing one
+      setHasRoute(true);
+      // Editor calls onUpdate callback million times when route is being edited. That's why we want to debounce onAddRoute event.
+      debouncedOnAddRoute(e);
     }
   };
 
@@ -121,9 +132,7 @@ const DrawRouteLayerComponent = (
       ref={composeRefs(externalRef, editorRef)}
       clickRadius={12}
       mode={modeHandler}
-      // Editor calls onUpdate callback million times when route is being edited. That's why we want to debounce the event.
-      onUpdate={debounce(onUpdate, 500)}
-      onSelect={onSelect}
+      onUpdate={onUpdate}
     />
   );
 };
