1b467fa6f10af4c2762c5103b1ae773c19437ccd
Tamas Pallos <tamas.pallos@vincit.fi>
2021-10-14 14:24:49 +0300

    Add CI job to verify if the generated graphql schema is up to date
    Also hardcoding the latest hasura version used in UI development so that the generated graphql schema doesn't keep on changing all the time

--- start-dependencies.sh
+++ start-dependencies.sh
@@ -14,7 +14,7 @@ LOCAL_VERSION=$(cat ./docker/RELEASE_VERSION.txt || echo "unknown")
 # download latest version of the docker-compose package in case it has changed
 if [ "$GITHUB_VERSION" != "$LOCAL_VERSION" ]; then
   echo "E2E docker-compose package is not up to date, downloading a new version."
-  curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/e2e-docker-compose.tar.gz --silent | tar -xf - -C ./docker/
+  curl -L https://github.com/HSLdevcom/jore4-flux/releases/download/e2e-docker-compose/e2e-docker-compose.tar.gz --silent | tar -xzf - -C ./docker/
 else
   echo "E2E docker-compose package is up to date, no need to download new version."
 fi
