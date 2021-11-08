ceb49e02ae3941db247f057b932842db84a136f9
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-11-08 14:34:03 +0200

    Implement logics for submitting stop form

--- src/components/map/GraphqlQueryExample.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useMyQueryQuery } from '../../generated/graphql';
-
-export const GraphqlQueryExample: React.FC = () => {
-  const { data } = useMyQueryQuery();
-  // eslint-disable-next-line no-console
-  console.log('Graphql example query result', data);
-  return null;
-};
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,9 @@ export interface Point {
   latitude: number;
   longitude: number;
 }
+
+export enum Direction {
+  Forward = 'forward',
+  Backward = 'backward',
+  BiDirectional = 'bidirectional',
+}
