579c8eea5b53b1568275796fa0f3574f8443728e
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-11-29 14:41:01 +0200

    Update migration to match current schema

--- docker/2524600800000_seed-data/up.sql
+++ docker/2524600800000_seed-data/up.sql
@@ -1,6 +1,6 @@
-INSERT INTO route.line (line_id,name_i18n,short_name_i18n,description_i18n,primary_vehicle_mode)
+INSERT INTO route.line (line_id,name_i18n,short_name_i18n,description_i18n,primary_vehicle_mode, priority)
 VALUES
-  ('101f800c-39ed-4d85-8ece-187cd9fe1c5e','65','Rautatientori - Veräjälaakso','Rautatientori - Kätilöopisto - Veräjälaakso','bus'),
-  ('9058c328-efdd-412c-9b2b-37b0f6a2c6fb','71','Rautatientori - Malmi as.','Rautatientori - Arabia - Malmi as.','bus'),
-  ('bbd1cb29-74c3-4fa1-ac86-918d7fa96fe2','785K','Rautatientori - Nikkilä','Rautatientori - Nikkilä','bus'),
-  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','1234','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus');
+  ('101f800c-39ed-4d85-8ece-187cd9fe1c5e','65','Rautatientori - Veräjälaakso','Rautatientori - Kätilöopisto - Veräjälaakso','bus', 10),
+  ('9058c328-efdd-412c-9b2b-37b0f6a2c6fb','71','Rautatientori - Malmi as.','Rautatientori - Arabia - Malmi as.','bus', 10),
+  ('bbd1cb29-74c3-4fa1-ac86-918d7fa96fe2','785K','Rautatientori - Nikkilä','Rautatientori - Nikkilä','bus', 10),
+  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','1234','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus', 10);
