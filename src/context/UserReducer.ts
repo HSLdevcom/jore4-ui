c77e10d280531308ef61e18b93958abf9f5ed93a
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-12-09 10:56:23 +0200

    Simplify/refactor UserReducer with `immer`
    Inspired by https://hswolff.com/blog/level-up-usereducer-with-immer/

--- src/context/UserReducer.ts
+++ src/context/UserReducer.ts
@@ -1,33 +1,32 @@
+import produce from 'immer';
 import { UserInfo } from '../types/UserInfo';
 import { UserContextData } from './UserContext';
-
-export const GET_USER_INFO_SUCCESS = 'GET_USER_INFO_SUCCESS';
-export const GET_USER_INFO_FAILURE = 'GET_USER_INFO_FAILURE';
 
 export const initialUserState: UserContextData = {
   userInfo: undefined,
   loggedIn: false,
 };
 
-export const userReducer = (
-  state: UserContextData,
-  action: { type: string; payload?: UserInfo },
+type UserInfoActions = 'reset' | 'login_success';
+
+const reducer = (
+  draft: UserContextData,
+  action: {
+    type: UserInfoActions;
+    payload?: UserInfo;
+  },
 ) => {
   const { type, payload } = action;
-
   switch (type) {
-    case GET_USER_INFO_SUCCESS:
-      return {
-        ...state,
-        userInfo: payload,
-        loggedIn: true,
-      };
-    case GET_USER_INFO_FAILURE:
-      return {
-        ...state,
-        ...initialUserState,
-      };
+    case 'login_success':
+      draft.userInfo = payload;
+      draft.loggedIn = true;
+      break;
+    case 'reset':
+      return initialUserState;
     default:
-      return state;
   }
+  return draft;
 };
+
+export const userReducer = produce(reducer);
