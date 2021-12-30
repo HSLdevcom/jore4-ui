3bcca6ae2e4683ba3a467d40985fd3a8a8c336b4
Jörg Schummer <jorg.schummer@vincit.fi>
2021-12-30 14:49:05 +0200

    Allow sample data migration to be downed and upped again

--- docker/2524600800000_seed-data/up.sql
+++ docker/2524600800000_seed-data/up.sql
@@ -3,4 +3,5 @@ VALUES
   ('101f800c-39ed-4d85-8ece-187cd9fe1c5e','65','Rautatientori - Veräjälaakso','Rautatientori - Kätilöopisto - Veräjälaakso','bus','65',10),
   ('9058c328-efdd-412c-9b2b-37b0f6a2c6fb','71','Rautatientori - Malmi as.','Rautatientori - Arabia - Malmi as.','bus','71',10),
   ('bbd1cb29-74c3-4fa1-ac86-918d7fa96fe2','785K','Rautatientori - Nikkilä','Rautatientori - Nikkilä','bus','785K',10),
-  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','1234','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus','1234',10);
+  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','1234','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus','1234',10)
+ON CONFLICT DO NOTHING;
