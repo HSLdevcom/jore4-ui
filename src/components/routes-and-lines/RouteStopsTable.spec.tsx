8260d6ef36ade8e5f8a6957d15b388093cb297b5
Jörg Schummer <jorg.schummer@vincit.fi>
2022-01-18 07:29:07 +0200

    Create page for editing line properties

--- src/components/routes-and-lines/RouteStopsTable.spec.tsx
+++ src/components/routes-and-lines/RouteStopsTable.spec.tsx
@@ -1,8 +1,8 @@
 import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { useGetLineDetailsByIdQuery } from '../../generated/graphql';
-import { mapLineDetailsResult } from '../../graphql/route';
+import { useGetLineDetailsWithRoutesByIdQuery } from '../../generated/graphql';
+import { mapLineDetailsWithRoutesResult } from '../../graphql/route';
 import { render } from '../../utils/test-utils';
 import { RouteStopsTable } from './RouteStopsTable';
 
@@ -12,117 +12,113 @@ describe(`<${RouteStopsTable.name} />`, () => {
   // this response is copy-pasted from the actual graphql response
   const mockResponse = {
     data: {
-      route_line: [
-        {
-          line_id: '101f800c-39ed-4d85-8ece-187cd9fe1c5e',
-          name_i18n: 'Rautatientori - Veräjälaakso',
-          short_name_i18n: 'Rautatientori - Veräjälaakso',
-          description_i18n: 'Rautatientori - Kätilöopisto - Veräjälaakso',
-          primary_vehicle_mode: 'bus',
-          validity_start: '2021-01-01T00:00:00+00:00',
-          validity_end: '2023-12-13T00:00:00+00:00',
-          priority: 10,
-          label: '65',
-          __typename: 'route_line',
-          line_routes: [
-            {
-              route_id: '03d55414-e5cf-4cce-9faf-d86ccb7e5f98',
-              description_i18n: null,
-              starts_from_scheduled_stop_point_id:
-                'e3528755-711f-4e4f-9461-7931a2c4bc6d',
-              ends_at_scheduled_stop_point_id:
-                'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
-              route_shape: {
-                type: 'LineString',
-                crs: {
-                  type: 'name',
-                  properties: { name: 'urn:ogc:def:crs:EPSG::4326' },
-                },
-                coordinates: [
-                  [24.927431159, 60.163633535, 8.809],
-                  [24.929195906, 60.164250755, 8.621],
-                  [24.932010801, 60.165237492, 10.901],
-                  [24.934523102, 60.166118268, 15.564],
-                ],
+      route_line_by_pk: {
+        line_id: '101f800c-39ed-4d85-8ece-187cd9fe1c5e',
+        name_i18n: 'Rautatientori - Veräjälaakso',
+        short_name_i18n: 'Rautatientori - Veräjälaakso',
+        description_i18n: 'Rautatientori - Kätilöopisto - Veräjälaakso',
+        primary_vehicle_mode: 'bus',
+        validity_start: '2021-01-01T00:00:00+00:00',
+        validity_end: '2023-12-13T00:00:00+00:00',
+        priority: 10,
+        label: '65',
+        __typename: 'route_line',
+        line_routes: [
+          {
+            route_id: '03d55414-e5cf-4cce-9faf-d86ccb7e5f98',
+            description_i18n: null,
+            starts_from_scheduled_stop_point_id:
+              'e3528755-711f-4e4f-9461-7931a2c4bc6d',
+            ends_at_scheduled_stop_point_id:
+              'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
+            route_shape: {
+              type: 'LineString',
+              crs: {
+                type: 'name',
+                properties: { name: 'urn:ogc:def:crs:EPSG::4326' },
               },
-              on_line_id: '101f800c-39ed-4d85-8ece-187cd9fe1c5e',
-              validity_start: '2021-01-01T00:00:00+00:00',
-              validity_end: '2023-12-13T00:00:00+00:00',
-              priority: 10,
-              label: '65 itään',
-              direction: 'outbound',
-              __typename: 'route_route',
-              starts_from_scheduled_stop_point: {
-                scheduled_stop_point_id: 'e3528755-711f-4e4f-9461-7931a2c4bc6d',
-                label: 'pysäkki A',
-                validity_start: '2021-01-01T00:00:00+00:00',
-                validity_end: '2023-12-13T00:00:00+00:00',
-                __typename: 'service_pattern_scheduled_stop_point',
-              },
-              ends_at_scheduled_stop_point: {
-                scheduled_stop_point_id: 'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
-                label: 'pysäkki B',
-                validity_start: '2021-01-01T00:00:00+00:00',
-                validity_end: '2023-12-13T00:00:00+00:00',
-                __typename: 'service_pattern_scheduled_stop_point',
-              },
-              route_journey_patterns: [
-                {
-                  journey_pattern_id: '43e1985d-4643-4415-8367-c4a37fbc0a87',
-                  on_route_id: '03d55414-e5cf-4cce-9faf-d86ccb7e5f98',
-                  scheduled_stop_point_in_journey_patterns: [
-                    {
-                      journey_pattern_id:
-                        '43e1985d-4643-4415-8367-c4a37fbc0a87',
-                      scheduled_stop_point_id:
-                        'e3528755-711f-4e4f-9461-7931a2c4bc6d',
-                      scheduled_stop_point_sequence: 0,
-                      is_timing_point: false,
-                      is_via_point: true,
-                      __typename:
-                        'journey_pattern_scheduled_stop_point_in_journey_pattern',
-                      scheduled_stop_point: {
-                        scheduled_stop_point_id:
-                          'e3528755-711f-4e4f-9461-7931a2c4bc6d',
-                        label: 'pysäkki A',
-                        validity_start: '2021-01-01T00:00:00+00:00',
-                        validity_end: '2023-12-13T00:00:00+00:00',
-                        __typename: 'service_pattern_scheduled_stop_point',
-                      },
-                    },
-                    {
-                      journey_pattern_id:
-                        '43e1985d-4643-4415-8367-c4a37fbc0a87',
-                      scheduled_stop_point_id:
-                        'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
-                      scheduled_stop_point_sequence: 1,
-                      is_timing_point: true,
-                      is_via_point: false,
-                      __typename:
-                        'journey_pattern_scheduled_stop_point_in_journey_pattern',
-                      scheduled_stop_point: {
-                        scheduled_stop_point_id:
-                          'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
-                        label: 'pysäkki B',
-                        validity_start: '2021-01-01T00:00:00+00:00',
-                        validity_end: '2023-12-13T00:00:00+00:00',
-                        __typename: 'service_pattern_scheduled_stop_point',
-                      },
-                    },
-                  ],
-                  __typename: 'journey_pattern_journey_pattern',
-                },
+              coordinates: [
+                [24.927431159, 60.163633535, 8.809],
+                [24.929195906, 60.164250755, 8.621],
+                [24.932010801, 60.165237492, 10.901],
+                [24.934523102, 60.166118268, 15.564],
               ],
             },
-          ],
-        },
-      ],
+            on_line_id: '101f800c-39ed-4d85-8ece-187cd9fe1c5e',
+            validity_start: '2021-01-01T00:00:00+00:00',
+            validity_end: '2023-12-13T00:00:00+00:00',
+            priority: 10,
+            label: '65 itään',
+            direction: 'outbound',
+            __typename: 'route_route',
+            starts_from_scheduled_stop_point: {
+              scheduled_stop_point_id: 'e3528755-711f-4e4f-9461-7931a2c4bc6d',
+              label: 'pysäkki A',
+              validity_start: '2021-01-01T00:00:00+00:00',
+              validity_end: '2023-12-13T00:00:00+00:00',
+              __typename: 'service_pattern_scheduled_stop_point',
+            },
+            ends_at_scheduled_stop_point: {
+              scheduled_stop_point_id: 'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
+              label: 'pysäkki B',
+              validity_start: '2021-01-01T00:00:00+00:00',
+              validity_end: '2023-12-13T00:00:00+00:00',
+              __typename: 'service_pattern_scheduled_stop_point',
+            },
+            route_journey_patterns: [
+              {
+                journey_pattern_id: '43e1985d-4643-4415-8367-c4a37fbc0a87',
+                on_route_id: '03d55414-e5cf-4cce-9faf-d86ccb7e5f98',
+                scheduled_stop_point_in_journey_patterns: [
+                  {
+                    journey_pattern_id: '43e1985d-4643-4415-8367-c4a37fbc0a87',
+                    scheduled_stop_point_id:
+                      'e3528755-711f-4e4f-9461-7931a2c4bc6d',
+                    scheduled_stop_point_sequence: 0,
+                    is_timing_point: false,
+                    is_via_point: true,
+                    __typename:
+                      'journey_pattern_scheduled_stop_point_in_journey_pattern',
+                    scheduled_stop_point: {
+                      scheduled_stop_point_id:
+                        'e3528755-711f-4e4f-9461-7931a2c4bc6d',
+                      label: 'pysäkki A',
+                      validity_start: '2021-01-01T00:00:00+00:00',
+                      validity_end: '2023-12-13T00:00:00+00:00',
+                      __typename: 'service_pattern_scheduled_stop_point',
+                    },
+                  },
+                  {
+                    journey_pattern_id: '43e1985d-4643-4415-8367-c4a37fbc0a87',
+                    scheduled_stop_point_id:
+                      'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
+                    scheduled_stop_point_sequence: 1,
+                    is_timing_point: true,
+                    is_via_point: false,
+                    __typename:
+                      'journey_pattern_scheduled_stop_point_in_journey_pattern',
+                    scheduled_stop_point: {
+                      scheduled_stop_point_id:
+                        'f8eace87-7901-4438-bfee-bb6f24f1c4c4',
+                      label: 'pysäkki B',
+                      validity_start: '2021-01-01T00:00:00+00:00',
+                      validity_end: '2023-12-13T00:00:00+00:00',
+                      __typename: 'service_pattern_scheduled_stop_point',
+                    },
+                  },
+                ],
+                __typename: 'journey_pattern_journey_pattern',
+              },
+            ],
+          },
+        ],
+      },
     },
-  } as ReturnType<typeof useGetLineDetailsByIdQuery>;
+  } as ReturnType<typeof useGetLineDetailsWithRoutesByIdQuery>;
 
   test('Renders the route with stops in case it has journery pattern', async () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const line = mapLineDetailsResult(mockResponse)!;
+    const line = mapLineDetailsWithRoutesResult(mockResponse)!;
     const { container, asFragment } = render(
       <BrowserRouter>
         <RouteStopsTable testId={testId} routes={line.line_routes} />
@@ -144,7 +140,7 @@ describe(`<${RouteStopsTable.name} />`, () => {
 
   test('Renders the route without stops in case it does not have a journery pattern', async () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const line = mapLineDetailsResult(mockResponse)!;
+    const line = mapLineDetailsWithRoutesResult(mockResponse)!;
 
     // removing journery pattern
     line.line_routes[0].route_journey_patterns = [];
