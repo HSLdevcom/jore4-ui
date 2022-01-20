99a20e37cca0b32f4172115a23feb6b63db21754
Tamas Pallos <tamas.pallos@vincit.fi>
2022-01-20 08:37:38 +0200

    Create utility for formatting dates on the UI

---
+++ src/time.spec.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon';
+import { i18n } from './i18n';
+import { formatDate } from './time';
+
+describe(`${formatDate.name}()`, () => {
+  beforeEach(() => {
+    i18n.changeLanguage('fi-FI');
+  });
+  const isoDate = '2017-04-20T11:32:00.000Z';
+  test('Should be able to format dates from ISO string inputs', () => {
+    const output = formatDate(DateTime.DATETIME_FULL, isoDate);
+    expect(output).toBe('20. huhtikuuta 2017 klo 14.32 UTC+3');
+  });
+
+  test('Should be able to format dates from luxon inputs', () => {
+    const date = DateTime.fromISO(isoDate);
+    const output = formatDate(DateTime.DATETIME_FULL, date);
+    expect(output).toBe('20. huhtikuuta 2017 klo 14.32 UTC+3');
+  });
+
+  test('Should be able to use default locale from i18n', () => {
+    i18n.changeLanguage('en-US');
+    const output = formatDate(DateTime.DATETIME_FULL, isoDate);
+    expect(output).toBe('April 20, 2017, 2:32 PM GMT+3');
+  });
+
+  test('Should be able to override default locale from i18n', () => {
+    const output = formatDate(DateTime.DATETIME_FULL, isoDate, 'en-US');
+    expect(output).toBe('April 20, 2017, 2:32 PM GMT+3');
+  });
+
+  test('Should always use Finnish time zone, regardless of the input', () => {
+    const date = DateTime.fromISO('2017-04-20T11:32:00.000-04:00');
+    const output = formatDate(DateTime.DATETIME_FULL, date);
+    expect(output).toBe('20. huhtikuuta 2017 klo 18.32 UTC+3');
+  });
+});
