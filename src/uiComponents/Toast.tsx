a7fa53f2199964214261cd429d1e0962bdc4e8ad
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-01-26 11:31:33 +0200

    Remove unneeded whitespaces

--- src/uiComponents/Toast.tsx
+++ src/uiComponents/Toast.tsx
@@ -17,16 +17,16 @@ const propsByType: Record<
     icon: 'icon-info text-white text-3xl',
     textColor: 'text-white',
     bg: 'bg-tweaked-brand',
-    border: ' border-tweaked-brand',
+    border: 'border-tweaked-brand',
   },
   success: {
     icon: 'icon-check-circle text-hsl-dark-green text-3xl',
     textColor: 'text-black',
     bg: 'bg-hsl-dark-green bg-opacity-25',
-    border: ' border-hsl-dark-green',
+    border: 'border-hsl-dark-green',
   },
   danger: {
-    icon: 'icon-alert-filled text-hsl-red text-3xl ',
+    icon: 'icon-alert-filled text-hsl-red text-3xl',
     textColor: 'text-black',
     bg: 'bg-hsl-red bg-opacity-25',
     border: ' border-hsl-red',
@@ -40,11 +40,11 @@ export const Toast = ({
 }: Props): JSX.Element => {
   const { icon, textColor, bg, border } = propsByType[type];
   return (
-    <div className={` bg-white rounded-md ${className}  `}>
+    <div className={`bg-white rounded-md ${className}`}>
       <div className={`${bg} ${border} border rounded-md`}>
         <Row className="items-center ml-16 mr-12 my-6">
           <i className={icon} />
-          <p className={`${textColor} text-sm ml-2 `}>{message}</p>
+          <p className={`${textColor} text-sm ml-2`}>{message}</p>
         </Row>
       </div>
     </div>
