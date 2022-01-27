b993f135b9b7f85f451afa67dad6558118f4017e
Tamas Pallos <tamas.pallos@vincit.fi>
2022-01-27 10:27:53 +0200

    Improve routes and lines main page UI to be closer to Figma design

--- src/components/RoutesTableRow.tsx
+++ src/components/RoutesTableRow.tsx
@@ -1,10 +1,12 @@
 import qs from 'qs';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { MdPinDrop } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { RouteRoute } from '../generated/graphql';
 import { Column, Row } from '../layoutComponents';
 import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
+import { mapToShortDate } from '../time';
 
 interface Props {
   className?: string;
@@ -12,17 +14,18 @@ interface Props {
 }
 
 export const RoutesTableRow = ({ className, route }: Props): JSX.Element => {
+  const { t } = useTranslation();
   const mapUrl = routes[Path.map].getLink();
   const openInMapUrl = `${mapUrl}?${qs.stringify({
     routeId: route.route_id,
   })}`;
   return (
-    <tr className={`border border-l-8 ${className}`}>
-      <td className="py-4 pl-16 pr-4 font-bold">
+    <tr className={`border ${className}`}>
+      <td className="border-l-8 border-hsl-dark-green py-4 pl-16 pr-4 font-bold">
         <Link to={routes[Path.lineDetails].getLink(route.on_line_id)}>
           <Row>
             <Column className="w-1/2">
-              <p className="text-4xl">{route.label}</p>
+              <p className="text-3xl">{route.label}</p>
               <p className="text-lg">
                 {route.starts_from_scheduled_stop_point?.label}
                 {' - '}
@@ -31,10 +34,13 @@ export const RoutesTableRow = ({ className, route }: Props): JSX.Element => {
             </Column>
             <Column className="w-1/2 text-right">
               <p className="text-lg font-bold">
-                Voimassa {route.validity_start} - {route.validity_end}
+                {t('validity.validDuring', {
+                  startDate: mapToShortDate(route.validity_start),
+                  endDate: mapToShortDate(route.validity_end),
+                })}
               </p>
               <p className="text-lg">
-                Muokattu dd.mm.yyyy hh:mm | S. Suunnittelija
+                !Muokattu dd.mm.yyyy hh:mm | S. Suunnittelija
               </p>
             </Column>
           </Row>
