3b2e8aefbc9b6f8ab97a38913cb0e7e8a5fe5903
Tamas Pallos <tamas.pallos@vincit.fi>
2022-01-31 11:05:01 +0200

    Display MIN/MAX dates in case validity period is not defined

--- src/components/RoutesTableRow.tsx
+++ src/components/RoutesTableRow.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { RouteRoute } from '../generated/graphql';
 import { Column, Row } from '../layoutComponents';
 import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
-import { mapToShortDate } from '../time';
+import { mapToShortDate, MAX_DATE, MIN_DATE } from '../time';
 
 interface Props {
   className?: string;
@@ -32,8 +32,8 @@ export const RoutesTableRow = ({ className, route }: Props): JSX.Element => {
               <Column className="w-1/2 text-right">
                 <p className="text-lg font-bold">
                   {t('validity.validDuring', {
-                    startDate: mapToShortDate(route.validity_start),
-                    endDate: mapToShortDate(route.validity_end),
+                    startDate: mapToShortDate(route.validity_start || MIN_DATE),
+                    endDate: mapToShortDate(route.validity_end || MAX_DATE),
                   })}
                 </p>
                 <p className="text-lg">
