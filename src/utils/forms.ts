53b406dfac9c2d8c2f9fb8c1b3c38c248e064710
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-02-08 12:52:37 +0200

    Add helper functions for calculating validityStart/validityEnd

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,3 +1,5 @@
+import { parseISODateString } from '../time';
+
 // Submits form implemented with `react-hook-form` by ref
 // useful in cases where ui designs prevent us from defining
 // submit button inside the form
@@ -7,3 +9,14 @@ export const submitFormByRef = (formRef: ExplicitAny) => {
     new Event('submit', { cancelable: true, bubbles: true }),
   );
 };
+
+// maps ISO date string (yyyy-mm-dd, returned e.g. from <input type="date" />)
+// to validity start DateTime object
+export const mapDateInputToValidityStart = (isoDate: string) =>
+  parseISODateString(isoDate);
+// maps ISO date string (yyyy-mm-dd, returned e.g. from <input type="date" />)
+// to validity end DateTime object
+export const mapDateInputToValidityEnd = (
+  isoDate: string,
+  isIndefinite = false,
+) => (isIndefinite ? null : parseISODateString(isoDate)?.endOf('day'));
