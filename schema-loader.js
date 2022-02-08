cd89ee378f6aaf6c88ac80de8dff7c8c0b449fd7
Tamas Pallos <tamas.pallos@vincit.fi>
2022-02-08 15:24:10 +0200

    Handle not null fields in generated graphql schemas
    We are using graphql-codegen's schema loader to overwrite some of hasura's generated graphql types and set them not nullable.

---
+++ schema-loader.js
@@ -0,0 +1,43 @@
+const { loadSchema } = require('@graphql-tools/load');
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+const { gql } = require('graphql-tag');
+/**
+ * merges custom definitions into the schema defined by hasura, overwriting some types (e.g. making them not nullable)
+ * based on https://github.com/hasura/graphql-engine/issues/3451#issuecomment-846426643
+ */
+module.exports = async (schemaString, config) => {
+  const hasuraSchema = await loadSchema(schemaString, config);
+
+  return mergeTypeDefs(
+    [
+      // Overrides come here...
+      // Note: if the fields/types change in Hasura, these overrides should also be updated
+      gql`
+        # setting fields not-nullable in route.route VIEW
+        type route_route {
+          route_id: uuid!
+          starts_from_scheduled_stop_point_id: uuid!
+          ends_at_scheduled_stop_point_id: uuid!
+          on_line_id: uuid!
+          priority: Int!
+          label: String!
+          direction: String!
+        }
+
+        # setting fields not-nullable in service_pattern.scheduled_stop_point VIEW
+        type service_pattern_scheduled_stop_point {
+          scheduled_stop_point_id: uuid!
+          measured_location: geography!
+          located_on_infrastructure_link_id: uuid!
+          direction: String!
+          label: String!
+          priority: Int!
+        }
+      `,
+      hasuraSchema,
+    ],
+    {
+      ignoreFieldConflicts: true,
+    },
+  );
+};
