3732e6406a92833b4d8b6bacf2a4d4d40c1cccf3
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-03-07 13:41:37 +0200

    Extract creating/editing line business logics to hooks

---
+++ src/hooks/useEditLine.ts
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next';
+import { FormState } from '../components/forms/LineForm';
+import { usePatchLineMutation } from '../generated/graphql';
+import { mapToVariables, showDangerToast, showSuccessToast } from '../utils';
+import { mapFormToMutation } from './useCreateLine';
+
+export const useEditLine = () => {
+  const { t } = useTranslation();
+  const [patchLine] = usePatchLineMutation();
+
+  const worker = async (lineId: UUID, state: FormState) => {
+    const variables = {
+      line_id: lineId,
+      ...mapFormToMutation(state),
+    };
+
+    try {
+      const result = await patchLine(mapToVariables(variables));
+      showSuccessToast(t('lines.saveSuccess'));
+      return result;
+    } catch (err) {
+      showDangerToast(`${t('errors.saveFailed')}, '${err}'`);
+      throw err;
+    }
+  };
+
+  return [worker];
+};
