ec01433f79d803662a0d3d8ff2d09f86d6ef52d5
Tamas Pallos <tamas.pallos@vincit.fi>
2022-03-18 14:19:09 +0200

    Allow showing/hiding unused stops for a the routes in list view

--- src/components/routes-and-lines/RouteStopsTable.tsx
+++ src/components/routes-and-lines/RouteStopsTable.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import { Switch } from '@headlessui/react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { RouteRoute } from '../../generated/graphql';
+import { Toggle } from '../../uiComponents';
 import { RouteStopsSection } from './RouteStopsSection'; // eslint-disable-line import/no-cycle
 
 interface Props {
@@ -9,12 +12,31 @@ interface Props {
 }
 
 export const RouteStopsTable = ({ className, routes, testId }: Props) => {
+  const { t } = useTranslation();
+  const [showUnusedStops, setShowUnusedStops] = useState(true);
+
   return (
-    // setting a fake "height: 1px" so that "height: 100%" would work for the table cells
-    <table className={`h-1 w-full ${className}`} data-testid={testId}>
-      {routes.map((item) => {
-        return <RouteStopsSection key={item.route_id} route={item} />;
-      })}
-    </table>
+    <>
+      <div className="flex items-center">
+        <Switch.Group>
+          <Switch.Label className="my-8 mr-8 font-normal">
+            {t('routes.showUnusedStops')}
+          </Switch.Label>
+          <Toggle checked={showUnusedStops} onChange={setShowUnusedStops} />
+        </Switch.Group>
+      </div>
+      {/* setting a fake "height: 1px" so that "height: 100%" would work for the table cells */}
+      <table className={`h-1 w-full ${className}`} data-testid={testId}>
+        {routes.map((item) => {
+          return (
+            <RouteStopsSection
+              key={item.route_id}
+              route={item}
+              showUnusedStops={showUnusedStops}
+            />
+          );
+        })}
+      </table>
+    </>
   );
 };
