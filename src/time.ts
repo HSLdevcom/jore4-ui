bd4cec7b23735e6724df09b2a67e8e86d50ee473
Tamas Pallos <tamas.pallos@vincit.fi>
2022-03-21 09:29:38 +0200

    #612: Automatically serialize all dates from/to graphql to luxon DateTime format
    Closes HSLdevcom/jore4#612

--- src/time.ts
+++ src/time.ts
@@ -1,20 +1,46 @@
+import isString from 'lodash/isString';
 import { DateTime, Settings } from 'luxon';
 import { i18n } from './i18n';
 
 Settings.defaultZone = 'Europe/Helsinki';
 
-// date formats known by luxon: https://moment.github.io/luxon/#/formatting?id=presets
-export const formatDate = (
-  format: Intl.DateTimeFormatOptions,
-  date?: DateTime | string,
-  locale?: string,
-) => {
+export type DateLike = DateTime | string;
+
+export function isDateLike(input?: unknown): input is DateLike {
+  return DateTime.isDateTime(input) || isString(input);
+}
+
+export const parseDate = (date?: DateLike | null) => {
+  // if null/undefined, return undefined
   if (!date) {
     return undefined;
   }
 
-  // assuming that string inputs are in ISO format
-  const dateTime = DateTime.isDateTime(date) ? date : DateTime.fromISO(date);
+  // if already DateTime, return as is
+  if (DateTime.isDateTime(date)) {
+    return date;
+  }
+
+  // if valid DateTime from date string, return it
+  // can handle yyyy-mm-dd date strings as well
+  const dt = DateTime.fromISO(date);
+  if (dt.isValid) {
+    return dt;
+  }
+
+  throw new Error(`Invalid date input: ${date}`);
+};
+
+// date formats known by luxon: https://moment.github.io/luxon/#/formatting?id=presets
+export const formatDate = (
+  format: Intl.DateTimeFormatOptions,
+  date?: DateLike | null,
+  locale?: string,
+) => {
+  const dateTime = parseDate(date);
+  if (!dateTime) {
+    return undefined;
+  }
 
   // if not explicitly defined, the i18n locale is used
   const dateLocale = locale || i18n.language;
@@ -22,17 +48,14 @@ export const formatDate = (
   return dateTime.setLocale(dateLocale).toLocaleString(format);
 };
 
-export const mapToShortDate = (date?: DateTime | string, locale?: string) =>
+export const mapToShortDate = (date?: DateLike | null, locale?: string) =>
   formatDate(DateTime.DATE_SHORT, date, locale);
 
-export const mapToShortDateTime = (date?: DateTime | string, locale?: string) =>
+export const mapToShortDateTime = (date?: DateLike | null, locale?: string) =>
   formatDate(DateTime.DATETIME_SHORT, date, locale);
 
-export const parseISODateString = (date?: string) =>
-  date ? DateTime.fromISO(date) : undefined;
-
-export const mapToISODate = (date: string) =>
-  date ? DateTime.fromISO(date).toISODate() : undefined;
+export const mapToISODate = (date?: DateLike | null) =>
+  parseDate(date)?.toISODate();
 
 export const MIN_DATE = DateTime.fromISO('1970-01-01').startOf('day');
 export const MAX_DATE = DateTime.fromISO('2050-12-31').endOf('day');
