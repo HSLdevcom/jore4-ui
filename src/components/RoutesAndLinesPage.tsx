a8bc5195578969a24bea4a50ca6e7d0000b93030
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-03-22 12:04:10 +0200

    Refactor RoutesAndLinesPage and add SearchContainer

--- src/components/RoutesAndLinesPage.tsx
+++ src/components/RoutesAndLinesPage.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { MapEditorContext } from '../context/MapEditorContext';
 import { ModalMapContext } from '../context/ModalMapContext';
 import {
-  RouteLine,
-  RouteRoute,
   useListChangingRoutesQuery,
   useListOwnLinesQuery,
 } from '../generated/graphql';
@@ -12,20 +10,35 @@ import { mapListChangingRoutesResult, mapListOwnLinesResult } from '../graphql';
 import { Container, Row } from '../layoutComponents';
 import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
 import { SimpleButton } from '../uiComponents';
-import { LineTableRow } from './LineTableRow'; // eslint-disable-line import/no-cycle
-import { RoutesTable } from './RoutesTable';
-import { RoutesTableRow } from './RoutesTableRow'; // eslint-disable-line import/no-cycle
+import { SearchContainer } from './routes-and-lines/search/conditions/SearchContainer';
+import { LinesResultTable } from './routes-and-lines/search/results/LinesResultTable'; // eslint-disable-line import/no-cycle
+import { RoutesResultTable } from './routes-and-lines/search/results/RoutesResultTable'; // eslint-disable-line import/no-cycle
+
+const RouteResults = () => {
+  const { t } = useTranslation();
+  const changingRoutesResult = useListChangingRoutesQuery();
+  const changingRoutes = mapListChangingRoutesResult(changingRoutesResult);
+
+  const ownLinesResult = useListOwnLinesQuery();
+  const ownLines = mapListOwnLinesResult(ownLinesResult);
+
+  return (
+    <>
+      <h2 className="text-bold mb-14 mt-12 text-4xl">
+        {t('routes.changingRoutes')}
+      </h2>
+      <RoutesResultTable routes={changingRoutes} />
+      <h2 className="text-bold mb-14 mt-12 text-4xl">{t('routes.ownLines')}</h2>
+      <LinesResultTable lines={ownLines} />
+    </>
+  );
+};
 
 export const RoutesAndLinesPage = (): JSX.Element => {
   const { t } = useTranslation();
   const { dispatch: modalMapDispatch } = useContext(ModalMapContext);
   const { dispatch: mapEditorDispatch } = useContext(MapEditorContext);
   const createLineReactRoute = routes[Path.createLine];
-  const changingRoutesResult = useListChangingRoutesQuery();
-  const changingRoutes = mapListChangingRoutesResult(changingRoutesResult);
-  const ownLinesResult = useListOwnLinesQuery();
-  const ownLines = mapListOwnLinesResult(ownLinesResult);
-
   const onOpenModalMap = () => {
     mapEditorDispatch({ type: 'reset' });
     modalMapDispatch({ type: 'open' });
@@ -46,24 +59,8 @@ export const RoutesAndLinesPage = (): JSX.Element => {
           {t('lines.createNew')}
         </SimpleButton>
       </Row>
-      <h2 className="mb-14 mt-12 text-4xl font-bold">
-        {t('routes.changingRoutes')}
-      </h2>
-      {changingRoutes && changingRoutes.length > 0 && (
-        <RoutesTable>
-          {changingRoutes.map((item: RouteRoute) => (
-            <RoutesTableRow key={item.route_id} route={item} />
-          ))}
-        </RoutesTable>
-      )}
-      <h2 className="mb-14 mt-12 text-4xl font-bold">{t('routes.ownLines')}</h2>
-      {ownLines && ownLines.length > 0 && (
-        <RoutesTable>
-          {ownLines.map((item: RouteLine) => (
-            <LineTableRow key={item.line_id} line={item} />
-          ))}
-        </RoutesTable>
-      )}
+      <SearchContainer />
+      <RouteResults />
     </Container>
   );
 };
