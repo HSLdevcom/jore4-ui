23cdde57794f9d7704ffaef21ac1293aaa5da537
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-03-25 16:03:56 +0200

    Refactor CloseIconButton to have optional label
    HSLdevcom/jore4/#655

--- src/uiComponents/CloseIconButton.tsx
+++ src/uiComponents/CloseIconButton.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 interface Props {
   onClick: () => void;
   className?: string;
+  label?: string;
 }
 
-export const CloseIconButton = ({ onClick, className }: Props): JSX.Element => {
+export const CloseIconButton = ({
+  onClick,
+  className,
+  label,
+}: Props): JSX.Element => {
   return (
     <button className={className || ''} type="button" onClick={onClick}>
+      {label}
       <i className="icon-close-large ml-4 text-lg" />
     </button>
   );
