4b3c0eb1e6eb866ea6d8db36f96462ac5ae3b167
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-03-30 15:07:00 +0300

    Move ModalMapContext state to redux
    Related to HSLdevcom/jore4#707

--- src/context/ContextProviders.tsx
+++ src/context/ContextProviders.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { MapEditorContextProvider } from './MapEditor';
 import { MapFilterContextProvider } from './MapFilter';
-import { ModalMapContextProvider } from './ModalMapContextProvider';
 import { UserContextProvider } from './UserContextProvider';
 
 export const ContextProviders: React.FC = ({ children }) => {
   return (
     <UserContextProvider>
-      <ModalMapContextProvider>
-        <MapEditorContextProvider>
-          <MapFilterContextProvider>{children}</MapFilterContextProvider>
-        </MapEditorContextProvider>
-      </ModalMapContextProvider>
+      <MapEditorContextProvider>
+        <MapFilterContextProvider>{children}</MapFilterContextProvider>
+      </MapEditorContextProvider>
     </UserContextProvider>
   );
 };
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,3 +1,4 @@
 export * from './selectors';
 export * from './slices/map';
+export * from './slices/modalMap';
 export * from './store';
