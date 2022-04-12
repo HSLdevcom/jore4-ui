9fc433ae904db8c4cc560af0dc0a27e3a3717f57
Eetu Niemi <eetu.niemi@vincit.fi>
2022-04-12 15:53:43 +0300

    Implement route selection in map view
    Resolves HSLdevcom/jore4#716

--- theme.js
+++ theme.js
@@ -11,6 +11,21 @@ const theme = {
     hslDarkGreen: '#3B7F00',
     hslRed: '#DC0451',
     cityBicycleYellow: '#FCBC19',
+    routes: {
+      bus: '#0074BF',
+      ferry: '#333333',
+      metro: '#FF6319',
+      train: '#8C4799',
+      tram: '#00985F',
+    },
+    stops: {
+      bus: '#004B7B',
+      ferry: '#333333',
+      metro: '#BF4A13',
+      train: '#924C9F',
+      tram: '#336D00',
+    },
+    selectedMapItem: '#DC0451',
   },
 };
 
