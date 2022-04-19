f7c36722e117bd68a2654b944b5bf3adcd828075
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-04-19 13:43:30 +0300

    Use shared component

--- src/components/map/MapHeader.tsx
+++ src/components/map/MapHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Row } from '../../layoutComponents';
+import { CloseIconButton } from '../../uiComponents';
 
 interface Props {
   onClose: () => void;
@@ -11,14 +12,11 @@ export const MapHeader: React.FC<Props> = ({ onClose }) => {
   return (
     <Row className="bg-white px-11 py-4">
       <h2 className="text-2xl font-bold">{t('map.joreMap')}</h2>
-      <button
-        className="ml-auto text-base font-bold text-brand"
-        type="button"
+      <CloseIconButton
+        className="ml-auto font-bold text-brand"
+        label={t('close')}
         onClick={onClose}
-      >
-        {t('close')}
-        <i className="icon-close-large ml-4 text-lg" />
-      </button>
+      />
     </Row>
   );
 };
