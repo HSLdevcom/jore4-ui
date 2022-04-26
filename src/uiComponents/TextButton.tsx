86d3eef83b59a62bbeb583351f404e86817186d0
Tamas Pallos <tamas.pallos@vincit.fi>
2022-04-26 13:13:20 +0300

    Add component for displaying a text button

---
+++ src/uiComponents/TextButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type Props = {
+  active?: boolean;
+  onClick: () => void;
+  className?: string;
+};
+
+export const TextButton: React.FC<Props> = ({
+  active,
+  onClick,
+  children,
+  className,
+}): JSX.Element => (
+  <button
+    onClick={onClick}
+    type="button"
+    className={`${className || ''} active:underline ${
+      active ? 'font-bold underline' : ''
+    }`}
+  >
+    {children}
+  </button>
+);
