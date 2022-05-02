HSLdevcom/jore4-ui

commit 38aed3275498bebff9c41a7a7a162588a9bc0ac7

Refactor route details to own file

src/router/Router.tsx
@@ -1,0 +1,103 @@
+/* eslint-disable no-underscore-dangle */
+import React, { FunctionComponent } from 'react';
+import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
+import { CreateNewLinePage } from '../components/CreateNewLinePage';
+import { Main } from '../components/Main';
+import { ModalMap } from '../components/map/ModalMap';
+import { Navbar } from '../components/navbar';
+import { EditLinePage } from '../components/routes-and-lines/EditLinePage';
+import { EditRoutePage } from '../components/routes-and-lines/EditRoutePage';
+import { LineDetailsPage } from '../components/routes-and-lines/LineDetailsPage';
+import { SearchResultPage } from '../components/routes-and-lines/search/SearchResultPage';
+import { RoutesAndLinesPage } from '../components/RoutesAndLinesPage';
+import { Path } from './routeDetails';
+
+export const Router: FunctionComponent = () => {
+  interface Route {
+    _routerRoute: Path;
+    _exact?: boolean; // When true, will only match if the path matches the location.pathname exactly.
+    component: React.ComponentType;
+  }
+
+  const ExampleResourceRoute: FunctionComponent = () => {
+    const { id } = useParams<{ id: string }>();
+    return React.createElement(
+      'h2',
+      null,
+      `Example resource route. Resouce id: ${id}`,
+    );
+  };
+
+  const FallbackRoute: FunctionComponent = () => {
+    return React.createElement('p', null, `404, page not found`);
+  };
+
+  const routes: Record<Path, Route> = {
+    [Path.root]: {
+      _routerRoute: Path.root,
+      _exact: true,
+      component: Main,
+    },
+    [Path.routes]: {
+      _routerRoute: Path.routes,
+      _exact: true,
+      component: RoutesAndLinesPage,
+    },
+    [Path.routesSearch]: {
+      _routerRoute: Path.routesSearch,
+      _exact: true,
+      component: SearchResultPage,
+    },
+    [Path.editRoute]: {
+      _routerRoute: Path.editRoute,
+      _exact: true,
+      component: EditRoutePage,
+    },
+    [Path.createLine]: {
+      _routerRoute: Path.createLine,
+      _exact: true,
+      component: CreateNewLinePage,
+    },
+    [Path.lineDetails]: {
+      _routerRoute: Path.lineDetails,
+      _exact: true,
+      component: LineDetailsPage,
+    },
+    [Path.editLine]: {
+      _routerRoute: Path.editLine,
+      _exact: true,
+      component: EditLinePage,
+    },
+    [Path.map]: {
+      _routerRoute: Path.map,
+      _exact: true,
+      // @ts-expect-error Something wrong due to forwardRef used in Map component?
+      component: Map,
+    },
+    [Path.exampleResource]: {
+      _routerRoute: Path.exampleResource,
+      component: ExampleResourceRoute,
+    },
+    [Path.fallback]: {
+      _routerRoute: Path.fallback,
+      component: FallbackRoute,
+    },
+  };
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        {Object.values(routes).map((route) => (
+          <Route
+            key={route._routerRoute}
+            path={route._routerRoute}
+            exact={route._exact || false}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+      <ModalMap />
+    </BrowserRouter>
+  );
+};

src/components/Router.tsx
@@ -1,25 +1,0 @@
-/* eslint-disable no-underscore-dangle */
-import React, { FunctionComponent } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { routes } from '../routes';
-import { ModalMap } from './map/ModalMap';
-import { Navbar } from './navbar';
-
-export const Router: FunctionComponent = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        {Object.values(routes).map((route) => (
-          <Route
-            key={route._routerRoute}
-            path={route._routerRoute}
-            exact={route._exact || false}
-            component={route.component}
-          />
-        ))}
-      </Switch>
-      <ModalMap />
-    </BrowserRouter>
-  );
-};

src/components/ConflictResolverModal.tsx
@@ -10,8 +10,7 @@ import {
 } from '../generated/graphql';
 import { mapPriorityToUiName } from '../i18n/uiNameMappings';
 import { Row } from '../layoutComponents';
-// eslint-disable-next-line import/no-cycle
-import { Path, routes } from '../routes';
+import { Path, routeDetails } from '../router/routeDetails';
 import { mapToShortDate } from '../time';
 import { Priority } from '../types/Priority';
 import { CloseIconButton, Modal, SimpleButton } from '../uiComponents';
@@ -47,7 +46,7 @@ export const mapRouteToCommonConflictItem = (
   priority: route.priority,
   label: route.label,
   id: route.route_id,
-  href: routes[Path.editRoute].getLink(route.route_id),
+  href: routeDetails[Path.editRoute].getLink(route.route_id),
 });
 
 export const mapLineToCommonConflictItem = (
@@ -58,7 +57,7 @@ export const mapLineToCommonConflictItem = (
   priority: line.priority,
   label: line.label,
   id: line.line_id,
-  href: routes[Path.lineDetails].getLink(line.line_id),
+  href: routeDetails[Path.lineDetails].getLink(line.line_id),
 });
 
 export const mapStopToCommonConflictItem = (

src/components/CreateNewLinePage.tsx
@@ -8,10 +8,9 @@ import {
 import { mapInsertLineOneResult } from '../graphql';
 import { useCreateLine } from '../hooks';
 import { Container, Row } from '../layoutComponents';
-import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
+import { routeDetails, Path } from '../router/routeDetails';
 import { Priority } from '../types/Priority';
 import { showSuccessToast } from '../utils';
-// eslint-disable-next-line import/no-cycle
 import {
   ConflictResolverModal,
   mapLineToCommonConflictItem,
@@ -55,7 +54,7 @@ export const CreateNewLinePage = (): JSX.Element => {
     // if line was successfully created, redirect to its page
     return (
       <Redirect
-        to={{ pathname: routes[Path.lineDetails].getLink(createdLineId) }}
+        to={{ pathname: routeDetails[Path.lineDetails].getLink(createdLineId) }}
       />
     );
   }

src/components/LineTableRow.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { RouteLine } from '../generated/graphql';
 import { useShowRoutesOnModal } from '../hooks';
 import { Column, Row } from '../layoutComponents';
-import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
+import { Path, routeDetails } from '../router/routeDetails';
 import { mapToShortDate, MAX_DATE, MIN_DATE } from '../time';
 import { IconButton } from '../uiComponents';
 
@@ -32,7 +32,7 @@ export const LineTableRow = ({ className, line }: Props): JSX.Element => {
     <tbody>
       <tr className={`border ${className}`}>
         <td className="border-l-8 border-hsl-dark-green py-4 pl-16 pr-4 font-bold">
-          <Link to={routes[Path.lineDetails].getLink(line.line_id)}>
+          <Link to={routeDetails[Path.lineDetails].getLink(line.line_id)}>
             <Row>
               <Column className="w-1/2">
                 <p className="text-3xl">{line.label}</p>

src/components/RoutesAndLinesPage.tsx
@@ -4,9 +4,9 @@ import { MapEditorContext } from '../context/MapEditor';
 import { useAppDispatch, useMapUrlQuery } from '../hooks';
 import { Container, Row } from '../layoutComponents';
 import { setIsModalMapOpenAction } from '../redux';
-import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
+import { Path, routeDetails } from '../router/routeDetails';
 import { SimpleButton } from '../uiComponents';
-import { RoutesAndLinesLists } from './routes-and-lines/RoutesAndLinesLists'; // eslint-disable-line import/no-cycle
+import { RoutesAndLinesLists } from './routes-and-lines/RoutesAndLinesLists';
 import { SearchContainer } from './routes-and-lines/search/conditions/SearchContainer';
 
 export const RoutesAndLinesPage = (): JSX.Element => {
@@ -14,7 +14,7 @@ export const RoutesAndLinesPage = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const { dispatch: mapEditorDispatch } = useContext(MapEditorContext);
   const { addMapOpenQueryParameter } = useMapUrlQuery();
-  const createLineReactRoute = routes[Path.createLine];
+  const createLineReactRoute = routeDetails[Path.createLine];
   const onOpenModalMap = () => {
     dispatch(setIsModalMapOpenAction(true));
     mapEditorDispatch({ type: 'reset' });

src/components/RoutesTableRow.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { RouteRoute } from '../generated/graphql';
 import { useShowRoutesOnModal } from '../hooks';
 import { Column, Row } from '../layoutComponents';
-import { Path, routes } from '../routes'; // eslint-disable-line import/no-cycle
+import { Path, routeDetails } from '../router/routeDetails';
 import { mapToShortDate, MAX_DATE, MIN_DATE } from '../time';
 import { IconButton } from '../uiComponents';
 
@@ -21,7 +21,7 @@ export const RoutesTableRow = ({ className, route }: Props): JSX.Element => {
     <tbody>
       <tr className={`border ${className}`}>
         <td className="border-l-8 border-hsl-dark-green py-4 pl-16 pr-4 font-bold">
-          <Link to={routes[Path.lineDetails].getLink(route.on_line_id)}>
+          <Link to={routeDetails[Path.lineDetails].getLink(route.on_line_id)}>
             <Row>
               <Column className="w-1/2">
                 <p className="text-3xl">{route.label}</p>

src/components/map/Map.tsx
@@ -18,7 +18,7 @@ import { ObservationDateOverlay } from './ObservationDateOverlay';
 import { DrawRouteLayer, RouteLayer, Routes } from './routes';
 import { RouteStopsOverlay } from './RouteStopsOverlay';
 import { StopFilterOverlay } from './StopFilterOverlay';
-import { Stops } from './stops'; // eslint-disable-line import/no-cycle
+import { Stops } from './stops';
 
 interface Props {
   drawable?: boolean;

commit 96e852092f0f01e4cba9eb74e09518e7b1f7baf4

Fix /map route
Seems like this got broken in commit
38aed3275498bebff9c41a7a7a162588a9bc0ac7
("Refactor route details to own file")

src/router/Router.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
+import { Map } from '../components/map';
 import { ModalMap } from '../components/map/ModalMap';
 import { Navbar } from '../components/navbar';
 import { CreateNewLinePage } from '../components/routes-and-lines/create-line/CreateNewLinePage';