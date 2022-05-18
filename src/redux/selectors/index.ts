bcfe72009be671856b045bd437aec2835a5eea08
Eetu Niemi <eetu.niemi@vincit.fi>
2022-05-18 12:22:38 +0300

    Map edited stop validity to string in redux store
    - Suppress warning about DateTime being non-serializable by converting DateTime to string ISO timestamp when stored in redux, and back to DateTime in selector.

--- src/redux/selectors/index.ts
+++ src/redux/selectors/index.ts
@@ -1,5 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { DateTime } from 'luxon';
+import { mapStoreStopToStop } from '../../graphql';
 import { RootState } from '../store';
 
 export const selectMap = (state: RootState) => state.map;
@@ -13,9 +14,8 @@ export const selectSelectedStopId = createSelector(
   (map) => map.selectedStopId,
 );
 
-export const selectEditedStopData = createSelector(
-  selectMap,
-  (map) => map.editedStopData,
+export const selectEditedStopData = createSelector(selectMap, (map) =>
+  map.editedStopData ? mapStoreStopToStop(map.editedStopData) : undefined,
 );
 
 export const selectIsCreateStopModeEnabled = createSelector(
