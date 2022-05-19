80b4c46cd263da55d463bbfc97c3dd06604e56cd
Eetu Niemi <eetu.niemi@vincit.fi>
2022-05-19 14:45:10 +0300

    Add direction information to route stops overlay

--- src/components/map/RouteStopsOverlay.tsx
+++ src/components/map/RouteStopsOverlay.tsx
@@ -10,6 +10,7 @@ import {
   mapRoutesDetailsResult,
 } from '../../graphql';
 import { getRouteStops, useAppDispatch, useAppSelector } from '../../hooks';
+import { mapDirectionToShortUiName } from '../../i18n/uiNameMappings';
 import {
   selectHasChangesInProgress,
   selectMapEditor,
@@ -130,6 +131,17 @@ export const RouteStopsOverlay = ({ className }: Props) => {
           </div>
         </div>
       </MapOverlayHeader>
+      <div className="flex items-center border-b py-2 px-3">
+        <div className="ml-1 flex h-6 w-6 items-center justify-center rounded-sm bg-brand font-bold text-white">
+          {mapDirectionToShortUiName(routeMetadata.direction)}
+        </div>
+        <div className="ml-2 flex flex-col">
+          <h2 className="text-base font-bold text-black">{routeName}</h2>
+          <div className="text-light text-xs text-gray-500">
+            {routeMetadata.label}
+          </div>
+        </div>
+      </div>
       {stopsToDisplay?.map(
         (routeStop) =>
           routeStop.stop && (
--- src/components/routes-and-lines/line-details/DirectionBadge.tsx
+++ src/components/routes-and-lines/line-details/DirectionBadge.tsx
@@ -1,4 +1,5 @@
 import { RouteDirectionEnum } from '../../../generated/graphql';
+import { mapDirectionToShortUiName } from '../../../i18n/uiNameMappings';
 
 type Props = {
   direction: RouteDirectionEnum;
@@ -8,7 +9,7 @@ export const DirectionBadge = ({ direction }: Props) => {
   return (
     <span className="relative mr-4 flex h-12 w-12 items-center justify-center bg-brand text-white">
       <span data-testid="direction-value">
-        {direction === RouteDirectionEnum.Outbound ? '1' : '2'}
+        {mapDirectionToShortUiName(direction)}
       </span>
       <i className="icon-opposite border-brank absolute -bottom-2 -right-2 rounded-full border bg-white text-sm text-brand" />
     </span>
