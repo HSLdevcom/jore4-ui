d36cb9a1d1c1d9c61bc7c33242761bbe63ef09ed
Eetu Niemi <eetu.niemi@vincit.fi>
2022-05-31 10:01:55 +0300

    Fix map page height
    - Percentage height does not work here, have to use absolute units.
    
    Resolves HSLdevcom/jore4#750
    Resolves HSLdevcom/jore4#786

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,7 +8,14 @@ const navMenuStyles = 'border-l border-black border-opacity-20';
 
 export const Navbar: FunctionComponent = () => {
   return (
-    <nav className="flex flex-row items-center bg-brand">
+    <nav
+      className="flex flex-row items-center bg-brand"
+      /* 
+        Set fixed height manually, so height is known 
+        and can used to calculate desired height for map component in MapPage.tsx
+      */
+      style={{ height: 'var(--navbar-height)' }}
+    >
       <BrandLogo className="pr-5" style={{ marginLeft: 29 }} />
       <NavLinks />
       <UserNavMenu className={`${navMenuStyles} ml-auto`} />
