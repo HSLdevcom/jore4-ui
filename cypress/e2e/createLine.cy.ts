f5e056608fe0fc77b168dd962db414174a6abfaa
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-06-14 16:48:31 +0300

    Improve line creation test
    Now test can be ran multiple times against same db without failing
    to constraint violation in case test was already ran and line with
    same id was already existing.

--- cypress/e2e/createLine.cy.ts
+++ cypress/e2e/createLine.cy.ts
@@ -1,5 +1,11 @@
 import { ConfirmSaveForm, LineForm } from '../pageObjects';
 
+const deleteLineByLabel = (label: string) => {
+  const query = 'DELETE FROM "route"."line" WHERE label=?';
+  cy.task('executeRawDbQuery', { query, bindings: label });
+};
+
+const testLabel = '7327';
 describe('Verify that creating new line works', () => {
   let lineForm: LineForm;
   let confirmSaveForm: ConfirmSaveForm;
@@ -8,9 +14,12 @@ describe('Verify that creating new line works', () => {
     confirmSaveForm = new ConfirmSaveForm();
     cy.mockLogin();
     cy.visit('/lines/create');
+    // delete label we are about to create (if exists) to avoid
+    // possible constraint violation
+    deleteLineByLabel(testLabel);
   });
   it('Creates new line as expected', () => {
-    lineForm.getLabelInput().type('7327');
+    lineForm.getLabelInput().type(testLabel);
     lineForm.getFinnishNameInput().type('Testilinja');
     lineForm.selectTransportTarget('Helsingin sisäinen liikenne');
     lineForm.selectVehicleType('Bussi');
