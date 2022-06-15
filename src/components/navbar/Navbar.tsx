2af0bfec5547ba877b945bc3cb4355017cf66a5a
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-06-15 13:26:45 +0300

    Move LoggedIn check outside of the component
    That makes more sense as now user of the ui component decides
    if she wants to limit visibility of the component.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { LoggedIn } from '../../layoutComponents/LoggedIn';
 import { BrandLogo } from './BrandLogo';
 import { LanguageDropdown } from './LanguageDropdown';
 import { NavLinks } from './NavLinks';
@@ -10,7 +11,9 @@ export const Navbar: FunctionComponent = () => {
   return (
     <nav className="flex flex-row items-center bg-brand">
       <BrandLogo className="pr-5" style={{ marginLeft: 29 }} />
-      <NavLinks />
+      <LoggedIn>
+        <NavLinks />
+      </LoggedIn>
       <UserNavMenu className={`${navMenuStyles} ml-auto`} />
       <LanguageDropdown className={navMenuStyles} />
     </nav>
