7f6a5289d48fc265f1be91228e19a0df42129d77
Tamas Pallos <tamas.pallos@vincit.fi>
2022-06-17 10:09:20 +0300

    Show alerts for routes and stops in line details view

--- src/components/routes-and-lines/line-details/RouteStopsRow.tsx
+++ src/components/routes-and-lines/line-details/RouteStopsRow.tsx
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon';
 import { useTranslation } from 'react-i18next';
 import { MdOutlineHistory } from 'react-icons/md';
 import { ServicePatternScheduledStopPoint } from '../../../generated/graphql';
+import { useAlertsAndHighLights } from '../../../hooks';
 import { Row, Visible } from '../../../layoutComponents';
 import {
   mapToShortDate,
@@ -40,16 +41,22 @@ export const RouteStopsRow = ({
   // is the stop via point on this route's journey pattern?
   const isViaPoint = scheduledStopPointInJourneyPattern?.is_via_point;
 
+  const { getAlertLevel, getAlertStyle } = useAlertsAndHighLights();
+  const alertStyle = getAlertStyle(getAlertLevel(stop));
+
   return (
     <tr
       className={`border border-l-8 ${
         stopBelongsToJourneyPattern ? '' : 'bg-background text-dark-grey'
       } ${className}`}
     >
-      <td className="py-4 pl-16 pr-4" data-testid="stop-row-label">
+      <td
+        className={`${alertStyle.listItemBorder || ''} p-4 pl-16 text-3xl`}
+        data-testid="stop-row-label"
+      >
         {stop.label}
       </td>
-      <td data-testid="stop-row-name">
+      <td className="w-auto" data-testid="stop-row-name">
         <Row className="items-center">
           <span>!Pysäkki X</span>
           <Visible visible={isViaPoint}>
@@ -57,17 +64,21 @@ export const RouteStopsRow = ({
           </Visible>
         </Row>
       </td>
-
-      <td className="pr-16 text-right" data-testid="stop-row-validity-period">
-        {stopBelongsToJourneyPattern
-          ? t('validity.validDuring', {
-              startDate: mapToShortDate(stop.validity_start || MIN_DATE),
-              endDate: mapToShortDate(stop.validity_end || MAX_DATE),
-            })
-          : t('stops.notPartOfRoute')}
+      <td className="p-4" data-testid="stop-row-validity-period">
+        <Row className="items-center justify-end">
+          {stopBelongsToJourneyPattern
+            ? t('validity.validDuring', {
+                startDate: mapToShortDate(stop.validity_start || MIN_DATE),
+                endDate: mapToShortDate(stop.validity_end || MAX_DATE),
+              })
+            : t('stops.notPartOfRoute')}
+          {alertStyle.icon && (
+            <i className={`${alertStyle.icon} ml-2 text-3xl`} />
+          )}
+        </Row>
       </td>
-      <td>
-        <Row className="items-center">
+      <td className="p-4">
+        <Row className="items-center justify-end">
           <span data-testid="stop-row-last-edited">
             !{mapToShortDateTime(DateTime.now())}
           </span>
