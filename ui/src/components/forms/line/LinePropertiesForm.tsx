a43c0c823c5f6a1fad6a2b0fafa9f504ad70a15c
Tamas Pallos <tamas.pallos@vincit.fi>
2022-06-20 16:41:59 +0300

    Add name versions to line form

--- ui/src/components/forms/line/LinePropertiesForm.tsx
+++ ui/src/components/forms/line/LinePropertiesForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { z } from 'zod';
 import {
@@ -6,14 +8,16 @@ import {
   RouteTypeOfLineEnum,
 } from '../../../generated/graphql';
 import { Row } from '../../../layoutComponents';
-import { InputField } from '../common';
+import { AccordionButton } from '../../../uiComponents/AccordionButton';
+import { InputField, localizedStringRequired } from '../common';
 import { LineTypeDropdown } from './LineTypeDropdown';
 import { TransportTargetDropdown } from './TransportTargetDropdown';
 import { VehicleModeDropdown } from './VehicleModeDropdown';
 
 export const schema = z.object({
   label: z.string().min(1),
-  finnishName: z.string().min(1),
+  name: localizedStringRequired,
+  shortName: localizedStringRequired,
   transportTarget: z.nativeEnum(HslRouteTransportTargetEnum),
   primaryVehicleMode: z.nativeEnum(ReusableComponentsVehicleModeEnum),
   typeOfLine: z.nativeEnum(RouteTypeOfLineEnum),
@@ -23,7 +27,11 @@ export type FormState = z.infer<typeof schema>;
 
 const testIds = {
   label: 'LinePropertiesForm:label',
+  showNameVersionsButton: 'LinePropertiesForm:showNameVersionsButton',
   finnishName: 'LinePropertiesForm:finnishName',
+  swedishName: 'LinePropertiesForm:swedishName',
+  finnishShortName: 'LinePropertiesForm:finnishShortName',
+  swedishShortName: 'LinePropertiesForm:swedishShortName',
   transportTargetDropdown: 'transport-target-input',
   vehicleModeDropdown: 'primary-vehicle-mode-input',
   lineTypeDropdown: 'type-of-line-input',
@@ -35,6 +43,17 @@ interface Props {
 
 export const LinePropertiesForm = ({ id, className }: Props): JSX.Element => {
   const { t } = useTranslation();
+  const { getValues } = useFormContext<FormState>();
+
+  // if either own of {Swedish name, Finnish short name, Swedish short name} are not defined, open
+  // the "name versions" section by default
+  const defaultShowNameVersions =
+    !getValues('name.sv_FI') ||
+    !getValues('shortName.fi_FI') ||
+    !getValues('shortName.sv_FI');
+  const [showNameVersions, setShowNameVersions] = useState<boolean>(
+    defaultShowNameVersions,
+  );
 
   return (
     <div id={id || ''} className={className || ''}>
@@ -55,10 +74,50 @@ export const LinePropertiesForm = ({ id, className }: Props): JSX.Element => {
           type="text"
           className="w-1/2"
           translationPrefix="lines"
-          fieldPath="finnishName"
+          fieldPath="name.fi_FI"
           testId={testIds.finnishName}
         />
       </Row>
+      <Row className="mb-5 items-center">
+        <span>{t('lines.showNameVersions')}</span>
+        <AccordionButton
+          className="ml-2"
+          isOpen={showNameVersions}
+          onToggle={setShowNameVersions}
+          testId={testIds.showNameVersionsButton}
+        />
+      </Row>
+      {showNameVersions && (
+        <>
+          <Row className="mb-5 space-x-10">
+            <InputField<FormState>
+              type="text"
+              className="w-1/2"
+              translationPrefix="lines"
+              fieldPath="name.sv_FI"
+              testId={testIds.swedishName}
+            />
+          </Row>
+          <Row className="mb-5 space-x-10">
+            <InputField<FormState>
+              type="text"
+              className="w-1/2"
+              translationPrefix="lines"
+              fieldPath="shortName.fi_FI"
+              testId={testIds.finnishShortName}
+            />
+          </Row>
+          <Row className="mb-5 space-x-10">
+            <InputField<FormState>
+              type="text"
+              className="w-1/2"
+              translationPrefix="lines"
+              fieldPath="shortName.sv_FI"
+              testId={testIds.swedishShortName}
+            />
+          </Row>
+        </>
+      )}
       <Row className="space-x-10">
         <InputField<FormState>
           className="w-1/4"
