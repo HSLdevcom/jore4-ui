HSLdevcom/jore4-ui

commit 42bc6815ace668c4d912bb52124bd3ae25f8f3ba

Fix navbar height from non-logged in users
Nov we have py-5 class both on NavLinks (which is
hidden from non-logged in users) and on LanguageDropdown
(which is always visible) and that keeps height consistent.
py-5 can't be removed from NavLinks as that would
mess up their border bottom alignment.

Resolves HSLdevcom/jore4#844

src/components/navbar/LanguageDropdown.tsx
@@ -22,7 +22,7 @@ export const LanguageDropdown: FunctionComponent<Props> = ({ className }) => {
 
   return (
     <div
-      className={`z-10 self-stretch text-white hover:bg-brand-darker ${className}`}
+      className={`z-10 self-stretch py-5 text-white hover:bg-brand-darker ${className}`}
     >
       <DropdownMenu
         buttonContent={currentLanguageText}

commit 4eea4ef4aa7df092b5cab527cf5d5f975ec5e668

Fix navbar layout
Commit 42bc6815ace668c4d912bb52124bd3ae25f8f3ba broke
white border on Language dropdown.

Now that is fixed. Should work on both non-logged in and
logged in users.

src/components/navbar/LanguageDropdown.tsx
@@ -22,7 +22,7 @@ export const LanguageDropdown: FunctionComponent<Props> = ({ className }) => {
 
   return (
     <div
-      className={`z-10 self-stretch py-5 text-white hover:bg-brand-darker ${className}`}
+      className={`z-10 self-stretch text-white hover:bg-brand-darker ${className}`}
     >
       <DropdownMenu
         buttonContent={currentLanguageText}

src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const navMenuStyles = 'border-l border-black border-opacity-20';
 export const Navbar: FunctionComponent = () => {
   return (
     <nav className="flex flex-row items-center bg-brand">
-      <BrandLogo className="pr-5" style={{ marginLeft: 29 }} />
+      <BrandLogo className="my-3.5 pr-5" style={{ marginLeft: 29 }} />
       <LoggedIn>
         <NavLinks />
       </LoggedIn>