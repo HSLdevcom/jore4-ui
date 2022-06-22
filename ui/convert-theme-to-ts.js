153d57c1353d42c8534d7dfaac4239fdb2bb0450
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-06-22 12:25:25 +0300

    Add missing filename extension & format file as `prettier` wants

--- ui/convert-theme-to-ts.js
+++ ui/convert-theme-to-ts.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 // this script reads our `./theme.js` file and converts it to
 // `./src/generated/theme.ts` file.
@@ -9,10 +11,12 @@
 //   use es module syntax
 // - TS compiler can't infer types of JS but it can for TS files
 
-const fs = require("fs");
-const prettier = require("prettier");
-const path = require("path");
-const theme = require("./theme.js");
+const fs = require('fs');
+const path = require('path');
+// prettier is installed to workspace root
+// eslint-disable-next-line import/no-extraneous-dependencies
+const prettier = require('prettier');
+const theme = require('./theme.js');
 
 const themeStr = JSON.stringify(theme);
 const js = `
@@ -21,9 +25,9 @@ export const theme = ${themeStr}
 
 try {
   fs.writeFileSync(
-    path.resolve(process.cwd(), "./src/generated/theme.ts"),
-    prettier.format(js, { parser: "babel" }),
-    "utf-8"
+    path.resolve(process.cwd(), './src/generated/theme.ts'),
+    prettier.format(js, { parser: 'babel' }),
+    'utf-8',
   );
 } catch (err) {
   console.log(err.message);
