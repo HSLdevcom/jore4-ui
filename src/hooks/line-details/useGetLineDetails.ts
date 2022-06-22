be2b0a205f7813851abc8f5a445e5beb78b2bb54
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-06-22 13:22:44 +0300

    Remove nullability from observation date
    Resolves HSLdevcom/jore4#871

--- src/hooks/line-details/useGetLineDetails.ts
+++ src/hooks/line-details/useGetLineDetails.ts
@@ -122,50 +122,39 @@ export const useGetLineDetails = () => {
   const [getHighestPriorityLineDetails] =
     useGetHighestPriorityLineDetailsWithRoutesAsyncQuery();
   const [line, setLine] = useState<RouteLine>();
-  const [observationDate, setObservationDate] = useState<
-    DateTime | null | undefined
-  >();
+  const [observationDate, setObservationDate] = useState<DateTime>();
 
-  const [selectedDate, showAll] = [
-    queryParams.selectedDate as string,
-    queryParams.showAll === 'true',
-  ];
+  const selectedDate = queryParams.selectedDate as string;
 
   const fetchLineDetails = async () => {
     if (lineDetailsResult) {
       const lineDetails = mapLineDetailsWithRoutesResult(lineDetailsResult);
-      const initialDate = showAll
-        ? undefined
-        : getInitialDate(
-            selectedDate,
-            lineDetails?.validity_start,
-            lineDetails?.validity_end,
-          );
+      const initialDate = getInitialDate(
+        selectedDate,
+        lineDetails?.validity_start,
+        lineDetails?.validity_end,
+      );
 
-      if (showAll) {
-        setLine(lineDetails);
-      } else {
-        const lineByDateResult = await getHighestPriorityLineDetails(
-          constructLineDetailsGqlFilters(lineDetails, initialDate),
-        );
+      const lineByDateResult = await getHighestPriorityLineDetails(
+        constructLineDetailsGqlFilters(lineDetails, initialDate),
+      );
 
-        const lineByDate =
-          mapHighestPriorityLineDetailsWithRoutesResult(lineByDateResult);
+      const lineByDate =
+        mapHighestPriorityLineDetailsWithRoutesResult(lineByDateResult);
 
-        const filteredLine =
-          !showAll && lineByDate
-            ? filterLineDetailsByDate(lineByDate)
-            : undefined;
+      const filteredLine = lineByDate
+        ? filterLineDetailsByDate(lineByDate)
+        : undefined;
 
-        setLine(filteredLine);
-      }
-      setObservationDate(initialDate);
+      setLine(filteredLine);
+
+      setObservationDate(initialDate as DateTime);
     }
   };
   useEffect(() => {
     fetchLineDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id, showAll, selectedDate, lineDetailsResult]);
+  }, [id, selectedDate, lineDetailsResult]);
 
   return {
     line,
