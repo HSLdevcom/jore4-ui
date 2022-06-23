11664b7b18ee3e5da9677e9d18f74eed22478f54
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-06-23 13:14:33 +0300

    Show visualize stop priority on StopPopup

--- ui/src/components/map/stops/StopPopup.tsx
+++ ui/src/components/map/stops/StopPopup.tsx
@@ -6,6 +6,7 @@ import { Popup } from 'react-map-gl';
 import { StopWithLocation } from '../../../graphql';
 import { Column, Row } from '../../../layoutComponents';
 import { mapToShortDate } from '../../../time';
+import { Priority } from '../../../types/Priority';
 import { CloseIconButton, SimpleButton } from '../../../uiComponents';
 import { mapLngLatToPoint } from '../../../utils';
 
@@ -27,6 +28,22 @@ const mapToValidityPeriod = (
   }`;
 };
 
+const mapPriorityToPresentation = (
+  t: TFunction,
+  priority: Priority = Priority.Standard,
+) => {
+  const presentations: Record<Priority, JSX.Element | null> = {
+    [Priority.Standard]: null,
+    [Priority.Temporary]: (
+      <i className="icon-temporary mr-1.5 text-lg text-city-bicycle-yellow" />
+    ),
+    [Priority.Draft]: (
+      <span className="font-bold">{t('priority.draft')} | </span>
+    ),
+  };
+  return presentations[priority];
+};
+
 export const StopPopup = ({
   stop,
   onEdit,
@@ -36,7 +53,7 @@ export const StopPopup = ({
 }: Props): JSX.Element => {
   const { t } = useTranslation();
   // eslint-disable-next-line camelcase
-  const { label, validity_start, validity_end } = stop;
+  const { label, priority, validity_start, validity_end } = stop;
   const location = mapLngLatToPoint(stop.measured_location.coordinates);
   return (
     <Popup
@@ -61,7 +78,12 @@ export const StopPopup = ({
             </Row>
           </Column>
         </Row>
-        <Row>{mapToValidityPeriod(t, validity_start, validity_end)}</Row>
+        <Row>
+          <span className="text-sm">
+            {mapPriorityToPresentation(t, priority)}
+            {mapToValidityPeriod(t, validity_start, validity_end)}
+          </span>
+        </Row>
         <Row className="mt-16">
           <SimpleButton className="h-full !px-3" onClick={onDelete} inverted>
             <MdDelete aria-label={t('map.deleteRoute')} className="text-lg" />
