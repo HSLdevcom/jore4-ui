a3169bc4703899fffa75f05fe1e7919f510d1b9b
Tamas Pallos <tamas.pallos@vincit.fi>
2022-07-05 08:58:19 +0300

    Fix graphql result mappers
    There was a bug where the missing result was always defaulted to a new instance of an empty array, thus resulting in an infinite rendering loop

--- ui/src/graphql/resultMappers.ts
+++ ui/src/graphql/resultMappers.ts
@@ -4,6 +4,11 @@ import { GqlQueryResult, GqlQueryResultData } from './types';
 type RouteLike = Pick<RouteRoute, '__typename'>;
 type QueryRootLike<T> = Pick<QueryRoot, '__typename'> & T;
 
+// Using a static, constant array (instead of `result || []`) as a default value for array queries
+// to avoid infinite render loop caused by constantly changing object reference. This array is
+// constant as we don't want anyone to write to this globally shared variable.
+const emptyArray = [] as const;
+
 // TODO: extend and generalize this for other query results too
 
 // eslint-disable-next-line camelcase
@@ -25,4 +30,4 @@ export const mapRouteResultToRoute = (
 
 export const mapRouteResultToRoutes = (
   result: GqlQueryResult<RouteQueryResult>,
-) => (result.data?.route_route || []) as RouteRoute[];
+) => (result.data?.route_route as RouteRoute[]) || emptyArray;
