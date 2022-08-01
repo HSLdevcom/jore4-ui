1fc5c6bc5db00daccde811829852adb980aa2dd6
Tamas Pallos <tamas.pallos@vincit.fi>
2022-08-01 15:13:54 +0300

    Add clarifying comment to RouteDirection enum

--- ui/src/types/RouteDirection.ts
+++ ui/src/types/RouteDirection.ts
@@ -1,5 +1,6 @@
 import { RouteDirectionEnum } from '../generated/graphql';
 
+// limit route direction enum only to have 'outbound' and 'inbound' values
 export const RouteDirection = {
   Outbound: RouteDirectionEnum.Outbound,
   Inbound: RouteDirectionEnum.Inbound,
