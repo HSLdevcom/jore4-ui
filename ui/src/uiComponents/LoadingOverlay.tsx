74128337142a72f5f761ba9f329a84a833ca290c
Eetu Niemi <eetu.niemi@vincit.fi>
2022-08-02 09:59:31 +0300

    Implement LoadingOverlay component

---
+++ ui/src/uiComponents/LoadingOverlay.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PulseLoader from 'react-spinners/PulseLoader';
+import { theme } from '../generated/theme';
+
+interface LoadingOverlayProps {
+  visible: boolean;
+  testId?: string;
+}
+
+export const LoadingOverlay: React.FC<LoadingOverlayProps> = ({
+  visible,
+  testId = '',
+}) => {
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div
+      className="fixed top-1/2 left-1/2 z-50 h-32 -translate-y-1/2 -translate-x-1/2 cursor-wait border border-light-grey bg-background px-20 shadow-lg"
+      data-testid={testId}
+    >
+      <PulseLoader
+        color={theme.colors.brand}
+        size={25}
+        cssOverride={{ margin: 'auto auto' }}
+        speedMultiplier={0.7}
+      />
+    </div>
+  );
+};
