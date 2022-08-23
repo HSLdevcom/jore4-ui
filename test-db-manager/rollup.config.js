f8d388aba0099a2ede27d7923beeb82ff742603d
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-08-23 13:25:13 +0300

    Update rollup config to emit also .d.ts files
    This paves the way for using code from test-db-manager also on
    cypress module.

--- test-db-manager/rollup.config.js
+++ test-db-manager/rollup.config.js
@@ -1,34 +1,49 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
-const config = {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs', // commonJS
-      sourcemap: true,
-    },
-    // not sure if we actually need esm build, but leaving it here for now
-    // as it doesn't affect build time much at least at this point.
-    {
-      file: 'dist/index.mjs',
-      format: 'esm', // ES Modules
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    nodeResolve({
-      // browser: true,
-    }),
-    commonjs({ include: ['./index.js', 'node_modules/**'] }),
-    typescript(),
-  ],
-};
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      // es output from ts build (ts-dist) is needed for including
+      // TS types (.d.ts files) to output
+      {
+        file: './ts-dist/index.js',
+        format: 'es',
+      },
+      {
+        file: 'dist/index.js',
+        format: 'cjs', // commonJS
+        sourcemap: true,
+      },
+      // not sure if we actually need esm build, but leaving it here for now
+      // as it doesn't affect build time much at least at this point.
+      {
+        file: 'dist/index.mjs',
+        format: 'esm', // ES Modules
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs({ include: ['./index.js', 'node_modules/**'] }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        compilerOptions: { declarationDir: './types' },
+      }),
+    ],
+  },
+  {
+    input: './ts-dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
 
 // eslint-disable-next-line import/no-default-export
 export default config;
