da1d4f422e25a83215cdcdbe95ad13c191829c4a
Eetu Niemi <eetu.niemi@vincit.fi>
2022-08-23 10:29:32 +0300

    Fix map loader showing when map is not open
    - In some rare cases map loader was displayed when map modal was not visible

--- ui/src/components/map/MapLoader.tsx
+++ ui/src/components/map/MapLoader.tsx
@@ -1,9 +1,10 @@
-import { useAppSelector } from '../../hooks';
+import { useAppSelector, useMapQueryParams } from '../../hooks';
 import { selectIsMapOperationLoading } from '../../redux';
 import { LoadingOverlay } from '../../uiComponents';
 
 export const MapLoader = () => {
+  const { isMapOpen } = useMapQueryParams();
   const isLoading = useAppSelector(selectIsMapOperationLoading);
 
-  return <LoadingOverlay visible={isLoading} />;
+  return <LoadingOverlay visible={isMapOpen && isLoading} />;
 };
