f547f7412c2c53da0dab8fffd8228261ae9681cf
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-08-26 11:37:41 +0300

    Add apollo error message handler
    Resolves HSLdevcom/jore4#739

---
+++ ui/src/utils/apolloErrors.ts
@@ -0,0 +1,33 @@
+import { ApolloError } from '@apollo/client';
+
+/** This will get the actual error message from ApolloError.
+ * There are three different types of errors: network error,
+ * graphQLError and client error. Returned message corresponds
+ * to the type of the message. If there are multiple error messages,
+ * they will be all included.
+ */
+export const getApolloErrorMessage = (err: ApolloError) => {
+  if (err.graphQLErrors.length) {
+    const errorMessages = err.graphQLErrors.map((gqlError) => {
+      // If it is an internal error, we get the message from internal object
+      // otherwise it is in the message field.
+      return gqlError.extensions?.internal?.error?.message || gqlError.message;
+    });
+
+    return `GraphQL errors: ${errorMessages.join(' | ')}`;
+  }
+
+  if (err.clientErrors.length) {
+    const errorMessages = err.clientErrors.map(
+      (clientError) => clientError.message,
+    );
+
+    return `Client error: ${errorMessages.join(' | ')}`;
+  }
+
+  if (err.networkError) {
+    return `Network error: ${err.networkError.message}`;
+  }
+
+  return err.message;
+};
