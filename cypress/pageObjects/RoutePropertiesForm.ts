c1e0089b1c145890baf28a76070620fd4f30288f
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-09-01 09:46:00 +0300

    Add database init test-utils and mapItemCreator

--- cypress/pageObjects/RoutePropertiesForm.ts
+++ cypress/pageObjects/RoutePropertiesForm.ts
@@ -1,3 +1,12 @@
+import { RouteDirectionEnum } from '@hsl/jore4-test-db-manager';
+
+export interface RouteFormInfo {
+  finnishName: string;
+  label: string;
+  direction: RouteDirectionEnum;
+  line: string;
+}
+
 export class RoutePropertiesForm {
   getForm() {
     return cy.get('#route-properties-form');
@@ -11,9 +20,19 @@ export class RoutePropertiesForm {
     return cy.getByTestId('RoutePropertiesFormComponent::finnishName');
   }
 
-  selectDirection(direction: string) {
+  selectDirection(direction: RouteDirectionEnum) {
     cy.getByTestId('RoutePropertiesFormComponent::directionDropdown').click();
-    cy.get('li').contains(direction).click();
+
+    switch (direction) {
+      case RouteDirectionEnum.Inbound:
+        cy.get('li').contains('2 - Keskustaan päin').click();
+        break;
+      case RouteDirectionEnum.Outbound:
+        cy.get('li').contains('1 - Keskustasta pois').click();
+        break;
+      default:
+        break;
+    }
   }
 
   selectLine(name: string) {
@@ -21,19 +40,22 @@ export class RoutePropertiesForm {
     cy.get('li').contains(name).click();
   }
 
-  fillRouteProperties(values: {
-    label: string;
-    finnishName: string;
-    direction: string;
-    line: string;
-  }) {
+  fillRouteProperties(values: RouteFormInfo) {
     this.getFinnishNameInput().type(values.finnishName);
     this.getLabelInput().type(values.label);
     this.selectDirection(values.direction);
     this.selectLine(values.line);
   }
 
+  /** Presses the Edit route modal's save button. Can be given forceAction = true
+   * to force the click without waiting for it's actionability (If it's covered
+   * by another element etc.).
+   * https://docs.cypress.io/api/commands/click#Arguments
+   */
   save(forceAction = false) {
-    return cy.getByTestId('Modal::saveButton').click({ force: forceAction });
+    return cy
+      .getByTestId('EditRouteModal')
+      .findByTestId('Modal::saveButton')
+      .click({ force: forceAction });
   }
 }
