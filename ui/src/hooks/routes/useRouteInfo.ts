c1c722b74c616d2b4ba9904e56b58a6dc9cd1996
Eetu Niemi <eetu.niemi@vincit.fi>
2022-09-21 14:28:15 +0300

    Refactor useEditRouteJourneyPattern

--- ui/src/hooks/routes/useRouteInfo.ts
+++ ui/src/hooks/routes/useRouteInfo.ts
@@ -2,7 +2,6 @@ import { gql } from '@apollo/client';
 import flow from 'lodash/flow';
 import { DateTime } from 'luxon';
 import {
-  RouteInfrastructureLinkAlongRoute,
   RouteMetadataFragment,
   RouteStopFieldsFragment,
   ScheduledStopPointDefaultFieldsFragment,
@@ -13,6 +12,7 @@ import { getRouteStopLabels, RouteInfraLink } from '../../graphql';
 import { selectHasChangesInProgress, selectMapEditor } from '../../redux';
 import { useAppSelector } from '../redux';
 import { filterHighestPriorityCurrentStops } from '../stops';
+import { mapInfrastructureLinksAlongRouteToRouteInfraLinks } from './useEditRouteJourneyPattern';
 import { mapRouteFormToInput } from './useEditRouteMetadata';
 import { extractJourneyPatternCandidateStops } from './useExtractRouteFromFeature';
 
@@ -51,14 +51,6 @@ const GQL_GET_LINKS_WITH_STOPS_BY_ROUTE_ID = gql`
     }
   }
 `;
-
-export const mapInfrastructureLinksAlongRouteToRouteInfraLinks = (
-  infraLinks: RouteInfrastructureLinkAlongRoute[],
-) =>
-  infraLinks?.map((link) => ({
-    ...link.infrastructure_link,
-    is_traversal_forwards: link.is_traversal_forwards,
-  })) || [];
 
 const mapInfraLinksAlongRouteWithStopsResultToRouteInfraLinks = flow(
   (result) => result.data?.route_infrastructure_link_along_route,
