ac8ef9b60c2a576f10f86e90fddc043641f5d550
Eetu Niemi <eetu.niemi@vincit.fi>
2022-10-04 10:21:16 +0300

    Extract route/line table row to separate component

--- ui/src/components/routes-and-lines/main/LineTableRow.tsx
+++ ui/src/components/routes-and-lines/main/LineTableRow.tsx
@@ -1,12 +1,7 @@
 import { gql } from '@apollo/client';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import { LineTableRowFragment } from '../../../generated/graphql';
-import { useAlertsAndHighLights, useShowRoutesOnModal } from '../../../hooks';
-import { Column, Row } from '../../../layoutComponents';
-import { Path, routeDetails } from '../../../router/routeDetails';
-import { mapToShortDate, MAX_DATE, MIN_DATE } from '../../../time';
-import { LocatorButton } from '../../../uiComponents';
+import { useShowRoutesOnModal } from '../../../hooks';
+import { RouteLineTableRow } from './RouteLineTableRow';
 
 interface Props {
   className?: string;
@@ -23,48 +18,19 @@ const GQL_LINE_TABLE_ROW = gql`
 `;
 
 export const LineTableRow = ({ className = '', line }: Props): JSX.Element => {
-  const { t } = useTranslation();
   const { showRouteOnMapByLineLabel } = useShowRoutesOnModal();
 
   const showLineRoutes = () => {
     showRouteOnMapByLineLabel(line);
   };
 
-  const { getAlertLevel, getAlertStyle } = useAlertsAndHighLights();
-  const alertStyle = getAlertStyle(getAlertLevel(line));
-
   return (
-    <tbody>
-      <tr className={`border border-light-grey ${className}`}>
-        <td className={`${alertStyle.listItemBorder || ''} w-20 p-4 align-top`}>
-          {alertStyle.icon && <i className={`${alertStyle.icon} text-3xl`} />}
-        </td>
-        <td className="py-4">
-          <Link to={routeDetails[Path.lineDetails].getLink(line.line_id)}>
-            <Row className="items-center">
-              <Column className="w-1/2 font-bold">
-                <p className="text-3xl">{line.label}</p>
-                <p>{line.name_i18n.fi_FI}</p>
-              </Column>
-              <Column className="w-1/2 text-right">
-                <p className="font-bold">
-                  {t('validity.validDuring', {
-                    startDate: mapToShortDate(line.validity_start || MIN_DATE),
-                    endDate: mapToShortDate(line.validity_end || MAX_DATE),
-                  })}
-                </p>
-                <p>!Muokattu dd.mm.yyyy hh:mm | S. Suunnittelija</p>
-              </Column>
-            </Row>
-          </Link>
-        </td>
-        <td className="w-20 p-6 align-middle">
-          <LocatorButton
-            onClick={showLineRoutes}
-            testId="LineTableRow::showLineRoutes"
-          />
-        </td>
-      </tr>
-    </tbody>
+    <RouteLineTableRow
+      rowItem={line}
+      lineId={line.line_id}
+      onLocatorButtonClick={showLineRoutes}
+      locatorButtonTestId="LineTableRow::showLineRoutes"
+      className={className}
+    />
   );
 };
--- ui/src/components/routes-and-lines/main/RouteTableRow.tsx
+++ ui/src/components/routes-and-lines/main/RouteTableRow.tsx
@@ -1,11 +1,6 @@
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import { RouteAllFieldsFragment } from '../../../generated/graphql';
-import { useAlertsAndHighLights, useShowRoutesOnModal } from '../../../hooks';
-import { Column, Row } from '../../../layoutComponents';
-import { Path, routeDetails } from '../../../router/routeDetails';
-import { mapToShortDate, MAX_DATE, MIN_DATE } from '../../../time';
-import { LocatorButton } from '../../../uiComponents';
+import { useShowRoutesOnModal } from '../../../hooks';
+import { RouteLineTableRow } from './RouteLineTableRow';
 
 interface Props {
   className?: string;
@@ -16,51 +11,23 @@ export const RouteTableRow = ({
   className = '',
   route,
 }: Props): JSX.Element => {
-  const { t } = useTranslation();
   const { showRouteOnMapByLabel } = useShowRoutesOnModal();
-
-  const { getAlertLevel, getAlertStyle } = useAlertsAndHighLights();
-  const alertStyle = getAlertStyle(getAlertLevel(route));
 
   const onClickShowRouteOnMap = () => {
     showRouteOnMapByLabel(route);
   };
 
   return (
-    <tbody>
-      <tr className={`border border-light-grey ${className}`}>
-        <td className={`${alertStyle.listItemBorder || ''} w-20 p-4 align-top`}>
-          {alertStyle.icon && <i className={`${alertStyle.icon} text-3xl`} />}
-        </td>
-        <td className="py-4">
-          <Link to={routeDetails[Path.lineDetails].getLink(route.on_line_id)}>
-            <Row className="items-center">
-              <Column className="w-1/2 font-bold">
-                <p className="text-2xl">{route.label}</p>
-                <p>{route.name_i18n?.fi_FI}</p>
-              </Column>
-              <Column className="w-1/2 text-right">
-                <p className="font-bold">
-                  {t('validity.validDuring', {
-                    startDate: mapToShortDate(route.validity_start || MIN_DATE),
-                    endDate: mapToShortDate(route.validity_end || MAX_DATE),
-                  })}
-                </p>
-                <p>!Muokattu dd.mm.yyyy hh:mm | S. Suunnittelija</p>
-              </Column>
-            </Row>
-          </Link>
-        </td>
-        <td className="w-20 p-6 align-middle">
-          <LocatorButton
-            onClick={onClickShowRouteOnMap}
-            disabled={
-              !route.route_shape /* some routes imported from jore3 are missing the geometry */
-            }
-            testId="RouteTableRow::showRoute"
-          />
-        </td>
-      </tr>
-    </tbody>
+    <RouteLineTableRow
+      rowItem={route}
+      lineId={route.on_line_id}
+      onLocatorButtonClick={
+        route.route_shape /* some routes imported from jore3 are missing the geometry */
+          ? onClickShowRouteOnMap
+          : undefined
+      }
+      locatorButtonTestId="RouteTableRow::showRoute"
+      className={className}
+    />
   );
 };
