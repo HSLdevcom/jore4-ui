138b43e131edfde9638336bc8d7dcbcccb2c2309
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-10-06 12:09:32 +0300

    Distinct gql routefilters from linefilters
    If the filtered attribute would be line property, we need to wrap the constructed
    gql property within route_line so that the query will actually query the property
    from the route's line.

--- ui/src/utils/search.ts
+++ ui/src/utils/search.ts
@@ -48,45 +48,72 @@ export const mapToSqlLikeValue = (str: string) => {
   return str.replaceAll('*', '%');
 };
 
-const transformToFilterAttribute = (
+const mapToFilterAttribute = (
   key: string,
   values: {
     value: string | number[];
     operator: string;
     replaceStar: boolean;
+    isLineProperty?: boolean;
   },
+  isRouteFilter: boolean,
 ) => {
-  return {
+  const filter = {
     [key]: {
       [values.operator]: values.replaceStar
         ? mapToSqlLikeValue(values.value as string) || '%'
         : values.value,
     },
   };
+
+  // If creating route filter but the property is for line, we then
+  // wrap the filter in route_line to get the filter from the route's line
+  return isRouteFilter && values.isLineProperty
+    ? { route_line: filter }
+    : filter;
+};
+
+/** Maps the given parameters to GQL filter attributes. Also takes in to
+ * consideration if we are generating route filter. In that case we wrap all
+ * line property filters in to route_line wrapper.
+ */
+const mapConfiguredParamsToFilterAttributes = (
+  configuredParams: SearchParametersGqlConfigurations,
+  isRouteFilter: boolean,
+) => {
+  const filterAttributes = Object.entries(configuredParams).map((entry) => {
+    const [key, value] = entry;
+    return mapToFilterAttribute(key, value, isRouteFilter);
+  });
+
+  // TODO: This Object.assign is fine for now, but this should be changed to
+  // reduce + merge combination when adding the next 'isLineProperty' filter,
+  // because it will result in duplicatekey's and Object.assign will overwrite
+  // duplicate keys instead of merging them.
+  // Converting array [{key: value}, ...] into a single object {key: value, ...}
+  return Object.assign({}, ...filterAttributes);
 };
 
 export const constructGqlFilterObject = (params: SearchConditions) => {
   const configuredParams = setConfigurations(params);
 
-  const result = Object.entries(configuredParams).map((entry) => {
-    const [key, value] = entry;
-    return transformToFilterAttribute(key, value);
-  });
+  const lineFilter = mapConfiguredParamsToFilterAttributes(
+    configuredParams,
+    false,
+  );
 
-  // Converting array [{key: value}, ...] into a single object {key: value, ...}
-  const filter = Object.assign({}, ...result);
+  const routeFilter = mapConfiguredParamsToFilterAttributes(
+    configuredParams,
+    true,
+  );
 
   // TODO: This will be changed to dynamic when the sorting feature is implemented
   // but until then, we should have the sorting by label and validity_start hardcoded
   const orderBy = [{ label: 'asc' }, { validity_start: 'asc' }];
 
   return {
-    lineFilter: {
-      ...filter,
-    },
-    routeFilter: {
-      ...filter,
-    },
+    lineFilter,
+    routeFilter,
     lineOrderBy: orderBy,
     routeOrderBy: orderBy,
   };
