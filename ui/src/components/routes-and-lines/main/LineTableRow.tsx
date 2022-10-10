eaf2102a954366e1d73d6f371082442484a72e40
Eetu Niemi <eetu.niemi@vincit.fi>
2022-10-10 14:25:55 +0300

    Implement route selection in line search results

--- ui/src/components/routes-and-lines/main/LineTableRow.tsx
+++ ui/src/components/routes-and-lines/main/LineTableRow.tsx
@@ -1,11 +1,21 @@
 import { gql } from '@apollo/client';
 import { LineTableRowFragment } from '../../../generated/graphql';
-import { useShowRoutesOnModal } from '../../../hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useShowRoutesOnModal,
+} from '../../../hooks';
+import {
+  deselectRouteLabelsAction,
+  selectExport,
+  selectRouteLabelsAction,
+} from '../../../redux';
 import { RouteLineTableRow } from './RouteLineTableRow';
 
 interface Props {
   className?: string;
   line: LineTableRowFragment;
+  isSelectable?: boolean;
 }
 
 const GQL_LINE_TABLE_ROW = gql`
@@ -17,12 +27,40 @@ const GQL_LINE_TABLE_ROW = gql`
   }
 `;
 
-export const LineTableRow = ({ className = '', line }: Props): JSX.Element => {
+export const LineTableRow = ({
+  className = '',
+  line,
+  isSelectable = false,
+}: Props): JSX.Element => {
   const { showRouteOnMapByLineLabel } = useShowRoutesOnModal();
+  const dispatch = useAppDispatch();
+  const { selectedRouteLabels } = useAppSelector(selectExport);
 
   const showLineRoutes = () => {
     showRouteOnMapByLineLabel(line);
   };
+
+  const onSelectChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selected = event.target.checked;
+    const selectAction = selected
+      ? selectRouteLabelsAction
+      : deselectRouteLabelsAction;
+
+    dispatch(selectAction(line.line_routes.map((route) => route.label)));
+  };
+
+  const hasRoutes = line.line_routes?.length > 0;
+
+  /**
+   * Entire line is selected for export if it has routes and each of those is selected.
+   * Selecting only subset of routes shouldn't be possible, as user can select routes
+   * from line list or from route list, not from both simultaneously.
+   */
+  const isSelected =
+    hasRoutes &&
+    line.line_routes.every((route) =>
+      selectedRouteLabels.includes(route.label),
+    );
 
   return (
     <RouteLineTableRow
@@ -31,6 +69,9 @@ export const LineTableRow = ({ className = '', line }: Props): JSX.Element => {
       onLocatorButtonClick={showLineRoutes}
       locatorButtonTestId="LineTableRow::showLineRoutes"
       className={className}
+      onSelectChanged={isSelectable ? onSelectChanged : undefined}
+      isSelected={isSelected}
+      selectionDisabled={!hasRoutes}
     />
   );
 };
