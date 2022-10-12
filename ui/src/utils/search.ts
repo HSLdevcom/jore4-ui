429f0645dccf2cb2d9c1cb9c827171c6ad72adc5
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-10-12 10:10:30 +0300

    Refine namings and add typings to search
    Search functions and variables were lacking some typings and had
    a lot of misleading namings, which made the code really hard to read.

--- ui/src/utils/search.ts
+++ ui/src/utils/search.ts
@@ -1,5 +1,13 @@
 import { produce } from 'immer';
-import { ReusableComponentsVehicleModeEnum } from '../generated/graphql';
+import {
+  OrderBy,
+  ReusableComponentsVehicleModeEnum,
+  RouteLineBoolExp,
+  RouteLineOrderBy,
+  RouteRouteBoolExp,
+  RouteRouteOrderBy,
+  SearchLinesAndRoutesQueryVariables,
+} from '../generated/graphql';
 import {
   DeserializedQueryStringParameters,
   SearchConditions,
@@ -13,7 +21,7 @@ type SearchParameterValueTypes =
   | number[]
   | ReusableComponentsVehicleModeEnum;
 
-type SearchParametersGqlConfigurations = {
+type SearchParametersGqlOptions = {
   priority: {
     value: Priority[];
     replaceStar: boolean;
@@ -34,10 +42,10 @@ type SearchParametersGqlConfigurations = {
 };
 
 /**
- * GQL filter configuration for label and priority, which are mandatory criteria
+ * GQL filter options for label and priority, which are mandatory criteria
  * to be given for the search.
  */
-const defaultSearchParametersGqlConfig: SearchParametersGqlConfigurations = {
+const defaultSearchParametersGqlOptions: SearchParametersGqlOptions = {
   priority: {
     value: [Priority.Standard, Priority.Temporary],
     replaceStar: false,
@@ -54,15 +62,20 @@ const defaultSearchParametersGqlConfig: SearchParametersGqlConfigurations = {
  * GQL filter configuration for primaryVehicleMode. This is used and placed
  * only if a precise primaryVehicleMode is set as search filter.
  */
-const primaryVehicleModeGqlConfig = {
+const primaryVehicleModeGqlOptions = {
   replaceStar: false,
   operator: '_eq',
   isLineProperty: true,
 };
 
-const setConfigurations = (queryParams: SearchConditions) => {
-  const params: SearchParametersGqlConfigurations = produce(
-    defaultSearchParametersGqlConfig,
+/**
+ * Constructs the search conditions GQL options from configurations defined in this
+ * file and also the value which comes from the SearchConditions query parameters.
+ * The resulting options are used to map the final GQL filter.
+ */
+const constructSearchConditionsGqlOptions = (queryParams: SearchConditions) => {
+  const gqlOptions: SearchParametersGqlOptions = produce(
+    defaultSearchParametersGqlOptions,
     (draft) => {
       draft.priority.value = queryParams.priorities;
       draft.label.value = queryParams.label;
@@ -73,23 +86,35 @@ const setConfigurations = (queryParams: SearchConditions) => {
         queryParams.primaryVehicleMode !== AllOptionEnum.All
       ) {
         draft.primary_vehicle_mode = {
-          ...primaryVehicleModeGqlConfig,
+          ...primaryVehicleModeGqlOptions,
           value: queryParams.primaryVehicleMode,
         };
       }
     },
   );
 
-  return params;
+  return gqlOptions;
 };
 
 export const mapToSqlLikeValue = (str: string) => {
   return str.replaceAll('*', '%');
 };
 
-const mapToFilterAttribute = (
+/**
+ * Maps search condition options in to final single GQL filter object.
+ * Required parameters for this is
+ * * key (property name e.g. 'label')
+ * * value (within options)
+ * * operator (which filtering operator to use in GQL, e.g. '_eq' for equal)
+ * * replaceStar (when the operator is i.e. '_like' we can use this flag to
+ *   replace all '*' to '%' which will mean 'any' in GQL filtering)
+ * There is also a optional parameter 'isLineProperty' which is used to
+ * define if the property should be filtered through 'route_line' property when
+ * mapping route filter (isRouteFilter = true).
+ */
+const mapSearchConditionOptionsToGqlFilter = (
   key: string,
-  values: {
+  options: {
     value: SearchParameterValueTypes;
     operator: string;
     replaceStar: boolean;
@@ -99,62 +124,84 @@ const mapToFilterAttribute = (
 ) => {
   const filter = {
     [key]: {
-      [values.operator]: values.replaceStar
-        ? mapToSqlLikeValue(values.value as string) || '%'
-        : values.value,
+      [options.operator]: options.replaceStar
+        ? mapToSqlLikeValue(String(options.value)) || '%'
+        : options.value,
     },
   };
 
   // If creating route filter but the property is for line, we then
-  // wrap the filter in route_line to get the filter from the route's line
-  return isRouteFilter && values.isLineProperty
+  // wrap the filter in route_line to get the filtering from the route_line property
+  return isRouteFilter && options.isLineProperty
     ? { route_line: filter }
     : filter;
 };
 
-/** Maps the given parameters to GQL filter attributes. Also takes in to
- * consideration if we are generating route filter. In that case we wrap all
+/**
+ * Constructs GQL filters for given search conditions. Also takes in to
+ * consideration if we are constructing route filter. In that case we wrap all
  * line property filters in to route_line wrapper.
  */
-const mapConfiguredParamsToFilterAttributes = (
-  configuredParams: SearchParametersGqlConfigurations,
-  isRouteFilter: boolean,
-) => {
-  const filterAttributes = Object.entries(configuredParams).map((entry) => {
+const constructSearchConditionGqlFilters = ({
+  searchConditions,
+  constructRouteFilter,
+}: {
+  searchConditions: SearchConditions;
+  constructRouteFilter: boolean;
+}): RouteLineBoolExp | RouteRouteBoolExp => {
+  const searchConditionOptions =
+    constructSearchConditionsGqlOptions(searchConditions);
+
+  const gqlFilters = Object.entries(searchConditionOptions).map((entry) => {
     const [key, value] = entry;
-    return mapToFilterAttribute(key, value, isRouteFilter);
+    return mapSearchConditionOptionsToGqlFilter(
+      key,
+      value,
+      constructRouteFilter,
+    );
   });
 
   // TODO: This Object.assign is fine for now, but this should be changed to
   // reduce + merge combination when adding the next 'isLineProperty' filter,
-  // because it will result in duplicatekey's and Object.assign will overwrite
-  // duplicate keys instead of merging them.
+  // because it will result in duplicate keys and Object.assign will overwrite
+  // instead of merging them.
   // Converting array [{key: value}, ...] into a single object {key: value, ...}
-  return Object.assign({}, ...filterAttributes);
+  return Object.assign({}, ...gqlFilters);
 };
 
-export const constructGqlFilterObject = (params: SearchConditions) => {
-  const configuredParams = setConfigurations(params);
+/**
+ * Constructs the search lines and routes GQL query variables from configurations
+ * defined in this file and the given search conditions.
+ */
+export const constructSearchLinesAndRoutesGqlQueryVariables = (
+  searchConditions: SearchConditions,
+): SearchLinesAndRoutesQueryVariables => {
+  const lineFilter = constructSearchConditionGqlFilters({
+    searchConditions,
+    constructRouteFilter: false,
+  });
 
-  const lineFilter = mapConfiguredParamsToFilterAttributes(
-    configuredParams,
-    false,
-  );
+  const routeFilter = constructSearchConditionGqlFilters({
+    searchConditions,
+    constructRouteFilter: true,
+  });
 
-  const routeFilter = mapConfiguredParamsToFilterAttributes(
-    configuredParams,
-    true,
-  );
-
-  // TODO: This will be changed to dynamic when the sorting feature is implemented
+  // TODO: These will be changed to dynamic when the sorting feature is implemented
   // but until then, we should have the sorting by label and validity_start hardcoded
-  const orderBy = [{ label: 'asc' }, { validity_start: 'asc' }];
+  const lineOrderBy: Array<RouteLineOrderBy> = [
+    { label: OrderBy.Asc },
+    { validity_start: OrderBy.Asc },
+  ];
+  const routeOrderBy: Array<RouteRouteOrderBy> = [
+    { label: OrderBy.Asc },
+    { validity_start: OrderBy.Asc },
+  ];
 
   return {
     lineFilter,
     routeFilter,
-    lineOrderBy: orderBy,
-    routeOrderBy: orderBy,
+    lineOrderBy,
+    routeOrderBy,
   };
 };
 
