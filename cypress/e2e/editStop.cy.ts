ea151a70345fd89991a839fc90f733afb6542e22
sr-vala <samuli.rasi@valagroup.com>
2022-10-24 15:18:34 +0300

    Add test that moves a stop on the map
    Resolves HSLdevcom/jore4#994

---
+++ cypress/e2e/editStop.cy.ts
@@ -0,0 +1,107 @@
+import {
+  buildStop,
+  InfraLinkInsertInput,
+  infrastructureLinks,
+  ReusableComponentsVehicleModeEnum,
+  StopInsertInput,
+  vehicleSubmodeOnInfrastructureLink,
+} from '@hsl/jore4-test-db-manager';
+import { ConfirmationDialog, FilterPanel, Map, StopForm } from '../pageObjects';
+import { insertToDbHelper, removeFromDbHelper } from '../utils';
+
+const testCoordinates = [24.925251259734353, 60.16287920585574, 0];
+
+const infraLinks: InfraLinkInsertInput[] = [
+  {
+    ...(infrastructureLinks[0] as InfraLinkInsertInput),
+    shape: {
+      type: 'LineString',
+      coordinates: [testCoordinates, testCoordinates],
+    },
+  },
+];
+
+const vehicleSubmodesOnInfrastructureLink = [
+  vehicleSubmodeOnInfrastructureLink[0],
+];
+
+const stops: StopInsertInput[] = [
+  {
+    ...buildStop({
+      label: 'Move stop test stop',
+      located_on_infrastructure_link_id: infraLinks[0].infrastructure_link_id,
+    }),
+    scheduled_stop_point_id: '68684b40-c4db-4c72-b3b8-c3307dde7a72',
+    measured_location: {
+      type: 'Point',
+      coordinates: testCoordinates,
+    },
+  },
+];
+
+const dbResources = {
+  infraLinks,
+  vehicleSubmodeOnInfrastructureLink: vehicleSubmodesOnInfrastructureLink,
+  stops,
+};
+
+const deleteCreatedResources = () => {
+  removeFromDbHelper(dbResources);
+};
+
+describe('Stop creation tests', () => {
+  let mapFilterPanel: FilterPanel;
+  let map: Map;
+  let confirmationDialog: ConfirmationDialog;
+  let stopForm: StopForm;
+
+  beforeEach(() => {
+    deleteCreatedResources();
+    insertToDbHelper(dbResources);
+
+    mapFilterPanel = new FilterPanel();
+    map = new Map();
+    confirmationDialog = new ConfirmationDialog();
+    stopForm = new StopForm();
+
+    cy.setupTests();
+    cy.mockLogin();
+    map.visit({
+      zoom: 15,
+      lat: testCoordinates[1],
+      lng: testCoordinates[0],
+    });
+  });
+
+  afterEach(() => {
+    deleteCreatedResources();
+  });
+
+  it(
+    'Should move a stop on the map',
+    // Map opening seems to take time, so we increase the timeout
+    { scrollBehavior: 'bottom', defaultCommandTimeout: 10000 },
+    () => {
+      mapFilterPanel.toggleShowStops(ReusableComponentsVehicleModeEnum.Bus);
+
+      map.waitForMapToLoad();
+
+      map.getStopByStopLabel(stops[0].label).click();
+
+      map.stopPopUp.getMoveButton().click();
+
+      map.clickRelativePoint(48, 52);
+
+      confirmationDialog.getConfirmButton().click();
+
+      cy.wait('@gqlEditStop').its('response.statusCode').should('equal', 200);
+
+      map.getStopByStopLabel(stops[0].label).click();
+
+      map.stopPopUp.getEditButton().click();
+
+      stopForm.getLatitudeInput().should('have.value', testCoordinates[1]);
+      stopForm.getLongitudeInput().should('have.value', testCoordinates[0]);
+    },
+  );
+});
