cac099fb8e290f21b5fad1fdf183154c135b4eda
Eetu Niemi <eetu.niemi@vincit.fi>
2022-10-26 15:31:17 +0300

    Use RouteGeometryLayer also when drawing / editing a route
    - Get rid of addRoute function, make drawn / edited route render similarly to existing route

--- ui/src/components/map/routes/index.ts
+++ ui/src/components/map/routes/index.ts
@@ -1,6 +1,7 @@
 export * from './DrawRouteLayer';
 export * from './EditRouteMetadataLayer';
 export * from './EditRouteModal';
+export * from './ExistingRouteGeometryLayer';
 export * from './LinePolygonLayer';
 export * from './RouteEditor';
 export * from './RouteGeometryLayer';
