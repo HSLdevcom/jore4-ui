ffc86f5282988ae251715b87bf924846813c27c8
Eetu Niemi <eetu.niemi@vincit.fi>
2022-10-26 14:39:35 +0300

    Add arrows to drawn route on map
    Resolves HSLdevcom/jore4#647

--- ui/src/utils/map/mapUtils.ts
+++ ui/src/utils/map/mapUtils.ts
@@ -1,3 +1,4 @@
+import { MapRef } from 'react-map-gl';
 // TODO: Can we import these somewhere?
 export type MaplibreGLMap = any; // eslint-disable-line @typescript-eslint/no-explicit-any
 export type Geometry = any; // eslint-disable-line @typescript-eslint/no-explicit-any
@@ -16,4 +17,16 @@ export const createGeometryLineBetweenPoints = (
     type: 'LineString',
     coordinates: [first, second],
   };
+};
+
+export const loadMapAssets = (mapRef: React.RefObject<MapRef>) => {
+  const map = mapRef.current?.getMap();
+
+  map?.loadImage('/img/arrow-right.png', (error: unknown, image: unknown) => {
+    if (error) throw error;
+
+    // Enable sdf to make enable icon coloring.
+    // https://docs.mapbox.com/help/troubleshooting/using-recolorable-images-in-mapbox-maps/
+    if (!map.hasImage('arrow')) map.addImage('arrow', image, { sdf: true });
+  });
 };
