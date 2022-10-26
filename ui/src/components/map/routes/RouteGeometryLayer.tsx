ee43adad176c6b0e4777b3e248c25f77b562e346
Eetu Niemi <eetu.niemi@vincit.fi>
2022-10-26 14:39:35 +0300

    Add arrows to drawn route on map
    Resolves HSLdevcom/jore4#647

--- ui/src/components/map/routes/RouteGeometryLayer.tsx
+++ ui/src/components/map/routes/RouteGeometryLayer.tsx
@@ -1,4 +1,5 @@
 import { theme } from '../../../generated/theme';
+import { LineArrowLayer } from './LineArrowLayer';
 import { LinePolygonLayer } from './LinePolygonLayer';
 
 const { colors } = theme;
@@ -19,19 +20,44 @@ export const RouteGeometryLayer = ({
 }: Props): JSX.Element => {
   const beforeId = isHighlighted ? undefined : 'route_base';
 
-  const paint: mapboxgl.LinePaint = {
-    'line-color': isHighlighted ? colors.selectedMapItem : defaultColor,
+  const color = isHighlighted ? colors.selectedMapItem : defaultColor;
+
+  // Offset line to right side of the infra link
+  const lineOffset = 6;
+
+  const linePaint: mapboxgl.LinePaint = {
+    'line-color': color,
     'line-width': isHighlighted ? 9 : 8,
-    'line-opacity': isHighlighted ? 1 : 0.75,
-    'line-offset': 6,
+    'line-offset': lineOffset,
   };
 
+  const arrowLayout: mapboxgl.SymbolLayout = {
+    'icon-offset': [0, lineOffset],
+  };
+
+  const arrowPaint: mapboxgl.SymbolPaint = {
+    'icon-color': color,
+  };
+
+  const lineLayerId = `${layerId}_line`;
+  const arrowLayerId = `${layerId}_arrows`;
+
   return (
-    <LinePolygonLayer
-      layerId={layerId}
-      geometry={geometry}
-      paint={paint}
-      beforeId={beforeId}
-    />
+    <>
+      <LinePolygonLayer
+        layerId={lineLayerId}
+        geometry={geometry}
+        paint={linePaint}
+        beforeId={beforeId}
+      />
+      <LineArrowLayer
+        layerId={arrowLayerId}
+        geometry={geometry}
+        layout={arrowLayout}
+        paint={arrowPaint}
+        beforeId={lineLayerId}
+        minzoom={12}
+      />
+    </>
   );
 };
