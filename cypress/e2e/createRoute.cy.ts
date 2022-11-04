e13979d985bcee7ce43895cd3ac29e69fcc2da30
sr-vala <samuli.rasi@valagroup.com>
2022-11-04 15:12:11 +0200

    Add test for creating a route with an indefinite validity end time
    Resolves HSLdevcom/jore4#1020

--- cypress/e2e/createRoute.cy.ts
+++ cypress/e2e/createRoute.cy.ts
@@ -1,12 +1,102 @@
 import {
+  buildStop,
+  StopInsertInput,
+  buildLine,
+  LineInsertInput,
   ReusableComponentsVehicleModeEnum,
   RouteDirectionEnum,
 } from '@hsl/jore4-test-db-manager';
+import { DateTime } from 'luxon';
 import { Map, RouteEditor } from '../pageObjects';
 import { FilterPanel } from '../pageObjects/FilterPanel';
 import { MapItemCreator } from '../pageObjects/MapItemCreator';
 import { RouteStopsOverlay } from '../pageObjects/RouteStopsOverlay';
+import { insertToDbHelper, removeFromDbHelper } from '../utils';
 import { deleteRoutesByLabel } from './utils';
+
+const testCoordinates1 = [24.92492146851626, 60.1634759878872, 0];
+const testCoordinates2 = [24.92904198486008, 60.16490775039894, 0];
+const testCoordinates3 = [24.932072417514647, 60.166003223527824, 0];
+
+const testRouteLabels = {
+  label1: 'T-reitti 1',
+  label2: 'T-reitti 2',
+  label3: 'T-reitti 3',
+  label4: 'Indefinite end time route',
+};
+
+const lines: LineInsertInput[] = [
+  {
+    ...buildLine({ label: '1 Test line 1' }),
+    line_id: '88f8f9fe-058b-49a2-ac8d-42d13488c7fb',
+  },
+  {
+    ...buildLine({ label: '1 Test line 2' }),
+    line_id: 'c0e4e702-60b6-4b90-9313-e463814a9422',
+  },
+  {
+    ...buildLine({ label: '1 Test line 3' }),
+    line_id: '71e19f0a-6eb3-40f2-9818-fb8ea5be135e',
+  },
+  {
+    ...buildLine({ label: '1 Line with indefinite end time' }),
+    line_id: 'ecbd895b-a720-4211-849f-ca380465c838',
+  },
+];
+
+const stops: StopInsertInput[] = [
+  {
+    ...buildStop({
+      label: 'Test stop 1',
+      located_on_infrastructure_link_id: '73bc2df9-f5af-4c38-a1dd-5ed1f71c90a8',
+    }),
+    validity_start: DateTime.fromISO('2020-03-20T22:00:00+00:00'),
+    scheduled_stop_point_id: '3f23a4c5-f527-4395-bd9f-bbc398f837df',
+    measured_location: {
+      type: 'Point',
+      coordinates: testCoordinates1,
+    },
+  },
+  {
+    ...buildStop({
+      label: 'Test stop 2',
+      located_on_infrastructure_link_id: 'ea69415a-9c54-4327-8836-f38b36d8fa99',
+    }),
+    validity_start: DateTime.fromISO('2020-03-20T22:00:00+00:00'),
+    scheduled_stop_point_id: '431a6791-f1f5-45d4-8c9d-9e154a2531e0',
+    measured_location: {
+      type: 'Point',
+      coordinates: testCoordinates2,
+    },
+  },
+  {
+    ...buildStop({
+      label: 'Test stop 3',
+      located_on_infrastructure_link_id: '13de61c2-3fc9-4255-955f-0a2350c389e1',
+    }),
+    validity_start: DateTime.fromISO('2020-03-20T22:00:00+00:00'),
+    scheduled_stop_point_id: '6c09b8d9-5952-4ee3-92b9-a7b4847517d3',
+    measured_location: {
+      type: 'Point',
+      coordinates: testCoordinates3,
+    },
+  },
+];
+
+const dbResources = {
+  lines,
+  stops,
+};
+
+const stopTestIds = {
+  testStop1: `Map::Stops::stopMarker::${stops[0].label}_Standard`,
+  testStop2: `Map::Stops::stopMarker::${stops[1].label}_Standard`,
+  testStop3: `Map::Stops::stopMarker::${stops[2].label}_Standard`,
+};
+
+const deleteCreatedResources = () => {
+  removeFromDbHelper(dbResources);
+};
 
 describe('Route creation', () => {
   let mapCreator: MapItemCreator;
@@ -18,44 +108,31 @@ describe('Route creation', () => {
     mapCreator = new MapItemCreator();
     const mapFilterPanel = new FilterPanel();
 
+    deleteCreatedResources();
+    deleteRoutesByLabel(Object.values(testRouteLabels));
+    insertToDbHelper(dbResources);
+
     map = new Map();
     routeStopsOverlay = new RouteStopsOverlay();
     routeEditor = new RouteEditor();
     cy.setupTests();
     cy.mockLogin();
 
-    cy.visit(
-      '/routes?mapOpen=true&lng=24.93021804533524&lat=60.164074274478054&z=15',
-    );
+    const mapLocation = [24.929689228090112, 60.16495016651525];
+
+    map.visit({
+      zoom: 15,
+      lat: mapLocation[1],
+      lng: mapLocation[0],
+    });
 
     mapFilterPanel.toggleShowStops(ReusableComponentsVehicleModeEnum.Bus);
 
     map.waitForMapToLoad();
   });
 
-  const testRouteLabels = {
-    label1: 'T-reitti 1',
-    label2: 'T-reitti 2',
-    label3: 'T-reitti 3',
-  };
-
-  // TODO: These stops (H1234, 1236) are currently coming from seed data and
-  // stops used by tests will be populated with test-db-manager in the future
-
-  const testStop1 = {
-    testId: 'Map::Stops::stopMarker::H1234_Standard',
-    label: 'H1234',
-  };
-  const testStop2 = {
-    testId: 'Map::Stops::stopMarker::H1236_Standard',
-    label: 'H1236',
-  };
-
-  beforeEach(() => {
-    deleteRoutesByLabel(Object.values(testRouteLabels));
-  });
-
-  after(() => {
+  afterEach(() => {
+    deleteCreatedResources();
     deleteRoutesByLabel(Object.values(testRouteLabels));
   });
 
@@ -70,20 +147,20 @@ describe('Route creation', () => {
           finnishName: routeName,
           label: testRouteLabels.label1,
           direction: RouteDirectionEnum.Outbound,
-          line: '65',
+          line: String(lines[0].label),
         },
         validityStartISODate: '2022-01-01',
-        validityEndISODate: '2022-12-01',
+        validityEndISODate: '2025-12-01',
         routePoints: [
           {
             rightOffset: -10,
             downOffset: 25,
-            mapMarkerTestId: testStop1.testId,
+            mapMarkerTestId: stopTestIds.testStop1,
           },
           {
             rightOffset: 35,
             downOffset: -20,
-            mapMarkerTestId: testStop2.testId,
+            mapMarkerTestId: stopTestIds.testStop3,
           },
         ],
       });
@@ -101,26 +178,26 @@ describe('Route creation', () => {
     { scrollBehavior: 'bottom', defaultCommandTimeout: 10000 },
     () => {
       const routeName = 'Testireitti 2';
-      const omittedStopsLabels = ['H1235'];
+      const omittedStopsLabels = [stops[1].label];
       mapCreator.createRoute({
         routeFormInfo: {
           finnishName: routeName,
           label: testRouteLabels.label2,
-          direction: RouteDirectionEnum.Outbound,
-          line: '65',
+          direction: RouteDirectionEnum.Inbound,
+          line: String(lines[1].label),
         },
         validityStartISODate: '2022-01-01',
-        validityEndISODate: '2022-12-01',
+        validityEndISODate: '2025-12-01',
         routePoints: [
           {
             rightOffset: -10,
             downOffset: 25,
-            mapMarkerTestId: testStop1.testId,
+            mapMarkerTestId: stopTestIds.testStop1,
           },
           {
             rightOffset: 35,
             downOffset: -20,
-            mapMarkerTestId: testStop2.testId,
+            mapMarkerTestId: stopTestIds.testStop3,
           },
         ],
         omittedStops: omittedStopsLabels,
@@ -141,32 +218,68 @@ describe('Route creation', () => {
     { scrollBehavior: 'bottom', defaultCommandTimeout: 10000 },
     () => {
       const routeName = 'Testireitti 3';
-      const omittedStopsLabels = ['H1235', 'H1234'];
+      const omittedStopsLabels = [stops[1].label, stops[2].label];
       mapCreator.createRoute({
         routeFormInfo: {
           finnishName: routeName,
           label: testRouteLabels.label3,
           direction: RouteDirectionEnum.Outbound,
-          line: '65',
+          line: String(lines[2].label),
         },
         validityStartISODate: '2022-01-01',
-        validityEndISODate: '2022-12-01',
+        validityEndISODate: '2025-12-01',
         routePoints: [
           {
             rightOffset: -10,
             downOffset: 25,
-            mapMarkerTestId: testStop1.testId,
+            mapMarkerTestId: stopTestIds.testStop1,
           },
           {
             rightOffset: 35,
             downOffset: -20,
-            mapMarkerTestId: testStop2.testId,
+            mapMarkerTestId: stopTestIds.testStop3,
           },
         ],
         omittedStops: omittedStopsLabels,
       });
 
       routeEditor.checkRouteSubmitFailure();
+    },
+  );
+
+  it(
+    'Should create new route with an indefinite validity end date',
+    { scrollBehavior: 'bottom', defaultCommandTimeout: 10000 },
+    () => {
+      const routeName = 'Testireitti 4';
+
+      mapCreator.createRoute({
+        routeFormInfo: {
+          finnishName: routeName,
+          label: testRouteLabels.label4,
+          direction: RouteDirectionEnum.Outbound,
+          line: String(lines[3].label),
+        },
+        validityStartISODate: '2022-01-01',
+        routePoints: [
+          {
+            rightOffset: -10,
+            downOffset: 30,
+            mapMarkerTestId: stopTestIds.testStop1,
+          },
+          {
+            rightOffset: 35,
+            downOffset: -10,
+            mapMarkerTestId: stopTestIds.testStop3,
+          },
+        ],
+      });
+
+      routeEditor.gqlRouteShouldBeCreatedSuccessfully();
+
+      routeEditor.checkRouteSubmitSuccess();
+
+      routeStopsOverlay.routeShouldExist(routeName);
     },
   );
 });
