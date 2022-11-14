bb9b5de38efc6a8059c7579e69e5ea4ee1584ead
sr-vala <samuli.rasi@valagroup.com>
2022-11-14 14:20:27 +0200

    Stop using seed data in tests
    Resolves HSLdevcom/jore4#1019

--- cypress/pageObjects/RouteStopsOverlay.ts
+++ cypress/pageObjects/RouteStopsOverlay.ts
@@ -21,10 +21,17 @@ export class RouteStopsOverlay {
     return this.getHeader().get('div').contains(routeName).should('exist');
   }
 
+  // Note: array of stopLabels should be ordered so that the lowest one is listed first.
+  // This way the upper menu does not cover the lower stop's menu button.
+  // TODO: Enforce this somehow in the code, or find out root cause of failure in CI.
   removeStopsFromRoute(stopLabels: string[]) {
     stopLabels.forEach((label) => {
+      this.getRouteStopRowMenu(label).should('exist');
+      this.getRouteStopRowMenu(label).should('be.visible');
       this.getRouteStopRowMenu(label).click();
-      this.getAddToJourneyPatternButton(label).click();
+      this.getAddToJourneyPatternButton(label).should('exist');
+      this.getAddToJourneyPatternButton(label).should('be.visible');
+      this.getAddToJourneyPatternButton(label).click({ force: true });
     });
   }
 
