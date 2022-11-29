8a6200027fbdc9241aedea4d042b479e1a7ca835
Eetu Niemi <eetu.niemi@vincit.fi>
2022-11-29 14:15:41 +0200

    Allow editing route variant information

--- ui/src/utils/gql.ts
+++ ui/src/utils/gql.ts
@@ -55,6 +55,11 @@ export const buildLabelGqlFilter = (label?: string) => ({
   label: { _eq: label },
 });
 
+/** Builds an object for gql to filter by variant */
+export const buildVariantGqlFilter = (variant?: number | null) => ({
+  variant: variant === null ? { _is_null: true } : { _eq: variant },
+});
+
 /** Builds an object for gql to filter by label using the '_like' operator.
  * This will means that all the '%' in the label are considered as 'any'
  */
