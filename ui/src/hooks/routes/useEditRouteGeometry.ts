944a1a0bcc15d6ac677a78a58be8700f9b83fa38
Eetu Niemi <eetu.niemi@vincit.fi>
2022-12-08 14:59:36 +0200

    Replace journey pattern stops instead of journey pattern in route edit
    Resolves HSLdevcom/jore4#1056

--- ui/src/hooks/routes/useEditRouteGeometry.ts
+++ ui/src/hooks/routes/useEditRouteGeometry.ts
@@ -1,7 +1,8 @@
+import { gql } from '@apollo/client';
 import { useTranslation } from 'react-i18next';
 import {
   GetRouteWithInfrastructureLinksDocument,
-  JourneyPatternJourneyPatternInsertInput,
+  JourneyPatternScheduledStopPointInJourneyPatternInsertInput,
   JourneyPatternStopFragment,
   RouteInfrastructureLinkAlongRouteInsertInput,
   RouteStopFieldsFragment,
@@ -19,8 +20,63 @@ import {
 } from '../../utils';
 import { useValidateRoute } from './useValidateRoute';
 
+const GQL_UPDATE_ROUTE_GEOMETRY = gql`
+  mutation UpdateRouteGeometry(
+    $route_id: uuid!
+    $journey_pattern_id: uuid!
+    $new_infrastructure_links: [route_infrastructure_link_along_route_insert_input!]!
+    $new_stops_in_journey_pattern: [journey_pattern_scheduled_stop_point_in_journey_pattern_insert_input!]!
+  ) {
+    delete_route_infrastructure_link_along_route(
+      where: { route_id: { _eq: $route_id } }
+    ) {
+      returning {
+        infrastructure_link_id
+        infrastructure_link_sequence
+        route_id
+      }
+    }
+
+    insert_route_infrastructure_link_along_route(
+      objects: $new_infrastructure_links
+    ) {
+      returning {
+        route_id
+        infrastructure_link_id
+        infrastructure_link_sequence
+        infrastructure_link {
+          infrastructure_link_id
+          shape
+        }
+        is_traversal_forwards
+      }
+    }
+
+    delete_journey_pattern_scheduled_stop_point_in_journey_pattern(
+      where: { journey_pattern_id: { _eq: $journey_pattern_id } }
+    ) {
+      returning {
+        scheduled_stop_point_label
+        scheduled_stop_point_sequence
+        journey_pattern_id
+      }
+    }
+
+    insert_journey_pattern_scheduled_stop_point_in_journey_pattern(
+      objects: $new_stops_in_journey_pattern
+    ) {
+      returning {
+        scheduled_stop_point_label
+        scheduled_stop_point_sequence
+        journey_pattern_id
+      }
+    }
+  }
+`;
+
 interface EditParams {
   routeId: UUID;
+  journeyPatternId: UUID;
   infraLinksAlongRoute: RouteInfraLink[];
   stopsEligibleForJourneyPattern: RouteStopFieldsFragment[];
   includedStopLabels: string[];
@@ -29,8 +85,9 @@ interface EditParams {
 
 interface EditChanges {
   routeId: UUID;
+  journeyPatternId: UUID;
   newInfrastructureLinks: RouteInfrastructureLinkAlongRouteInsertInput[];
-  newJourneyPattern: JourneyPatternJourneyPatternInsertInput;
+  newStopsInJourneyPattern: JourneyPatternScheduledStopPointInJourneyPatternInsertInput[];
 }
 
 /**
@@ -49,23 +106,22 @@ export const useEditRouteGeometry = () => {
     stopsEligibleForJourneyPattern,
     includedStopLabels,
     journeyPatternStops,
+    journeyPatternId,
   }: EditParams) => {
     await validateJourneyPattern({ includedStopLabels });
 
     const changes: EditChanges = {
       routeId,
+      journeyPatternId,
       newInfrastructureLinks: mapInfraLinksAlongRouteToGraphQL(
         infraLinksAlongRoute,
       ).map((link) => ({ ...link, route_id: routeId })),
-      newJourneyPattern: {
-        on_route_id: routeId,
-        scheduled_stop_point_in_journey_patterns:
-          buildJourneyPatternStopSequence({
-            stopsEligibleForJourneyPattern,
-            includedStopLabels,
-            journeyPatternStops,
-          }),
-      },
+      newStopsInJourneyPattern: buildJourneyPatternStopSequence({
+        stopsEligibleForJourneyPattern,
+        includedStopLabels,
+        journeyPatternStops,
+        journeyPatternId,
+      }),
     };
 
     return changes;
@@ -75,8 +131,9 @@ export const useEditRouteGeometry = () => {
     changes: EditChanges,
   ): UpdateRouteGeometryMutationVariables => ({
     route_id: changes.routeId,
+    journey_pattern_id: changes.journeyPatternId,
     new_infrastructure_links: changes.newInfrastructureLinks,
-    new_journey_pattern: changes.newJourneyPattern,
+    new_stops_in_journey_pattern: changes.newStopsInJourneyPattern,
   });
 
   // default handler that can be used to show error messages as toast
