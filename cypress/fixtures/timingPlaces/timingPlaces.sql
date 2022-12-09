62d64bfc1e254ae6e3364b0ae6a631052cfa98ce
sr-vala <samuli.rasi@valagroup.com>
2022-12-09 12:38:55 +0200

    Add tests that add timing places to stops
    Resolves HSLdevcom/jore4#1046

---
+++ cypress/fixtures/timingPlaces/timingPlaces.sql
@@ -0,0 +1,11 @@
+INSERT INTO timing_pattern.timing_place (timing_place_id,"label",description) VALUES
+  ('d094b604-3860-4e2f-a601-9dad6f9827b9','1AACKT','{"fi_FI": "Aino Ackten tie"}'),
+  ('fece6456-e6fe-4a44-8c61-ee0a6031d799','1ALAKT','{"fi_FI": "Alakiventie"}'),
+  ('194e347c-670e-4f03-b645-71bfb7d05031','1ALAM','{"fi_FI": "Ala-Malmin tori"}'),
+  ('1bd78f7f-3b13-41f9-8967-4611c8ab0026','1ALAML','{"fi_FI": "Ala-Malmi"}'),
+  ('15ae9e77-5727-4033-a2bf-925142dd158d','1ALATI','{"fi_FI": "Ala-Tikkurila"}'),
+  ('413e6251-fe32-4c4d-b258-f85a02793655','1ALKU','{"fi_FI": "Alkutie"}'),
+  ('87e05267-5ba0-4bc6-b126-0ece9c47af9f','1ALPKY','{"fi_FI": "Alppikylä"}'),
+  ('3ab0d252-48f2-435e-b07a-dff2d9ef7e11','1ALT','{"fi_FI": "Ala-Tikkurila"}'),
+  ('cc282ec9-3894-439f-bad4-2f29c491ed99','1ARAB','{"fi_FI": "Arabia"}'),
+  ('d1a1b9cf-c91d-407c-93bb-7fb0ec6f3e85','1ARB','{"fi_FI": "Arabianranta"}')ON CONFLICT DO NOTHING;
