3e988688b533fc9387b45ec4fc2843d7614ff44b
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-12-13 09:46:02 +0200

    Improve timetable seed data
    - Generate seed data with builder functions and loops
    - Remove most of hardcoded uuid values as having those makes live
      difficult when seeding data e.g. in loop, and even if we would have
      those we would still have difficulties when "re-seeding" db without
      emptying it e.g. after seed dataset is modified.
    
    Related to HSLdevcom/jore4#1023

--- test-db-manager/src/datasets/timetables/vehicleServiceBlocks.ts
+++ test-db-manager/src/datasets/timetables/vehicleServiceBlocks.ts
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid';
 import { VehicleServiceBlockInsertInput } from '../../types';
 import { seedVehicleServices } from './vehicleServices';
 
@@ -5,31 +6,31 @@ export const seedVehicleServiceBlocks: VehicleServiceBlockInsertInput[] = [
   // Vehicle 1 Mon-Fri
   {
     vehicle_service_id: seedVehicleServices[0].vehicle_service_id,
-    block_id: '3ebce471-1ef2-4951-af34-6a9ff0660cb8',
+    block_id: uuid(),
   },
   // Vehicle 1 Sat
   {
     vehicle_service_id: seedVehicleServices[1].vehicle_service_id,
-    block_id: '1ca443fe-d6fe-4124-88a0-ed061077aeb6',
+    block_id: uuid(),
   },
   // Vehicle 1 Sun
   {
     vehicle_service_id: seedVehicleServices[2].vehicle_service_id,
-    block_id: 'd51c6443-ec88-497c-9786-114482de05a5',
+    block_id: uuid(),
   },
   // Vehicle 2 Mon-Fri
   {
     vehicle_service_id: seedVehicleServices[0].vehicle_service_id,
-    block_id: '1ad90735-7294-475e-bf06-80f5f2163377',
+    block_id: uuid(),
   },
   // Vehicle 2 Sat
   {
     vehicle_service_id: seedVehicleServices[1].vehicle_service_id,
-    block_id: '9d37fa3c-c228-41e5-99e9-6e351160a4f5',
+    block_id: uuid(),
   },
   // Vehicle 2 Sun
   {
     vehicle_service_id: seedVehicleServices[2].vehicle_service_id,
-    block_id: '19b647c7-a515-4993-bd07-abb0f2948cfe',
+    block_id: uuid(),
   },
 ];
