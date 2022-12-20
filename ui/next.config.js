7c82aa086a8a4c07ce7ee2e9fa2c800bb50d86c0
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2022-12-20 10:38:15 +0200

    Update next.js to latest version
    Resolves HSLdevcom/jore4#884

--- ui/next.config.js
+++ ui/next.config.js
@@ -16,4 +16,11 @@ module.exports = {
     };
     return config;
   },
+  eslint: {
+    // Don't run eslint as part of builds.
+    // We have separate CI job to run it, so it is not useful to run it
+    // as part of builds. (It also seems like `next lint` command does not
+    // respect our eslint config defined in `.eslintrc.js`.)
+    ignoreDuringBuilds: true,
+  },
 };
