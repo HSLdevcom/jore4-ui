368746bc08bf963406face1d585395067f8e5c76
Eetu Niemi <eetu.niemi@vincit.fi>
2023-01-03 09:47:40 +0200

    Use Hastus microservice to export routes
    - Note: Hastus import/export microservice is not yet in docker-compose setup. It has to be started separately from its repository for this to work until in is added to docker-compose setup.

---
+++ ui/src/api/hastus.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { DateTime } from 'luxon';
+import { Priority } from '../types/Priority';
+
+interface CommonExportParams {
+  readonly labels: string[];
+  readonly priority: Priority;
+}
+
+interface ExportBody extends CommonExportParams {
+  readonly observationDate: string;
+}
+
+interface ExportParams extends CommonExportParams {
+  readonly observationDate: DateTime;
+}
+
+const apiClient = axios.create({
+  baseURL: '/api/hastus',
+});
+
+const exportRoutes = (payload: ExportBody) =>
+  apiClient.post('/export/routes', payload, {
+    responseType: 'blob',
+    headers: {
+      // TODO: Authenticate properly
+      'x-hasura-admin-secret': 'hasura',
+      'x-hasura-role': 'admin',
+    },
+  });
+
+export const exportRoutesToHastus = async ({
+  labels,
+  priority,
+  observationDate,
+}: ExportParams) => {
+  const request: ExportBody = {
+    labels,
+    priority,
+    observationDate: observationDate.toISODate(),
+  };
+  const response = await exportRoutes(request);
+
+  return response;
+};
---
+++ ui/src/hooks/routes/useExportRoutes.ts
@@ -0,0 +1,35 @@
+import { exportRoutesToHastus as exportToHastus } from '../../api/hastus';
+import { downloadFile } from '../../utils';
+import { useSearchQueryParser } from '../search';
+import { useObservationDateQueryParam } from '../urlQuery';
+
+export const useExportRoutes = () => {
+  const { observationDate } = useObservationDateQueryParam();
+  const { search } = useSearchQueryParser();
+
+  const { priorities } = search;
+
+  // Routes can be exported to Hastus only when there is only 1 priority selected
+  const canExport = priorities?.length === 1;
+
+  const exportRoutesToHastus = async (routeLabels: string[]) => {
+    if (!canExport) {
+      throw new Error(
+        `Can't export routes, expected exactly 1 priority but got "${priorities}"`,
+      );
+    }
+
+    const response = await exportToHastus({
+      labels: routeLabels,
+      priority: priorities[0],
+      observationDate,
+    });
+
+    downloadFile(
+      response.data,
+      `jore4-export-${observationDate.toISODate()}.csv`,
+    );
+  };
+
+  return { canExport, exportRoutesToHastus };
+};
