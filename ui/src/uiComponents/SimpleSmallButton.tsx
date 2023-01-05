233b80f0bd754a0cd626eb77ed08a298b919f2e6
Eetu Niemi <eetu.niemi@vincit.fi>
2023-01-05 15:55:56 +0200

    Allow disabling SimpleSmallButton

--- ui/src/uiComponents/SimpleSmallButton.tsx
+++ ui/src/uiComponents/SimpleSmallButton.tsx
@@ -4,9 +4,8 @@ type Props = {
   onClick: () => void;
   testId?: string;
   className?: string;
+  disabled?: boolean;
 };
-
-const commonClassNames = 'font-bold border w-20 rounded text-sm font-light';
 
 export const SimpleSmallButton = ({
   label,
@@ -14,17 +13,24 @@ export const SimpleSmallButton = ({
   onClick,
   testId,
   className = '',
-}: Props): JSX.Element => (
-  <button
-    onClick={onClick}
-    type="button"
-    data-testid={testId}
-    className={`${commonClassNames} ${
-      inverted
-        ? 'border-grey bg-white text-gray-900 hover:border-brand active:border-brand'
-        : 'border-brand bg-brand text-white hover:bg-opacity-50 active:bg-opacity-50'
-    } ${className}`}
-  >
-    {label}
-  </button>
-);
+  disabled,
+}: Props): JSX.Element => {
+  const colorClassNames = inverted
+    ? 'border-grey bg-white text-gray-900 hover:border-brand active:border-brand'
+    : 'border-brand bg-brand text-white hover:bg-opacity-50 active:bg-opacity-50';
+  const disabledClassNames = disabled ? 'pointer-events-none opacity-70' : '';
+
+  const commonClassNames = `font-bold border w-20 rounded text-sm font-light ${colorClassNames} ${disabledClassNames} `;
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      data-testid={testId}
+      className={`${commonClassNames} ${className}`}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
