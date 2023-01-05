01afcde73465dc2ed6abd726e0c3abbae05df390
Eetu Niemi <eetu.niemi@vincit.fi>
2023-01-05 15:21:29 +0200

    Extend TimetablePriority enum with Staging value

--- ui/src/types/Priority.ts
+++ ui/src/types/Priority.ts
@@ -5,8 +5,9 @@ export enum Priority {
 }
 
 export enum TimetablePriority {
-  Standard = 10,
-  Temporary = 20,
-  Special = 30,
-  Draft = 40,
+  Standard = 10, // used for "normal" in-use entities
+  Temporary = 20, // overrides Standard, used for temporary adjustments
+  Special = 25, // special day that overrides Standard and Temporary
+  Draft = 30, // overrides Special, Temporary and Standard, not visible to external systems
+  Staging = 40, // imported from Hastus, not in use until priority is changed
 }
