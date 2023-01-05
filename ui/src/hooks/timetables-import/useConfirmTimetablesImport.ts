c1470a7c97326786916afa81e60ba651f2b92e25
Eetu Niemi <eetu.niemi@vincit.fi>
2023-01-05 16:00:39 +0200

    Implement importing timetables without preview
    Resolves HSLdevcom/jore4#947

---
+++ ui/src/hooks/timetables-import/useConfirmTimetablesImport.ts
@@ -0,0 +1,70 @@
+import { gql } from '@apollo/client';
+import { pipe } from 'remeda';
+import {
+  useChangeStagingVehicleScheduleFramePriorityMutation,
+  useGetStagingVehicleScheduleFramesQuery,
+} from '../../generated/graphql';
+import { TimetablePriority } from '../../types/Priority';
+import { mapToVariables } from '../../utils';
+
+const GQL_GET_STAGING_VEHICLE_SCHEDULE_FRAMES = gql`
+  query GetStagingVehicleScheduleFrames {
+    timetables {
+      timetables_vehicle_schedule_vehicle_schedule_frame(
+        where: { priority: { _eq: 40 } }
+      ) {
+        validity_end
+        validity_start
+        name_i18n
+        vehicle_schedule_frame_id
+        priority
+        vehicle_services {
+          ...vehicle_service_with_journeys
+        }
+      }
+    }
+  }
+`;
+
+const GQL_CHANGE_STAGING_VEHICLE_SCHEDULE_FRAME_PRIORITY = gql`
+  mutation ChangeStagingVehicleScheduleFramePriority($newPriority: Int!) {
+    timetables {
+      timetables_update_vehicle_schedule_vehicle_schedule_frame(
+        where: { priority: { _eq: 40 } }
+        _set: { priority: $newPriority }
+      ) {
+        returning {
+          priority
+          validity_end
+          validity_start
+          name_i18n
+          vehicle_schedule_frame_id
+        }
+      }
+    }
+  }
+`;
+
+export const useConfirmTimetablesImport = () => {
+  const [changeTimetablesPriority] =
+    useChangeStagingVehicleScheduleFramePriorityMutation();
+
+  const importedScheduleFrames = useGetStagingVehicleScheduleFramesQuery();
+
+  const confirmTimetablesImport = async (priority: TimetablePriority) => {
+    await changeTimetablesPriority(mapToVariables({ newPriority: priority }));
+  };
+
+  const vehicleServiceCount =
+    pipe(
+      importedScheduleFrames.data?.timetables?.timetables_vehicle_schedule_vehicle_schedule_frame.flatMap(
+        (frame) =>
+          frame.vehicle_services.flatMap((service) =>
+            service.blocks.flatMap((block) => block.vehicle_journeys),
+          ),
+      ),
+      (vehicleServices) => vehicleServices?.length,
+    ) || 0;
+
+  return { confirmTimetablesImport, vehicleServiceCount };
+};
