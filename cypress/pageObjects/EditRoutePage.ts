617175175a336cc34b30bed60d11a89805ae702c
sr-vala <samuli.rasi@valagroup.com>
2023-01-09 09:54:50 +0200

    Add test that deletes a route
    Resolves HSLdevcom/jore4#1064

--- cypress/pageObjects/EditRoutePage.ts
+++ cypress/pageObjects/EditRoutePage.ts
@@ -1,5 +1,6 @@
 import { UUID } from '../types';
 import { ChangeValidityForm } from './ChangeValidityForm';
+import { ConfirmationDialog } from './ConfirmationDialog';
 import { RoutePropertiesForm } from './RoutePropertiesForm';
 import { TerminusNameInputs } from './TerminusNameInputs';
 
@@ -10,11 +11,17 @@ export class EditRoutePage {
 
   changeValidityForm = new ChangeValidityForm();
 
+  confirmationDialog = new ConfirmationDialog();
+
   visit(routeid: UUID) {
     cy.visit(`/routes/${routeid}/edit`);
   }
 
   getSaveRouteButton() {
     return cy.getByTestId('EditRoutePage::saveButton');
+  }
+
+  getDeleteRouteButton() {
+    return cy.getByTestId('EditRoutePage::deleteButton');
   }
 }
