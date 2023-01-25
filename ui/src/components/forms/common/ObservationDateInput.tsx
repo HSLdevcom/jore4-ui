a7a2241c316a39c3cb4baa7b5aace9378a44ab25
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-01-25 08:16:52 +0200

    Fix ObservationDateInput label position to be permanent
    Before this the label's position was dependent on parent elements. But we want
    it to be always on top of the input.

--- ui/src/components/forms/common/ObservationDateInput.tsx
+++ ui/src/components/forms/common/ObservationDateInput.tsx
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 import { useTranslation } from 'react-i18next';
+import { Column, Row } from '../../../layoutComponents';
 
 interface Props {
   value: DateTime;
@@ -15,15 +16,16 @@ export const ObservationDateInput = ({
   const { t } = useTranslation();
   const dateInputId = 'observation-date-input';
   return (
-    <>
-      <label htmlFor={dateInputId}>{t('filters.observationDate')}</label>
-      <input
-        type="date"
-        value={value.toISODate()}
-        onChange={(e) => onChange(DateTime.fromISO(e.target.value))}
-        id={dateInputId}
-        className={className}
-      />
-    </>
+    <Row className={className}>
+      <Column>
+        <label htmlFor={dateInputId}>{t('filters.observationDate')}</label>
+        <input
+          type="date"
+          value={value.toISODate()}
+          onChange={(e) => onChange(DateTime.fromISO(e.target.value))}
+          id={dateInputId}
+        />
+      </Column>
+    </Row>
   );
 };
