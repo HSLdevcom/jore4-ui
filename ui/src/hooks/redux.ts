c9459647e02f8b8cd9d18e44f53fc5c667dac9ee
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2023-01-30 12:40:42 +0200

    Update redux-related dependencies
    - Redux itself was already up-to-date
    - `useAppDispatch` defined as in latest redux-toolkit docs:
       https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type

--- ui/src/hooks/redux.ts
+++ ui/src/hooks/redux.ts
@@ -1,23 +1,19 @@
 import {
-  ActionCreatorWithOptionalPayload,
-  ActionCreatorWithoutPayload,
   ActionCreatorWithPayload,
   ActionCreatorWithPreparedPayload,
 } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux';
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppAction = <
   TPayload extends ExplicitAny,
   TActionPayload extends ExplicitAny,
 >(
   action:
-    | ActionCreatorWithOptionalPayload<TPayload>
-    | ActionCreatorWithPreparedPayload<TPayload[], TActionPayload>
-    | ActionCreatorWithPayload<TPayload>
-    | ActionCreatorWithoutPayload,
+    | ActionCreatorWithPreparedPayload<TPayload[], TActionPayload, string>
+    | ActionCreatorWithPayload<TPayload, string>,
 ) => {
   const dispatch = useAppDispatch();
   return (actionPayload: TPayload) => dispatch(action(actionPayload));
