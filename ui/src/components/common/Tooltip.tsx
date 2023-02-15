527e585254c26615e79a9c04604ddb81ce09d598
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-02-15 12:40:00 +0200

    Add tooltip component
    Take it in to use with LocatorButton.
    
    Resolves HSLdevcom/jore4#1125

---
+++ ui/src/components/common/Tooltip.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  message: string;
+  className?: string;
+  children: JSX.Element;
+}
+
+/**
+ * Adds tooltip for hovering over children. Give delay and position via className
+ * Example:
+ * <Tooltip className="bottom-11 delay-700" message="Tooltip message">
+ *   ...
+ * </Tooltip>
+ */
+export const Tooltip = ({ message, className = '', children }: Props) => {
+  return (
+    <div className="group relative flex justify-center">
+      <span
+        className={`absolute z-10 w-max scale-0 rounded-md bg-dark-grey p-2 text-xs 
+        text-white transition-all group-hover:scale-100 ${className}`}
+      >
+        {message}
+      </span>
+      {children}
+    </div>
+  );
+};
--- ui/src/uiComponents/LocatorButton.tsx
+++ ui/src/uiComponents/LocatorButton.tsx
@@ -1,4 +1,6 @@
+import { useTranslation } from 'react-i18next';
 import { MdPinDrop } from 'react-icons/md';
+import { Tooltip } from '../components/common/Tooltip';
 import { IconButton } from './IconButton';
 import { commonHoverStyle } from './SimpleButton';
 
@@ -20,15 +22,18 @@ export const LocatorButton = ({
   className = '',
 }: Props): JSX.Element => {
   const disabledStyle = '!bg-background opacity-70 pointer-events-none';
+  const { t } = useTranslation();
   return (
-    <IconButton
-      className={`h-10 w-10 rounded-full border border-grey bg-white text-tweaked-brand ${commonHoverStyle} ${
-        disabled ? disabledStyle : ''
-      } ${className}`}
-      onClick={onClick}
-      disabled={disabled}
-      icon={<MdPinDrop className="text-2xl" />}
-      testId={testId || testIds.button}
-    />
+    <Tooltip className="bottom-11 delay-700" message={t('map.showOnMap')}>
+      <IconButton
+        className={`h-10 w-10 rounded-full border border-grey bg-white text-tweaked-brand ${commonHoverStyle} ${
+          disabled ? disabledStyle : ''
+        } ${className}`}
+        onClick={onClick}
+        disabled={disabled}
+        icon={<MdPinDrop className="text-2xl" />}
+        testId={testId || testIds.button}
+      />
+    </Tooltip>
   );
 };
