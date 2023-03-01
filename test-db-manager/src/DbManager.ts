4cb43797a5ed0d33b3628e126f0b223e65120c59
sr-vala <samuli.rasi@valagroup.com>
2023-03-01 15:01:20 +0200

    Parallelize e2e tests
    Resolves HSLdevcom/jore4#965

--- test-db-manager/src/DbManager.ts
+++ test-db-manager/src/DbManager.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import { Knex } from 'knex';
-import { getDbConfig } from './config';
+import { e2eDatabaseConfig } from './config';
 
-export const getDbConnection = (knexConfig = getDbConfig()) => {
+export const getDbConnection = (knexConfig = e2eDatabaseConfig) => {
   // TODO: not sure how to properly import knex without getting type errors
   // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
   const db = require('knex')({ client: 'pg', connection: knexConfig });
