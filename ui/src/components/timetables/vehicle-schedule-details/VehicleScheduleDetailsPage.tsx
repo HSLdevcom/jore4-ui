76b6bb07853954d7cf172141eacfab99e59b0160
Eetu Niemi <eetu.niemi@vincit.fi>
2023-03-02 11:44:56 +0200

    Add close button to timetables view

--- ui/src/components/timetables/vehicle-schedule-details/VehicleScheduleDetailsPage.tsx
+++ ui/src/components/timetables/vehicle-schedule-details/VehicleScheduleDetailsPage.tsx
@@ -1,13 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import {
+  TimetablesView,
   useGetLineDetails,
   useTimetableVersionsReturnToQueryParam,
+  useTimetablesViewState,
 } from '../../../hooks';
 import { Container } from '../../../layoutComponents';
 import { SimpleButton } from '../../../uiComponents';
 import { ObservationDateControl } from '../../common/ObservationDateControl';
 import { FormColumn, FormRow } from '../../forms/common';
 import { PageHeader } from '../../routes-and-lines/common/PageHeader';
+import { TimetableNavigation } from './TimetableNavigation';
 import { VehicleRouteTimetables } from './VehicleRouteTimetables';
 
 export const VehicleScheduleDetailsPage = (): JSX.Element => {
@@ -17,6 +20,18 @@ export const VehicleScheduleDetailsPage = (): JSX.Element => {
 
   const { getVersionsUrl } = useTimetableVersionsReturnToQueryParam();
 
+  const { routeLabel, setShowDefaultView, activeView } =
+    useTimetablesViewState();
+
+  // For default view show all routes,
+  // Otherwise show only selected view.
+  // TODO: Add route toggles to header, after that only show
+  // selected routes for default view as well
+  const displayedRoutes =
+    activeView === TimetablesView.DEFAULT
+      ? line?.line_routes || []
+      : line?.line_routes.filter((route) => route.label === routeLabel) || [];
+
   return (
     <div>
       <PageHeader>
@@ -25,6 +40,9 @@ export const VehicleScheduleDetailsPage = (): JSX.Element => {
           {t('lines.line', { label: line?.label })}
         </h1>
       </PageHeader>
+      {line && activeView !== TimetablesView.DEFAULT && (
+        <TimetableNavigation onClose={setShowDefaultView} />
+      )}
       <Container className="py-10">
         <FormRow mdColumns={2} className="mb-8 ">
           <ObservationDateControl className="max-w-max" />
@@ -39,7 +57,7 @@ export const VehicleScheduleDetailsPage = (): JSX.Element => {
             )}
           </FormColumn>
         </FormRow>
-        <VehicleRouteTimetables routes={line?.line_routes || []} />
+        <VehicleRouteTimetables routes={displayedRoutes} />
       </Container>
     </div>
   );
