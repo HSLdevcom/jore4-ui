6b7463022474a59432c89bde36902315b904600d
Eetu Niemi <eetu.niemi@vincit.fi>
2023-03-22 15:25:42 +0200

    Remove label from vehicle journey popover

--- ui/src/components/timetables/passing-times-by-stop/VehicleJourneyPopover.tsx
+++ ui/src/components/timetables/passing-times-by-stop/VehicleJourneyPopover.tsx
@@ -1,5 +1,4 @@
 import { Duration } from 'luxon';
-import { useTranslation } from 'react-i18next';
 import { mapDurationToShortTime } from '../../../time';
 import { Popover } from '../../../uiComponents';
 
@@ -12,15 +11,12 @@ export const VehicleJourneyPopover = ({
   passingTime,
   onClose,
 }: Props): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <Popover className="ml-10" onClose={onClose}>
       <div className="mb-1 space-x-3">
-        <h5 className="inline text-lg">!Label</h5>
-        <span className="text-sm">
-          {t('timetables.at', { time: mapDurationToShortTime(passingTime) })}
-        </span>
+        <h5 className="inline text-lg">
+          {mapDurationToShortTime(passingTime)}
+        </h5>
       </div>
       <p className="text-sm">!Kalustotyyppi</p>
       <p className="text-sm">!Liikennöitsijä</p>
