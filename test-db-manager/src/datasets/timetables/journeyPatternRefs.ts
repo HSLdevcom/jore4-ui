7c445c803fe2b668e88edbf0e05be900486090e4
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-03-23 15:34:33 +0200

    Update pinned hasura image

--- test-db-manager/src/datasets/timetables/journeyPatternRefs.ts
+++ test-db-manager/src/datasets/timetables/journeyPatternRefs.ts
@@ -5,6 +5,7 @@ export const seedJourneyPatternRefs: JourneyPatternRefInsertInput[] = [
   {
     // route 641, direction 1
     journey_pattern_ref_id: 'a6f626c8-e743-4c38-b9c1-c537ffb2863f',
+    type_of_line: 'stopping_bus_service',
     // NOTE: journey_pattern with id 'a9136ad8-d185-4c7b-9969-057b65dc9b00'
     // is defined in hasura seed data. It is just used here as an example.
     // This links timetable data to route 641.
@@ -19,6 +20,7 @@ export const seedJourneyPatternRefs: JourneyPatternRefInsertInput[] = [
   {
     // route 641, direction 2
     journey_pattern_ref_id: '4f80b9c2-21b2-4460-9e38-0d0691b29cbe',
+    type_of_line: 'stopping_bus_service',
     // NOTE: same comments for journey_pattern with id
     // '4f80b9c2-21b2-4460-9e38-0d0691b29cbe' as stated above
     journey_pattern_id: '4f80b9c2-21b2-4460-9e38-0d0691b29cbe',
@@ -28,6 +30,7 @@ export const seedJourneyPatternRefs: JourneyPatternRefInsertInput[] = [
   {
     // Route 65x variant 3, outbound.
     // NOTE: the journey pattern is defined in hasura seed data.
+    type_of_line: 'stopping_bus_service',
     journey_pattern_ref_id: '8b4586f1-6cc0-49ab-b30b-442fff9cbef4',
     journey_pattern_id: '2b7fa547-6eb5-4878-8053-6bbd6e9cbfc0',
     observation_timestamp: DateTime.fromISO('2023-07-01T00:00:00+00:00'),
