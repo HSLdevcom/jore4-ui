07210705cdf499754a4c570884c859c943407fd5
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-03-24 12:42:42 +0200

    Add file import upload logic
    Resolves HSLdevcom/jore4#1071
    
    Resolves HSLdevcom/jore4#1086

--- ui/src/components/timetables/import/ImportTimetablesPage.tsx
+++ ui/src/components/timetables/import/ImportTimetablesPage.tsx
@@ -1,41 +1,82 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router';
 import { useTimetablesImport } from '../../../hooks/timetables-import/useTimetablesImport';
 import { Container, Row } from '../../../layoutComponents';
 import { Path } from '../../../router/routeDetails';
-import { SimpleButton } from '../../../uiComponents';
+import { CloseIconButton, SimpleButton } from '../../../uiComponents';
+import { FormRow } from '../../forms/common';
 import { ConfirmTimetablesImportModal } from './ConfirmTimetablesImportModal';
+import { FileImportDragAndDrop } from './FileImportDragAndDrop';
 
 const testIds = {
+  uploadButton: 'ImportTimetablesPage::uploadButton',
   saveButton: 'ImportTimetablesPage::saveButton',
+  previewButton: 'ImportTimetablesPage::previewButton',
+  closeButton: 'ImportTimetablesPage::closeButton',
 };
 
 export const ImportTimetablesPage = (): JSX.Element => {
   const { t } = useTranslation();
-  const { vehicleJourneys } = useTimetablesImport();
+  const { vehicleJourneys, sendToHastusImporter } = useTimetablesImport();
+  const history = useHistory();
+
+  const [fileList, setFileList] = useState<File[] | null>(null);
   const [isSavingImport, setIsSavingImport] = useState(false);
 
-  const importedTimetablesExist = vehicleJourneys.length > 0;
+  const handleClose = () => {
+    history.push(Path.timetables);
+  };
 
+  const handleSave = () => {
+    setIsSavingImport(true);
+  };
+
+  const handleUpload = async () => {
+    if (fileList?.length) {
+      const result = await sendToHastusImporter(fileList);
+      setFileList(result.failedFiles);
+    }
+  };
+
+  const importedTimetablesExist = vehicleJourneys.length > 0;
   return (
     <Container>
-      <h1>{t('timetables.importTimetablesFromHastus')}</h1>
-      <Row className="space-x-3">
-        <SimpleButton
-          testId={testIds.saveButton}
-          onClick={() => setIsSavingImport(true)}
-          disabled={!importedTimetablesExist}
-        >
-          {t('save')}
-        </SimpleButton>
-        <SimpleButton
-          testId={testIds.saveButton}
-          href={Path.timetablesImportPreview}
-          disabled={!importedTimetablesExist}
-        >
-          {t('timetables.openPreview')}
-        </SimpleButton>
+      <Row>
+        <h1>{t('import.importTimetablesFromHastus')}</h1>
+        <CloseIconButton
+          testId={testIds.closeButton}
+          label={t('close')}
+          className="ml-auto text-base font-bold text-brand"
+          onClick={handleClose}
+        />
       </Row>
+      <FileImportDragAndDrop fileList={fileList} setFileList={setFileList} />
+      <FormRow mdColumns={2}>
+        <SimpleButton
+          testId={testIds.uploadButton}
+          onClick={handleUpload}
+          disabled={!fileList?.length}
+        >
+          {t('import.uploadFiles')}
+        </SimpleButton>
+        <div className="flex justify-end space-x-4">
+          <SimpleButton
+            testId={testIds.saveButton}
+            onClick={handleSave}
+            disabled={!importedTimetablesExist}
+          >
+            {t('save')}
+          </SimpleButton>
+          <SimpleButton
+            testId={testIds.previewButton}
+            href={Path.timetablesImportPreview}
+            disabled={!importedTimetablesExist}
+          >
+            {t('import.openPreview')}
+          </SimpleButton>
+        </div>
+      </FormRow>
       <ConfirmTimetablesImportModal
         isOpen={isSavingImport}
         onClose={() => setIsSavingImport(false)}
