c2f0242e074401e73a570f2815a36c9dd8a74dd9
Eetu Niemi <eetu.niemi@vincit.fi>
2023-04-04 13:19:20 +0300

    Remove infralink insert helpers from tests

--- cypress/e2e/utils/tasks.ts
+++ cypress/e2e/utils/tasks.ts
@@ -1,9 +1,7 @@
 import {
   getDbConnection,
-  truncateDb,
   hasuraApi,
-  insertVehicleSubmodeOnInfraLink,
-  removeVehicleSubmodeOnInfraLink,
+  truncateDb,
 } from '@hsl/jore4-test-db-manager';
 
 const db = getDbConnection();
@@ -45,19 +43,4 @@ export const executeRawDbQuery = ({ query, bindings }: any) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const hasuraAPI = (request: any) => {
   return hasuraApi(request);
-};
-
-// Note: next 2 functions exists only because name of table
-// 'infrastructure_network.vehicle_submode_on_infrastructure_link'
-// is too long for hasura to handle. Generally db
-// should be accessed through "hasuraApi" function and this kind
-// of specific functions should be avoided.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const insertVehicleSubmodOnInfraLinks = (request: any) => {
-  return insertVehicleSubmodeOnInfraLink(db, request);
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const removeVehicleSubmodOnInfraLinks = (request: any) => {
-  return removeVehicleSubmodeOnInfraLink(db, request);
 };
