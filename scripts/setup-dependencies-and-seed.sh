0417b1aae2a8927bfcf07729581b23c08e9eaf4f
Eetu Niemi <eetu.niemi@vincit.fi>
2023-04-17 16:37:34 +0300

    Add script for easily setting up dev env

---
+++ scripts/setup-dependencies-and-seed.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+cd $(dirname "$0")/..
+
+read -p "Warning: all the current data in the database will be overwritten! Are you sure (y/n)? " REPLY
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Start dependencies
+sh ./scripts/start-dependencies.sh
+
+# Download routes and lines dump if it does not exist
+if [ ! -e jore4dump.pgdump ]
+then
+    echo "" | ./scripts/development.sh dump:download
+fi
+
+# Import routes and lines dump to db
+echo "y" | ./scripts/development.sh dump:import
+
+# Seed timetables
+cd ./test-db-manager
+yarn seed2
+
+echo "All done! Happy coding! :)"
