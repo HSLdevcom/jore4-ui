129e9724cd34fa1de59a479f14b2212dfecf05a5
Eetu Niemi <eetu.niemi@vincit.fi>
2023-04-18 10:26:48 +0300

    Seed all databases with infrastructure links

--- scripts/seed-infrastructure-links.sh
+++ scripts/seed-infrastructure-links.sh
@@ -4,14 +4,14 @@ cd $(dirname "$0")/..
 
 DB_CONNECTION_STRING=postgresql://dbadmin:adminpassword@localhost:5432/jore4e2e
 
-echo "Seeding infrastructure links..."
+echo "Seeding infrastructure links for db $1..."
 
 SUCCESS=false
 while ! $SUCCESS; do
   echo "Checking if infrastructure link schema exists..."
-  if [[ $(docker exec testdb psql $DB_CONNECTION_STRING -AXqtc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'infrastructure_network' AND tablename = 'infrastructure_link');") = "t" ]]; then
+  if [[ $(docker exec $1 psql $DB_CONNECTION_STRING -AXqtc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'infrastructure_network' AND tablename = 'infrastructure_link');") = "t" ]]; then
     echo "Schema found! Seeding infrastructure links..."
-      docker exec -i testdb psql $DB_CONNECTION_STRING < test-db-manager/src/dumps/infraLinks/infraLinks.sql; 
+      docker exec -i $1 psql $DB_CONNECTION_STRING < test-db-manager/src/dumps/infraLinks/infraLinks.sql;
       SUCCESS=true
   fi
   sleep 2
