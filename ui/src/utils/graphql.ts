f001e8c8a93c07ea707f8a155c824bd405cd99fd
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-04-18 14:08:20 +0300

    Add versions page new logic

--- ui/src/utils/graphql.ts
+++ ui/src/utils/graphql.ts
@@ -26,3 +26,21 @@ export const removeFromApolloCache = (
   cache.evict(cached);
   cache.gc();
 };
+
+/**
+ * It seems that hasura requires function parameter arrays to be
+ * formatted as follows: {1,2,3,4,5}. This function takes in
+ * an array and formats it correctly for hasura.
+ */
+export const convertArrayTypeForHasura = <T>(array: T[]): string => {
+  const convertedItems = array.map((item) => {
+    // If there is array inside of array, we need to do the same conversion to
+    // the inner array by calling this function recursively
+    if (Array.isArray(item)) {
+      return convertArrayTypeForHasura(item);
+    }
+    return item;
+  });
+
+  return `{${convertedItems.join(',')}}`;
+};
