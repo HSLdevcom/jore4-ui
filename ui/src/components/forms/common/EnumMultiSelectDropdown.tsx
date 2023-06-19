8d5f39ca3358424997791942a803556570c8758f
Jarno Perholehto <jarno.perholehto@vincit.fi>
2023-06-19 12:04:33 +0300

    Add LineTypeMultiSelectDropdown

---
+++ ui/src/components/forms/common/EnumMultiSelectDropdown.tsx
@@ -0,0 +1,75 @@
+import first from 'lodash/first';
+import isUndefined from 'lodash/isUndefined';
+import { useTranslation } from 'react-i18next';
+import { MultiSelectListbox } from '../../../uiComponents';
+import { OptionRenderPropArg } from '../../../uiComponents/ListboxOptions';
+import { AllOptionEnum, getEnumValues } from '../../../utils';
+import { EnumDropdownProps } from './EnumDropdown';
+
+const testIds = {
+  enumDropdown: 'EnumDropdown::button',
+};
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const EnumMultiSelectDropdown = <TEnum extends Object>({
+  id,
+  testId,
+  enumType,
+  uiNameMapper,
+  placeholder,
+  value,
+  ...formInputProps
+}: EnumDropdownProps<TEnum>): JSX.Element => {
+  const { t } = useTranslation();
+
+  // To handle array values in multi select component
+  const mappedValue = value ? value.split(',') : [];
+
+  const values = getEnumValues({ ...AllOptionEnum, ...enumType });
+
+  const mapToOption = (item: string) => ({
+    key: item,
+    value: item,
+    render: function EnumOption(optionRenderProps: OptionRenderPropArg) {
+      return (
+        <div className="cursor-default">
+          <input
+            type="checkbox"
+            className="mr-2 h-5 w-6"
+            checked={optionRenderProps.selected}
+            readOnly
+          />
+          <div className="ml-1 mr-2">
+            {uiNameMapper(item as unknown as TEnum)}
+          </div>
+        </div>
+      );
+    },
+  });
+
+  const getButtonContent = (inputValue?: string): string => {
+    const enumValues = inputValue?.split(',');
+    if (isUndefined(enumValues) || (first(enumValues) === '' && !!enumValues.length)) {
+      return placeholder;
+    }
+    if (enumValues.length === values.length) {
+      return uiNameMapper(AllOptionEnum.All as unknown as TEnum);
+    }
+    if (enumValues.length > 1) {
+      return t('selected', { count: enumValues.length });
+    }
+    return uiNameMapper(enumValues[0] as unknown as TEnum);
+  };
+
+  const options = values.map((item) => mapToOption(item));
+  return (
+    <MultiSelectListbox
+      id={id}
+      testId={testId || testIds.enumDropdown}
+      buttonContent={getButtonContent(value)}
+      options={options}
+      value={mappedValue}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...formInputProps}
+    />
+  );
+};
---
+++ ui/src/uiComponents/MultiSelectListbox.spec.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { render } from '../utils/test-utils';
+import { MultiSelectListbox } from './MultiSelectListbox';
+
+describe('<MultiSelectListbox />', () => {
+  const testId = 'multiselectlistbox1';
+  const buttonContent = `button`;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const onChange = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const onBlur = () => {};
+  const options = [
+    { key: 'option1', value: 'value1', render: () => 'label1' },
+    { key: 'option2', value: 'value2', render: () => 'label2' },
+  ];
+
+  test('Opens dropdown when clicked', async () => {
+    render(
+      <MultiSelectListbox
+        testId={testId}
+        buttonContent={buttonContent}
+        onChange={onChange}
+        onBlur={onBlur}
+        options={options}
+      />,
+    );
+
+    // dropdown is collapsed:
+    expect(screen.queryByText('button')).toBeVisible();
+    expect(screen.queryByText('label1')).toBeNull();
+
+    // click dropdown to open it:
+    const openDropdownButton = screen.getByTestId(testId);
+    fireEvent.click(openDropdownButton);
+
+    // dropdown is open:
+    expect(screen.queryByText('button')).toBeVisible();
+    expect(screen.queryByText('label1')).toBeVisible();
+  });
+});
