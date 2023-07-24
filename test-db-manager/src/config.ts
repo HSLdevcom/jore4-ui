f854f685106660fcc341a981d971dc52c6fbe1c0
Samuli Rasi <samuli.rasi@valagroup.com>
2023-07-24 13:48:27 +0300

    Configurations to enable parallel e2e test execution
    Separate database and Hasura configurations for the CI and local test execution
    
    Resolves HSLdevcom/jore4#1332

--- test-db-manager/src/config.ts
+++ test-db-manager/src/config.ts
@@ -15,25 +15,45 @@ const testDbPort: Record<CurrentExecutorIndex, number> = {
   [CurrentExecutorIndex.default]: 6432,
 };
 
+const ciTestDbHost: Record<CurrentExecutorIndex, string> = {
+  [CurrentExecutorIndex.e2e1]: 'jore4-testdb-e2e1',
+  [CurrentExecutorIndex.e2e2]: 'jore4-testdb-e2e2',
+  [CurrentExecutorIndex.e2e3]: 'jore4-testdb-e2e3',
+  [CurrentExecutorIndex.default]: 'jore4-testdb',
+};
+
 const getTestDbPort = () => {
   const currentExecutorIndex =
     process.env.CYPRESS_THREAD || CurrentExecutorIndex.default;
+
+  if (process.env.CI) {
+    return '5432';
+  }
   return testDbPort[currentExecutorIndex];
 };
 
-const databaseBaseConfig = {
-  host: process.env.POSTGRES_HOST || '127.0.0.1',
-  port: process.env.POSTGRES_PORT || getTestDbPort(),
-  user: process.env.POSTGRES_USER || 'dbadmin',
-  password: process.env.POSTGRES_PASSWORD || 'adminpassword',
+const getTestDbHost = () => {
+  const currentExecutorIndex =
+    process.env.CYPRESS_THREAD || CurrentExecutorIndex.default;
+
+  if (process.env.CI) {
+    return ciTestDbHost[currentExecutorIndex];
+  }
+  return '127.0.0.1';
 };
 
 export const e2eDatabaseConfig: DatabaseConnectionInfo = {
-  ...databaseBaseConfig,
+  host: getTestDbHost(),
+  port: getTestDbPort(),
+  user: process.env.POSTGRES_USER || 'dbadmin',
+  password: process.env.POSTGRES_PASSWORD || 'adminpassword',
   database: process.env.POSTGRES_DB || 'jore4e2e',
 };
 
 export const timetablesDatabaseConfig: DatabaseConnectionInfo = {
-  ...databaseBaseConfig,
+  host: getTestDbHost(),
+  port: getTestDbPort(),
+  user: process.env.POSTGRES_USER || 'dbadmin',
+  password: process.env.POSTGRES_PASSWORD || 'adminpassword',
   database: 'timetablesdb',
 };
--- test-db-manager/src/hasuraApi.ts
+++ test-db-manager/src/hasuraApi.ts
@@ -1,18 +1,30 @@
 import fetch from 'cross-fetch';
 import { CurrentExecutorIndex } from './db-helpers/enums';
 
-const hasuraURL: Record<CurrentExecutorIndex, string> = {
+// integration tests need this
+const localhostHasuraURL: Record<CurrentExecutorIndex, string> = {
   [CurrentExecutorIndex.e2e1]: 'http://127.0.0.1:3211/v1/graphql',
   [CurrentExecutorIndex.e2e2]: 'http://127.0.0.1:3212/v1/graphql',
   [CurrentExecutorIndex.e2e3]: 'http://127.0.0.1:3213/v1/graphql',
   [CurrentExecutorIndex.default]: 'http://127.0.0.1:3201/v1/graphql',
 };
 
+// e2e tests need this in the CI
+const e2eCIHasuraURL: Record<CurrentExecutorIndex, string> = {
+  [CurrentExecutorIndex.e2e1]: 'http://jore4-hasura-e2e1:8080/v1/graphql',
+  [CurrentExecutorIndex.e2e2]: 'http://jore4-hasura-e2e2:8080/v1/graphql',
+  [CurrentExecutorIndex.e2e3]: 'http://jore4-hasura-e2e3:8080/v1/graphql',
+  [CurrentExecutorIndex.default]: 'http://jore4-hasura:8080/v1/graphql',
+};
+
 const getHasuraURL = () => {
   const currentExecutorIndex =
     process.env.CYPRESS_THREAD || CurrentExecutorIndex.default;
 
-  return hasuraURL[currentExecutorIndex];
+  if (process.env.CI && process.env.CYPRESS) {
+    return e2eCIHasuraURL[currentExecutorIndex];
+  }
+  return localhostHasuraURL[currentExecutorIndex];
 };
 
 export const hasuraApi = async (jsonPayload: unknown): Promise<unknown> => {
@@ -24,7 +36,5 @@ export const hasuraApi = async (jsonPayload: unknown): Promise<unknown> => {
       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET || 'hasura',
     },
   };
-  return fetch(process.env.HASURA_API_URL || getHasuraURL(), req).then(
-    (response) => response.json(),
-  );
+  return fetch(getHasuraURL(), req).then((response) => response.json());
 };
