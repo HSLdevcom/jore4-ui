ba6266503e5f3180f2c1589e87779784fec3757a
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-07-26 08:58:09 +0300

    Change routeHasTimetables parameter type to more narrow
    Initially it was Pick<RouteTableRowFragment, 'route_journey_patterns', but this means that
    if RouteTableRowFragment's route_journey_patterns get's additions, they are required here
    as well, even though this function which determines if route has any timetables, can suffice
    with only journey_pattern_refs from route_journey_patterns

--- ui/src/utils/route.ts
+++ ui/src/utils/route.ts
@@ -4,11 +4,27 @@ import {
 } from '../generated/graphql';
 
 /**
+ * Type that includes route_journey_patterns with journey pattern refs. This is used
+ * for checking if route has timetables, and this is used for type checking that the
+ * object has only the necessary information. So the routeHasTimetables() can be called
+ * with any route object, as long as it has
+ * route_journey_patterns: {
+ *   journey_pattern_refs
+ * }
+ */
+type RouteJourneyPatternsWithJourneyPatternRefs = {
+  route_journey_patterns: Pick<
+    RouteTableRowFragment['route_journey_patterns'][0],
+    'journey_pattern_refs'
+  >[];
+};
+
+/**
  * Checks if the route has any vehicle_journey's existing. If there is
  * that means that the route has timetables.
  */
 export const routeHasTimetables = (
-  route: Pick<RouteTableRowFragment, 'route_journey_patterns'>,
+  route: RouteJourneyPatternsWithJourneyPatternRefs,
 ) =>
   route.route_journey_patterns.some((routeJourneyPattern) =>
     routeJourneyPattern.journey_pattern_refs.some(
