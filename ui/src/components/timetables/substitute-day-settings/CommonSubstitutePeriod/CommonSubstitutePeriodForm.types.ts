67b17589154c5b45bf197023349e96fbe3102920
Jarno Perholehto <jarno.perholehto@vincit.fi>
2023-08-23 13:33:17 +0300

    Add better validation to common period form
    - SubstituteDayOfWeek and lineTypes can be empty strings to show placeholder
    - Clear validation errors when "hiding" item
    - Add needed translations

--- ui/src/components/timetables/substitute-day-settings/CommonSubstitutePeriod/CommonSubstitutePeriodForm.types.ts
+++ ui/src/components/timetables/substitute-day-settings/CommonSubstitutePeriod/CommonSubstitutePeriodForm.types.ts
@@ -1,17 +1,50 @@
+import isEmpty from 'lodash/isEmpty';
 import { z } from 'zod';
 import { SubstituteDayOfWeek } from '../../../../types/enums';
 import { requiredDate } from '../../../forms/common/customZodSchemas';
 
-const commonDay = z.object({
-  periodId: z.string().optional(),
-  periodName: z.string(),
-  supersededDate: requiredDate,
-  substituteDayOfWeek: z.nativeEnum(SubstituteDayOfWeek),
-  lineTypes: z.string(),
-  fromDatabase: z.boolean(),
-  created: z.boolean(),
-  isPreset: z.boolean(),
-});
+interface CommonDay {
+  created: boolean;
+  fromDatabase: boolean;
+  substituteDayOfWeek: string | SubstituteDayOfWeek;
+  lineTypes: string;
+  periodId?: string;
+  periodName: string;
+  supersededDate: string;
+  isPreset: boolean;
+}
+
+type CommonDayProperty = 'substituteDayOfWeek' | 'lineTypes';
+
+const commonDayValidation = (
+  data: CommonDay,
+  property: CommonDayProperty,
+): boolean => {
+  if (data.created && !data.fromDatabase) {
+    return !isEmpty(data[property]);
+  }
+  return true;
+};
+
+const commonDay = z
+  .object({
+    periodId: z.string().optional(),
+    periodName: z.string(),
+    supersededDate: requiredDate,
+    substituteDayOfWeek: z.nativeEnum(SubstituteDayOfWeek).or(z.string()),
+    lineTypes: z.string(),
+    fromDatabase: z.boolean(),
+    created: z.boolean(),
+    isPreset: z.boolean(),
+  })
+  .refine((data) => commonDayValidation(data, 'substituteDayOfWeek'), {
+    message: 'substituteDayOfWeek',
+    path: ['substituteDayOfWeek'],
+  })
+  .refine((data) => commonDayValidation(data, 'lineTypes'), {
+    message: 'lineTypes',
+    path: ['lineTypes'],
+  });
 
 export const schema = z.object({
   commonDays: commonDay.array(),
