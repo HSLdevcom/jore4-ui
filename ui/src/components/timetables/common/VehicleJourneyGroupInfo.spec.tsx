ae88d8416c5ffa2f0796e592bbb561edaad7833c
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-09-05 15:17:30 +0300

    Add component test for VehicleJourneyGroupInfo
    Writing this test also uncovered the fact that the component takes in
    a lot of unnecessary information.
    
    Resolves HSLdevcom/jore4#1262

---
+++ ui/src/components/timetables/common/VehicleJourneyGroupInfo.spec.tsx
@@ -0,0 +1,106 @@
+import { DateTime, Duration } from 'luxon';
+import { VehicleJourneyWithServiceFragment } from '../../../generated/graphql';
+import { VehicleJourneyGroup } from '../../../hooks';
+import { TimetablePriority } from '../../../types/enums';
+import { render } from '../../../utils/test-utils';
+import { VehicleJourneyGroupInfo } from './VehicleJourneyGroupInfo';
+
+describe(`<${VehicleJourneyGroupInfo.name} />`, () => {
+  // TODO: this component requires way too much irrelevant information as parameter and
+  // should be refactored to take in only the necessarities.
+
+  const baseVehicleJourney: VehicleJourneyWithServiceFragment = {
+    vehicle_journey_id: '',
+    journey_pattern_ref_id: '',
+    journey_pattern_ref: {
+      journey_pattern_id: '',
+      journey_pattern_ref_id: '',
+    },
+    block: {
+      block_id: '',
+      vehicle_service: {
+        vehicle_service_id: '',
+        day_type_id: '',
+        vehicle_schedule_frame: {
+          vehicle_schedule_frame_id: '',
+          priority: TimetablePriority.Standard,
+          validity_start: DateTime.fromISO('2023-01-01'),
+          validity_end: DateTime.fromISO('2023-05-31'),
+          created_at: DateTime.fromISO('2023-01-01'),
+        },
+        day_type: {
+          label: '',
+          day_type_id: '',
+          name_i18n: {},
+        },
+      },
+      vehicle_service_id: '',
+    },
+    start_time: Duration.fromISO('PT08H'),
+    end_time: Duration.fromISO('PT09H'),
+    timetabled_passing_times: [],
+  };
+
+  const vehicleJourneyGroup: VehicleJourneyGroup = {
+    vehicleScheduleFrameId: '9feedd34-7a62-4de1-ac2e-1b8bdd584c67',
+    dayType: {
+      day_type_id: '',
+      label: '',
+      name_i18n: {
+        en_US: '',
+        fi_FI: '',
+        sv_FI: '',
+      },
+    },
+    priority: TimetablePriority.Standard,
+    validity: {
+      validityStart: DateTime.fromISO('2023-01-01'),
+      validityEnd: DateTime.fromISO('2023-05-31'),
+    },
+    vehicleJourneys: [
+      {
+        ...baseVehicleJourney,
+        start_time: Duration.fromISO('PT08H10M'),
+      },
+      {
+        ...baseVehicleJourney,
+        start_time: Duration.fromISO('PT08H15M'),
+      },
+      {
+        ...baseVehicleJourney,
+        start_time: Duration.fromISO('PT08H20M'),
+      },
+    ],
+  };
+
+  test('Renders VehicleJourneyGroupInfo with imported timetable', async () => {
+    const { getByText, getByTestId } = render(
+      <VehicleJourneyGroupInfo vehicleJourneyGroup={vehicleJourneyGroup} />,
+    );
+
+    expect(getByText('1.1.2023 - 31.5.2023')).toBeVisible();
+    expect(getByText('3 lähtöä')).toBeVisible();
+    expect(getByText('08:10 ... 08:20')).toBeVisible();
+    expect(
+      getByTestId('VehicleJourneyGroupInfo::changeValidityButton'),
+    ).toBeEnabled();
+  });
+
+  test('Render VehicleJourneyGroupInfo with substitute timetable (no vsf id)', async () => {
+    const { getByText, getByTestId } = render(
+      <VehicleJourneyGroupInfo
+        vehicleJourneyGroup={{
+          ...vehicleJourneyGroup,
+          vehicleScheduleFrameId: null,
+        }}
+      />,
+    );
+
+    expect(getByText('1.1.2023 - 31.5.2023')).toBeVisible();
+    expect(getByText('3 lähtöä')).toBeVisible();
+    expect(getByText('08:10 ... 08:20')).toBeVisible();
+    expect(
+      getByTestId('VehicleJourneyGroupInfo::changeValidityButton'),
+    ).toBeDisabled();
+  });
+});
