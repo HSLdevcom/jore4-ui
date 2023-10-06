c6cddce6f5bf6e17c23b898f481a28a5ff33b911
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-10-06 16:53:28 +0300

    Refactor DialogWithButtons width handling
    A mere class name is more flexible.
    Can't pass it in normal className prop because
    multiple Tailwind max-width classes seem to break the layout.

--- ui/src/components/forms/route/RouteDraftStopsConfirmationDialog.tsx
+++ ui/src/components/forms/route/RouteDraftStopsConfirmationDialog.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { DialogWithButtons } from '../../../uiComponents';
 
@@ -44,7 +43,7 @@ export const RouteDraftStopsConfirmationDialog = ({
       ]}
       onCancel={onCancel}
       className={className}
-      isWide
+      widthClassName="max-w-2xl"
     />
   );
 };
