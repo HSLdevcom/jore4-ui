e15c369f51dc801de96e811d600a5805d67276ba
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-10-16 09:50:27 +0300

    Duplicate PriorityForm to timetables module
    First step in refactoring.
    Would need to add more flags to this component,
    will rather just split it to two separate ones.

---
+++ cypress/pageObjects/TimetablesImportPriorityForm.ts
@@ -0,0 +1,69 @@
+import { TimetablePriority } from '@hsl/jore4-test-db-manager';
+
+export interface PriorityFormInfo {
+  priority?: TimetablePriority;
+}
+
+export class TimetablesImportPriorityForm {
+  setAsStandard() {
+    return cy
+      .getByTestId('TimetablesImportPriorityForm::standardPriorityButton')
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+  }
+
+  setAsDraft() {
+    return cy
+      .getByTestId('TimetablesImportPriorityForm::draftPriorityButton')
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+  }
+
+  setAsTemporary() {
+    return cy
+      .getByTestId('TimetablesImportPriorityForm::temporaryPriorityButton')
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+  }
+
+  setPriority = (priority: TimetablePriority) => {
+    switch (priority) {
+      case TimetablePriority.Draft:
+        this.setAsDraft();
+        break;
+      case TimetablePriority.Temporary:
+        this.setAsTemporary();
+        break;
+      case TimetablePriority.Standard:
+        this.setAsStandard();
+        break;
+      default:
+        throw new Error(`Unknown priority "${priority}"`);
+    }
+  };
+
+  assertSelectedPriority(priority: TimetablePriority | undefined) {
+    switch (priority) {
+      case TimetablePriority.Draft:
+        cy.getByTestId(
+          'TimetablesImportPriorityForm::draftPriorityButton',
+        ).should('have.attr', 'data-selected', 'true');
+        break;
+      case TimetablePriority.Temporary:
+        cy.getByTestId(
+          'TimetablesImportPriorityForm::temporaryPriorityButton',
+        ).should('have.attr', 'data-selected', 'true');
+        break;
+      case TimetablePriority.Standard:
+        cy.getByTestId(
+          'TimetablesImportPriorityForm::standardPriorityButton',
+        ).should('have.attr', 'data-selected', 'true');
+        break;
+      default:
+        throw new Error(`Unknown priority "${priority}"`);
+    }
+  }
+}
