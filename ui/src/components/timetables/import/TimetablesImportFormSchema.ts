a6bbd090ffe9aed06046c82d0d21b8ec7f8bba2a
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-10-20 15:40:38 +0300

    Automatically set value of Special priority checkbox
    Based on if there are any Special days or not.
    If there are, then priority must be special -> set.
    If there are not, then priority can not be special -> hide.

--- ui/src/components/timetables/import/TimetablesImportFormSchema.ts
+++ ui/src/components/timetables/import/TimetablesImportFormSchema.ts
@@ -1,7 +1,26 @@
 import { z } from 'zod';
+import { TimetablePriority } from '../../../types/enums';
 import { timetableImportStrategyFormSchema } from './TimetableImportStrategyForm';
 import { timetablesImportPriorityFormSchema } from './TimetablesImportPriorityForm';
 
 export const timetablesImportFormSchema =
   timetablesImportPriorityFormSchema.merge(timetableImportStrategyFormSchema);
 export type FormState = z.infer<typeof timetablesImportFormSchema>;
+
+const defaultValues: Partial<FormState> = {
+  timetableImportStrategy: 'replace',
+  // No default for priority, this is on purpose: design decision.
+};
+export const getDefaultValues = ({
+  importingSomeSpecialDays,
+}: {
+  importingSomeSpecialDays: boolean;
+}): Partial<FormState> => {
+  if (importingSomeSpecialDays) {
+    return {
+      ...defaultValues,
+      priority: TimetablePriority.Special,
+    };
+  }
+  return defaultValues;
+};
