c9fa017559f93fd08283a3d298e5b48025398a46
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-10-23 10:30:34 +0300

    Show a warning if there are both single and multi day timetables staged
    In this case the timetables cannot be imported,
    because single days would become special days
    and others can not be special days.
    
    Would require different kind of handling,
    eg. automatically setting special day priority on backend.

---
+++ ui/src/components/timetables/import/SpecialDayMixedPrioritiesWarning.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'react-i18next';
+import { MdWarning } from 'react-icons/md';
+
+export const SpecialDayMixedPrioritiesWarning = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="relative mt-8 flex flex-row space-x-4 rounded-lg border border-hsl-highlight-yellow-dark bg-hsl-highlight-yellow-light p-6">
+      <MdWarning className="mr-2 inline h-6 w-6 text-hsl-red" role="img" />
+      <div className="flex flex-row">
+        {t('import.specialDayMixedPrioritiesWarning1')}
+        <br />
+        {t('import.specialDayMixedPrioritiesWarning2')}
+      </div>
+    </div>
+  );
+};
