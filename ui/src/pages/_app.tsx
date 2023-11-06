7a3f4b6ef851cb8fec4e70b00836004ad3baf41f
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-11-06 16:14:12 +0200

    Refactor index.tsx to have the structure for the pages.
    We already had the title and footer in the index.tsx and all the routing and providers should also be there.

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import 'maplibre-gl/dist/maplibre-gl.css';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
-import { UserProvider } from '../auth/UserProvider';
 import '../generated/fontello/css/hsl-icons.css';
-import { ApolloProvider } from '../graphql';
 import '../i18n';
-import { ReduxProvider } from '../redux';
-import { Router } from '../router/Router';
 import '../styles/globals.css';
-import { Toaster } from '../uiComponents/Toaster';
 
 const SafeHydrate = ({ children }: { children: JSX.Element }) => {
   // We are not using SSR. Use useEffect as workaround to
@@ -27,15 +22,8 @@ const SafeHydrate = ({ children }: { children: JSX.Element }) => {
 const Jore4App = ({ Component, pageProps }: AppProps) => {
   return (
     <SafeHydrate>
-      <ApolloProvider>
-        <ReduxProvider>
-          <UserProvider />
-          <Router />
-          <Toaster />
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </ReduxProvider>
-      </ApolloProvider>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
     </SafeHydrate>
   );
 };
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
+import { UserProvider } from '../auth/UserProvider';
+import { ApolloProvider } from '../graphql';
+import { ReduxProvider } from '../redux';
+import { Router } from '../router/Router';
+import { Toaster } from '../uiComponents/Toaster';
 
 const Index = () => {
   const { t } = useTranslation();
@@ -9,7 +14,13 @@ const Index = () => {
         <title>JORE4 Testiversio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <ApolloProvider>
+        <ReduxProvider>
+          <UserProvider />
+          <Router />
+          <Toaster />
+        </ReduxProvider>
+      </ApolloProvider>
       <footer className="mt-6 flex justify-center">
         <p>{t('version', { version: process.env.NEXT_PUBLIC_GIT_HASH })}</p>
       </footer>
