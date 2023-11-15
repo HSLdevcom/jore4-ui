85e8772a535d080c41b255ef8658001944f34a29
Jarno Perholehto <jarno.perholehto@vincit.fi>
2023-11-15 13:36:28 +0200

    Remove original seed file
    - `seed.ts` was not used anymore and it doesn't work with current data model
    - Even if we want to `seed.ts` back, it's still in git history
    - Replaced `seed.ts` with `seed2.ts`, didn't see a reason not to do it.
    - Removed references to `seed2.ts/js`
    - Removed matrix strategy from `check-seed-generators.yml`

--- test-db-manager/rollup.config.mjs
+++ test-db-manager/rollup.config.mjs
@@ -60,18 +60,6 @@ const config = [
     plugins: commonPlugins,
   },
   {
-    input: 'src/seed2.ts',
-    external: commonExternals,
-    output: [
-      {
-        file: 'dist/seed2.js',
-        format: 'cjs', // commonJS
-        sourcemap: true,
-      },
-    ],
-    plugins: commonPlugins,
-  },
-  {
     input: './ts-dist/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [dts()],
