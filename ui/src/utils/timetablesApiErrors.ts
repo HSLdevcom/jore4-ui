79f75385edd77486f5e243fea066fe28a97f3dee
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-11-17 11:50:45 +0200

    Add method for extracting the raw SQL error from transaction system errors
    Possibly to be used later in a different error dialog.

--- ui/src/utils/timetablesApiErrors.ts
+++ ui/src/utils/timetablesApiErrors.ts
@@ -29,3 +29,13 @@ export const extractErrorType = (
     ? errorType
     : 'UnknownError';
 };
+
+export const extractRawSqlError = (error: ApolloError): string | null => {
+  // See `TransactionSystemExtensions` in timetables API.
+  const sqlErrorMessageExtension =
+    error?.graphQLErrors[0]?.extensions?.sqlErrorMessage;
+
+  return typeof sqlErrorMessageExtension === 'string'
+    ? sqlErrorMessageExtension
+    : null;
+};
