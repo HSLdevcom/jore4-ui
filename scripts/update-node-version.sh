9a9a0b1cd205c4a6219a5bb4078216adfd31cd94
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-12-07 14:20:44 +0200

    Add a script which can update Node version
    Add a script which updates Node version in every file that currently
    uses it.

---
+++ scripts/update-node-version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $(dirname "$0")/..
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 version_number"
+    echo "No version given, exiting..."
+else
+    NODE_VERSION="$1"
+
+    sed -i "s/\"node\": \".*\",/\"node\": \"$NODE_VERSION\",/g" package.json
+    sed -i "s/\"@types\/node\": \".*\",/\"@types\/node\": \"^$NODE_VERSION\",/g" ui/package.json
+    sed -i "s/\"@types\/node\": \".*\",/\"@types\/node\": \"^$NODE_VERSION\",/g" cypress/package.json
+    sed -i "s/ARG NODE_VERSION='.*'/ARG NODE_VERSION='$NODE_VERSION'/g" Dockerfile.cypress
+    sed -i "s/FROM node:.*-alpine/FROM node:$NODE_VERSION-alpine/g" Dockerfile
+    sed -i "s/node-version: '.*'/node-version: '$NODE_VERSION'/g" .github/workflows/check-generated-resources.yml
+    sed -i "s/node-version: '.*'/node-version: '$NODE_VERSION'/g" .github/workflows/check-seed-generators.yml
+    sed -i "s/node-version: '.*'/node-version: '$NODE_VERSION'/g" .github/workflows/ci.yml
+    sed -i "s/node-version: '.*'/node-version: '$NODE_VERSION'/g" .github/workflows/run-integration-tests.yml
+fi
