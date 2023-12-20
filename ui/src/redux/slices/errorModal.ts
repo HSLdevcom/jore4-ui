783a3874ba1a8971028c4bba46643b695348b154
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-12-20 10:09:00 +0200

    Add a single error variant of error modal
    Largely same as the error list version,
    except without the message (sub title really)
    and background (whose main purpose is to separate different error items).

--- ui/src/redux/slices/errorModal.ts
+++ ui/src/redux/slices/errorModal.ts
@@ -13,6 +13,8 @@ export interface ErrorListItem extends ErrorDetails {
 interface IState {
   isOpen: boolean;
   errorModalTitle: string;
+  // These are mutually exclusive. See actions below.
+  singleErrorDetails?: ErrorDetails;
   errorList: ErrorListItem[];
 }
 
@@ -35,9 +37,23 @@ const slice = createSlice({
     ) => {
       state.errorModalTitle = action.payload.errorModalTitle;
       state.errorList = action.payload.errorList;
+      state.singleErrorDetails = undefined; // Not used in this case.
+      state.isOpen = true;
+    },
+    openSingleErrorModal: (
+      state: IState,
+      action: PayloadAction<{
+        errorModalTitle: string;
+        errorDetails: ErrorDetails;
+      }>,
+    ) => {
+      state.errorModalTitle = action.payload.errorModalTitle;
+      state.singleErrorDetails = action.payload.errorDetails;
+      state.errorList = []; // Not used in this case.
       state.isOpen = true;
     },
     closeErrorModal: (state: IState) => {
+      state.singleErrorDetails = undefined;
       state.errorList = [];
       state.isOpen = false;
     },
@@ -46,6 +62,7 @@ const slice = createSlice({
 
 export const {
   openErrorListModal: openErrorListModalAction,
+  openSingleErrorModal: openSingleErrorModalAction,
   closeErrorModal: closeErrorModalAction,
 } = slice.actions;
 
