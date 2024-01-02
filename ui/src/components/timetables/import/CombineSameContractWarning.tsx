549a1fcdfee881c8744466d2c875ffb0a6f7590f
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-01-02 17:52:45 +0200

    Adjust the warning icon size in confirm timetables import form

--- ui/src/components/timetables/import/CombineSameContractWarning.tsx
+++ ui/src/components/timetables/import/CombineSameContractWarning.tsx
@@ -3,10 +3,12 @@ import { MdWarning } from 'react-icons/md';
 
 interface Props {
   className?: string;
+  iconClassName?: string;
 }
 
 export const CombineSameContractWarning = ({
   className = '',
+  iconClassName = '',
 }: Props): JSX.Element => {
   const { t } = useTranslation();
 
@@ -14,7 +16,10 @@ export const CombineSameContractWarning = ({
     <div
       className={`relative mt-8 flex flex-row space-x-4 rounded-lg border border-hsl-highlight-yellow-dark bg-hsl-highlight-yellow-light p-6 ${className}`}
     >
-      <MdWarning className="mr-2 inline h-6 w-6 text-hsl-red" role="img" />
+      <MdWarning
+        className={`mr-2 inline text-hsl-red ${iconClassName}`}
+        role="img"
+      />
       <div className="flex flex-row">
         {t('import.combineSameContractWarning')}
       </div>
