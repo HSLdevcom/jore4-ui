b4abac0e2816deb2ffe084b5d5d91a830b140c1e
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-01-03 08:26:25 +0200

    Add tests for timetables import contents table
    As integration tests rather than for each individual child component,
    to better enable possible refactoring in the future.
    
    Pretty much all of the tested text contents are also present
    in the generated snapshots, but thought it better to test
    these explicitly.
    
    Resolves https://github.com/HSLdevcom/jore4/issues/1264

--- ui/src/components/routes-and-lines/line-details/DirectionBadge.tsx
+++ ui/src/components/routes-and-lines/line-details/DirectionBadge.tsx
@@ -2,9 +2,11 @@ import { RouteDirectionEnum } from '../../../generated/graphql';
 import { mapDirectionToShortUiName } from '../../../i18n/uiNameMappings';
 
 const testIds = {
+  container: 'DirectionBadge::container',
   directionBadge: (direction: RouteDirectionEnum) =>
     `DirectionBadge::${direction}`,
 };
+export const directionBadgeTestIds = testIds;
 
 type Props = {
   titleName: string;
@@ -20,6 +22,7 @@ export const DirectionBadge = ({
   return (
     <span
       title={titleName}
+      data-testid={testIds.container}
       className={`relative flex h-9 w-9 items-center justify-center bg-brand text-2xl font-bold text-white ${className}`}
     >
       <span data-testid={testIds.directionBadge(direction)}>
