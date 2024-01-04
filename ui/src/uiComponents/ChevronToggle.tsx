45705ab6d0fa17e88666e7d661a15f530de362c2
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-04 13:32:57 +0200

    Add groundwork for button title rework
    Due to diving too deep too quickly, there are lots of changes done at the moment, so I'm trying to break them into more manageable pieces.
    This first commit contains all of the texts so far, and the components that use will come in more semantically grouped commits.
    
    Also add title attributes to very root level or independent components.
    The optional tags will be taken out before merging to main, to enforce future buttons to include titles or other aria-labels.

--- ui/src/uiComponents/ChevronToggle.tsx
+++ ui/src/uiComponents/ChevronToggle.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { IconButton } from './IconButton';
 
@@ -5,24 +6,36 @@ type Props = {
   isToggled: boolean;
   testId?: string;
   onClick: () => void;
+  controls: string;
 };
 
 export const ChevronToggle = ({
   isToggled,
   onClick,
   testId,
+  controls,
 }: Props): JSX.Element => {
+  const { t } = useTranslation();
   const iconClassName = 'text-3xl text-tweaked-brand';
   return (
     <IconButton
+      title={
+        isToggled
+          ? t('accessibility:button.shrinkSearch')
+          : t('accessibility:button.expandSearch')
+      }
       onClick={onClick}
       icon={
         isToggled ? (
-          <FaChevronUp className={iconClassName} />
+          <FaChevronUp className={iconClassName} aria-hidden />
         ) : (
-          <FaChevronDown className={iconClassName} />
+          <FaChevronDown className={iconClassName} aria-hidden />
         )
       }
+      ariaAttributes={{
+        ariaExpanded: isToggled,
+        ariaControls: controls,
+      }}
       testId={testId}
     />
   );
