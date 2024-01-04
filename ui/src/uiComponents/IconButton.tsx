703f3824584e73bf96372b5c782fc67144cf1d1b
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-04 13:32:57 +0200

    Add groundwork for button title rework
    Due to diving too deep too quickly, there are lots of changes done at the moment, so I'm trying to break them into more manageable pieces.
    This first commit contains all of the texts so far, and the components that use will come in more semantically grouped commits.
    
    Also add title attributes to very root level or independent components.
    The optional tags will be taken out before merging to main, to enforce future buttons to include titles or other aria-labels.

--- ui/src/uiComponents/IconButton.tsx
+++ ui/src/uiComponents/IconButton.tsx
@@ -3,27 +3,39 @@ import { addClassName } from '../utils/components';
 
 interface Props {
   testId?: string;
+  title?: string;
   className?: string;
   disabled?: boolean;
   icon: React.ReactNode;
+  ariaAttributes?: {
+    ariaExpanded?: boolean;
+    ariaControls?: string;
+    ariaLabel?: string;
+  };
   onClick: () => void;
 }
 
 export const IconButton: React.FC<Props> = ({
   testId,
+  title,
   className = '',
   disabled,
   icon,
+  ariaAttributes,
   onClick,
 }) => {
   const iconClassName = 'inline text-center';
   return (
     <button
       data-testid={testId}
+      title={title}
       className={`text-center ${className}`}
       type="button"
       onClick={onClick}
       disabled={disabled}
+      aria-expanded={ariaAttributes?.ariaExpanded}
+      aria-controls={ariaAttributes?.ariaControls}
+      aria-label={ariaAttributes?.ariaLabel}
     >
       {React.isValidElement(icon) ? addClassName(icon, iconClassName) : icon}
     </button>
