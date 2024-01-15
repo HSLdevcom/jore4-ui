f1801f18ba3efceba4a06842981640b81bcecc1e
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-15 07:56:33 +0200

    Refactor Tooltip into built-in title
    For simplicity's sake, the custom Tooltip element was deemed to cause unnecessary fiddling with aria-labels, when a simple title-attribute suffices.

--- ui/src/components/routes-and-lines/line-details/ExpandableRouteRow.tsx
+++ ui/src/components/routes-and-lines/line-details/ExpandableRouteRow.tsx
@@ -123,6 +123,7 @@ export const ExpandableRouteRow = ({
             !route.route_shape /* some routes imported from jore3 are missing the geometry */
           }
           testId={testIds.showRouteButton}
+          tooltipText={route.label}
         />
       </td>
       <td className="w-20">
