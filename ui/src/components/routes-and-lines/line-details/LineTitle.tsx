df08be19b79efd6fb8d827a2720a1d8351033902
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-16 13:03:47 +0200

    Refactor accessibility translation hierarchy
    Accessibility translations didn't match the pre-existing translation hierarchy, now it does and is better and clearer for it.

--- ui/src/components/routes-and-lines/line-details/LineTitle.tsx
+++ ui/src/components/routes-and-lines/line-details/LineTitle.tsx
@@ -74,7 +74,7 @@ export const LineTitle: React.FC<Props> = ({
         </span>
         {onCreateRoute && (
           <IconButton
-            title={t(`accessibility:button.route.createNew`)}
+            title={t(`accessibility:routes.createNew`)}
             testId={testIds.createRouteButton}
             icon={<AiFillPlusCircle className="ml-2 text-3xl text-brand" />}
             onClick={onCreateRoute}
