cc8e6e7bbd8fb3f6b79441ce6ad9b1a93b04d939
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-26 13:40:02 +0200

    Add optional id prop to FormColumn and Column

--- ui/src/layoutComponents/Column.tsx
+++ ui/src/layoutComponents/Column.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 interface Props {
   className?: string;
+  id?: string;
 }
 
-export const Column: React.FC<Props> = ({ className = '', children }) => {
-  return <div className={`flex flex-col ${className}`}>{children}</div>;
+export const Column: React.FC<Props> = ({
+  className = '',
+  id = '',
+  children,
+}) => {
+  return (
+    <div id={id} className={`flex flex-col ${className}`}>
+      {children}
+    </div>
+  );
 };
