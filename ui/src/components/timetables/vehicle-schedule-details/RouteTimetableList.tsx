0c56572a22f87b3a245c23b80327426731c059ea
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-29 21:18:41 +0200

    Add accessibility features to AccordionButton
    Adds open and close titles for the AccordionButton, and when those are redundant (e.g. button text or label suffice) one can use the ariaLabel attribute.
    Additionally small DRY fixes for some of the labels/names/etc that are used in the tooltips and elements elsewhere.

--- ui/src/components/timetables/vehicle-schedule-details/RouteTimetableList.tsx
+++ ui/src/components/timetables/vehicle-schedule-details/RouteTimetableList.tsx
@@ -7,8 +7,8 @@ interface Props {
 export const RouteTimetableList = ({ routeIds }: Props): JSX.Element => {
   return (
     <div className="grid gap-y-5">
-      {routeIds.map((item) => (
-        <RouteTimetablesSection key={item} routeId={item} />
+      {routeIds.map((item, index) => (
+        <RouteTimetablesSection key={item} routeId={item} index={index} />
       ))}
     </div>
   );
