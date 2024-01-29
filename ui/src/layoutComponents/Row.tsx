2b5b01baa520a050c3838743b2cf24612d977b6d
Niki Niittymäki <niki.niittymaki@vincit.fi>
2024-01-29 23:38:30 +0200

    Add optional tooltip to Row element

--- ui/src/layoutComponents/Row.tsx
+++ ui/src/layoutComponents/Row.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 interface Props {
   className?: string;
   testId?: string;
+  tooltip?: string;
 }
 
 export const Row: React.FC<Props> = ({
   className = '',
   children,
   testId = null,
+  tooltip = '',
 }) => {
   return (
-    <div className={`flex flex-row ${className}`} data-testid={testId}>
+    <div
+      className={`flex flex-row ${className}`}
+      title={tooltip}
+      data-testid={testId}
+    >
       {children}
     </div>
   );
