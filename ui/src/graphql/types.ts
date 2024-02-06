82dac4f1312d24c818292acd74790f036ea51242
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-02-06 10:46:21 +0200

    Query and display basic stop place details from Tiamat
    Resolves https://github.com/HSLdevcom/jore4/issues/1656

--- ui/src/graphql/types.ts
+++ ui/src/graphql/types.ts
@@ -24,6 +24,7 @@ export type GqlScalar =
   | LocalizedString;
 
 // this actually includes more than just the entity types, but at least is updated automatically by graphql-codegen
+// Note: this does NOT include eg. timetables and stop registry DBs.
 export type GqlEntityType = keyof QueryRoot;
 
 export type GqlEntity = {
@@ -31,8 +32,17 @@ export type GqlEntity = {
   [key: string]: GqlEntity | GqlEntity[] | GqlScalar;
 };
 
-export const isGqlEntity = (entity: unknown): entity is GqlEntity =>
+/**
+ * Like `isGqlEntity` but not as type safe.
+ * Can be used to check entity types that are not included in `GqlEntityType`.
+ */
+export const hasTypeName = (
+  entity: unknown,
+): entity is { __typename: string } =>
   isObject(entity) && '__typename' in entity;
+
+export const isGqlEntity = (entity: unknown): entity is GqlEntity =>
+  hasTypeName(entity);
 
 export const isGqlEntityArray = (entity: unknown): entity is GqlEntity[] =>
   isArray(entity) && entity.every(isGqlEntity);
