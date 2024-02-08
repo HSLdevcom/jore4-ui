aa80537ef377be6e8ccf907843b6d5ec89b77c71
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-02-08 14:00:56 +0200

    Add base for stop details view E2E tests
    With routes and lines DB data only.
    The test data was partly adapted from editRoute.cy.ts suite.

---
+++ cypress/pageObjects/stop-registry/StopDetailsPage.ts
@@ -0,0 +1,23 @@
+import { UUID } from '../../types';
+
+export class StopDetailsPage {
+  visit(scheduledStopPointId: UUID) {
+    cy.visit(`/stop-registry/stops/${scheduledStopPointId}`);
+  }
+
+  page() {
+    return cy.getByTestId('StopDetailsPage::page');
+  }
+
+  label() {
+    return cy.getByTestId('StopTitleRow::label');
+  }
+
+  names() {
+    return cy.getByTestId('StopTitleRow::names');
+  }
+
+  validityPeriod() {
+    return cy.getByTestId('StopDetailsPage::validityPeriod');
+  }
+}
