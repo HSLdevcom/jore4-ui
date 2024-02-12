59f2bfeb1679ebf41b4dc1a42515974dcdb1c065
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-02-12 11:54:51 +0200

    Add stop details page link to stop popup in map

--- ui/src/generated/graphql.tsx
+++ ui/src/generated/graphql.tsx
@@ -19126,6 +19126,7 @@ export type RouteTableRowFragment = {
 
 export type StopPopupInfoFragment = {
   __typename?: 'service_pattern_scheduled_stop_point';
+  scheduled_stop_point_id: UUID;
   label: string;
   priority: number;
   validity_start?: luxon.DateTime | null;
@@ -23864,6 +23865,7 @@ export const RouteTableRowFragmentDoc = gql`
 `;
 export const StopPopupInfoFragmentDoc = gql`
   fragment stop_popup_info on service_pattern_scheduled_stop_point {
+    scheduled_stop_point_id
     label
     priority
     validity_start
