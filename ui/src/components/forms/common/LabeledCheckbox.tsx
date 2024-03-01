e876ee7fd0f23d72a4dc33949ef721c861dd9981
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-03-01 19:16:16 +0200

    Implement the custom checkbox layout

--- ui/src/components/forms/common/LabeledCheckbox.tsx
+++ ui/src/components/forms/common/LabeledCheckbox.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { SimpleButton } from '../../../uiComponents';
 
 interface Props {
   label: string;
@@ -8,9 +7,7 @@ interface Props {
   id?: string;
   testId?: string;
   className?: string;
-  containerClassName?: string;
   selected?: boolean;
-  selectedClassName?: string;
   disabled?: boolean;
   disabledTooltip?: string;
 }
@@ -21,28 +18,66 @@ export const LabeledCheckbox = ({
   tooltip,
   id,
   testId,
-  className,
-  containerClassName,
+  className = '',
   selected,
-  selectedClassName,
   disabled,
   disabledTooltip,
 }: Props): JSX.Element => {
+  const getStyleIfEnabled = (style: string) => (disabled ? '' : style);
+  const colorsSelected = 'text-white';
+  const colorsInverted = 'text-tweaked-brand bg-white';
+  const borderSelected = `border-tweaked-brand ${getStyleIfEnabled(
+    'hover:border-tweaked-brand-darker30',
+  )}`;
+  const borderInverted = 'border-grey';
+
+  const styles = {
+    labelSelected: `${colorsSelected} ${borderSelected} border-2 py-[5px] pl-[5px] pr-[12px] bg-tweaked-brand`,
+    labelUnselected: `${colorsInverted} ${borderInverted} border py-[6px] pl-[6px] pr-[13px]
+      ${getStyleIfEnabled(
+        'hover:border-2 hover:py-[5px] hover:pl-[5px] hover:pr-[12px] hover:border-tweaked-brand',
+      )}`, // Hover border is thicker, so paddings need to be reduced to maintain size...
+    inputSelected: `${colorsInverted} ${borderSelected}`,
+    inputUnselected: `${colorsSelected} ${borderInverted} before:opacity-0 ${getStyleIfEnabled(
+      'group-hover:bg-background group-hover:border-grey',
+    )}`,
+  };
+
   return (
-    <SimpleButton
-      id={id}
-      testId={testId}
-      className={className}
-      containerClassName={containerClassName}
+    // Using button instead of label because button can have focus and label can't.
+    // With label we would have to rely on the input's focus, and some styling wouldn't be possible,
+    // eg. styling the label based on input:focus-visible.
+    <button
+      type="button"
+      className={`
+        group inline-flex cursor-default select-none items-center gap-2 rounded-[5px] border-solid
+        text-sm font-bold
+        focus-visible:outline focus-visible:outline-2 focus-visible:outline-black
+        disabled:cursor-not-allowed disabled:opacity-70
+        ${selected ? styles.labelSelected : styles.labelUnselected}
+        ${className}
+      `}
       onClick={onClick}
-      tooltip={tooltip}
-      selected={selected}
-      inverted={!selected}
-      invertedClassName={selectedClassName}
       disabled={disabled}
-      disabledTooltip={disabledTooltip}
+      title={disabled ? disabledTooltip : tooltip}
     >
-      {label}
-    </SimpleButton>
+      <input
+        id={id}
+        tabIndex={-1} // Focus the button instead.
+        data-testid={testId}
+        className={
+          ' h-6 w-6 border p-0 text-[18px]' +
+          ' icon-check flex items-center justify-center' +
+          ' appearance-none' + // Unset global styles.
+          ' focus-visible:outline-none' + // Disable the outline at least on Chrome, eg. when tabbing.
+          ` ${selected ? styles.inputSelected : styles.inputUnselected}`
+        }
+        type="checkbox"
+        onChange={onClick}
+        checked={selected}
+        disabled={disabled}
+      />
+      <span>{label}</span>
+    </button>
   );
 };
