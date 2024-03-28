fd6916c3e48c42bdd97ba93868700152ae827d21
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-03-28 10:22:55 +0200

    Add e2e tests for viewing stop place signage details

--- ui/src/components/stop-registry/stops/stop-details/SignageDetailsViewCard.tsx
+++ ui/src/components/stop-registry/stops/stop-details/SignageDetailsViewCard.tsx
@@ -6,6 +6,17 @@ import { DetailRow } from './DetailRow';
 import { LabeledDetail } from './LabeledDetail';
 import { optionalBooleanToUiText } from './utils';
 
+const testIds = {
+  container: 'SignageDetailsViewCard::container',
+  signType: 'SignageDetailsViewCard::signType',
+  numberOfFrames: 'SignageDetailsViewCard::numberOfFrames',
+  lineSignage: 'SignageDetailsViewCard::lineSignage',
+  signageInstructionExceptions:
+    'SignageDetailsViewCard::signageInstructionExceptions',
+  replacesRailSign: 'SignageDetailsViewCard::replacesRailSign',
+  mainLineSign: 'SignageDetailsViewCard::mainLineSign',
+};
+
 interface Props {
   stop: StopWithDetails;
 }
@@ -22,33 +33,39 @@ export const SignageDetailsViewCard = ({ stop }: Props): JSX.Element => {
     );
 
   return (
-    <div>
+    <div data-testid={testIds.container}>
       <DetailRow>
         <LabeledDetail
           title={t('stopDetails.signs.signType')}
           detail={signType}
+          testId={testIds.signType}
         />
         <LabeledDetail
           title={t('stopDetails.signs.numberOfFrames')}
           detail={generalSign?.numberOfFrames?.toString()}
+          testId={testIds.numberOfFrames}
         />
         <LabeledDetail
           title={t('stopDetails.signs.lineSignage')}
           detail={optionalBooleanToUiText(generalSign?.lineSignage)}
+          testId={testIds.lineSignage}
         />
         <LabeledDetail
           title={t('stopDetails.signs.signageInstructionExceptions')}
           detail={generalSign?.note?.value}
+          testId={testIds.signageInstructionExceptions}
         />
       </DetailRow>
       <DetailRow>
         <LabeledDetail
           title={t('stopDetails.signs.replacesRailSign')}
           detail={optionalBooleanToUiText(generalSign?.replacesRailSign)}
+          testId={testIds.replacesRailSign}
         />
         <LabeledDetail
           title={t('stopDetails.signs.mainLineSign')}
           detail={optionalBooleanToUiText(generalSign?.mainLineSign)}
+          testId={testIds.mainLineSign}
         />
       </DetailRow>
     </div>
