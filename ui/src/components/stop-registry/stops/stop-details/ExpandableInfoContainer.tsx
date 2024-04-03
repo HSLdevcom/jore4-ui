c5602f8a09bd0c56f84c5f8bb8f7abd5766eaee3
Henrik Hartiala <henrik.hartiala@vincit.fi>
2024-04-03 18:39:59 +0300

    Add possibility for editMode to ExpandableInfoContainer

--- ui/src/components/stop-registry/stops/stop-details/ExpandableInfoContainer.tsx
+++ ui/src/components/stop-registry/stops/stop-details/ExpandableInfoContainer.tsx
@@ -1,27 +1,41 @@
 import { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { Visible } from '../../../../layoutComponents';
 import { SimpleButton } from '../../../../uiComponents';
+import { SlimSimpleButton } from './SlimSimpleButton';
 
 interface Props {
   isExpanded: boolean;
   onToggle: () => void;
   title: string | ReactNode;
+  onCancel?: () => void;
+  onSave?: () => void;
+  isEditMode?: boolean;
+  toggleEditMode?: () => void;
   testIdPrefix?: string;
 }
 
 const testIds = {
   toggle: (prefix: string) => `${prefix}::toggle`,
   content: (prefix: string) => `${prefix}::content`,
+  editButton: (prefix: string) => `${prefix}::editButton`,
+  cancelButton: (prefix: string) => `${prefix}::cancelButton`,
+  saveButton: (prefix: string) => `${prefix}::saveButton`,
 };
 
 export const ExpandableInfoContainer: React.FC<Props> = ({
   isExpanded,
   onToggle,
+  onCancel,
+  onSave,
   title,
   testIdPrefix = '',
+  isEditMode,
+  toggleEditMode,
   children,
 }) => {
+  const { t } = useTranslation();
   return (
     <div className="my-3 [&>*]:border-border-hsl-blue">
       <div
@@ -31,9 +45,16 @@ export const ExpandableInfoContainer: React.FC<Props> = ({
         `}
       >
         <h4>{title}</h4>
-        <div>
-          {/* TODO: add other buttons, possibly depending on context. */}
-          <SimpleButton
+        <div className="flex space-x-2">
+          {!isEditMode && toggleEditMode && (
+            <SlimSimpleButton
+              testId={testIds.editButton(testIdPrefix)}
+              onClick={toggleEditMode}
+            >
+              {t('edit')}
+            </SlimSimpleButton>
+          )}
+          <SlimSimpleButton
             onClick={onToggle}
             inverted={!isExpanded}
             testId={testIds.toggle(testIdPrefix)}
@@ -43,15 +64,45 @@ export const ExpandableInfoContainer: React.FC<Props> = ({
             ) : (
               <FaChevronDown className="text-tweaked-brand" aria-hidden />
             )}
-          </SimpleButton>
+          </SlimSimpleButton>
         </div>
       </div>
       <Visible visible={isExpanded}>
         <div
           data-testid={testIds.content(testIdPrefix)}
-          className="rounded-b-lg border-x border-b p-5 [&>hr]:mt-5"
+          className={`${
+            isEditMode ? '' : 'rounded-b-lg border-b'
+          } border-x p-5 [&>hr]:mt-5`}
         >
           {children}
+        </div>
+      </Visible>
+      <Visible visible={isExpanded && isEditMode}>
+        <div
+          className={`
+          flex items-center justify-end space-x-2 rounded-b-lg
+          border
+          bg-hsl-neutral-blue px-4 py-2 ${isExpanded ? '' : 'rounded-b-lg'}
+        `}
+        >
+          {onCancel && (
+            <SimpleButton
+              onClick={onCancel}
+              inverted
+              testId={testIds.cancelButton(testIdPrefix)}
+            >
+              {t('cancel')}
+            </SimpleButton>
+          )}
+          {onSave && (
+            <SimpleButton
+              onClick={onSave}
+              id="save-button"
+              testId={testIds.saveButton(testIdPrefix)}
+            >
+              {t('save')}
+            </SimpleButton>
+          )}
         </div>
       </Visible>
     </div>
