e9e2a951d05bf41df5ca07b033767fd43e575270
Henrik Hartiala <henrik.hartiala@vincit.fi>
2024-04-03 16:16:46 +0300

    Specified TimingPlace imports There were some scenarios where this more loosen import was causing dependency cycles. So by tightening up these imports to be more precise, we can avoid dependency cycles.

--- ui/src/components/forms/stop/TimingPlaceModal.tsx
+++ ui/src/components/forms/stop/TimingPlaceModal.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { useAppDispatch } from '../../../hooks';
+import { useAppDispatch } from '../../../hooks/redux';
 import { closeTimingPlaceModalAction } from '../../../redux/slices/modals';
 import { Modal, ModalBody, ModalHeader } from '../../../uiComponents';
 import { CreateTimingPlaceForm } from './CreateTimingPlaceForm';
