c6b5c8a7752ec39ba5b095be6c23d1a30c8080c7
Henrik Hartiala <henrik.hartiala@vincit.fi>
2024-04-03 19:24:02 +0300

    Add StopPlace basic information edit functionality
    Now this is a big one and some parts are still under consideration. So here
    we add the edit mode to basic information. The form files are pretty simple, but
    it is still under consideration that should all the form fields still be in only one file,
    or possibly have the form fields in their own components, but have the schema
    in the "parent" file.
    
    Other things to note: we can't achieve any kind of transactionality in frontend for the requests,
    and we are heavily trusting that the second update (to tiamat) goes in successfully if the update
    to routes and lines database is successful. But there is already at least one case where this is not
    the case: tiamat has e.g. an constraint that a 'rail replacement' can only be applied to bus typed stop.
    This is something we need to restrict in front end and that is not yet done in this iteration.
    
    Minor things: there are some TODO's still present, some are waiting for tiamat changes, some are
    just notes and will be removed in the final version of the PR. As is this commit message.
    
    Resolves HSLdevcom/jore4#1680

---
+++ ui/src/components/stop-registry/stops/stop-details/basic-details/StopLabelAndNameFormRow.tsx
@@ -0,0 +1,49 @@
+import { Column } from '../../../../../layoutComponents';
+import { FormRow, InputField } from '../../../../forms/common';
+import { StopBasicDetailsFormState } from './schema';
+
+const testIds = {
+  label: 'StopBasicDetailsForm::label',
+  publicCode: 'StopBasicDetailsForm::publicCode',
+  nameFin: 'StopBasicDetailsForm::nameFin',
+  nameSwe: 'StopBasicDetailsForm::nameSwe',
+};
+
+export const StopLabelAndNameFormRow = () => {
+  return (
+    <FormRow mdColumns={4}>
+      <Column>
+        <InputField<StopBasicDetailsFormState>
+          type="text"
+          translationPrefix="stopDetails"
+          fieldPath="label"
+          testId={testIds.label}
+        />
+      </Column>
+      <Column>
+        <InputField<StopBasicDetailsFormState>
+          type="text"
+          translationPrefix="stopDetails"
+          fieldPath="publicCode"
+          testId={testIds.publicCode}
+        />
+      </Column>
+      <Column>
+        <InputField<StopBasicDetailsFormState>
+          type="text"
+          translationPrefix="stopDetails"
+          fieldPath="nameFin"
+          testId={testIds.nameFin}
+        />
+      </Column>
+      <Column>
+        <InputField<StopBasicDetailsFormState>
+          type="text"
+          translationPrefix="stopDetails"
+          fieldPath="nameSwe"
+          testId={testIds.nameSwe}
+        />
+      </Column>
+    </FormRow>
+  );
+};
--- ui/src/hooks/stop-registry/index.ts
+++ ui/src/hooks/stop-registry/index.ts
@@ -1,2 +1,3 @@
 export * from './search';
 export * from './useGetStopDetails';
+export * from './useEditStopBasicDetails';
