c30c2bb9698538f875859dadd48d66da8969566b
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-04-05 16:29:52 +0300

    Display out of operation label in header
    For all states except in operation.

--- ui/src/components/stop-registry/stops/stop-details/StopHeaderSummaryRow.tsx
+++ ui/src/components/stop-registry/stops/stop-details/StopHeaderSummaryRow.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { StopWithDetails } from '../../../../hooks';
+import { mapStopPlaceStateToUiName } from '../../../../i18n/uiNameMappings';
+import { StopPlaceState } from '../../../../types/stop-registry';
 import { StopTypeLabel } from './StopTypeLabel';
 
 interface Props {
@@ -12,6 +14,8 @@ export const StopHeaderSummaryRow: React.FC<Props> = ({
   className = '',
 }) => {
   const { t } = useTranslation();
+  const stopState = stopDetails.stop_place?.stopState;
+
   return (
     <div className={`flex ${className}`}>
       <div className="flex items-center gap-5">
@@ -31,6 +35,11 @@ export const StopHeaderSummaryRow: React.FC<Props> = ({
           hasType={!!stopDetails?.stop_place?.stopType.virtual}
           text={t('stopPlaceTypes.virtual')}
         />
+        {stopState && stopState !== StopPlaceState.InOperation && (
+          <div className="rounded-md bg-dark-grey py-1 px-4 text-center text-sm uppercase leading-normal text-white">
+            {mapStopPlaceStateToUiName(stopState)}
+          </div>
+        )}
       </div>
     </div>
   );
