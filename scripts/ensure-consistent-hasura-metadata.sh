92ead2788df7b1d4fc1c369f43b8f700fb23f7ce
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-04-08 13:18:56 +0300

    Fix inconsistent Hasura metadata by reloading when starting dev/e2e environments
    The metadata sometimes ends up in an inconsistent state,
    preventing normal behavior of the GraphQL API.
    One example of this is when one restarts their computer.
    The depends_on settings in docker compose file take care of proper
    start up order when initially creating the containers,
    but this does not happen when they are restarted after computer reboot.
    So what happens is, Hasura is started before Tiamat
    and ends up with inconsistent metadata.
    
    This can be fixed manually by triggering metadata reload from Hasura console.
    Same thing can be done via the Hasura API.
    Similar thing is done on CI, see https://github.com/HSLdevcom/jore4-tools/commit/af15d8f2612c3def63f8097d87eec051613da607
    This script tries to automate that for local environments.
    
    For dev and local e2e environments, we now check the metadata state
    when we _think_ there _could_ be metadata inconsistencies,
    and then reload if needed.
    The reload can still be done explicitly with  a --force flag as well.
    The dev and local e2e environments live in different containers and ports
    so they need to be dealt with separately, thus the --e2e flag for toggling this.
    
    Added this check to yarn commands that typically are run first when
    restarting the dev / local e2e environments, that is,
    "yarn dev" and "yarn test:e2e:open".

---
+++ scripts/ensure-consistent-hasura-metadata.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# A script for checking that Hasura metadata is in a consistent state,
+# and if necessary to reload it to (hopefully) fix it.
+
+set -euo pipefail
+
+cd $(dirname "$0")/..
+
+HASURA_PASSWORD=$(cat ./docker/secret-hasura-hasura-admin-secret)
+FORCE=false
+PORT=3201
+for param in $@
+do
+  if [ "$param" = "--e2e" ]
+  then
+    PORT=3211
+  fi
+  if [ "$param" = "--force" ]
+  then
+    FORCE=true
+  fi
+done
+
+# Check current situation, that is, if metadata needs reloading.
+IS_CONSISTENT="false"
+if [ $FORCE == false ]; then
+  echo "Fetching metadata consistency information from port $PORT..."
+
+  RESPONSE=$(
+    curl --header "Content-Type: application/json" \
+        --header "x-hasura-admin-secret: $HASURA_PASSWORD" \
+        --request POST \
+        --data '{"type":"get_inconsistent_metadata","args":{}}' \
+        --silent \
+        localhost:$PORT/v1/metadata
+  )
+
+  # There should be a "is_consistent" field in the response, eg:
+  #   "is_consistent": false
+  # Parse the boolean value:
+  IS_CONSISTENT=$(echo "$RESPONSE" | grep -oP '"is_consistent":\s*(?:true|false)' | sed 's/"is_consistent":\s*//')
+
+  if [[ "$IS_CONSISTENT" != "false" && "$IS_CONSISTENT" != "true" ]]; then
+    echo "Unrecognized value for is_consistent: $IS_CONSISTENT"
+    exit 1
+  fi
+fi
+
+if [ "$IS_CONSISTENT" = "true" ]; then
+  echo "Metadata is consistent."
+  exit 0
+fi
+
+# Metadata is inconsistent: reload should fix it.
+echo "Reloading metadata..."
+curl --header "Content-Type: application/json" \
+     --header "x-hasura-admin-secret: $HASURA_PASSWORD" \
+     --request POST \
+     --data '{"type":"reload_metadata","args":{"reload_remote_schemas":true,"reload_sources":true}}' \
+     --silent \
+     localhost:$PORT/v1/metadata
+echo ""
+echo "Metadata reloaded."
