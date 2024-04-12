6696822876409a5d1703b423a0d694b5d3dd6c39
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-04-12 17:43:02 +0300

    Make clearing number of lines field possible
    Before this the input was getting marked as invalid and throwing an error:
      invalid_type: Expected number, received nan

--- ui/src/components/stop-registry/stops/stop-details/signage-details/schema.ts
+++ ui/src/components/stop-registry/stops/stop-details/signage-details/schema.ts
@@ -3,7 +3,9 @@ import { StopPlaceSignType } from '../../../../../types/stop-registry';
 
 export const signageDetailsFormSchema = z.object({
   signType: z.nativeEnum(StopPlaceSignType).optional().nullable(),
-  numberOfFrames: z.number().optional().nullable(),
+  numberOfFrames: z
+    .union([z.number().min(0), z.nan(), z.undefined(), z.null()])
+    .transform((v) => (Number.isNaN(v) ? undefined : v)),
   lineSignage: z.boolean().optional().nullable(),
   replacesRailSign: z.boolean().optional().nullable(),
   mainLineSign: z.boolean().optional().nullable(),
