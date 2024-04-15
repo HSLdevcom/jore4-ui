522dd65e7442e9761688b0e42e51559e2a823fe7
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-04-15 18:03:38 +0300

    Add editing of stop location details
    Currently without editing of location. That would require further validation
    that we don't want to deal with right now (and details of it are not 100% clear).

--- ui/src/components/stop-registry/stops/stop-details/location-details/LocationDetailsSection.tsx
+++ ui/src/components/stop-registry/stops/stop-details/location-details/LocationDetailsSection.tsx
@@ -1,15 +1,59 @@
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StopWithDetails, useToggle } from '../../../../../hooks';
+import {
+  StopWithDetails,
+  useEditStopLocationDetails,
+  useToggle,
+} from '../../../../../hooks';
+import { showSuccessToast, submitFormByRef } from '../../../../../utils';
 import { ExpandableInfoContainer } from '../layout';
+import { LocationDetailsForm } from './LocationDetailsForm';
 import { LocationDetailsViewCard } from './LocationDetailsViewCard';
+import { LocationDetailsFormState } from './schema';
 
 interface Props {
   stop: StopWithDetails;
 }
 
+const mapLocationDetailsToFormState = (
+  stop: StopWithDetails,
+): Partial<LocationDetailsFormState> => {
+  return {
+    streetAddress: stop.stop_place?.streetAddress,
+    postalCode: stop.stop_place?.postalCode,
+    // Note: the location exists in stop_place as well, but stops db (measured_location) is the master data for it.
+    latitude: stop.measured_location.coordinates[1],
+    longitude: stop.measured_location.coordinates[0],
+    altitude: stop.measured_location.coordinates[2],
+    functionalArea: stop.stop_place?.functionalArea,
+  };
+};
+
 export const LocationDetailsSection = ({ stop }: Props): JSX.Element => {
   const { t } = useTranslation();
   const [isExpanded, toggleIsExpanded] = useToggle(true);
+  const { saveStopPlaceLocationDetails, defaultErrorHandler } =
+    useEditStopLocationDetails();
+  const [isEditMode, toggleEditMode] = useToggle(false);
+
+  const onCancel = () => {
+    toggleEditMode();
+  };
+  const formRef = useRef<ExplicitAny>(null);
+  const onSave = () => submitFormByRef(formRef);
+
+  const onSubmit = async (state: LocationDetailsFormState) => {
+    try {
+      await saveStopPlaceLocationDetails({ state, stop });
+
+      showSuccessToast(t('stops.editSuccess'));
+      toggleEditMode();
+    } catch (err) {
+      defaultErrorHandler(err as Error);
+    }
+  };
+
+  const defaultValues = mapLocationDetailsToFormState(stop);
 
   return (
     <ExpandableInfoContainer
@@ -17,8 +61,20 @@ export const LocationDetailsSection = ({ stop }: Props): JSX.Element => {
       isExpanded={isExpanded}
       title={t('stopDetails.location.title')}
       testIdPrefix="LocationDetailsSection"
+      isEditMode={isEditMode}
+      onCancel={onCancel}
+      onSave={onSave}
+      toggleEditMode={toggleEditMode}
     >
-      <LocationDetailsViewCard stop={stop} />
+      {isEditMode && !!defaultValues ? (
+        <LocationDetailsForm
+          defaultValues={defaultValues}
+          ref={formRef}
+          onSubmit={onSubmit}
+        />
+      ) : (
+        <LocationDetailsViewCard stop={stop} />
+      )}
     </ExpandableInfoContainer>
   );
 };
--- ui/src/hooks/stop-registry/index.ts
+++ ui/src/hooks/stop-registry/index.ts
@@ -1,4 +1,5 @@
 export * from './search';
 export * from './useGetStopDetails';
 export * from './useEditStopBasicDetails';
+export * from './useEditStopLocationDetails';
 export * from './useEditStopSignageDetails';
