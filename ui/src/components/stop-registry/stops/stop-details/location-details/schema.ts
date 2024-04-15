64722dedb04d792d03bb0c2af70e0c4ba5eebc4b
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-04-15 18:03:38 +0300

    Add editing of stop location details
    Currently without editing of location. That would require further validation
    that we don't want to deal with right now (and details of it are not 100% clear).

---
+++ ui/src/components/stop-registry/stops/stop-details/location-details/schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { requiredNumber } from '../../../../forms/common';
+
+export const locationDetailsFormSchema = z.object({
+  streetAddress: z.string().optional().nullable(),
+  postalCode: z.string().optional().nullable(),
+  latitude: z.number(),
+  longitude: z.number(),
+  altitude: z.number(),
+  // We don't currently have a mechanism to remove values from keyValues, where this will end up.
+  // Could add, but didn't deem it necessary yet. Easier to just make this required.
+  functionalArea: requiredNumber,
+});
+
+export type LocationDetailsFormState = z.infer<
+  typeof locationDetailsFormSchema
+>;
