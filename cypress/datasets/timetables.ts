f784ff7d6901f507a6fc526ff701de7a5dcce862
Henrik Hartiala <henrik.hartiala@vincit.fi>
2024-05-28 12:43:09 +0300

    Create timetables e2e basedataset and use it on timetableVersionDetailsPanel
    This timetable base dataset now consists of two vehicle services that
    both have 3 round trips. So 6 departures per direction.
    - Use it on timetableVersionDetailsPanel e2e test

---
+++ cypress/datasets/timetables.ts
@@ -0,0 +1,471 @@
+import { defaultDayTypeIds } from '@hsl/timetables-data-inserter';
+import cloneDeep from 'lodash/cloneDeep';
+import { DateTime, Duration } from 'luxon';
+import { journeyPatterns } from './base';
+
+/**
+ * This timetable base timetable data input is based on the routes and lines
+ * baseDbResources introduced in ./base.ts
+ *
+ * This data currently (27.5.24) consists of two vehicle services with each having
+ * 3 back and forth trips
+ */
+const baseTimetableDataInput = {
+  _journey_pattern_refs: {
+    route901inbound: {
+      route_label: '901',
+      route_direction: 'inbound',
+      journey_pattern_id: journeyPatterns[1].journey_pattern_id,
+      _stop_points: [
+        {
+          scheduled_stop_point_sequence: 0,
+          scheduled_stop_point_label: 'E2E005',
+          timing_place_label: '1EIRA',
+        },
+        {
+          scheduled_stop_point_sequence: 1,
+          scheduled_stop_point_label: 'E2E006',
+          timing_place_label: '1AURLA',
+        },
+        {
+          scheduled_stop_point_sequence: 2,
+          scheduled_stop_point_label: 'E2E007',
+        },
+        {
+          scheduled_stop_point_sequence: 3,
+          scheduled_stop_point_label: 'E2E008',
+        },
+        {
+          scheduled_stop_point_sequence: 4,
+          scheduled_stop_point_label: 'E2E009',
+          timing_place_label: '1AACKT',
+        },
+      ],
+    },
+    route901outbound: {
+      route_label: '901',
+      route_direction: 'outbound',
+      journey_pattern_id: journeyPatterns[0].journey_pattern_id,
+      _stop_points: [
+        {
+          scheduled_stop_point_sequence: 1,
+          scheduled_stop_point_label: 'E2E001',
+          timing_place_label: '1AACKT',
+        },
+        {
+          scheduled_stop_point_sequence: 2,
+          scheduled_stop_point_label: 'E2E002',
+        },
+        {
+          scheduled_stop_point_sequence: 3,
+          scheduled_stop_point_label: 'E2E003',
+          timing_place_label: '1AURLA',
+        },
+        {
+          scheduled_stop_point_sequence: 4,
+          scheduled_stop_point_label: 'E2E004',
+        },
+        {
+          scheduled_stop_point_sequence: 5,
+          scheduled_stop_point_label: 'E2E005',
+          timing_place_label: '1EIRA',
+        },
+      ],
+    },
+  },
+  _vehicle_schedule_frames: {
+    year2023: {
+      validity_start: DateTime.fromISO('2023-01-01'),
+      validity_end: DateTime.fromISO('2023-12-31'),
+      name: '2023',
+      booking_label: '2023 booking label',
+      _vehicle_services: {
+        saturdayFirstVehicle: {
+          day_type_id: defaultDayTypeIds.SATURDAY,
+          _blocks: {
+            block: {
+              _vehicle_journeys: {
+                route99Inbound1: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT7H05M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT7H12M'),
+                      departure_time: Duration.fromISO('PT7H12M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT7H19M'),
+                      departure_time: Duration.fromISO('PT7H20M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT7H24M'),
+                      departure_time: Duration.fromISO('PT7H25M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT7H29M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound1: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT7H30M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT7H37M'),
+                      departure_time: Duration.fromISO('PT7H37M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT7H40M'),
+                      departure_time: Duration.fromISO('PT7H41M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT7H44M'),
+                      departure_time: Duration.fromISO('PT7H44M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT7H48M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Inbound2: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT7H50M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT7H57M'),
+                      departure_time: Duration.fromISO('PT7H57M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT8H00M'),
+                      departure_time: Duration.fromISO('PT8H01M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT8H05M'),
+                      departure_time: Duration.fromISO('PT8H06M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT8H09M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound2: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT8H10M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT8H17M'),
+                      departure_time: Duration.fromISO('PT8H17M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT8H21M'),
+                      departure_time: Duration.fromISO('PT8H22M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT8H29M'),
+                      departure_time: Duration.fromISO('PT8H29M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT8H34M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Inbound3: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT9H30M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT9H35M'),
+                      departure_time: Duration.fromISO('PT9H35M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT9H42M'),
+                      departure_time: Duration.fromISO('PT9H46M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT9H50M'),
+                      departure_time: Duration.fromISO('PT9H51M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT9H53M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound3: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT9H55M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT10H00M'),
+                      departure_time: Duration.fromISO('PT10H00M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT10H04M'),
+                      departure_time: Duration.fromISO('PT10H05M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT10H12M'),
+                      departure_time: Duration.fromISO('PT10H12M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT10H17M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+        saturdaySecondVehicle: {
+          day_type_id: defaultDayTypeIds.SATURDAY,
+          _blocks: {
+            block: {
+              _vehicle_journeys: {
+                route99Inbound1: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT7H15M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT7H22M'),
+                      departure_time: Duration.fromISO('PT7H22M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT7H29M'),
+                      departure_time: Duration.fromISO('PT7H30M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT7H34M'),
+                      departure_time: Duration.fromISO('PT7H35M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT7H39M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound1: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT7H40M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT7H47M'),
+                      departure_time: Duration.fromISO('PT7H47M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT7H50M'),
+                      departure_time: Duration.fromISO('PT7H51M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT7H54M'),
+                      departure_time: Duration.fromISO('PT7H54M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT7H58M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Inbound2: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT8H00M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT8H07M'),
+                      departure_time: Duration.fromISO('PT8H07M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT8H10M'),
+                      departure_time: Duration.fromISO('PT8H11M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT8H15M'),
+                      departure_time: Duration.fromISO('PT8H16M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT8H20M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound2: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT8H20M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT8H27M'),
+                      departure_time: Duration.fromISO('PT8H27M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT8H31M'),
+                      departure_time: Duration.fromISO('PT8H32M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT8H39M'),
+                      departure_time: Duration.fromISO('PT8H39M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT8H44M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Inbound3: {
+                  _journey_pattern_ref_name: 'route901inbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT9H40M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E006',
+                      arrival_time: Duration.fromISO('PT9H45M'),
+                      departure_time: Duration.fromISO('PT9H45M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E007',
+                      arrival_time: Duration.fromISO('PT9H52M'),
+                      departure_time: Duration.fromISO('PT9H56M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E008',
+                      arrival_time: Duration.fromISO('PT10H00M'),
+                      departure_time: Duration.fromISO('PT10H01M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E009',
+                      arrival_time: Duration.fromISO('PT10H03M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+                route99Outbound3: {
+                  _journey_pattern_ref_name: 'route901outbound',
+                  _passing_times: [
+                    {
+                      _scheduled_stop_point_label: 'E2E001',
+                      arrival_time: null,
+                      departure_time: Duration.fromISO('PT10H05M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E002',
+                      arrival_time: Duration.fromISO('PT10H10M'),
+                      departure_time: Duration.fromISO('PT10H10M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E003',
+                      arrival_time: Duration.fromISO('PT10H14M'),
+                      departure_time: Duration.fromISO('PT10H15M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E004',
+                      arrival_time: Duration.fromISO('PT10H22M'),
+                      departure_time: Duration.fromISO('PT10H22M'),
+                    },
+                    {
+                      _scheduled_stop_point_label: 'E2E005',
+                      arrival_time: Duration.fromISO('PT10H27M'),
+                      departure_time: null,
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+/**
+ * Returns a clone of baseTimetableDatainput for timetables datainserter
+ *  so that the caller can modify the data freely without side effects
+ */
+export const getClonedBaseTimetableDataInput = () =>
+  cloneDeep(baseTimetableDataInput);
