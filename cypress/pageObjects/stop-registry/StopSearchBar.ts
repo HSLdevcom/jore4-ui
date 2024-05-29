e4101eef0351f2827bee8772a2da048f486fe1fb
Jarno Perholehto <jarno.perholehto@vincit.fi>
2024-05-29 12:28:04 +0300

    Add e2e tests for search criteria: Municipality

--- cypress/pageObjects/stop-registry/StopSearchBar.ts
+++ cypress/pageObjects/stop-registry/StopSearchBar.ts
@@ -18,4 +18,31 @@ export class StopSearchBar {
   getSearchButton() {
     return cy.getByTestId('StopSearchBar::searchButton');
   }
+
+  getMunicipalitiesDropdown() {
+    return cy.getByTestId(
+      'StopSearchBar::municipalitiesDropdown::ListboxButton',
+    );
+  }
+
+  getMunicipalitiesOptions() {
+    return cy.getByTestId(
+      'StopSearchBar::municipalitiesDropdown::ListboxOptions',
+    );
+  }
+
+  openMunicipalityDropdown() {
+    this.getMunicipalitiesDropdown().click();
+  }
+
+  toggleMunicipality(municipality: string) {
+    this.getMunicipalitiesOptions().contains(municipality).click();
+  }
+
+  isMunicipalitySelected(municipality: string) {
+    this.getMunicipalitiesOptions()
+      .contains(municipality)
+      .get('input')
+      .should('be.checked');
+  }
 }
