836d9da22663e0382240b77ac184852346f2b443
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-05-29 13:15:58 +0300

    Display accessibility icon for fully accessible stop in stop details header

--- ui/src/components/stop-registry/stops/stop-details/StopHeaderSummaryRow.tsx
+++ ui/src/components/stop-registry/stops/stop-details/StopHeaderSummaryRow.tsx
@@ -1,6 +1,11 @@
 import { useTranslation } from 'react-i18next';
-import { StopWithDetails } from '../../../../hooks';
+import {
+  StopAccessibilityLevel,
+  StopWithDetails,
+  useCalculateStopAccessibilityLevel,
+} from '../../../../hooks';
 import { mapStopPlaceStateToUiName } from '../../../../i18n/uiNameMappings';
+import { Visible } from '../../../../layoutComponents';
 import { StopPlaceState } from '../../../../types/stop-registry';
 import { StopTypeLabel } from './StopTypeLabel';
 
@@ -14,10 +19,29 @@ export const StopHeaderSummaryRow: React.FC<Props> = ({
   className = '',
 }) => {
   const { t } = useTranslation();
+  const { calculateStopAccessibilityLevel } =
+    useCalculateStopAccessibilityLevel();
+
+  const accessibilityLevel = calculateStopAccessibilityLevel(
+    stopDetails.stop_place,
+  );
+  const isAccessible =
+    accessibilityLevel === StopAccessibilityLevel.FullyAccessible;
+  const anyIconsShown = isAccessible;
+
   const stopState = stopDetails.stop_place?.stopState;
 
   return (
-    <div className={`flex ${className}`}>
+    <div className={`flex items-center ${className}`}>
+      <Visible visible={anyIconsShown}>
+        <Visible visible={isAccessible}>
+          <i
+            className="icon-accessible text-4xl text-dark-grey"
+            title={t('stopDetails.measurements.accessible')}
+          />
+        </Visible>
+        <div className="mx-2 h-8 border-l border-dark-grey"> </div>
+      </Visible>
       <div className="flex items-center gap-5">
         <StopTypeLabel
           hasType={!!stopDetails?.stop_place?.stopType.mainLine}
