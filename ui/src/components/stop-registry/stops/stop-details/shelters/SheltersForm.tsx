d246f81c3a264987809c5d13569be74f6412ba14
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-06-05 09:25:25 +0300

    Implement removing and adding shelters

--- ui/src/components/stop-registry/stops/stop-details/shelters/SheltersForm.tsx
+++ ui/src/components/stop-registry/stops/stop-details/shelters/SheltersForm.tsx
@@ -1,9 +1,14 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
 import { FormProvider, useFieldArray, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
 import { Visible } from '../../../../../layoutComponents';
-import { HorizontalSeparator } from '../layout';
-import { SheltersFormState, sheltersFormSchema } from './schema';
+import { HorizontalSeparator, SlimSimpleButton } from '../layout';
+import {
+  SheltersFormState,
+  mapShelterDataToFormState,
+  sheltersFormSchema,
+} from './schema';
 import { ShelterFormFields } from './ShelterFormFields';
 
 const testIds = {
@@ -22,16 +27,30 @@ const SheltersFormComponent = (
   { className, defaultValues, onSubmit }: Props,
   ref: ExplicitAny,
 ): JSX.Element => {
+  const { t } = useTranslation();
   const methods = useForm<SheltersFormState>({
     defaultValues,
     resolver: zodResolver(sheltersFormSchema),
   });
   const { control, handleSubmit } = methods;
 
-  const { fields: shelters } = useFieldArray({
+  const {
+    append,
+    fields: shelters,
+    remove,
+  } = useFieldArray({
     control,
     name: 'shelters',
   });
+  const addNewShelter = () => {
+    append(mapShelterDataToFormState({}));
+  };
+  const onRemoveShelter = (idx: number) => {
+    // TODO: confirmation. Either here or with some more complex mechanism, check with design.
+    remove(idx);
+  };
+  const isLast = (idx: number) => idx === shelters.length - 1;
+
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FormProvider {...methods}>
@@ -39,11 +58,33 @@ const SheltersFormComponent = (
         {shelters.map((shelter, idx) => (
           <div key={shelter.id} data-testid={testIds.shelter}>
             <ShelterFormFields index={idx} />
-            <Visible visible={idx !== shelters.length - 1}>
+            <div className="mb-8 mt-6 flex gap-4">
+              <SlimSimpleButton
+                testId={testIds.deleteShelter}
+                onClick={() => onRemoveShelter(idx)}
+                inverted
+              >
+                {t('stopDetails.shelters.deleteShelter')}
+              </SlimSimpleButton>
+              <Visible visible={isLast(idx)}>
+                <SlimSimpleButton
+                  testId={testIds.addShelter}
+                  onClick={addNewShelter}
+                >
+                  {t('stopDetails.shelters.addShelter')}
+                </SlimSimpleButton>
+              </Visible>
+            </div>
+            <Visible visible={!isLast(idx)}>
               <HorizontalSeparator className="my-4" />
             </Visible>
           </div>
         ))}
+        <Visible visible={!shelters.length}>
+          <SlimSimpleButton testId={testIds.addShelter} onClick={addNewShelter}>
+            {t('stopDetails.shelters.addShelter')}
+          </SlimSimpleButton>
+        </Visible>
       </form>
     </FormProvider>
   );
