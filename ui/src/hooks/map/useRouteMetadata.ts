254f73e46d70a2d8da0b460b7508d610ff142fc5
Jarno Perholehto <jarno.perholehto@vincit.fi>
2024-06-10 13:39:02 +0300

    Upgrade react-map-gl to 7.1
    - Remove unnecessary dependencies and update react-map-gl to 7.1
    - Replace HTMLOverlay with CustomOverlay
      -- 7.1 version of react-map-gl doesn't provide HTMLOverlay anymore
    - Fix Stops component
      -- MapContext is replaced with hook `useMap`
      -- MapEvent changed to MapLayerMouseEvent
      -- Offset is not needed
    - Change MapEvent to MapLayerMouseEvent
    - Fix Popup
      -- Popup component API has changed in 7.1
    - Remove unnecessary property `drawable`
    - Replace CallbackEvent with MapLayerMouseEvent
    - Use Maplibre typings
      -- Remove MaplibreGLMap type declaration
      -- Use MapInstace type from react-map-gl/maplibre
    - Fix capabilities to draw and edit route on map
      -- Custom drawControl-component to use MapboxDraw controls
      -- Custom layer for editing and drawing route. Mostly all business logic
      in this component is reused from old DrawRouteLayer
    - Fix image loading
    - Disable 'edit route'-button when already editing
    - Remove not used custom cursor code.
      - `auto` value in cursor works properly.
      - Add `crosshair` cursor when drawing a route
    - Style route drawing and limit draw modes to 1
      -- When drawing we only want one mode and that is drawing. All other
      functionalities are handled by our custom implementation. Because of
      custom implementation, we need to manually fix the order of the
      layers when drawing a route. This is done by moving layers that come
      after draw layer to below it.
      Note: MapBoxDraw adds suffix `.cold` to layer id.
    - Fix styling for components rendered on top of the map
    - Fix Layer types
      -- Create interfaces for arrow and line paint/layout. React-map-gl
      doesn't offer these types.
    - Add missing URL for disabling map tile rendering completely in CI. This is need to fully mock mapStyle loading.
    - Change mapStyle related intercepts to return status code 200 instead of 404. If 404 is returned MapLibre's internal loading state is always `false` and e2e tests dont' work
    - When editing routes, handles shown aren't anymore html-elements and test logic needed to change

---
+++ ui/src/hooks/map/useRouteMetadata.ts
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+import { useAppSelector } from '..';
+import { selectEditedRouteData } from '../../redux';
+import { parseDate } from '../../time';
+
+export const useRouteMetadata = () => {
+  const editedRouteData = useAppSelector(selectEditedRouteData);
+
+  return useMemo(() => {
+    if (!editedRouteData.metaData) {
+      return undefined;
+    }
+
+    const { validityStart, validityEnd, priority } = editedRouteData.metaData;
+    if (
+      validityStart === undefined ||
+      validityEnd === undefined ||
+      priority === undefined
+    ) {
+      return undefined;
+    }
+
+    return {
+      validity_start: parseDate(validityStart),
+      validity_end: parseDate(validityEnd),
+      priority,
+    };
+  }, [editedRouteData.metaData]);
+};
