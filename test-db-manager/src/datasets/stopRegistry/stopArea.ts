2f52a7109647113997c237685e0622d4b0084c5c
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-06-17 15:09:25 +0300

    Insert seed stop area

---
+++ test-db-manager/src/datasets/stopRegistry/stopArea.ts
@@ -0,0 +1,93 @@
+import { DateTime } from 'luxon';
+import {
+  StopRegistryGeoJsonType,
+  StopRegistryGroupOfStopPlacesInput,
+} from '../../generated/graphql';
+
+export type StopAreaSeedData = {
+  label: string;
+  name: string;
+  locationLat: number;
+  locationLong: number;
+  validityStart: DateTime;
+  validityEnd?: DateTime;
+  members: Array<string>;
+};
+
+export type StopAreaInput = {
+  memberLabels: Array<string>;
+  stopArea: Partial<StopRegistryGroupOfStopPlacesInput>;
+};
+
+const mapToStopAreaInput = (seedStopArea: StopAreaSeedData): StopAreaInput => {
+  return {
+    memberLabels: seedStopArea.members,
+    stopArea: {
+      name: {
+        lang: 'fin',
+        value: seedStopArea.label,
+      },
+      description: {
+        lang: 'fin',
+        value: seedStopArea.name,
+      },
+      validBetween: {
+        fromDate: seedStopArea.validityStart.toISO(),
+        toDate: seedStopArea.validityEnd?.toISO() || null,
+      },
+      geometry: seedStopArea.locationLat &&
+        seedStopArea.locationLong && {
+          coordinates: [[seedStopArea.locationLong, seedStopArea.locationLat]],
+          type: StopRegistryGeoJsonType.Point,
+        },
+    },
+  };
+};
+
+const basicStart = DateTime.fromISO('2020-01-01T00:00:00.001');
+const basicEnd = DateTime.fromISO('2050-01-01T00:00:00.001');
+
+const route35StopAreas: Array<StopAreaSeedData> = [
+  {
+    label: 'X1300',
+    name: 'Munkkivuori',
+    locationLat: 60.206001,
+    locationLong: 24.879646,
+    validityStart: basicStart,
+    validityEnd: basicEnd,
+    members: ['H1398', 'H1416'],
+  },
+  {
+    label: 'X1301',
+    name: 'Luuvaniementie',
+    locationLat: 60.205531,
+    locationLong: 24.883556,
+    validityStart: basicStart,
+    validityEnd: basicEnd,
+    members: ['H1451', 'H1452'],
+  },
+  {
+    label: 'X1302',
+    name: 'Rakuunantie',
+    locationLat: 60.20054,
+    locationLong: 24.883959,
+    validityStart: basicStart,
+    validityEnd: basicEnd,
+    members: ['H1456', 'H1458'],
+  },
+];
+
+const finnooSeedData: StopAreaSeedData = {
+  label: 'X1234',
+  name: 'Finnoo',
+  validityStart: basicStart,
+  validityEnd: basicEnd,
+  locationLong: 24.708,
+  locationLat: 60.156,
+  members: ['E4464', 'E4461'],
+};
+
+const seedData: Array<StopAreaSeedData> = [...route35StopAreas, finnooSeedData];
+
+export const seedStopAreas: Array<StopAreaInput> =
+  seedData.map(mapToStopAreaInput);
--- test-db-manager/src/seedStopRegistry.ts
+++ test-db-manager/src/seedStopRegistry.ts
@@ -1,11 +1,22 @@
 /* eslint-disable no-console */
-import { StopPlaceInput, seedStopPlaces } from './datasets';
+import {
+  StopAreaInput,
+  StopPlaceInput,
+  StopPlaceNetexRef,
+  seedStopAreas,
+  seedStopPlaces,
+} from './datasets';
+import { StopRegistryVersionLessEntityRefInput } from './generated/graphql';
 import { insertStopPlaceForScheduledStopPoint } from './graphql-helpers';
 import { hasuraApi } from './hasuraApi';
+import { mapToInsertStopAreaMutation } from './queries';
 import { mapToGetStopPointByLabelQuery } from './queries/routesAndLines';
-import { GetStopPointByLabelResult } from './types';
+import { GetStopPointByLabelResult, InsertStopAreaResult } from './types';
 
-const insertStopPlace = async ({ label, stopPlace }: StopPlaceInput) => {
+const insertStopPlace = async ({
+  label,
+  stopPlace,
+}: StopPlaceInput): Promise<StopPlaceNetexRef> => {
   // Find related scheduled stop point.
   const stopPointResult = (await hasuraApi(
     mapToGetStopPointByLabelQuery(label),
@@ -40,6 +51,7 @@ const insertStopPlace = async ({ label, stopPlace }: StopPlaceInput) => {
         `Stop point ${label} (${stopPointId}) already has a stop place with id ${stopPoint.stop_place_ref}. Overwrote with ${stopPlaceRef}.`,
       );
     }
+    return { netexId: stopPlaceRef, label };
   } catch (error) {
     console.error(
       'An error occurred while inserting stop place!',
@@ -51,7 +63,39 @@ const insertStopPlace = async ({ label, stopPlace }: StopPlaceInput) => {
   }
 };
 
+const insertStopArea = async (
+  stopArea: StopAreaInput,
+  stopPlaces: Map<string, string>,
+) => {
+  const area = {
+    ...stopArea.stopArea,
+    members: stopArea.memberLabels.map(
+      (label) =>
+        ({
+          ref: stopPlaces.get(label),
+        } as StopRegistryVersionLessEntityRefInput),
+    ),
+  };
+
+  try {
+    const returnValue = (await hasuraApi(
+      mapToInsertStopAreaMutation(area),
+    )) as InsertStopAreaResult;
+    if (returnValue.data == null) {
+      throw new Error('Null data returned from Tiamat');
+    }
+  } catch (error) {
+    console.error(
+      'An error occurred while inserting stop area!',
+      stopArea.stopArea.name.value,
+      error,
+    );
+    throw error;
+  }
+};
+
 const seedStopRegistry = async () => {
+  const collectedStopIds: Map<string, string> = new Map();
   console.log('Inserting stop places...');
   // Need to run these sequentially. Will get transaction errors if trying to do concurrently.
   for (let index = 0; index < seedStopPlaces.length; index++) {
@@ -60,10 +104,25 @@ const seedStopRegistry = async () => {
       `Stop point ${stopPoint?.label}: stop place insert starting...`,
     );
     // eslint-disable-next-line no-await-in-loop
-    await insertStopPlace(stopPoint);
+    const netexRef = await insertStopPlace(stopPoint);
+    collectedStopIds.set(netexRef.label, netexRef.netexId);
     console.log(`Stop point ${stopPoint?.label}: stop place insert finished!`);
   }
   console.log(`Inserted ${seedStopPlaces.length} stop places.`);
+
+  console.log('Inserting stop areas...');
+  for (let index = 0; index < seedStopAreas.length; index++) {
+    const stopArea = seedStopAreas[index];
+    console.log(
+      `Stop area ${stopArea?.stopArea.name.value}: stop area insert starting...`,
+    );
+    // eslint-disable-next-line no-await-in-loop
+    await insertStopArea(stopArea, collectedStopIds);
+    console.log(
+      `Stop area ${stopArea?.stopArea.name.value}: stop area insert finished!`,
+    );
+  }
+  console.log(`Inserted ${seedStopAreas.length} stop areas.`);
 };
 
 seedStopRegistry();
