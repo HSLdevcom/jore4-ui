1c85625d56985191f4a522d50b5f4fd2a7c63fe2
Henrik Hartiala <henrik.hartiala@vincit.fi>
2024-06-20 14:58:23 +0300

    Add stop place insert when creating stop on map
    Earlier when we created a stop, we only created the scheduled_stop_point to
    routes and lines database, but we need to also add an stop place entry to tiamat.
    After that we need to update the newly created scheduled_stop_point's stop_place_ref
    to point to the newly created stopPlace.
    NOTE: These three queries are needed, but they are not in a transaction yet. So if one of the latter
    queries fail, there is no rollbacks. This could've been all in one "mutation" instead of three separate, but
    I think that could give the impression that they are done in a transaction which it isn't since they are actually
    in different databases and GQL does not support transactions between multiple databases.
    Also a minor thing but moved GQL_INSERT_STOP to useCreateStop.ts.

---
+++ ui/src/hooks/stop-registry/useCreateStopPlace.ts
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+import { Position } from '@turf/helpers';
+import {
+  StopRegistryGeoJsonType,
+  useInsertStopPlaceMutation,
+} from '../../generated/graphql';
+
+export interface InsertStopPlaceInput {
+  label: string;
+  coordinates: Position;
+}
+
+const GQL_INSERT_STOP_PLACE = gql`
+  mutation InsertStopPlace($object: stop_registry_StopPlaceInput) {
+    stop_registry {
+      mutateStopPlace(StopPlace: $object) {
+        publicCode
+        id
+        quays {
+          publicCode
+        }
+      }
+    }
+  }
+`;
+
+export const useCreateStopPlace = () => {
+  const [insertStopPlaceMutation] = useInsertStopPlaceMutation();
+
+  const mapToInsertStopPlaceVariables = ({
+    label,
+    coordinates,
+  }: InsertStopPlaceInput) => ({
+    variables: {
+      object: {
+        // TODO: change the name to the actual name after we add the inputs for it.
+        // For now we use label as placeholder so that we can create the tiamat instance of the stop
+        name: { lang: 'fin', value: label },
+        quays: [{ publicCode: label }],
+        geometry: {
+          // Due to tiamat reasons, we need to wrap the coordinates in an array.
+          coordinates: [coordinates],
+          type: StopRegistryGeoJsonType.Point,
+        },
+      },
+    },
+  });
+
+  return {
+    mapToInsertStopPlaceVariables,
+    insertStopPlaceMutation,
+  };
+};
