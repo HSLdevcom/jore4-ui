b46183ce5cf7710677a1e45a52ddb72959e7f338
Jesse Jaara <jesse.jaara@vincit.fi>
2024-06-25 16:41:58 +0300

    Add basic StopArea visualization to the map

---
+++ ui/src/components/map/stop-areas/StopAreaPopup.tsx
@@ -0,0 +1,100 @@
+import noop from 'lodash/noop';
+import { useTranslation } from 'react-i18next';
+import { MdDelete } from 'react-icons/md';
+import { Popup } from 'react-map-gl/maplibre';
+import { StopAreaMinimalShowOnMapFieldsFragment } from '../../../generated/graphql';
+import { Column, Row } from '../../../layoutComponents';
+import { CloseIconButton, SimpleButton } from '../../../uiComponents';
+import {
+  getGeometryPoint,
+  getNameFromAlternatives,
+  mapToValidityPeriod,
+} from '../../../utils';
+
+const testIds = {
+  label: 'StopAreaPopup::label',
+  closeButton: 'StopAreaPopup::closeButton',
+  deleteButton: 'StopAreaPopup::deleteButton',
+  editButton: 'StopAreaPopup::editButton',
+  moveButton: 'StopAreaPopup::moveButton',
+};
+
+type StopAreaPopupProps = {
+  area: StopAreaMinimalShowOnMapFieldsFragment;
+  onClose: () => void;
+};
+
+export const StopAreaPopup = ({ area, onClose }: StopAreaPopupProps) => {
+  const { t } = useTranslation();
+
+  const point = getGeometryPoint(area.centroid);
+  const areaLabel = getNameFromAlternatives(
+    area.name_value,
+    area.name_lang,
+    area.alternative_names.map((it) => it.alternative_name),
+  );
+
+  if (!point || !areaLabel) {
+    return null;
+  }
+
+  return (
+    <Popup
+      anchor="top"
+      className="min-w-80"
+      closeOnClick={false}
+      closeButton={false}
+      latitude={point.latitude}
+      longitude={point.longitude}
+    >
+      <div className="p-2">
+        <Row>
+          <Column className="w-full">
+            <Row className="items-center">
+              <h3 data-testid={testIds.label}>
+                {t('stopArea.areaWithLabel', { areaLabel })}
+              </h3>
+              <CloseIconButton
+                className="ml-auto"
+                onClick={onClose}
+                testId={testIds.closeButton}
+              />
+            </Row>
+          </Column>
+        </Row>
+
+        <Row className="text-sm">
+          {mapToValidityPeriod(t, area.from_date, area.to_date)}
+        </Row>
+
+        <Row className="mt-16">
+          <SimpleButton
+            className="h-full !px-3"
+            disabled
+            onClick={noop}
+            inverted
+            testId={testIds.deleteButton}
+          >
+            <MdDelete aria-label={t('stopArea.delete')} className="text-xl" />
+          </SimpleButton>
+          <SimpleButton
+            containerClassName="ml-auto"
+            disabled
+            onClick={noop}
+            testId={testIds.moveButton}
+          >
+            {t('move')}
+          </SimpleButton>
+          <SimpleButton
+            containerClassName="ml-2"
+            disabled
+            onClick={noop}
+            testId={testIds.editButton}
+          >
+            {t('edit')}
+          </SimpleButton>
+        </Row>
+      </div>
+    </Popup>
+  );
+};
---
+++ ui/src/components/map/stop-areas/StopAreas.tsx
@@ -0,0 +1,78 @@
+import { useEffect } from 'react';
+import {
+  StopAreaMinimalShowOnMapFieldsFragment,
+  useGetStopAreasByLocationQuery,
+} from '../../../generated/graphql';
+import { useAppAction, useAppSelector, useLoader } from '../../../hooks';
+import {
+  Operation,
+  selectMapViewport,
+  selectSelectedStopAreaId,
+  setSelectedMapStopAreaIdAction,
+} from '../../../redux';
+import {
+  buildWithinViewportGqlGeometryFilter,
+  notNullish,
+} from '../../../utils';
+import { EditStopAreaLayer } from './EditStopAreaLayer';
+import { StopArea } from './StopArea';
+
+export const StopAreas = () => {
+  const setSelectedMapStopAreaId = useAppAction(setSelectedMapStopAreaIdAction);
+  const selectedStopAreaId = useAppSelector(selectSelectedStopAreaId);
+
+  const { setIsLoading } = useLoader(Operation.FetchStopAreas);
+
+  const viewport = useAppSelector(selectMapViewport);
+  const stopAreasResult = useGetStopAreasByLocationQuery({
+    variables: {
+      measured_location_filter: buildWithinViewportGqlGeometryFilter(viewport),
+    },
+  });
+
+  useEffect(() => {
+    /**
+     * Here we sync getStopAreasByLocationQuery query loading state with useLoader hook state.
+     *
+     * We could also use useLoader's immediatelyOn option instead of useEffect,
+     * but using options to dynamically control loading state feels semantically wrong.
+     */
+    setIsLoading(stopAreasResult.loading);
+  }, [setIsLoading, stopAreasResult.loading]);
+
+  const onClick = (area: StopAreaMinimalShowOnMapFieldsFragment) =>
+    setSelectedMapStopAreaId(area.netex_id ?? undefined);
+
+  const onPopupClose = () => setSelectedMapStopAreaId(undefined);
+
+  const data = stopAreasResult.loading
+    ? stopAreasResult.previousData
+    : stopAreasResult.data;
+  const areas = data?.stops_database?.areas?.filter(notNullish) ?? [];
+
+  const editedArea = areas.find(
+    (area) => area?.netex_id === selectedStopAreaId,
+  );
+
+  return (
+    <>
+      {areas.map((area) => (
+        <StopArea
+          area={area}
+          selected={area.netex_id === editedArea?.netex_id}
+          key={area.id}
+          onClick={onClick}
+        />
+      ))}
+
+      {editedArea ? (
+        <>
+          <EditStopAreaLayer
+            editedArea={editedArea}
+            onPopupClose={onPopupClose}
+          />
+        </>
+      ) : null}
+    </>
+  );
+};
