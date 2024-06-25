ec93d181cf9a8e24e40a42112f5548242b68ca00
Jesse Jaara <jesse.jaara@vincit.fi>
2024-06-25 16:42:18 +0300

    Visualize StopArea members on the map

---
+++ ui/src/components/map/stop-areas/MemberStops.tsx
@@ -0,0 +1,66 @@
+import { useMemo } from 'react';
+import { StopAreaMinimalShowOnMapFieldsFragment } from '../../../generated/graphql';
+import { getPointPosition, notNullish } from '../../../utils';
+import { LinePaint, LineRenderLayer } from '../routes';
+import { MemberStop } from './MemberStop';
+
+const memberLinePaint: Partial<LinePaint> = {
+  'line-offset': 0,
+  'line-width': 4,
+};
+
+function useMemberLines(
+  area: StopAreaMinimalShowOnMapFieldsFragment,
+): GeoJSON.MultiLineString | null {
+  return useMemo(() => {
+    const areaPosition = getPointPosition(area.centroid);
+    const nonNullStops = area.members
+      .map((member) => member.stop_place)
+      .filter(notNullish);
+
+    if (!areaPosition || !nonNullStops) {
+      return null;
+    }
+
+    return {
+      type: 'MultiLineString',
+      coordinates: nonNullStops
+        .map((it) => it.centroid)
+        .map(getPointPosition)
+        .filter(notNullish)
+        .map((memberPosition) => [areaPosition, memberPosition]),
+    };
+  }, [area]);
+}
+
+function useMemberStops(area: StopAreaMinimalShowOnMapFieldsFragment) {
+  return useMemo(
+    () => area.members?.map((member) => member?.stop_place).filter(notNullish),
+    [area],
+  );
+}
+
+type MemberStopsProps = {
+  area: StopAreaMinimalShowOnMapFieldsFragment;
+};
+
+export const MemberStops = ({ area }: MemberStopsProps) => {
+  const memberLines = useMemberLines(area);
+  const memberStops = useMemberStops(area);
+
+  return (
+    <>
+      {memberStops.map((stop) => (
+        <MemberStop key={stop.id} stop={stop} />
+      ))}
+
+      {memberLines ? (
+        <LineRenderLayer
+          layerId="MemberLines"
+          geometry={memberLines}
+          paint={memberLinePaint}
+        />
+      ) : null}
+    </>
+  );
+};
