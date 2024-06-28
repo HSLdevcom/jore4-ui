8d619a552f50ea5ca5f894c60f687a87717ab38e
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-06-28 13:07:36 +0300

    Show different shelter section header when there are no shelters
    For opening edit mode with one new shelter.

---
+++ ui/src/components/stop-registry/stops/stop-details/layout/EmptyListHeaderButtons.tsx
@@ -0,0 +1,40 @@
+import noop from 'lodash/noop';
+import { FaChevronDown } from 'react-icons/fa';
+import { SlimSimpleButton } from './SlimSimpleButton';
+
+interface Props {
+  addNewItemText: string;
+  onAddNewItem: () => void;
+  testIdPrefix?: string;
+}
+
+const testIds = {
+  toggle: (prefix: string) => `${prefix}::toggle`,
+  addNewItemButton: (prefix: string) => `${prefix}::addNewItemButton`,
+};
+
+export const EmptyListHeaderButtons = ({
+  addNewItemText,
+  onAddNewItem,
+  testIdPrefix = '',
+}: Props) => {
+  return (
+    <>
+      <SlimSimpleButton
+        testId={testIds.addNewItemButton(testIdPrefix)}
+        inverted
+        onClick={onAddNewItem}
+      >
+        {addNewItemText}
+      </SlimSimpleButton>
+      <SlimSimpleButton
+        onClick={noop}
+        inverted
+        disabled
+        testId={testIds.toggle(testIdPrefix)}
+      >
+        <FaChevronDown className="text-white" aria-hidden />
+      </SlimSimpleButton>
+    </>
+  );
+};
