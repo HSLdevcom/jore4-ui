1f8d171933b49d460c322bb01a24e2e3657fe986
Jesse Jaara <jesse.jaara@vincit.fi>
2024-07-12 15:14:32 +0300

    Dependencies|UI: Update minor pacakge versions
    Updates most of the minor versions of the actual UI side's
    dependencies.
    
    Some packages have not been updated, and have instead been pinned to
    the specific old version.
    
    * Luxon
      - Pinned to version 3.3.0.
      - Version update needs to be coordinated with the test-db-manager,
        as UI tests include code from the tdbm.
      - Typing have been updated to latest version of 3.3 branch
        (3.3.8). Previously they were completely wrong and on version
        3.2.1. As result an update to the typing's "settings" has been
        made. More info on the code side.
    
    * Apollo client:
      - Pinned to version 3.4.11.
      - Version update needs to be coordinates with codegen.
      - With never versions the custom AsyncQuery plugin could be dropped
        completely.
    
    * @headlessui/react:
      - Bound to version 1.7.10.
      - Breaks unit tests, possibly due to version conflicts with some of
        the testing related packages.
    
    * utf-8-validate:
      - Pinned to version 6.0.3.
      - 6.0.4 does not support Node 18 Docker images, they have
        incompatible version of Python, needed for node-gyp.
    
    * zod:
      - Pinned to version 3.21.4.
      - Version change need to be coordinated with test-data-inserter.
    
    * @types: mapbox__mapbox-gl-draw / react-router / redux-logger
      - Moved into devDependencies section
    
    * @types/redux-logger
      - Pinned to version 3.0.9.
      - No update to actual redux-logger.
      - Newer typings are for V5 of Redux.
    
    Code changes:
    
    * react-icons: GoPrimitiveDot renamed to GoDotFill
    * useAppAction:
      - Fixed to work with newer types from packages.
      - Additionally it now support multi-argument prepared actions.
    * hookWrapper: Fixed to accept ReactNode as child.
    
    Other notes:
    
    * graphql package is used by several of the subprojects, yet it is
      only declared under the UI project. Thus updating it under the UI
      project inderectly updates it for the other sub projects
      too. Including for the codegen subproject. This results in changes
      to then JSON dump of the Hasura GrapgQL schema file.

--- ui/src/hooks/redux.ts
+++ ui/src/hooks/redux.ts
@@ -3,22 +3,57 @@ import {
   ActionCreatorWithPayload,
   ActionCreatorWithPreparedPayload,
   ActionCreatorWithoutPayload,
+  PayloadAction,
 } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../redux';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppAction = <
-  TPayload extends ExplicitAny,
-  TActionPayload extends ExplicitAny,
+
+// Dispacted versions of ActionCreator -types. === Just the callable function part.
+type ActionDispatcherWithoutPayload<TType extends string> = () => PayloadAction<
+  undefined,
+  TType
+>;
+
+type ActionDispatcherWithPayload<TPayload, TType extends string> = (
+  payload: TPayload,
+) => PayloadAction<TPayload, TType>;
+
+type ActionDispatcherWithOptionalPayload<TPayload, TType extends string> = (
+  payload?: TPayload,
+) => PayloadAction<TPayload, TType>;
+
+type ActionDispatcherWithPreparedPayload<
+  TPayload,
+  TInput extends unknown[],
+  TType extends string,
+> = (...args: TInput) => PayloadAction<TPayload, TType>;
+
+// Proper overload for each individual ActionCreator -type.
+// Includes proper typings for multi argument prepared actions.
+export function useAppAction<TType extends string>(
+  action: ActionCreatorWithoutPayload<TType>,
+): ActionDispatcherWithoutPayload<TType>;
+
+export function useAppAction<TPayload, TType extends string>(
+  action: ActionCreatorWithPayload<TPayload, TType>,
+): ActionDispatcherWithPayload<TPayload, TType>;
+
+export function useAppAction<TPayload, TType extends string>(
+  action: ActionCreatorWithOptionalPayload<TPayload, TType>,
+): ActionDispatcherWithOptionalPayload<TPayload, TType>;
+
+export function useAppAction<
+  TPayload,
+  TInput extends unknown[],
+  TType extends string,
 >(
-  action:
-    | ActionCreatorWithOptionalPayload<TPayload>
-    | ActionCreatorWithPreparedPayload<TPayload[], TActionPayload>
-    | ActionCreatorWithPayload<TPayload>
-    | ActionCreatorWithoutPayload,
-) => {
+  action: ActionCreatorWithPreparedPayload<TInput, TPayload, TType>,
+): ActionDispatcherWithPreparedPayload<TPayload, TInput, TType>;
+
+export function useAppAction(action: ExplicitAny): ExplicitAny {
   const dispatch = useAppDispatch();
-  return (actionPayload: TPayload) => dispatch(action(actionPayload));
-};
+  return (...params: ExplicitAny[]) => dispatch(action(...params));
+}
