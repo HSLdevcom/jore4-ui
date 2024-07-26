9cf1d84cfe3ea961a50bebce5c8cf3f130cc6dc3
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-07-26 09:48:42 +0300

    Add dropdown menu for adding stop areas and terminals
    The SimpleRoundButton does have some problems still and often (always?)
    requires some workarounds to get it positioned correctly.
    In this case we use setting width for the container (larger than actual button)
    and justify-content properties on parents to center it properly.
    Looks alright in this case, but might require similar hacks if used elsewhere.
    The root cause seems to be aspect-ratio, which does set the width correctly
    based on height, but unfortunately this does _not_ expand the parent height
    which then messes up alignment.

---
+++ ui/src/components/map/MapFooterActionsDropdown.tsx
@@ -0,0 +1,63 @@
+import { Menu } from '@headlessui/react';
+import noop from 'lodash/noop';
+import { useTranslation } from 'react-i18next';
+import { MdMoreVert } from 'react-icons/md';
+import { SimpleDropdownMenuItem, SimpleRoundButton } from '../../uiComponents';
+import {
+  AlignDirection,
+  SimpleDropdownMenuItems,
+} from '../../uiComponents/SimpleDropdownMenuItems';
+
+const testIds = {
+  menu: 'MapFooterActionsDropdown::menu',
+  removeStop: 'MapFooterActionsDropdown::menu',
+};
+
+interface Props {
+  tooltip: string;
+  disabled?: boolean;
+  onCreateNewStopArea: () => void;
+}
+
+export const MapFooterActionsDropdown = ({
+  tooltip,
+  disabled,
+  onCreateNewStopArea,
+}: Props): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <Menu as="div" className="relative inline-flex h-auto">
+      {({ open }) => (
+        <>
+          <Menu.Button
+            data-testid={testIds.menu}
+            as="div"
+            className="flex w-12 justify-around"
+          >
+            <SimpleRoundButton
+              tooltip={tooltip}
+              disabled={disabled}
+              onClick={noop}
+              inverted={!open}
+            >
+              <MdMoreVert className="aria-hidden text-4xl" />
+            </SimpleRoundButton>
+          </Menu.Button>
+          <SimpleDropdownMenuItems open={open} alignItems={AlignDirection.Top}>
+            <SimpleDropdownMenuItem
+              onClick={noop}
+              text={t('map.createNewTerminal') + ' TODO'} // eslint-disable-line prefer-template
+              testId={testIds.removeStop}
+            />
+            <SimpleDropdownMenuItem
+              onClick={onCreateNewStopArea}
+              text={t('map.createNewStopArea')}
+              testId={testIds.removeStop}
+            />
+          </SimpleDropdownMenuItems>
+        </>
+      )}
+    </Menu>
+  );
+};
