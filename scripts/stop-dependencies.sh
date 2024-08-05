7b99ebd9646fa68ae79bc986b34d590e25002cca
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-08-05 12:25:55 +0300

    Use docker compose in scripts
    Switch from docker-compose to docker compose
    Use newer github actions which use docker compose

--- scripts/stop-dependencies.sh
+++ scripts/stop-dependencies.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 cd $(dirname "$0")/..
 
-docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml -f ./docker/docker-compose.e2e.yml down --volumes
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml -f ./docker/docker-compose.e2e.yml down --volumes
