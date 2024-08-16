a9d860b0bd4c968a14cd3b3b8d0ac7f96e5b53c4
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-08-16 14:04:23 +0300

    Rename SaveStopArea operation
    Since we also use it for delete.

--- ui/src/components/map/stop-areas/EditStopAreaLayer.tsx
+++ ui/src/components/map/stop-areas/EditStopAreaLayer.tsx
@@ -72,7 +72,7 @@ export const EditStopAreaLayer = forwardRef<
   const setIsMoveStopAreaModeEnabled = useAppAction(
     setIsMoveStopAreaModeEnabledAction,
   );
-  const { setIsLoading } = useLoader(Operation.SaveStopArea);
+  const { setIsLoading } = useLoader(Operation.ModifyStopArea);
 
   const isExistingStopArea = !!editedArea.id;
   const defaultDisplayedEditor = isExistingStopArea
