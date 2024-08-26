8a024fd91ff2e18c9c272fccb4fe459d6c6aa7f6
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-08-26 10:35:18 +0300

    Add editing of maintainer organisation details

---
+++ ui/src/hooks/stop-registry/useUpsertOrganisation.ts
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+import { useTranslation } from 'react-i18next';
+import {
+  StopRegistryOrganisationInput,
+  useUpsertOrganisationMutation,
+} from '../../generated/graphql';
+import { showDangerToastWithError } from '../../utils';
+
+const GQL_UPSERT_ORGANISATION = gql`
+  mutation UpsertOrganisation($objects: [stop_registry_OrganisationInput]) {
+    stop_registry {
+      mutateOrganisation(Organisation: $objects) {
+        id
+      }
+    }
+  }
+`;
+
+export const useUpsertOrganisation = () => {
+  const { t } = useTranslation();
+  const [upsertOrganisationMutation] = useUpsertOrganisationMutation();
+
+  const upsertOrganisation = async (input: StopRegistryOrganisationInput) => {
+    const result = await upsertOrganisationMutation({
+      variables: { objects: [input] },
+    });
+    const organisation = result.data?.stop_registry?.mutateOrganisation?.[0];
+    return organisation;
+  };
+
+  const defaultErrorHandler = (err: unknown) => {
+    showDangerToastWithError(t('errors.saveFailed'), err);
+  };
+
+  return {
+    upsertOrganisation,
+    defaultErrorHandler,
+  };
+};
