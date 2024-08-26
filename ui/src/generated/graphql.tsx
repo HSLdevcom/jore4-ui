6db568ec2189aa31470bbb8721ce51f774dbc2e4
Tommi Leinamo <tommi.leinamo@vincit.fi>
2024-08-26 09:39:49 +0300

    Update Tiamat to include some fixes to Organisations

--- ui/src/generated/graphql.tsx
+++ ui/src/generated/graphql.tsx
@@ -9596,6 +9596,7 @@ export type StopRegistryContact = {
   id?: Maybe<Scalars['String']['output']>;
   phone?: Maybe<Scalars['String']['output']>;
   url?: Maybe<Scalars['String']['output']>;
+  version?: Maybe<Scalars['String']['output']>;
 };
 
 export type StopRegistryContactInput = {
