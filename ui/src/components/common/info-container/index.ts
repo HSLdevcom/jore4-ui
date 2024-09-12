e9fbe0bc271be49dce87fe76f3caf128e1d5c0fc
Jesse Jaara <jesse.jaara@vincit.fi>
2024-09-12 13:47:18 +0300

    Add generic <InfoContainer> component
    A more generic replacement for <ExpandableInfoContainer> that can be
    used on StopArea and Terminal pages.
    
    * Both editing and expansion features can be toggled on and off.
    * Supports color themes, without having to resort to complex CSS classes.
    * Adds expansion animation.

---
+++ ui/src/components/common/info-container/index.ts
@@ -0,0 +1,6 @@
+export * from './DefaultHeaderButtons';
+export * from './InfoContainer';
+export * from './InfoContainerColors';
+export * from './InfoContainerControls';
+export * from './InfoContainerHeaderButtonsProps';
+export * from './useInfoContainerControls';
