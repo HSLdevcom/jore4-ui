799d3137516e868a1d9bb48822ce4bbcc21083c6
Jesse Jaara <jesse.jaara@vincit.fi>
2024-09-12 16:18:30 +0300

    Add support for lg columns to <FormRow>

--- ui/src/components/forms/common/FormRow.tsx
+++ ui/src/components/forms/common/FormRow.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from 'react';
 
 interface Props {
   className?: string;
+  lgColumns?: number;
   mdColumns?: number;
   smColumns?: number;
   testId?: string;
@@ -11,16 +12,20 @@ interface Props {
 export const FormRow: FC<Props> = ({
   className = '',
   mdColumns = 1,
+  lgColumns = mdColumns,
   smColumns = mdColumns,
   children,
   testId,
 }) => {
   const baseClassName = 'grid grid-cols-1 gap-y-5';
+
+  const lgClassName = `lg:grid-cols-${lgColumns} lg:gap-x-8`;
   const mdClassName = `md:grid-cols-${mdColumns} md:gap-x-8`;
   const smClassName = `sm:grid-cols-${smColumns} sm:gap-x-4`;
+
   return (
     <div
-      className={`${className} ${baseClassName} ${mdClassName} ${smClassName}`}
+      className={`${className} ${baseClassName} ${lgClassName} ${mdClassName} ${smClassName}`}
       data-testid={testId}
     >
       {children}
