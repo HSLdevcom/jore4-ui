618315507c9a5b9bce854d8645ea11175cd47030
Jesse Jaara <jesse.jaara@vincit.fi>
2024-09-16 14:32:44 +0300

    <StopTableRow>: Fix <TdName> styles
    * Instead of having the border on the nested <div> it should go on the
      wrapping <td>. Else it wont span the full height of the row.
    * Removed double padding from the name element.

--- ui/src/components/stop-registry/search/StopTableRow/NameTd.tsx
+++ ui/src/components/stop-registry/search/StopTableRow/NameTd.tsx
@@ -4,7 +4,7 @@ import { StopRowTdProps } from './StopRowTdProps';
 
 export const NameTd: FC<StopRowTdProps> = ({ className, stop }) => (
   <td className={className}>
-    <div className="border-l border-l-background pl-3">
+    <div>
       <Row>{stop.stop_place?.nameFin}</Row>
       <Row>{stop.stop_place?.nameSwe}</Row>
     </div>
