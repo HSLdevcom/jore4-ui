11643b6e5e6dcde762badf3445b45a2af2034a2d
Jesse Jaara <jesse.jaara@vincit.fi>
2024-09-19 14:34:41 +0300

    E2E: Allow expecting a Success Toast but fail on other types
    Instead of simply waiting for a Success Toast, allow one to wait for
    any Toast, and then assert that it is of a correct type.
    
    Simply waiting for Success Toast can result in unhelpful screenshots
    if the waited operation fails and shows an Error Toast instead of a
    Success. But due to Cypress waiting for the Success to appear the
    Error Toast will eventually dissappear from the page, and won't be
    included in the screen shots.

--- cypress/pageObjects/Toast.ts
+++ cypress/pageObjects/Toast.ts
@@ -26,4 +26,20 @@ export class Toast {
   checkWarningToastHasMessage(message: string) {
     this.getWarningToast().contains(message);
   }
+
+  expectSuccessToast(message?: string) {
+    // Find any toast
+    cy.get('[data-testElementType="toast"]')
+      // And wait it to become fully visible
+      .should('have.css', 'opacity', '1')
+      // Then assert it is of a right type.
+      .then((toast) => {
+        // eslint-disable-next-line jest/valid-expect
+        expect(toast).have.attr('data-testid', 'success-toast');
+      });
+
+    if (message) {
+      this.checkSuccessToastHasMessage(message);
+    }
+  }
 }
