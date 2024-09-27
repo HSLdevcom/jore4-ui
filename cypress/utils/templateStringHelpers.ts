8bf6711daad64d7ec4948491a4afa34ac1482fdf
Jesse Jaara <jesse.jaara@vincit.fi>
2024-09-27 16:46:21 +0300

    Add tests to make sure Stop Place gets updated

---
+++ cypress/utils/templateStringHelpers.ts
@@ -0,0 +1,45 @@
+const objectToStringCache = new WeakMap<object, string>();
+
+function serializationHelper(key: string, value: unknown): unknown {
+  if (value === undefined) {
+    return '<<<original value was undefined and replaced with this string>>>';
+  }
+
+  return value;
+}
+
+function expressionToString(expression: unknown): string {
+  if (typeof expression === 'object' && expression) {
+    if (!objectToStringCache.has(expression)) {
+      objectToStringCache.set(
+        expression,
+        JSON.stringify(expression, serializationHelper, 2),
+      );
+    }
+
+    return objectToStringCache.get(expression) as string;
+  }
+
+  return String(expression);
+}
+
+// Basically replaces objects with JSON versions
+export function debug(
+  stringParts: TemplateStringsArray,
+  ...expressions: ReadonlyArray<unknown>
+): string {
+  const totalParts = stringParts.length + expressions.length;
+  const resultArray = new Array<string>(totalParts);
+
+  for (let i = 0; i < totalParts; i += 1) {
+    const j = Math.floor(i / 2);
+
+    if (i % 2 === 0) {
+      resultArray[i] = stringParts[j];
+    } else {
+      resultArray[i] = expressionToString(expressions[j]);
+    }
+  }
+
+  return resultArray.join('');
+}
---
+++ cypress/utils/time.ts
@@ -0,0 +1,6 @@
+import { Settings } from 'luxon';
+
+Settings.throwOnInvalid = true;
+Settings.defaultZone = 'Europe/Helsinki';
+
+export const TS_WANTS_AN_EXPORT = 0;
