b7c31361659909d94b032e52c748ff3cce4bc05a
Suvi Kankare <suvi.kankare@vincit.fi>
2024-10-15 14:19:10 +0300

    Group info spots based on shelters

---
+++ ui/src/components/stop-registry/stops/stop-details/info-spots/SheltersInfoSpots.tsx
@@ -0,0 +1,50 @@
+import compact from 'lodash/compact';
+import React, { FC, useMemo } from 'react';
+import {
+  InfoSpotDetailsFragment,
+  ShelterEquipmentDetailsFragment,
+} from '../../../../../generated/graphql';
+import { StopWithDetails } from '../../../../../hooks';
+import { InfoSpotsSection } from './InfoSpotsSection';
+
+type Props = {
+  readonly stop: StopWithDetails;
+};
+type ShelterAndSpots = [
+  ShelterEquipmentDetailsFragment,
+  ReadonlyArray<InfoSpotDetailsFragment>,
+];
+
+const useSheltersInfoSpots = (
+  stop: StopWithDetails,
+): ReadonlyArray<ShelterAndSpots> => {
+  return useMemo(() => {
+    const infoSpots: Array<InfoSpotDetailsFragment> = compact(
+      stop.stop_place?.infoSpots ?? [],
+    );
+    const shelters: Array<ShelterEquipmentDetailsFragment> = compact(
+      stop.stop_place?.quays?.[0]?.placeEquipments?.shelterEquipment ?? [],
+    );
+
+    return shelters.map((shelter) => {
+      const shelterSpots = infoSpots.filter((infoSpot) =>
+        infoSpot.infoSpotLocations?.some((location) => location === shelter.id),
+      );
+      return [shelter, shelterSpots] as ShelterAndSpots;
+    });
+  }, [stop]);
+};
+
+export const SheltersInfoSpotsSection: FC<Props> = ({ stop }) => {
+  const items = useSheltersInfoSpots(stop);
+
+  return items.map(([shelter, shelterInfoSpots], shelterIndex) => (
+    <InfoSpotsSection
+      key={shelter.id}
+      infoSpots={shelterInfoSpots}
+      shelter={shelter}
+      stop={stop}
+      shelterIndex={shelterIndex}
+    />
+  ));
+};
