69bc944a9ec8e1eed6230eebe6c1782f03f33b49
Jesse Jaara <jesse.jaara@vincit.fi>
2024-10-24 16:17:57 +0300

    StopSearch: Prepare useTypedUrlState hook implementation

---
+++ ui/src/components/stop-registry/search/utils/useStopSearchUrlState.ts
@@ -0,0 +1,99 @@
+import identity from 'lodash/identity';
+import { DateTime } from 'luxon';
+import {
+  UrlStateDeserializers,
+  UrlStateSerializers,
+  serializeState,
+  toEnum,
+  useTypedUrlState,
+} from '../../../../hooks';
+import { StopRegistryMunicipality } from '../../../../types/enums';
+import { AllOptionEnum, numberEnumEntries } from '../../../../utils';
+import {
+  SearchBy,
+  SearchFor,
+  StopSearchFilters,
+  defaultFilters,
+} from '../types';
+import {
+  handleAllMunicipalities,
+  knownMunicipalities,
+} from './handleAllMunicipalities';
+
+type StopSearchUrlState = StopSearchFilters;
+
+const SEPRATOR = ',';
+
+const serializers: UrlStateSerializers<StopSearchUrlState> = {
+  query: identity,
+  elyNumber: identity,
+  searchBy: identity,
+  searchFor: identity,
+  observationDate: (date) => date.toISODate(),
+  municipalities: (them) =>
+    them
+      .map((enumValue) =>
+        enumValue === AllOptionEnum.All
+          ? enumValue
+          : // Reverse mapping for municipality → Enum value (number) to enum Key (string)
+            StopRegistryMunicipality[enumValue],
+      )
+      .join(SEPRATOR),
+};
+
+const lowerCaseMunicipalities: ReadonlyArray<
+  [string, StopRegistryMunicipality]
+> = numberEnumEntries(StopRegistryMunicipality).map(([key, value]) => [
+  key.toLowerCase(),
+  value,
+]);
+
+function parseMunicipalities(
+  value: string,
+): Array<StopRegistryMunicipality | AllOptionEnum> {
+  const parsed = value.split(SEPRATOR).map((enumKeyOrNumberValue) => {
+    if (enumKeyOrNumberValue === AllOptionEnum.All) {
+      return AllOptionEnum.All;
+    }
+
+    const asNumber = Number(enumKeyOrNumberValue);
+    if (knownMunicipalities.includes(asNumber)) {
+      return asNumber as StopRegistryMunicipality;
+    }
+
+    const lowercased = enumKeyOrNumberValue.toLowerCase();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, byName] =
+      lowerCaseMunicipalities.find(([key]) => key === lowercased) ?? [];
+    if (byName) {
+      return byName;
+    }
+
+    throw new TypeError(`Value (${value}) is not a valid municipality!`);
+  });
+
+  return handleAllMunicipalities(parsed);
+}
+
+const deserializers: UrlStateDeserializers<StopSearchUrlState> = {
+  query: identity,
+  elyNumber: identity,
+  searchBy: toEnum(Object.values(SearchBy)),
+  searchFor: toEnum(Object.values(SearchFor)),
+  observationDate: (value) => DateTime.fromISO(value),
+  municipalities: parseMunicipalities,
+};
+
+const defaultValues: StopSearchUrlState = defaultFilters;
+
+export function useStopSearchUrlState() {
+  return useTypedUrlState<StopSearchUrlState>(
+    serializers,
+    deserializers,
+    defaultValues,
+  );
+}
+
+export function stopSearchUrlStateToSearch(state: StopSearchUrlState): string {
+  return serializeState(serializers, defaultValues, state).toString();
+}
