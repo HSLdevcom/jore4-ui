a391f167819d7e681b0930203c4be11478d92465
Jesse Jaara <jesse.jaara@vincit.fi>
2024-10-24 16:00:58 +0300

    StopSearch: Add new SearchCriteriaRadioButtons input component
    Proper form input component

---
+++ ui/src/components/stop-registry/search/StopSearchBar/SearchCriteriaRadioButtons.tsx
@@ -0,0 +1,61 @@
+import { FC, useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { twMerge } from 'tailwind-merge';
+import { SearchBy, StopSearchFilters } from '../types';
+
+function useOptions() {
+  const { t } = useTranslation();
+
+  return useMemo(
+    () => [
+      {
+        name: SearchBy.LabelOrName,
+        label: t('stopRegistrySearch.byLabel'),
+      },
+      {
+        name: SearchBy.Address,
+        label: t('stopRegistrySearch.byAddress'),
+      },
+      {
+        name: SearchBy.Line,
+        label: t('stopRegistrySearch.byLine'),
+      },
+    ],
+    [t],
+  );
+}
+
+type SearchCriteriaRadioButtonsProps = {
+  readonly className?: string;
+};
+
+export const SearchCriteriaRadioButtons: FC<
+  SearchCriteriaRadioButtonsProps
+> = ({ className }) => {
+  const { register } = useFormContext<StopSearchFilters>();
+
+  const searchByOptions = useOptions();
+
+  return (
+    <div className={twMerge('flex space-x-4', className)}>
+      {searchByOptions.map((option) => (
+        <label
+          htmlFor={option.name}
+          className="inline-flex text-nowrap"
+          key={option.name}
+        >
+          <input
+            className="mr-2 h-[20px] w-[20px] border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            type="radio"
+            id={option.name}
+            data-testid={`SearchCriteriaRadioButtons::${option.name}`}
+            value={option.name}
+            {...register('searchBy')}
+          />
+          {option.label}
+        </label>
+      ))}
+    </div>
+  );
+};
