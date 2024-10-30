22ed83e017df1e0f17344d4c329537dd37521f3e
Jesse Jaara <jesse.jaara@vincit.fi>
2024-10-30 16:18:24 +0200

    StopAreaSearch: Add StopAreaSelector component

---
+++ ui/src/components/stop-registry/search/for-stop-areas/StopAreaSelector.tsx
@@ -0,0 +1,47 @@
+import { FC, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { StopGroupSelector, StopGroupSelectorItem } from '../components';
+import { FindStopAreaInfo } from './useFindStopAreas';
+
+type StopAreaSelectorProps = {
+  readonly activeStopId: string | null;
+  readonly className?: string;
+  readonly stopAreas: ReadonlyArray<FindStopAreaInfo>;
+  readonly setActiveStopId: (activeLineId: string | null) => void;
+};
+
+export const StopAreaSelector: FC<StopAreaSelectorProps> = ({
+  activeStopId,
+  className,
+  stopAreas,
+  setActiveStopId,
+}) => {
+  const { t } = useTranslation();
+
+  const groups: ReadonlyArray<StopGroupSelectorItem<string>> = useMemo(
+    () =>
+      stopAreas.map(
+        ({
+          id,
+          netex_id: netextId,
+          name_value: label,
+          description_value: title,
+        }) => ({
+          id: id.toString(10),
+          label: label ?? netextId ?? id.toString(10),
+          title: title ?? '',
+        }),
+      ),
+    [stopAreas],
+  );
+
+  return (
+    <StopGroupSelector
+      className={className}
+      groups={groups}
+      label={t('stopRegistrySearch.stopAreas')}
+      onSelect={setActiveStopId}
+      selected={activeStopId ?? null}
+    />
+  );
+};
