72330427580989a5b07ef867498b67dcb0ac2188
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-06 11:19:39 +0200

    Cleanup ui/src/types/index.ts
    Move actual definitions to sub files, as the index file should not
    have any definitions in itself.

--- ui/src/types/index.ts
+++ ui/src/types/index.ts
@@ -1,23 +1,4 @@
-export interface Point {
-  longitude: number;
-  latitude: number;
-  elevation?: number;
-}
-
-export type Coords = { x: number; y: number };
-
-// Make given keys of a type required, leave the rest as-is
-export type RequiredKeys<T, K extends keyof T> = Required<Pick<T, K>> &
-  Omit<T, K>;
-
-// Make given keys of a type optional, leave the rest as-is
-export type OptionalKeys<T, K extends keyof T> = Partial<Pick<T, K>> &
-  Omit<T, K>;
-
-// Make given keys of a type not nullable, leave the rest as-is
-export type NonNullableKeys<T, K extends keyof T> = {
-  [P in K]: NonNullable<T[P]>;
-} & Omit<T, K>;
-
-export * from './StopSearchConditions';
+export * from './GeoHelpers';
+export * from './KeyHelpers';
 export * from './stopAreaByIdResult';
+export * from './StopSearchConditions';
