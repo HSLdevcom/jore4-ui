91b3f59c283ac2e1eff50d06da7821cc8a51c0ac
Ville Pynttäri <ville.pynttari@vincit.fi>
2024-11-07 15:19:58 +0200

    35325 - Added box to line details to show information when observation date is outside its validity period

--- cypress/e2e/editLineInfo.cy.ts
+++ cypress/e2e/editLineInfo.cy.ts
@@ -6,6 +6,7 @@ import {
 } from '@hsl/jore4-test-db-manager';
 import { Tag } from '../enums';
 import { LineDetailsPage, LineForm } from '../pageObjects';
+import { ObservationDateControl } from '../pageObjects/ObservationDateControl';
 import { insertToDbHelper } from '../utils';
 
 const testInputs = {
@@ -30,10 +31,12 @@ const dbResources = {
 describe('Line editing', () => {
   let lineForm: LineForm;
   let lineDetailsPage: LineDetailsPage;
+  let observationDateControl: ObservationDateControl;
   beforeEach(() => {
     cy.task('resetDbs');
     lineForm = new LineForm();
     lineDetailsPage = new LineDetailsPage();
+    observationDateControl = new ObservationDateControl();
 
     cy.setupTests();
     cy.mockLogin();
@@ -66,6 +69,26 @@ describe('Line editing', () => {
       lineDetailsPage.lineValidityPeriod
         .getLineValidityPeriod()
         .should('have.text', '1.1.2022 - ');
+    },
+  );
+
+  it(
+    'should show not valid for selected day text',
+    { tags: [Tag.Smoke, Tag.Lines] },
+    () => {
+      lineDetailsPage.getEditLineButton().click();
+      lineForm.changeValidityForm.validityPeriodForm.setStartDate('2022-01-01');
+      lineForm.save();
+
+      const beforeValidityPeriod = '2021-01-31';
+      observationDateControl.setObservationDate(beforeValidityPeriod);
+      cy.getByTestId('LineNotValidForDayBox::notValidText').should(
+        'be.visible',
+      );
+
+      const withinValidityPeriod = '2022-01-01';
+      observationDateControl.setObservationDate(withinValidityPeriod);
+      cy.getByTestId('LineNotValidForDayBox::notValidText').should('not.exist');
     },
   );
 });
