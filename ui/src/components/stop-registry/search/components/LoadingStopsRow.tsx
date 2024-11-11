149d40268479fa379e8613de406416146fe06581
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-11 12:39:42 +0200

    StopSearch: Add 'searching...' subtext to LoadingStopRow

--- ui/src/components/stop-registry/search/components/LoadingStopsRow.tsx
+++ ui/src/components/stop-registry/search/components/LoadingStopsRow.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import PulseLoader from 'react-spinners/PulseLoader';
 import { theme } from '../../../../generated/theme';
 
@@ -7,14 +8,18 @@ const testIds = {
 };
 
 export const LoadingStopsRow: FC = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="flex items-center justify-center border border-light-grey p-8">
+    <div className="flex-column items-center justify-center border border-light-grey p-8">
       <PulseLoader
         data-testid={testIds.loader}
         color={theme.colors.brand}
         size={25}
         speedMultiplier={0.7}
       />
+
+      <span className="mt-4">{t('search.searching')}</span>
     </div>
   );
 };
