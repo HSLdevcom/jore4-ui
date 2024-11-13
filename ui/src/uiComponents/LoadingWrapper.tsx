303844779289a28a504432d0429dce8b67119f98
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-13 15:53:37 +0200

    Add Suspense support to LoadingWrapper

--- ui/src/uiComponents/LoadingWrapper.tsx
+++ ui/src/uiComponents/LoadingWrapper.tsx
@@ -1,4 +1,5 @@
-import { FC, ReactNode } from 'react';
+import noop from 'lodash/noop';
+import { FC, ReactNode, Suspense } from 'react';
 import { PulseLoader } from 'react-spinners';
 import { theme } from '../generated/theme';
 
@@ -13,35 +14,45 @@ interface Props {
   children: ReactNode;
 }
 
-/**
- * This loading wrapper will render a React spinner if the loading parameter is true,
- * but it will render the children if the loading parameter is false. So you can wrap the
- * elements that need to be hidden during the loading with this, hence the name.
- * There is also an optional loading text below the spinner.
- */
-export const LoadingWrapper: FC<Props> = ({
+const Loader: FC<Omit<Props, 'children' | 'loading'>> = ({
   testId,
-  children,
   loadingText,
   className,
-  loading = true,
   size = 25,
   color = theme.colors.brand,
   speedMultiplier = 0.7,
-}) => {
-  if (loading) {
-    return (
-      <div data-testid={testId} className={className}>
-        <div className="inline-flex flex-col items-center">
-          <PulseLoader
-            color={color}
-            size={size}
-            speedMultiplier={speedMultiplier}
-          />
-          {loadingText && <span className="mt-4">{loadingText}</span>}
-        </div>
-      </div>
-    );
-  }
-  return <>{children}</>;
+}) => (
+  <div data-testid={testId} className={className}>
+    <div className="inline-flex flex-col items-center">
+      <PulseLoader
+        color={color}
+        size={size}
+        speedMultiplier={speedMultiplier}
+      />
+      {loadingText && <span className="mt-4">{loadingText}</span>}
+    </div>
+  </div>
+);
+
+// A component that simply triggers the React suspense
+const TriggerSuspense: FC = () => {
+  throw new Promise(noop);
 };
+
+/**
+ * This loading wrapper will render a React spinner if the loading parameter is true,
+ * or if a React Suspense is triggered from some child component.
+ * Otherwise, renders the children. So you can wrap the elements that need to be
+ * hidden during the loading with this, hence the name.
+ * There is also an optional loading text below the spinner.
+ */
+export const LoadingWrapper: FC<Props> = ({
+  children,
+  loading = true,
+  ...loaderProps
+}) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Suspense fallback={<Loader {...loaderProps} />}>
+    {loading ? <TriggerSuspense /> : <>{children}</>}
+  </Suspense>
+);
