eb15103e3581edb6dc5a4c7f7bcc167ba3d137fe
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-13 15:39:49 +0200

    Add top level Suspense support
    Adds a high level fallback spinner for React Suspense feature. A more
    appropriate spinner should be defined on a lower level closer to where
    a suspenseful child component is used.

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,9 @@
+import React, { Suspense } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
+import PulseLoader from 'react-spinners/PulseLoader';
 import { UserProvider } from '../auth/UserProvider';
+import { theme } from '../generated/theme';
 import { ApolloProvider } from '../graphql';
 import { ReduxProvider } from '../redux';
 import { Router } from '../router/Router';
@@ -18,9 +21,26 @@ const Index = () => {
         </Helmet>
         <ApolloProvider>
           <ReduxProvider>
-            <UserProvider />
-            <Router />
-            <Toaster />
+            <Suspense
+              fallback={
+                <PulseLoader
+                  color={theme.colors.brand}
+                  size={25}
+                  cssOverride={{
+                    display: 'inline-block',
+                    position: 'relative',
+                    paddingTop: '2rem',
+                    left: '50%',
+                    transform: 'translate(-50%, 0)',
+                  }}
+                  speedMultiplier={0.7}
+                />
+              }
+            >
+              <UserProvider />
+              <Router />
+              <Toaster />
+            </Suspense>
           </ReduxProvider>
         </ApolloProvider>
         <footer className="mt-6 flex justify-center">
