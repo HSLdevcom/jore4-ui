c596d499adbfa24c5fd1a8ae8c09ce6019a25717
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-18 17:40:30 +0200

    Continuation for dirty fix

--- ui/src/hooks/line-details/useGetLineDetails.ts
+++ ui/src/hooks/line-details/useGetLineDetails.ts
@@ -236,6 +236,8 @@ export const useGetLineDetails = () => {
 
       return filteredLine;
     }
+
+    return null;
   }, [getHighestPriorityLineDetails, lineDetailsResult, observationDate]);
 
   useEffect(() => {
