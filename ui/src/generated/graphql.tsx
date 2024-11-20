d38b45d54b9bbce63b53500773f95097b4b2623f
Jesse Jaara <jesse.jaara@vincit.fi>
2024-11-20 14:24:20 +0200

    Store stop's priority, validityStart & validityEnd values in Timat key-values

--- ui/src/generated/graphql.tsx
+++ ui/src/generated/graphql.tsx
@@ -69750,6 +69750,29 @@ export type GetStopWithRouteGraphDataByIdQuery = {
         } | null;
       };
     }>;
+    stop_place?: Array<
+      | {
+          __typename?: 'stop_registry_ParentStopPlace';
+          id?: string | null;
+          version?: string | null;
+          keyValues?: Array<{
+            __typename?: 'stop_registry_KeyValues';
+            key?: string | null;
+            values?: Array<string | null> | null;
+          } | null> | null;
+        }
+      | {
+          __typename?: 'stop_registry_StopPlace';
+          id?: string | null;
+          version?: string | null;
+          keyValues?: Array<{
+            __typename?: 'stop_registry_KeyValues';
+            key?: string | null;
+            values?: Array<string | null> | null;
+          } | null> | null;
+        }
+      | null
+    > | null;
     vehicle_mode_on_scheduled_stop_point: Array<{
       __typename?: 'service_pattern_vehicle_mode_on_scheduled_stop_point';
       vehicle_mode: ReusableComponentsVehicleModeEnum;
@@ -72125,6 +72148,11 @@ export type EditStopPlaceMutation = {
         __typename?: 'stop_registry_GeoJSON';
         coordinates?: GeoJSON.Position | null;
       } | null;
+      keyValues?: Array<{
+        __typename?: 'stop_registry_KeyValues';
+        key?: string | null;
+        values?: Array<string | null> | null;
+      } | null> | null;
     } | null> | null;
   } | null;
 };
@@ -77975,6 +78003,14 @@ export const GetStopWithRouteGraphDataByIdDocument = gql`
           }
         }
       }
+      stop_place {
+        id
+        version
+        keyValues {
+          key
+          values
+        }
+      }
     }
   }
   ${ScheduledStopPointAllFieldsFragmentDoc}
@@ -79714,6 +79750,10 @@ export const EditStopPlaceDocument = gql`
         }
         geometry {
           coordinates
+        }
+        keyValues {
+          key
+          values
         }
       }
     }
