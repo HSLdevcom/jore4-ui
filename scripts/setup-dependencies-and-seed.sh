a600f67506e04b627ea1d40d79ad920c243b4659
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-12-12 14:50:26 +0200

    Rework setup scripts to use new dumps
    Combine setup-dependenceis-and-seed.sh and
    start-dependencies.sh into development.sh.
    Rework development.sh to use new dumps.

--- scripts/setup-dependencies-and-seed.sh
+++ scripts/setup-dependencies-and-seed.sh
@@ -3,35 +3,5 @@ set -euo pipefail
 
 cd $(dirname "$0")/..
 
-read -p "Warning: all the current data in the database will be overwritten! Are you sure (y/n)? " REPLY
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+scripts/development.sh setup:env "$@"
 
-# Start dependencies
-./scripts/start-dependencies.sh "$@"
-
-# Fetch Digitransit API key if local environment variable file does not exist
-if [ ! -e ui/.env.local ]
-then
-    echo "" | ./scripts/development.sh digitransit:fetch
-fi
-
-# Download routes and lines dump if it does not exist
-if [ ! -e jore4dump.pgdump ]
-then
-    echo "" | ./scripts/development.sh dump:download
-fi
-
-# Import routes and lines dump to db
-echo "y" | ./scripts/development.sh dump:import
-
-# Seed timetables and stop registry
-cd ./test-db-manager
-yarn build
-yarn seed
-
-../scripts/check-for-new-image-versions.sh
-
-echo "All done! Happy coding! :)"
