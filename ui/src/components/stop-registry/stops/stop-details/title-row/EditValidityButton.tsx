662ad6616b78e9e433081143357058ed0d37f5ab
Jesse Jaara <jesse.jaara@vincit.fi>
2024-12-16 20:33:32 +0200

    Implement placeholder action buttons on StopDetails page
    * Placeholder button for: Edit Stop validity period and/or priority.
    * Proper: OpenOnMap -button.
    * ExtraActions -menu with:
      - Placeholder meny items for: Make a new copy of stop -menu action.

---
+++ ui/src/components/stop-registry/stops/stop-details/title-row/EditValidityButton.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { StopWithDetails } from '../../../../../hooks';
+
+const DISABLE_UNTIL_IMPLEMENTED = true;
+
+const testIds = {
+  button: 'StopDetailsPage::editValidityButton',
+};
+
+type EditValidityButtonProps = {
+  readonly className?: string;
+  readonly stop: StopWithDetails | null;
+};
+
+export const EditValidityButton: FC<EditValidityButtonProps> = ({
+  className,
+  stop,
+}) => {
+  return (
+    <button
+      className={twMerge(
+        'h-11 w-11',
+        'flex items-center justify-center',
+        'rounded-full border border-grey',
+        'disabled:pointer-events-none disabled:bg-background disabled:opacity-70',
+        className,
+      )}
+      data-testid={testIds.button}
+      disabled={!stop || DISABLE_UNTIL_IMPLEMENTED}
+      type="button"
+    >
+      <i className="icon-calendar aria-hidden text-2xl text-brand" />
+    </button>
+  );
+};
---
+++ ui/src/components/stop-registry/stops/stop-details/title-row/OpenOnMapButton.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { twMerge } from 'tailwind-merge';
+import { StopWithDetails } from '../../../../../hooks';
+import { LocatorButton } from '../../../../../uiComponents';
+import { useOpenStopOnMap } from '../../../search/StopTableRow/utils';
+
+type OpenOnMapButtonProps = {
+  readonly className?: string;
+  readonly label: string;
+  readonly stop: StopWithDetails | null;
+};
+
+export const OpenOnMapButton: FC<OpenOnMapButtonProps> = ({
+  className,
+  label,
+  stop,
+}) => {
+  const { t } = useTranslation();
+
+  const openStopOnMap = useOpenStopOnMap();
+  const onClick = () => {
+    if (stop && stop.stop_place !== null) {
+      openStopOnMap({ ...stop, stop_place: stop.stop_place });
+    }
+  };
+
+  return (
+    <LocatorButton
+      className={twMerge('h-11 w-11', className)}
+      disabled={!stop}
+      onClick={onClick}
+      tooltipText={t('accessibility:common.showOnMap', { label })}
+    />
+  );
+};
