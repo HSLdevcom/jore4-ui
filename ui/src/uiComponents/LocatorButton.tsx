385424c3a65fae507d8bea90e0444e1708cb19cf
Jesse Jaara <jesse.jaara@vincit.fi>
2024-12-16 20:24:28 +0200

    Improve LocatorButton

--- ui/src/uiComponents/LocatorButton.tsx
+++ ui/src/uiComponents/LocatorButton.tsx
@@ -1,4 +1,6 @@
+import { FC } from 'react';
 import { MdPinDrop } from 'react-icons/md';
+import { twMerge } from 'tailwind-merge';
 import { IconButton } from './IconButton';
 import { commonHoverStyle } from './SimpleButton';
 
@@ -6,27 +8,31 @@ const testIds = {
   button: 'LocatorButton::button',
 };
 
-interface Props {
+type LocatorButtonProps = {
   testId?: string;
   disabled?: boolean;
   onClick: () => void;
   className?: string;
   tooltipText: string;
-}
+};
 
-export const LocatorButton = ({
+export const LocatorButton: FC<LocatorButtonProps> = ({
   testId,
   onClick,
   disabled,
   className = '',
   tooltipText,
-}: Props): React.ReactElement => {
-  const disabledStyle = '!bg-background opacity-70 pointer-events-none';
+}) => {
   return (
     <IconButton
-      className={`h-10 w-10 rounded-full border border-grey bg-white text-tweaked-brand ${commonHoverStyle} ${
-        disabled ? disabledStyle : ''
-      } ${className}`}
+      className={twMerge(
+        `h-10 w-10`,
+        `rounded-full border border-grey`,
+        `bg-white text-tweaked-brand`,
+        commonHoverStyle,
+        'disabled:pointer-events-none disabled:bg-background disabled:opacity-70',
+        className,
+      )}
       tooltip={tooltipText}
       onClick={onClick}
       disabled={disabled}
