7803e7b5ddde1fd535fe59f5a13795ee211d6101
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-13 19:39:20 +0200

    From now on, download the JORE4 Docker Compose bundle from the "jore4-docker-compose-bundle" repository.
    Also make minor spelling fixes to the README and GitHub workflow files.

--- scripts/development.sh
+++ scripts/development.sh
@@ -4,6 +4,14 @@ set -eo pipefail
 
 cd "$(dirname "$0")"/..
 
+# By default, the tip of the main branch of the jore4-docker-compose-bundle
+# repository is used as the commit reference, which determines the version of
+# the Docker Compose bundle to download. For debugging purposes, this default
+# can be overridden by some other commit reference (e.g., commit SHA or its
+# initial substring), which you can pass via the `BUNDLE_REF` environment
+# variable.
+DOCKER_COMPOSE_BUNDLE_REF=${BUNDLE_REF:-main}
+
 DUMP_ROUTES_FILENAME="routes-12-2024.pgdump"
 DUMP_TIMETABLES_FILENAME="timetables-12-2024.pgdump"
 DUMP_STOPS_FILENAME="stopdb-12-2024.pgdump"
@@ -99,6 +107,75 @@ function check_pinned_image {
   fi
 }
 
+# Download Docker Compose bundle from the "jore4-docker-compose-bundle"
+# repository. GitHub CLI is required to be installed.
+#
+# A commit reference is read from global `DOCKER_COMPOSE_BUNDLE_REF` variable,
+# which should be set based on the script execution arguments.
+download_docker_compose_bundle() {
+  local commit_ref="$DOCKER_COMPOSE_BUNDLE_REF"
+
+  local repo_name="jore4-docker-compose-bundle"
+  local repo_owner="HSLdevcom"
+
+  # Check GitHub CLI availability.
+  if ! command -v gh &> /dev/null; then
+    echo "Please install the GitHub CLI (gh) on your machine."
+    exit 1
+  fi
+
+  # Make sure the user is authenticated to GitHub.
+  gh auth status || gh auth login
+
+  echo "Using the commit reference '${commit_ref}' to fetch a Docker Compose bundle..."
+
+  # First, try to find a commit on GitHub that matches the given reference.
+  # This function exits with an error code if no matching commit is found.
+  local commit_sha
+  commit_sha=$(
+    gh api \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "repos/${repo_owner}/${repo_name}/commits/${commit_ref}" \
+      --jq '.sha'
+  )
+
+  echo "Commit with the following SHA digest was found: ${commit_sha}"
+
+  local zip_file="/tmp/${repo_name}.zip"
+  local unzip_target_dir_prefix="/tmp/${repo_owner}-${repo_name}"
+
+  # Remove old temporary directories if any remain.
+  rm -fr "$unzip_target_dir_prefix"-*
+
+  echo "Downloading the JORE4 Docker Compose bundle..."
+
+  # Download Docker Compose bundle from the jore4-docker-compose-bundle
+  # repository as a ZIP file.
+  gh api "repos/${repo_owner}/${repo_name}/zipball/${commit_sha}" > "$zip_file"
+
+  # Extract ZIP file contents to a temporary directory.
+  unzip -q "$zip_file" -d /tmp
+
+  # Clean untracked files from `docker` directory even if they are git-ignored.
+  git clean -fx ./docker
+
+  echo "Copying JORE4 Docker Compose bundle files to ./docker directory..."
+
+  # Copy files from the `docker-compose` directory of the ZIP file to your
+  # local `docker` directory.
+  mv "$unzip_target_dir_prefix"-*/docker-compose/* ./docker
+
+  # Remove the temporary files and directories created above.
+  rm -fr "$zip_file" "$unzip_target_dir_prefix"-*
+
+  echo "Generating a release version file for the downloaded bundle..."
+
+  # Create a release version file containing the SHA digest of the referenced
+  # commit.
+  echo "$commit_sha" > ./docker/RELEASE_VERSION.txt
+}
+
 function start_docker_images {
   echo "Running docker compose command: $DOCKER_COMPOSE_CMD"
 
@@ -110,16 +187,7 @@ function stop_dependencies {
 }
 
 function start_dependencies {
-  if ! command -v gh; then
-    echo "Please install the github gh tool on your machine."
-    exit 1
-  fi
-
-  # initialize package folder
-  mkdir -p ./docker
-
-  echo "Downloading latest version of E2E docker-compose package..."
-  curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
+  download_docker_compose_bundle
 
   local additional_images=""
   if [ "$INCLUDE_E2E" = true ]; then
@@ -199,20 +267,11 @@ function setup_environment {
     exit 1
   fi
 
-  if ! command -v gh; then
-    echo "Please install the github gh tool on your machine."
-    exit 1
-  fi
-
   if [ ! -f ui/.env.local ]; then
     download_digitransit_key
   fi
 
-  # initialize package folder
-  mkdir -p ./docker
-
-  echo "Downloading latest version of E2E docker-compose package..."
-  curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
+  download_docker_compose_bundle
 
   start_docker_images $DOCKER_TESTDB_IMAGE
 
