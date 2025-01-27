4230c65f38eb9a9eaf4cfd4973742ed32ff3f0db
Ville Pynttäri <ville.pynttari@vincit.fi>
2025-01-27 14:04:36 +0200

    48597 - Modified saving stop point to set stop place ref from quay instad of stop place

--- ui/src/generated/graphql.tsx
+++ ui/src/generated/graphql.tsx
@@ -71334,6 +71334,7 @@ export type InsertStopPlaceMutation = {
       id?: string | null;
       quays?: Array<{
         __typename?: 'stop_registry_Quay';
+        id?: string | null;
         publicCode?: string | null;
       } | null> | null;
       keyValues?: Array<{
@@ -80158,6 +80159,7 @@ export const InsertStopPlaceDocument = gql`
         publicCode
         id
         quays {
+          id
           publicCode
         }
         keyValues {
