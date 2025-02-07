b6094f22c02521c7149a8a604e6bf6d58e767c20
Suvi Kankare <suvi.kankare@vincit.fi>
2025-02-07 14:56:00 +0200

    Update stop area editing

--- ui/src/components/stop-registry/stop-areas/stop-area-details/components/StopAreaTitleRow.tsx
+++ ui/src/components/stop-registry/stop-areas/stop-area-details/components/StopAreaTitleRow.tsx
@@ -1,9 +1,8 @@
+import compact from 'lodash/compact';
 import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
 import { twMerge } from 'tailwind-merge';
 import { PageTitle } from '../../../../common';
 import { ObservationDateControl } from '../../../../common/ObservationDateControl';
-import { StopTypeLabel } from '../../../stops/stop-details/StopTypeLabel';
 import { StopAreaComponentProps } from './StopAreaComponentProps';
 
 const testIds = {
@@ -16,29 +15,19 @@ export const StopAreaTitleRow: FC<StopAreaComponentProps> = ({
   area,
   className = '',
 }) => {
-  const { t } = useTranslation();
-
   return (
     <div className={twMerge('flex items-center', className)}>
       <i className="icon-bus-alt mr-2 text-3xl text-tweaked-brand" />
-      <PageTitle.H1 className="mr-2" testId={testIds.name}>
-        {area.name ?? ''}
+      <PageTitle.H1
+        className="mr-2"
+        testId={testIds.name}
+        titleText={compact([area.privateCode?.value, area.name]).join(' ')}
+      >
+        {area.privateCode?.value ?? ''}
       </PageTitle.H1>
 
-      <div className="text-xl" data-testid={testIds.privateCode}>
-        {area.privateCode?.value ?? null}
-      </div>
-
-      <div className="pl-5" data-testid={testIds.weighting}>
-        <StopTypeLabel
-          hasType={!!area?.weighting}
-          text={t('stopPlaceTypes.interchange')}
-        />
-
-        <StopTypeLabel
-          hasType={!!area?.submode}
-          text={t('stopPlaceTypes.railReplacement')}
-        />
+      <div className="text-xl" data-testid={testIds.name}>
+        {area.name ?? null}
       </div>
 
       <div className="flex-grow" />
