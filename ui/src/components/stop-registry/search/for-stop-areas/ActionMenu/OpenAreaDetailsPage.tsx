a9ddc734b22ee8d0a00362509c00e92b2bb52ec4
Suvi Kankare <suvi.kankare@vincit.fi>
2025-02-27 12:55:19 +0200

    Add link and three dot menu to stop area in search

---
+++ ui/src/components/stop-registry/search/for-stop-areas/ActionMenu/OpenAreaDetailsPage.tsx
@@ -0,0 +1,33 @@
+import { forwardRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { Path, routeDetails } from '../../../../../router/routeDetails';
+import { SimpleDropdownMenuItem } from '../../../../../uiComponents';
+
+type OpenDetailsProps = {
+  readonly className?: string;
+  readonly netexId: string | null | undefined;
+  readonly testId: string;
+};
+
+export const OpenDetails = forwardRef<HTMLButtonElement, OpenDetailsProps>(
+  ({ className, netexId, testId }, ref) => {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    return (
+      <SimpleDropdownMenuItem
+        className={className}
+        text={t('stopRegistrySearch.stopAreaRowActions.openDetails')}
+        onClick={() =>
+          navigate(routeDetails[Path.stopAreaDetails].getLink(netexId))
+        }
+        testId={testId}
+        ref={ref}
+        disabled={!netexId}
+      />
+    );
+  },
+);
+
+OpenDetails.displayName = 'OpenDetails';
