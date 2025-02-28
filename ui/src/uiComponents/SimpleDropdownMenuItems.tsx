8c06df9279e1b61989eecf7224ee147caefbbe30
Jesse Jaara <jesse.jaara@vincit.fi>
2025-02-28 12:15:02 +0200

    Cleanup & improve `<SimpleDropdownMenuItems>`
    * Make sure the items are always rendered as block items, so that they
      get listed vertically instead of getting rendered horizontally after
      each other.
    * Add new Alignment values to make sure the menu is fully below the
      button, instead of partially obscuring it.
    * Use `twMerge` and `twJoin` to handle the class name concatenation.

--- ui/src/uiComponents/SimpleDropdownMenuItems.tsx
+++ ui/src/uiComponents/SimpleDropdownMenuItems.tsx
@@ -1,55 +1,66 @@
 import { Menu, Transition } from '@headlessui/react';
-import React, { Fragment, ReactNode } from 'react';
-import { addClassName } from '../utils/components';
+import React, { FC, Fragment, ReactNode } from 'react';
+import { twJoin } from 'tailwind-merge';
+import { addClassName } from '../utils';
 import { dropdownTransition } from './Listbox';
 
-interface Props {
-  isOpen: boolean;
-  children: ReactNode;
-  alignItems: AlignDirection;
-}
+type SimpleDropdownMenuItemsProps = {
+  readonly isOpen: boolean;
+  readonly children: ReactNode;
+  readonly alignItems: AlignDirection;
+};
 
 export enum AlignDirection {
   Top,
   Right,
+  RightBottom,
   Left,
+  LeftBottom,
   NoAlign,
 }
 
-const getAlignClassName = (alignItems: AlignDirection) => {
+function getAlignClassName(alignItems: AlignDirection) {
   switch (alignItems) {
     case AlignDirection.Top:
       return 'bottom-[calc(100%+10px)]';
     case AlignDirection.Right:
       return 'left-0';
+    case AlignDirection.RightBottom:
+      return 'left-0 top-full';
     case AlignDirection.Left:
       return 'right-0';
+    case AlignDirection.LeftBottom:
+      return 'right-0 top-full';
     default:
       return '';
   }
-};
+}
 
-export const SimpleDropdownMenuItems = ({
+const commonClassName = `absolute z-10 origin-top-right overflow-visible bg-white text-black shadow-md focus:outline-none`;
+const commonMenuItemClassName = `border-x border-b first-of-type:border-t whitespace-nowrap border-black w-full py-1 px-2 focus:outline-none text-left block`;
+const activeMenuItemClassName = `bg-dark-grey text-white`;
+
+export const SimpleDropdownMenuItems: FC<SimpleDropdownMenuItemsProps> = ({
   isOpen,
   children,
   alignItems,
-}: Props) => {
+}) => {
   const alignClassName = getAlignClassName(alignItems);
-  const commonClassName = `${alignClassName} absolute z-10 origin-top-right overflow-visible bg-white text-black shadow-md focus:outline-none`;
-  const commonMenuItemClassName = `border-x border-b first-of-type:border-t whitespace-nowrap border-black w-full py-1 px-2 focus:outline-none text-left`;
-  const activeMenuItemClassName = `${commonMenuItemClassName} bg-dark-grey text-white`;
 
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <Transition show={isOpen} as={Fragment} {...dropdownTransition}>
-      <Menu.Items static className={commonClassName}>
+      <Menu.Items static className={twJoin(commonClassName, alignClassName)}>
         {React.Children.map(children, (child) =>
           React.isValidElement(child) ? (
             <Menu.Item>
               {({ active }) =>
                 addClassName(
                   child,
-                  active ? activeMenuItemClassName : commonMenuItemClassName,
+                  twJoin(
+                    commonMenuItemClassName,
+                    active ? activeMenuItemClassName : '',
+                  ),
                 )
               }
             </Menu.Item>
