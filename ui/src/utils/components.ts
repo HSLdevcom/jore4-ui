d05fa2d0658f70bfe3fc9ffa39d22f4f1104bcad
Jesse Jaara <jesse.jaara@vincit.fi>
2025-02-28 12:15:02 +0200

    Cleanup & improve `<SimpleDropdownMenuItems>`
    * Make sure the items are always rendered as block items, so that they
      get listed vertically instead of getting rendered horizontally after
      each other.
    * Add new Alignment values to make sure the menu is fully below the
      button, instead of partially obscuring it.
    * Use `twMerge` and `twJoin` to handle the class name concatenation.

--- ui/src/utils/components.ts
+++ ui/src/utils/components.ts
@@ -1,14 +1,9 @@
 import React, { ReactElement } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 // appends a classname to an existing react element
-export const addClassName = (element: ReactElement, newClassName: string) => {
-  const className = element.props.className
-    ? `${element.props.className} ${newClassName}`
-    : newClassName;
-
-  const childProps = {
-    className,
-  };
-
-  return React.cloneElement(element, childProps);
+export const addClassName = (element: ReactElement, newClassNames: string) => {
+  return React.cloneElement(element, {
+    className: twMerge(element.props.className, newClassNames),
+  });
 };
