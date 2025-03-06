96675afd17cefd952191e351464f52dbf2ffd4b3
Suvi Kankare <suvi.kankare@vincit.fi>
2025-03-06 15:56:14 +0200

    Add e2e test to stop area title links

--- cypress/pageObjects/stop-registry/SearchForStopAreas.ts
+++ cypress/pageObjects/stop-registry/SearchForStopAreas.ts
@@ -3,7 +3,18 @@ export class SearchForStopAreas {
     return cy.getByTestId('StopAreaSearch::label');
   }
 
+  getStopAreaLink() {
+    return cy.getByTestId('StopAreaSearch::link');
+  }
+
   getLocatorButton() {
     return cy.getByTestId('StopAreaSearch::locatorButton');
   }
+
+  getActionMenu = () => cy.getByTestId('StopAreaHeader::actionMenu');
+
+  getActionMenuShowDetails = () =>
+    cy.getByTestId('StopAreaSearch::showStopAreaDetails');
+
+  getActionMenuShowOnMap = () => cy.getByTestId('StopAreaSearch::showOnMap');
 }
