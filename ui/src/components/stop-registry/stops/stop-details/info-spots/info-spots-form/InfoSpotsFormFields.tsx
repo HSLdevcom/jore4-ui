4eada7f725d4019785b8a1cfb4a21623f482b522
suvi <kankare.suvi@gmail.com>
2025-04-04 13:07:14 +0300

    Add passenger info -icon

--- ui/src/components/stop-registry/stops/stop-details/info-spots/info-spots-form/InfoSpotsFormFields.tsx
+++ ui/src/components/stop-registry/stops/stop-details/info-spots/info-spots-form/InfoSpotsFormFields.tsx
@@ -80,9 +80,13 @@ export const InfoSpotFormFields: FC<Props> = ({
     <Column className="space-y-4">
       <div className="bg-background p-5">
         <span className="mb-2.5 block text-xl">
-          <i className="icon-poster mr-2.5 text-hsl-red" aria-hidden />
-          {t('stopDetails.infoSpots.infoSpot')}{' '}
-          {infoSpotsData[infoSpotIndex]?.label ?? ''}
+          <i
+            className="icon-passenger-info mr-2.5 text-brand"
+            role="presentation"
+          />
+          {t('stopDetails.infoSpots.infoSpot', {
+            infoSpot: infoSpotsData[infoSpotIndex]?.label,
+          })}
         </span>
         <Row className="flex-wrap items-end gap-4 py-2.5 lg:flex-nowrap">
           <InputField<InfoSpotsFormState>
@@ -181,11 +185,9 @@ export const InfoSpotFormFields: FC<Props> = ({
             onClick={() => onRemove(infoSpotIndex)}
             inverted
           >
-            {t(
-              toBeDeleted
-                ? 'stopDetails.infoSpots.cancelDeleteInfoSpot'
-                : 'stopDetails.infoSpots.deleteInfoSpot',
-            )}
+            {toBeDeleted
+              ? t('stopDetails.infoSpots.cancelDeleteInfoSpot')
+              : t('stopDetails.infoSpots.deleteInfoSpot')}
           </SlimSimpleButton>
         </FormRow>
       </div>
@@ -203,7 +205,7 @@ export const InfoSpotFormFields: FC<Props> = ({
       ) : (
         <Row className="!mt-0 items-center p-5">
           <span data-testid={testIds.noPosters}>
-            <i className="icon-alert mr-2.5 text-hsl-red" aria-hidden />
+            <i className="icon-alert mr-2.5 text-hsl-red" role="presentation" />
             {t('stopDetails.infoSpots.noPosters')}
           </span>
           <AddNewButton
