ee0b0cd7550094bfc0743c1448b77aa49b4e5f43
Jesse Jaara <jesse.jaara@vincit.fi>
2025-04-10 13:59:13 +0300

    Improve <LineRenderLayer> component
    * Add support for more paint properties.
    * Make types readonly.
    * Hoist const data to top level.

--- ui/src/components/map/routes/LineRenderLayer.tsx
+++ ui/src/components/map/routes/LineRenderLayer.tsx
@@ -1,44 +1,48 @@
+import { FC } from 'react';
 import { Layer, Source } from 'react-map-gl/maplibre';
 import { theme } from '../../../generated/theme';
 import { mapGeoJSONtoFeature } from '../../../utils';
 
-export interface LinePaint {
-  'line-color': string;
-  'line-width': number;
-  'line-offset': number;
-}
+export type LinePaint = {
+  readonly 'line-color': string;
+  readonly 'line-dasharray'?: number[];
+  readonly 'line-offset': number;
+  readonly 'line-opacity'?: number;
+  readonly 'line-width': number;
+};
 
-interface LineLayout {
-  'line-join': 'round' | 'bevel' | 'miter' | undefined;
-  'line-cap': 'round' | 'butt' | 'square' | undefined;
-}
-interface Props {
-  layerId: string;
-  geometry: GeoJSON.LineString | GeoJSON.MultiLineString;
-  beforeId?: string;
-  layout?: Partial<LineLayout>;
-  paint?: Partial<LinePaint>;
-}
+type LineLayout = {
+  readonly 'line-join': 'round' | 'bevel' | 'miter' | undefined;
+  readonly 'line-cap': 'round' | 'butt' | 'square' | undefined;
+};
+
+type LineRenderLayerProps = {
+  readonly layerId: string;
+  readonly geometry: GeoJSON.LineString | GeoJSON.MultiLineString;
+  readonly beforeId?: string;
+  readonly layout?: Partial<LineLayout>;
+  readonly paint?: Partial<LinePaint>;
+};
+
+const defaultLayout: LineLayout = {
+  'line-join': 'round',
+  'line-cap': 'round',
+};
+
+const defaultPaint = {
+  'line-color': theme.colors.routes.bus,
+  'line-width': 8,
+  'line-offset': 6,
+};
 
 // this layer renders a static line
-export const LineRenderLayer = ({
+export const LineRenderLayer: FC<LineRenderLayerProps> = ({
   layerId,
   geometry,
   beforeId,
   layout,
   paint,
-}: Props) => {
-  const defaultLayout: LineLayout = {
-    'line-join': 'round',
-    'line-cap': 'round',
-  };
-
-  const defaultPaint = {
-    'line-color': theme.colors.routes.bus,
-    'line-width': 8,
-    'line-offset': 6,
-  };
-
+}) => {
   return (
     <Source type="geojson" data={mapGeoJSONtoFeature(geometry)}>
       <Layer
