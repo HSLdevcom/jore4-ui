aa058b063a613d8cdffe109ce49ddc94ce26a3cc
suvi <suvi.kankare@vincit.fi>
2025-04-22 14:34:28 +0300

    Modify stop area modal design

--- ui/src/components/map/stop-areas/StopAreaForm.tsx
+++ ui/src/components/map/stop-areas/StopAreaForm.tsx
@@ -1,9 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import React, { ForwardRefRenderFunction } from 'react';
-import { FormProvider, UseFormReturn, useForm } from 'react-hook-form';
-import { useTranslation } from 'react-i18next';
+import { FormProvider, useForm } from 'react-hook-form';
 import { twMerge } from 'tailwind-merge';
-import { Column } from '../../../layoutComponents';
 import { mapToISODate } from '../../../time';
 import { EnrichedStopPlace } from '../../../types';
 import { mapLngLatToPoint } from '../../../utils';
@@ -14,21 +12,22 @@ import {
   ValidityPeriodForm,
 } from '../../forms/common';
 import {
-  NameConsistencyChecker,
-  SelectMemberStopsDropdown,
-  TypedName,
-} from '../../forms/stop-area';
-import {
   StopAreaFormState as FormState,
   stopAreaFormSchema,
   stopAreaMemberStopSchema,
 } from '../../forms/stop-area/stopAreaFormSchema';
+import { StopAreaNames } from './StopAreaNames';
 
 const testIds = {
   form: 'StopAreaFormComponent::form',
+  showHideButton: 'StopAreaFormComponent::showHideButton',
   privateCode: 'StopAreaFormComponent::privateCode',
   name: 'StopAreaFormComponent::name',
   nameSwe: 'StopAreaFormComponent::nameSwe',
+  nameLongFin: 'StopAreaFormComponent::nameLongFin',
+  nameLongSwe: 'StopAreaFormComponent::nameLongSwe',
+  abbreviationFin: 'StopAreaFormComponent::abbreviationFin',
+  abbreviationSwe: 'StopAreaFormComponent::abbreviationSwe',
   latitude: 'StopAreaFormComponent::latitude',
   longitude: 'StopAreaFormComponent::longitude',
   quays: 'StopAreaFormComponent::quays',
@@ -71,27 +70,6 @@ export const mapStopAreaDataToFormState = (stopArea: EnrichedStopPlace) => {
   return formState;
 };
 
-function getOverriddenNames(
-  methods: UseFormReturn<FormState>,
-): ReadonlyArray<TypedName> {
-  const [name] = methods.watch(['name']);
-  return [
-    {
-      type: 'TRANSLATION',
-      lang: 'fin',
-      value: name,
-    },
-    // No short name or translations in this form.
-  ];
-}
-
-function getMemberStopIds(
-  methods: UseFormReturn<FormState>,
-): ReadonlyArray<string> {
-  const quays = methods.watch('quays');
-  return quays.map((it) => it.id);
-}
-
 type Props = {
   readonly className?: string;
   readonly defaultValues: Partial<FormState>;
@@ -102,20 +80,12 @@ type Props = {
 const StopAreaFormComponent: ForwardRefRenderFunction<
   HTMLFormElement,
   Props
-> = (
-  { className = '', defaultValues, editedStopAreaId, onSubmit }: Props,
-  ref,
-) => {
-  const { t } = useTranslation();
-
+> = ({ className = '', defaultValues, onSubmit }: Props, ref) => {
   const methods = useForm<FormState>({
     defaultValues,
     resolver: zodResolver(stopAreaFormSchema),
   });
   const { handleSubmit } = methods;
-
-  const memberStopIds = getMemberStopIds(methods);
-  const overriddenNames = getOverriddenNames(methods);
 
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
@@ -126,83 +96,44 @@ const StopAreaFormComponent: ForwardRefRenderFunction<
         onSubmit={handleSubmit(onSubmit)}
         ref={ref}
       >
-        <h3>{t('stopArea.stopArea')}</h3>
-        <FormColumn>
+        <FormColumn className={twMerge('bg-background p-4', className)}>
           <FormRow mdColumns={2}>
-            <Column>
-              <InputField<FormState>
-                type="text"
-                translationPrefix="stopArea"
-                fieldPath="privateCode"
-                testId={testIds.privateCode}
-              />
-            </Column>
-            <Column>
-              <InputField<FormState>
-                type="text"
-                translationPrefix="stopArea"
-                fieldPath="name"
-                testId={testIds.name}
-              />
-            </Column>
-            <Column>
-              <InputField<FormState>
-                type="text"
-                translationPrefix="stopArea"
-                fieldPath="nameSwe"
-                testId={testIds.nameSwe}
-              />
-            </Column>
-          </FormRow>
-          <FormRow mdColumns={4}>
-            <Column>
-              <InputField<FormState>
-                type="number"
-                translationPrefix="map"
-                fieldPath="latitude"
-                testId={testIds.latitude}
-                step="any"
-              />
-            </Column>
-            <Column>
-              <InputField<FormState>
-                type="number"
-                translationPrefix="map"
-                fieldPath="longitude"
-                testId={testIds.longitude}
-                step="any"
-              />
-            </Column>
-            <Column className="col-span-2">
-              <InputField<FormState>
-                fieldPath="quays"
-                translationPrefix="stopArea"
-                testId={testIds.quays}
-                // eslint-disable-next-line react/no-unstable-nested-components
-                inputElementRenderer={({ value, onChange }) => (
-                  <SelectMemberStopsDropdown
-                    disabled
-                    editedStopAreaId={editedStopAreaId}
-                    // The form related component typings have been effed up.
-                    // Everything is typed as a string.
-                    // Cast to Any until the form-typings get fixed (huge rewrite)
-                    value={value as ExplicitAny}
-                    onChange={onChange}
-                  />
-                )}
-              />
-            </Column>
-          </FormRow>
-          <FormRow mdColumns={2}>
-            <Column className="col-start-2">
-              <NameConsistencyChecker.NameAndMembersForm
-                memberStopIds={memberStopIds}
-                stopAreaId={editedStopAreaId}
-                stopAreaNameOverrides={overriddenNames}
-              />
-            </Column>
+            <InputField<FormState>
+              type="text"
+              translationPrefix="stopArea"
+              fieldPath="privateCode"
+              testId={testIds.privateCode}
+            />
+
+            <InputField<FormState>
+              type="text"
+              translationPrefix="stopArea"
+              fieldPath="name"
+              testId={testIds.name}
+            />
           </FormRow>
           <FormRow>
+            <StopAreaNames />
+          </FormRow>
+        </FormColumn>
+        <FormColumn>
+          <FormRow mdColumns={2} className="p-4">
+            <InputField<FormState>
+              type="number"
+              translationPrefix="map"
+              fieldPath="latitude"
+              testId={testIds.latitude}
+              step="any"
+            />
+            <InputField<FormState>
+              type="number"
+              translationPrefix="map"
+              fieldPath="longitude"
+              testId={testIds.longitude}
+              step="any"
+            />
+          </FormRow>
+          <FormRow className="border-t border-light-grey p-4">
             <ValidityPeriodForm />
           </FormRow>
         </FormColumn>
