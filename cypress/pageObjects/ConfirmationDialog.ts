60874fe6184735ea32003046551a8a13198eeb88
suvi <suvi.kankare@vincit.fi>
2025-04-23 12:08:22 +0300

    Fix stop area map tests

--- cypress/pageObjects/ConfirmationDialog.ts
+++ cypress/pageObjects/ConfirmationDialog.ts
@@ -2,4 +2,8 @@ export class ConfirmationDialog {
   getConfirmButton() {
     return cy.getByTestId('ConfirmationDialog::confirmButton');
   }
+
+  getCancelButton() {
+    return cy.getByTestId('ConfirmationDialog::cancelButton');
+  }
 }
