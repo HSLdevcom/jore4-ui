332910d192317b5720b0bd53fd26e11eb3d7d7b9
Teemu Mäkinen <teemu.makinen@vincit.fi>
2025-05-16 13:11:41 +0300

    Update environment variable replace script
    Update replace-environment-variables.sh to accommodate
    new naming format of variables in the app javascript file

--- scripts/docker/replace-environment-variables.sh
+++ scripts/docker/replace-environment-variables.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ -n "$NEXT_PUBLIC_DIGITRANSIT_API_KEY" ]; then
-    sed -i -e "s/[[:alnum:]_]\+\.env\.NEXT_PUBLIC_DIGITRANSIT_API_KEY/\"$NEXT_PUBLIC_DIGITRANSIT_API_KEY\"/g" /usr/share/nginx/html/_next/static/chunks/pages/*.js
+    sed -i -e "s/[[:alnum:]_\$]\+\.env\.NEXT_PUBLIC_DIGITRANSIT_API_KEY/\"$NEXT_PUBLIC_DIGITRANSIT_API_KEY\"/g" /usr/share/nginx/html/_next/static/chunks/pages/*.js
 fi
