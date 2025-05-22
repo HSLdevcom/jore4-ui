bd796b6941eb73226cfcb6548e3fc0e5c74b01e1
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-05-22 16:01:25 +0300

    Simplify useUrlQuery

--- ui/src/types/types.d.ts
+++ ui/src/types/types.d.ts
@@ -47,7 +47,7 @@ type ValueOf<T> = T[keyof T];
 // Trick for fixing a known issue with isArray handling of ReadonlyArrays in Typescript
 // https://github.com/microsoft/TypeScript/issues/17002
 interface ArrayConstructor {
-  isArray(
-    arg: ReadonlyArray<ExplicitAny> | ExplicitAny,
-  ): arg is ReadonlyArray<ExplicitAny>;
+  isArray<T = unknown>(
+    arg: ReadonlyArray<T> | unknown,
+  ): arg is ReadonlyArray<T>;
 }
