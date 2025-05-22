cd7f6c06e25f1c93c4d19079a25fdaa7355ba514
Suvi Kankare <suvi.kankare@vincit.fi>
2025-05-22 12:23:50 +0300

    Add tests for terminal view

--- cypress/pageObjects/stop-registry/index.ts
+++ cypress/pageObjects/stop-registry/index.ts
@@ -8,3 +8,5 @@ export * from './StopGroupSelector';
 export * from './StopSearchBar';
 export * from './StopSearchByLine';
 export * from './StopSearchResultsPage';
+export * from './terminals';
+export * from './TerminalDetailsPage';
