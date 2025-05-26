c9c7d8bf7fad08d14c906d1eddb601f5baa9a7b1
Suvi Kankare <suvi.kankare@vincit.fi>
2025-05-26 13:58:27 +0300

    Create tests for terminal edit

---
+++ cypress/pageObjects/stop-registry/terminals/TerminalLocationDetailsEdit.ts
@@ -0,0 +1,16 @@
+export class TerminalLocationDetailsEdit {
+  getStreetAddress = () =>
+    cy.getByTestId('TerminalLocationDetailsEdit::streetAddress');
+
+  getPostalCode = () =>
+    cy.getByTestId('TerminalLocationDetailsEdit::postalCode');
+
+  getMunicipality = () =>
+    cy.getByTestId('TerminalLocationDetailsEdit::municipality');
+
+  getFareZone = () => cy.getByTestId('TerminalLocationDetailsEdit::fareZone');
+
+  getLatitude = () => cy.getByTestId('TerminalLocationDetailsEdit::latitude');
+
+  getLongitude = () => cy.getByTestId('TerminalLocationDetailsEdit::longitude');
+}
