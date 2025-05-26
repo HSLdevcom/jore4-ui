efc5bacc9833968279b54b76ee9c3f0ee6c0ef6e
Suvi Kankare <suvi.kankare@vincit.fi>
2025-05-26 12:26:32 +0300

    Add hook and schema for terminal editing

---
+++ ui/src/components/stop-registry/terminals/components/basic-details/basic-details-form/useEditTerminalDetails.tsx
@@ -0,0 +1,127 @@
+import compact from 'lodash/compact';
+import { useCallback } from 'react';
+import {
+  StopRegistryNameType,
+  StopRegistryParentStopPlaceInput,
+} from '../../../../../../generated/graphql';
+import { EnrichedParentStopPlace } from '../../../../../../types';
+import { patchAlternativeNames, patchKeyValues } from '../../../../../../utils';
+import { useUpsertTerminal } from '../../../useUpsertTerminal';
+import { TerminalFormState } from './schema';
+
+type UpsertTerminalInputs = {
+  readonly terminal: EnrichedParentStopPlace;
+  readonly state: TerminalFormState;
+};
+
+const mapFormStateToInput = ({
+  terminal,
+  state,
+}: UpsertTerminalInputs): StopRegistryParentStopPlaceInput => {
+  const { id } = terminal;
+
+  return {
+    id,
+    alternativeNames: patchAlternativeNames(terminal, [
+      {
+        name: { lang: 'swe', value: state.nameSwe },
+        nameType: StopRegistryNameType.Translation,
+      },
+      {
+        name: { lang: 'eng', value: state.nameEng },
+        nameType: StopRegistryNameType.Translation,
+      },
+      {
+        name: { lang: 'swe', value: state.abbreviationSwe },
+        nameType: StopRegistryNameType.Other,
+      },
+      {
+        name: { lang: 'fin', value: state.abbreviationFin },
+        nameType: StopRegistryNameType.Other,
+      },
+      {
+        name: { lang: 'eng', value: state.abbreviationEng },
+        nameType: StopRegistryNameType.Other,
+      },
+      {
+        name: { lang: 'fin', value: state.nameLongFin },
+        nameType: StopRegistryNameType.Alias,
+      },
+      {
+        name: { lang: 'swe', value: state.nameLongSwe },
+        nameType: StopRegistryNameType.Alias,
+      },
+      {
+        name: { lang: 'eng', value: state.nameLongEng },
+        nameType: StopRegistryNameType.Alias,
+      },
+    ]),
+    privateCode: {
+      value: state.privateCode,
+      type: 'HSL/JORE-4',
+    },
+    name: {
+      value: state.name,
+      lang: 'fin',
+    },
+    description: {
+      value: state.description.value ?? '',
+      lang: state.description.lang ?? 'fin',
+    },
+    validBetween: null,
+    keyValues: patchKeyValues(
+      terminal,
+      compact([
+        state.terminalType
+          ? {
+              key: 'terminalType',
+              values: [state.terminalType],
+            }
+          : undefined,
+        state.departurePlatforms
+          ? {
+              key: 'departurePlatforms',
+              values: [state.departurePlatforms],
+            }
+          : undefined,
+        state.arrivalPlatforms
+          ? {
+              key: 'arrivalPlatforms',
+              values: [state.arrivalPlatforms],
+            }
+          : undefined,
+        state.loadingPlatforms
+          ? {
+              key: 'loadingPlatforms',
+              values: [state.loadingPlatforms],
+            }
+          : undefined,
+        state.electricCharging
+          ? {
+              key: 'electricCharging',
+              values: [state.electricCharging],
+            }
+          : undefined,
+      ]),
+    ),
+  };
+};
+
+export const useUpsertTerminalDetails = () => {
+  const { initializeTerminal, upsertTerminal, defaultErrorHandler } =
+    useUpsertTerminal();
+
+  const upsertTerminalDetails = useCallback(
+    async (inputs: UpsertTerminalInputs) => {
+      const input = mapFormStateToInput(inputs);
+      return upsertTerminal(input);
+    },
+    [upsertTerminal],
+  );
+
+  return {
+    initializeTerminal,
+    upsertTerminalDetails,
+    defaultErrorHandler,
+  };
+};
---
+++ ui/src/components/stop-registry/terminals/useUpsertTerminal.tsx
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  StopRegistryParentStopPlaceInput,
+  useUpsertTerminalMutation,
+} from '../../../generated/graphql';
+import { EnrichedParentStopPlace } from '../../../types';
+import { showDangerToastWithError } from '../../../utils';
+import { getEnrichedParentStopPlace } from './useGetTerminalDetails';
+
+const GQL_UPSERT_TERMINAL = gql`
+  mutation UpsertTerminal($input: stop_registry_ParentStopPlaceInput!) {
+    stop_registry {
+      mutateParentStopPlace(ParentStopPlace: $input) {
+        ...parent_stop_place_details
+      }
+    }
+  }
+`;
+
+const initializeTerminal = (
+  terminalLocation: GeoJSON.Point,
+): EnrichedParentStopPlace => {
+  return {
+    geometry: {
+      coordinates: terminalLocation.coordinates,
+    },
+  };
+};
+
+export const useUpsertTerminal = () => {
+  const { t } = useTranslation();
+  const [upsertTerminalMutation] = useUpsertTerminalMutation({
+    awaitRefetchQueries: true,
+    refetchQueries: ['getParentStopPlaceDetails'],
+  });
+
+  const upsertTerminal = useCallback(
+    async (input: StopRegistryParentStopPlaceInput) => {
+      const result = await upsertTerminalMutation({
+        variables: { input },
+      });
+
+      return getEnrichedParentStopPlace(
+        result.data?.stop_registry?.mutateParentStopPlace?.at(0),
+      );
+    },
+    [upsertTerminalMutation],
+  );
+
+  const defaultErrorHandler = (err: unknown) => {
+    showDangerToastWithError(t('errors.saveFailed'), err);
+  };
+
+  return {
+    initializeTerminal,
+    upsertTerminal,
+    defaultErrorHandler,
+  };
+};
