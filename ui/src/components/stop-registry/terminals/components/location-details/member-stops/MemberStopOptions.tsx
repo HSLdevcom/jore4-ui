862f0b61bf28acbb8eb310e0eb9ca5b7b563251a
Suvi Kankare <suvi.kankare@vincit.fi>
2025-06-04 11:44:51 +0300

    Create dropdown for selecting member stops

---
+++ ui/src/components/stop-registry/terminals/components/location-details/member-stops/MemberStopOptions.tsx
@@ -0,0 +1,35 @@
+import { Combobox as HUICombobox } from '@headlessui/react';
+import { FC } from 'react';
+import { MdOutlineAddCircle } from 'react-icons/md';
+import { mapToShortDate } from '../../../../../../time';
+import { SelectedStop } from '../location-details-form/schema';
+
+const testIds = {
+  option: 'MemberStopOptions::option',
+};
+
+type MemberStopOptionsProps = {
+  readonly options: ReadonlyArray<SelectedStop>;
+};
+
+export const MemberStopOptions: FC<MemberStopOptionsProps> = ({ options }) => {
+  return options.map((stop) => (
+    <HUICombobox.Option
+      as="div"
+      key={stop.quayId}
+      value={stop}
+      className="flex cursor-pointer items-center border-b p-2 text-left focus:outline-none ui-active:bg-dark-grey ui-active:text-white"
+      data-testid={testIds.option}
+    >
+      <span>
+        <strong>{stop.publicCode}</strong> {stop.name}
+        <br />
+        {mapToShortDate(stop.validityStart)}
+        <span className="mx-1">-</span>
+        {mapToShortDate(stop.validityEnd)}
+      </span>
+      <div className="flex-grow" />
+      <MdOutlineAddCircle className="text-hsl mx-1 text-2xl text-brand" />
+    </HUICombobox.Option>
+  ));
+};
