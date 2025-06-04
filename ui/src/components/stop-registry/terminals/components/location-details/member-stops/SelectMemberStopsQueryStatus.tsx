8f42ffcba917952b226e70be601de9681a63e263
Suvi Kankare <suvi.kankare@vincit.fi>
2025-06-04 11:44:51 +0300

    Create dropdown for selecting member stops

---
+++ ui/src/components/stop-registry/terminals/components/location-details/member-stops/SelectMemberStopsQueryStatus.tsx
@@ -0,0 +1,44 @@
+import { Combobox as HUICombobox } from '@headlessui/react';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { SelectedStop } from '../location-details-form/schema';
+
+export const FETCH_MORE_OPTION: SelectedStop = {
+  id: 'FETCH_MORE',
+  name: '',
+  quayId: '',
+  publicCode: '',
+  validityStart: '',
+  validityEnd: '',
+  indefinite: false,
+};
+
+type SelectMemberStopQueryStatusProps = {
+  readonly allFetched: boolean;
+  readonly loading: boolean;
+  readonly query: string;
+};
+
+export const SelectMemberStopQueryStatus: FC<
+  SelectMemberStopQueryStatusProps
+> = ({ allFetched, loading, query }) => {
+  const { t } = useTranslation();
+
+  if (query === '' || allFetched) {
+    return null;
+  }
+
+  if (loading) {
+    return <p>{t('search.searching')}</p>;
+  }
+
+  return (
+    <HUICombobox.Option
+      as="div"
+      value={FETCH_MORE_OPTION}
+      className="cursor-pointer border-b p-2 text-left font-bold focus:outline-none ui-active:bg-dark-grey ui-active:text-white"
+    >
+      {t('search.showMore')}
+    </HUICombobox.Option>
+  );
+};
