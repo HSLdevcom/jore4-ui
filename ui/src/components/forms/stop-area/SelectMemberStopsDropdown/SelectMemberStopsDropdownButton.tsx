06ad4facc6dedfbee439a51a940fefe76d43d68a
Suvi Kankare <suvi.kankare@vincit.fi>
2025-06-10 16:30:49 +0300

    Update dropdown

--- ui/src/components/forms/stop-area/SelectMemberStopsDropdown/SelectMemberStopsDropdownButton.tsx
+++ ui/src/components/forms/stop-area/SelectMemberStopsDropdown/SelectMemberStopsDropdownButton.tsx
@@ -1,37 +1,18 @@
 import { Combobox as HUICombobox } from '@headlessui/react';
 import { FC } from 'react';
 import { MdSearch } from 'react-icons/md';
-import { StopAreaFormMember } from '../stopAreaFormSchema';
 
 const testIds = {
   button: 'SelectMemberStopsDropdownButton',
 };
 
-type SelectMemberStopsDropdownButtonProps = {
-  readonly selected: ReadonlyArray<StopAreaFormMember>;
-};
-
-export const SelectMemberStopsDropdownButton: FC<
-  SelectMemberStopsDropdownButtonProps
-> = ({ selected }) => {
+export const SelectMemberStopsDropdownButton: FC = () => {
   return (
     <HUICombobox.Button
       data-testid={testIds.button}
-      className="absolute inset-y-0 right-0 flex h-full w-full items-center justify-end px-3 text-left focus:outline-none"
+      className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none"
     >
-      <span className="hidden ui-not-open:block">
-        {selected.map((stop, i) => (
-          <span
-            key={stop.id}
-            title={`${stop.scheduled_stop_point.label}: ${stop.name.value}`}
-          >
-            {stop.scheduled_stop_point.label}
-            {i < selected.length - 1 ? ', ' : ''}
-          </span>
-        ))}
-      </span>
-
-      <MdSearch className="ml-auto mr-1 text-2xl text-tweaked-brand" />
+      <MdSearch className="mr-1 text-2xl text-tweaked-brand" />
       <i
         className="icon-arrow text-tweaked-brand transition duration-150 ease-in-out ui-open:-rotate-180 ui-not-open:rotate-0"
         style={{ fontSize: 10 }}
