fb76e3ed9799374597fb95111041f6fe02cf8a77
Suvi Kankare <suvi.kankare@vincit.fi>
2025-06-12 10:38:42 +0300

    Get more information from stop versions

--- ui/src/components/stop-registry/stops/versions/types/StopVersion.ts
+++ ui/src/components/stop-registry/stops/versions/types/StopVersion.ts
@@ -5,8 +5,10 @@ import { StopVersionStatus } from './StopVersionStatus';
 
 export type StopVersion = {
   readonly id: number;
+  readonly public_code: string;
   readonly netex_id: string;
   readonly stop_place_netex_id: string;
+  readonly stop_place_name: string;
   readonly validity_start: DateTime;
   readonly validity_end: DateTime | null;
   readonly priority: Priority;
