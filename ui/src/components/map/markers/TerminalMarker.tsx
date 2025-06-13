61715b6118b8810178d03ee4c6658acb47478f79
Jesse Jaara <jesse.jaara@vincit.fi>
2025-06-13 16:17:56 +0300

    Create base Map Terminal components

---
+++ ui/src/components/map/markers/TerminalMarker.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+
+type TerminalMarkerProps = {
+  readonly selected?: boolean;
+  readonly isPlaceholder?: boolean;
+  readonly size?: number;
+  readonly testId?: string;
+};
+
+export const TerminalMarker: FC<TerminalMarkerProps> = ({
+  selected = false,
+  isPlaceholder = false,
+  size = 30,
+  testId,
+}) => {
+  const width = `${size}px`;
+  const fontSize = `${size + 15}px`;
+
+  return (
+    <div
+      data-placeholder={isPlaceholder}
+      data-selected={selected}
+      data-testid={testId}
+      className="flex items-center justify-center rounded bg-tweaked-brand text-white hover:bg-tweaked-brand-darker30 data-[placeholder=true]:bg-dark-grey data-[selected=true]:bg-hsl-dark-80"
+      style={{ width, height: width, fontSize }}
+      aria-hidden
+    >
+      <i className="icon-bus" />
+    </div>
+  );
+};
---
+++ ui/src/components/map/terminals/Terminal.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import { Marker } from 'react-map-gl/maplibre';
+import { MapEntityEditorViewState } from '../../../redux';
+import { getGeometryPoint } from '../../../utils';
+import { TerminalMarker } from '../markers';
+import { MapTerminal } from '../types';
+
+const testIds = {
+  terminal: ({ private_code_value: id }: MapTerminal) =>
+    `Map::MapTerminal::terminal::${id}`,
+};
+
+type TerminalProps = {
+  readonly terminal: MapTerminal;
+  readonly mapTerminalViewState: MapEntityEditorViewState;
+  readonly onClick: (area: MapTerminal) => void;
+  readonly selected: boolean;
+};
+
+export const Terminal: FC<TerminalProps> = ({
+  terminal,
+  mapTerminalViewState,
+  selected,
+  onClick,
+}) => {
+  // If the terminal is being moved, we use different styles for the stop
+  // to indicate the placeholder of the old location
+  const isPlaceholder =
+    selected && mapTerminalViewState === MapEntityEditorViewState.MOVE;
+
+  const point = getGeometryPoint(terminal.centroid);
+  if (!point) {
+    return null;
+  }
+
+  return (
+    <Marker
+      className="z-[2]"
+      longitude={point.longitude}
+      latitude={point.latitude}
+      onClick={() => onClick(terminal)}
+    >
+      <TerminalMarker
+        selected={selected}
+        isPlaceholder={isPlaceholder}
+        testId={testIds.terminal(terminal)}
+      />
+    </Marker>
+  );
+};
