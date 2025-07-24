23e1e12ab2794c8df9142056b3d5d0f3dc3fb0cb
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-07-24 14:44:01 +0300

    Implement E2E tests for creating terminal on map

--- cypress/pageObjects/MapModal.ts
+++ cypress/pageObjects/MapModal.ts
@@ -8,6 +8,7 @@ import { RouteStopsOverlay } from './RouteStopsOverlay';
 import { StopAreaForm } from './StopAreaForm';
 import { StopAreaPopup } from './StopAreaPopup';
 import { NewStopFormInfo, StopForm } from './StopForm';
+import { NewTerminalFormInfo, TerminalForm } from './TerminalForm';
 import { Toast } from './Toast';
 
 export class MapModal {
@@ -18,6 +19,8 @@ export class MapModal {
   routePropertiesForm = new RoutePropertiesForm();
 
   stopForm = new StopForm();
+
+  terminalForm = new TerminalForm();
 
   stopAreaPopup = new StopAreaPopup();
 
@@ -53,13 +56,43 @@ export class MapModal {
     this.stopForm.save();
   };
 
+  /**
+   * This creates terminal at a location that is specified by percentages of the viewport'sg width and height.
+   */
+  createTerminalAtLocation = ({
+    terminalFormInfo,
+    clickRelativePoint,
+  }: {
+    terminalFormInfo: NewTerminalFormInfo;
+    clickRelativePoint: { xPercentage: number; yPercentage: number };
+  }) => {
+    this.mapFooter.addTerminal();
+
+    this.map.clickRelativePoint(
+      clickRelativePoint.xPercentage,
+      clickRelativePoint.yPercentage,
+    );
+
+    this.terminalForm.fillFormForNewTerminal(terminalFormInfo);
+
+    this.terminalForm.save();
+  };
+
   checkStopSubmitSuccessToast() {
     this.toast.expectSuccessToast('Pysäkki luotu');
+  }
+
+  checkTerminalSubmitSuccessToast() {
+    this.toast.expectSuccessToast('Terminaali luotu');
   }
 
   gqlStopShouldBeCreatedSuccessfully() {
     expectGraphQLCallToSucceed('@gqlInsertStopPoint');
     expectGraphQLCallToSucceed('@gqlInsertQuayIntoStopPlace');
+  }
+
+  gqlTerminalShouldBeCreatedSuccessfully() {
+    expectGraphQLCallToSucceed('@gqlCreateTerminal');
   }
 
   getCloseButton() {
---
+++ cypress/pageObjects/TerminalForm.ts
@@ -0,0 +1,99 @@
+import { ChangeValidityForm } from './ChangeValidityForm';
+import { CreateTimingPlaceForm } from './CreateTimingPlaceForm';
+import { PriorityForm } from './PriorityForm';
+import { ValidityPeriodFormInfo } from './ValidityPeriodForm';
+
+export interface BaseTerminalFormInfo extends ValidityPeriodFormInfo {
+  name?: string;
+  nameSwe?: string;
+  longitude?: string;
+  latitude?: string;
+  timingPlace?: string;
+  versionName?: string;
+  versionDescription?: string;
+}
+
+export interface NewTerminalFormInfo extends BaseTerminalFormInfo {
+  privateCode: string;
+  stops: string[];
+}
+
+export class TerminalForm {
+  changeValidityForm = new ChangeValidityForm();
+
+  createTimingPlaceForm = new CreateTimingPlaceForm();
+
+  priorityForm = new PriorityForm();
+
+  getPrivateCodeInput() {
+    return cy.getByTestId('TerminalFormComponent::privateCode');
+  }
+
+  getLongitudeInput() {
+    return cy.getByTestId('TerminalFormComponent::longitude');
+  }
+
+  getLatitudeInput() {
+    return cy.getByTestId('TerminalFormComponent::latitude');
+  }
+
+  getNameInput() {
+    return cy.getByTestId('TerminalFormComponent::name');
+  }
+
+  getNameSweInput() {
+    return cy.getByTestId('TerminalFormComponent::nameSwe');
+  }
+
+  showTerminalNameForm() {
+    return cy.getByTestId('TerminalFormComponent::showHideButton').click();
+  }
+
+  getModal() {
+    return cy.getByTestId('EditTerminalModal');
+  }
+
+  fillBaseForm(values: BaseTerminalFormInfo) {
+    if (values.name) {
+      this.getNameInput().clearAndType(values.name);
+    }
+
+    this.showTerminalNameForm();
+
+    if (values.nameSwe) {
+      this.getNameSweInput().clearAndType(values.nameSwe);
+    }
+
+    if (values.latitude) {
+      this.getLatitudeInput().clearAndType(values.latitude);
+    }
+
+    if (values.longitude) {
+      this.getLongitudeInput().clearAndType(values.longitude);
+    }
+
+    this.changeValidityForm.validityPeriodForm.fillForm(values);
+  }
+
+  fillFormForNewTerminal(values: NewTerminalFormInfo) {
+    this.getPrivateCodeInput().clearAndType(values.privateCode);
+    this.fillBaseForm(values);
+
+    values.stops.forEach((stop) => {
+      cy.getByTestId('SelectMemberStopsDropdownButton').click();
+      cy.getByTestId('SelectMemberStopsDropdown::input').clearAndType(stop);
+      cy.getByTestId('MemberStopOptions::option').click();
+    });
+  }
+
+  /** Clicks the Edit stop modal's save button. Can be given forceAction = true
+   * to force the click without waiting for it's actionability (If it's covered
+   * by another element etc.).
+   * https://docs.cypress.io/api/commands/click#Arguments
+   */
+  save(forceAction = false) {
+    return this.getModal()
+      .findByTestId('Modal::saveButton')
+      .click({ force: forceAction });
+  }
+}
