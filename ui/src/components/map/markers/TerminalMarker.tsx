0044fca4a3fbf0ae584bf7fa0a3415542a02e899
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-08-01 13:59:50 +0300

    Make old terminal location icon correct style when moving terminal

--- ui/src/components/map/markers/TerminalMarker.tsx
+++ ui/src/components/map/markers/TerminalMarker.tsx
@@ -25,13 +25,16 @@ export const TerminalMarker: FC<TerminalMarkerProps> = ({
       data-placeholder={isPlaceholder}
       data-selected={selected}
       data-testid={testId}
-      className="flex cursor-pointer items-center justify-center rounded bg-tweaked-brand text-white hover:bg-tweaked-brand-darker30 data-[placeholder=true]:bg-dark-grey data-[selected=true]:bg-background-hsl-button-selected"
+      className="flex cursor-pointer items-center justify-center rounded bg-tweaked-brand text-white hover:bg-tweaked-brand-darker30 data-[placeholder=true]:border data-[placeholder=true]:border-dashed data-[placeholder=true]:border-grey data-[placeholder=true]:bg-white data-[selected=true]:bg-background-hsl-button-selected"
       style={{ width, height: width, fontSize }}
       aria-hidden
       onClick={onClick}
       title={title}
     >
-      <i className="icon-bus" />
+      <i
+        data-placeholder={isPlaceholder}
+        className="icon-bus data-[placeholder=true]:text-grey"
+      />
     </div>
   );
 };
