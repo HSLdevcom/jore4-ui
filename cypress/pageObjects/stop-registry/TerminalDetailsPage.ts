911ee36ee67c2ad839ceffe744afa73a87e149cd
Suvi Kankare <suvi.kankare@vincit.fi>
2025-08-15 11:16:27 +0300

    Add tests for terminal stops tab

--- cypress/pageObjects/stop-registry/TerminalDetailsPage.ts
+++ cypress/pageObjects/stop-registry/TerminalDetailsPage.ts
@@ -31,4 +31,27 @@ export class TerminalDetailsPage {
   validityPeriod() {
     return cy.getByTestId('TerminalVersioningRow::validityPeriod');
   }
+
+  getTabSelector() {
+    return {
+      getStopsTab: () => cy.getByTestId('TerminalDetailsPage::stopsTabButton'),
+      getBasicDetailsTab: () =>
+        cy.getByTestId('TerminalDetailsPage::basicDetailsTabButton'),
+    };
+  }
+
+  getStopsSection() {
+    return {
+      getTitle: () => cy.getByTestId('TerminalDetailsPage::stopsTitle'),
+      getStopAreas: () =>
+        cy.getByTestId('TerminalDetailsPage::stopAreaSection'),
+      getNthStopArea(index: number) {
+        return this.getStopAreas().eq(index);
+      },
+      getStopAreaHeader: () =>
+        cy.getByTestId('TerminalDetailsPage::stopAreaHeader'),
+      getStopAreaStopsTable: () =>
+        cy.getByTestId('TerminalDetailsPage::stopAreaStopsTable'),
+    };
+  }
 }
