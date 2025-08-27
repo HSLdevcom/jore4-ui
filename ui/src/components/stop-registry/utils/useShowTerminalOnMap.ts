c50887c2277a0f3136b3e5f221b09e7947688c69
Suvi Kankare <suvi.kankare@vincit.fi>
2025-08-27 15:30:15 +0300

    Add map link to terminal view

---
+++ ui/src/components/stop-registry/utils/useShowTerminalOnMap.ts
@@ -0,0 +1,47 @@
+import {
+  useAppDispatch,
+  useMapQueryParams,
+  useObservationDateQueryParam,
+} from '../../../hooks';
+import {
+  FilterType,
+  MapEntityEditorViewState,
+  resetMapState,
+  setMapTerminalViewStateAction,
+  setSelectedTerminalIdAction,
+  setStopFilterAction,
+} from '../../../redux';
+import { Point } from '../../../types';
+
+export function useShowTerminalOnMap() {
+  const dispatch = useAppDispatch();
+
+  // Get existing observationDate or default, but don't touch the URL
+  const { observationDate } = useObservationDateQueryParam({
+    initialize: false,
+  });
+  const { openMapWithParameters } = useMapQueryParams();
+
+  return (id: string | undefined, point: Point) => {
+    dispatch(resetMapState()).then(() => {
+      dispatch(setSelectedTerminalIdAction(id));
+      dispatch(setMapTerminalViewStateAction(MapEntityEditorViewState.POPUP));
+      dispatch(
+        setStopFilterAction({
+          filterType: FilterType.ShowAllBusStops,
+          isActive: false,
+        }),
+      );
+
+      openMapWithParameters({
+        viewPortParams: {
+          latitude: point.latitude,
+          longitude: point.longitude,
+          zoom: 18,
+        },
+        observationDate,
+        displayedRouteParams: {},
+      });
+    });
+  };
+}
