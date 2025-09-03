9e3f4592f0dd3fb6432e0feab4dc0e5614f9e1e6
Suvi Kankare <suvi.kankare@vincit.fi>
2025-09-03 15:06:03 +0300

    Create search view for terminals

---
+++ ui/src/components/stop-registry/search/for-terminals/TerminalSearchNoStops.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { MdOpenInNew } from 'react-icons/md';
+import { Link } from 'react-router';
+import { useObservationDateQueryParam } from '../../../../hooks';
+import { Path, routeDetails } from '../../../../router/routeDetails';
+import { FindStopPlaceInfo } from '../components/StopPlaceSharedComponents/useFindStopPlaces';
+
+const testIds = {
+  noStopsInTerminal: 'TerminalSearch:noStopsText',
+  terminalLink: 'TerminalSearch::noStopsLink',
+};
+
+type TerminalSearchNoStopsProps = {
+  readonly stopPlace: FindStopPlaceInfo;
+};
+
+export const TerminalSearchNoStops: FC<TerminalSearchNoStopsProps> = ({
+  stopPlace: terminal,
+}) => {
+  const { t } = useTranslation();
+
+  const { observationDate } = useObservationDateQueryParam({
+    initialize: false,
+  });
+
+  return (
+    <div
+      className="ml-2 mt-4 flex w-full flex-row items-center gap-1"
+      data-testid={testIds.noStopsInTerminal}
+    >
+      <i className="icon-info text-2xl text-brand" />
+      <p>{t('terminalDetails.noStops')}</p>
+      <Link
+        to={routeDetails[Path.terminalDetails].getLink(terminal.private_code, {
+          observationDate,
+        })}
+        data-testid={testIds.terminalLink}
+        title={t('accessibility:terminals.showTerminalDetails', {
+          terminalLabel: terminal.name_value,
+        })}
+        className="flex flex-row items-center gap-1 font-bold"
+      >
+        <span>{t('stopRegistrySearch.goToTerminalDetails')}</span>
+        <MdOpenInNew />
+      </Link>
+    </div>
+  );
+};
