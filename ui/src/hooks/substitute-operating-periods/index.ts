bc4d6a0c1b443b8769f7214a17fca6cb44dfb0d0
Suvi Kankare <suvi.kankare@vincit.fi>
2025-09-11 11:04:40 +0300

    Move substitute period hooks to related folders

--- ui/src/hooks/substitute-operating-periods/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './useDeleteSubstituteOperatingPeriod';
-export * from './useEditSubstituteOperatingPeriod';
-export * from './useGetSubstituteOperatingPeriod';
-export * from './useCreateSubstituteOperatingPeriod';
