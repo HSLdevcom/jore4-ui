79b733e0efb01cb44687791f98893f82235107fa
Jesse Jaara <jesse.jaara@vincit.fi>
2025-09-11 17:45:06 +0300

    StopSearch: Map new filters to query where conditions

--- ui/src/components/stop-registry/search/by-stop/filtersToQueryVariables.ts
+++ ui/src/components/stop-registry/search/by-stop/filtersToQueryVariables.ts
@@ -3,11 +3,12 @@ import { StopsDatabaseQuayNewestVersionBoolExp } from '../../../../generated/gra
 import { knownPriorityValues } from '../../../../types/enums';
 import {
   AllOptionEnum,
+  NullOptionEnum,
   buildOptionalSearchConditionGqlFilter,
   mapToSqlLikeValue,
 } from '../../../../utils';
 import { buildSearchStopByLabelOrNameFilter } from '../../utils/buildSearchStopByLabelOrNameFilter';
-import { SearchBy, StopSearchFilters } from '../types';
+import { InfoSpotSize, SearchBy, StopSearchFilters } from '../types';
 
 function buildAddressLikeFilter(
   value: string,
@@ -78,6 +79,129 @@ function buildSearchStopsPriorityFilter({
   };
 }
 
+function buildTransportationModeFilter({
+  transportationMode,
+}: StopSearchFilters): StopsDatabaseQuayNewestVersionBoolExp {
+  if (transportationMode.includes(AllOptionEnum.All)) {
+    return {};
+  }
+
+  return {
+    stop_place: {
+      transport_mode: {
+        // In DB tiamat stores the enum valu in ALL UPPERCASE
+        _in: transportationMode.map((it) => it.toUpperCase()),
+      },
+    },
+  };
+}
+
+function buildStopStateFilter({
+  stopState,
+}: StopSearchFilters): StopsDatabaseQuayNewestVersionBoolExp {
+  if (stopState.includes(AllOptionEnum.All)) {
+    return {};
+  }
+
+  return { stop_state: { _in: stopState } };
+}
+
+function buildShelterFilter({
+  shelter,
+}: StopSearchFilters): StopsDatabaseQuayNewestVersionBoolExp {
+  if (shelter.includes(AllOptionEnum.All)) {
+    return {};
+  }
+
+  if (shelter.includes(NullOptionEnum.Null)) {
+    return {
+      _not: {
+        equipments: { installed: { dtype: { _eq: 'ShelterEquipment' } } },
+      },
+    };
+  }
+
+  return {
+    equipments: {
+      installed: {
+        dtype: { _eq: 'ShelterEquipment' },
+        shelter_type: { _in: shelter },
+      },
+    },
+  };
+}
+
+function buildElectricityFilter({
+  electricity,
+}: StopSearchFilters): StopsDatabaseQuayNewestVersionBoolExp {
+  if (electricity.includes(AllOptionEnum.All)) {
+    return {};
+  }
+
+  if (electricity.includes(NullOptionEnum.Null)) {
+    return {
+      _or: [
+        // Has no shelter at all
+        {
+          _not: {
+            equipments: { installed: { dtype: { _eq: 'ShelterEquipment' } } },
+          },
+        },
+        // Or no shelters with electricity
+        {
+          _not: {
+            equipments: {
+              installed: {
+                dtype: { _eq: 'ShelterEquipment' },
+                shelter_electricity: { _is_null: false },
+              },
+            },
+          },
+        },
+      ],
+    };
+  }
+
+  return {
+    equipments: {
+      installed: {
+        dtype: { _eq: 'ShelterEquipment' },
+        shelter_electricity: { _in: electricity },
+      },
+    },
+  };
+}
+
+function isSizeObject(
+  value: StopSearchFilters['infoSpots'][number],
+): value is InfoSpotSize {
+  return typeof value === 'object';
+}
+
+function buildInfoSpotsFilter({
+  infoSpots,
+}: StopSearchFilters): StopsDatabaseQuayNewestVersionBoolExp {
+  if (infoSpots.includes(AllOptionEnum.All)) {
+    return {};
+  }
+
+  if (infoSpots.includes(NullOptionEnum.Null)) {
+    // Aka, where does not exist
+    return { info_spot_locations: { location_netex_id: { _is_null: true } } };
+  }
+
+  return {
+    info_spot_locations: {
+      info_spot: {
+        _or: infoSpots.filter(isSizeObject).map(({ width, height }) => ({
+          width: { _eq: width },
+          height: { _eq: height },
+        })),
+      },
+    },
+  };
+}
+
 export function buildSearchStopsGqlQueryVariables(
   filters: StopSearchFilters,
 ): StopsDatabaseQuayNewestVersionBoolExp {
@@ -89,10 +213,13 @@ export function buildSearchStopsGqlQueryVariables(
   >(mapToSqlLikeValue(filters.elyNumber), buildElyNumberFilter);
 
   const municipalityFilter = buildSearchStopsMunicipalityFilter(filters);
-
   const observationDateFilter = buildSearchStopsObservationDateFilter(filters);
-
   const priorityFilter = buildSearchStopsPriorityFilter(filters);
+  const transportationModeFilter = buildTransportationModeFilter(filters);
+  const stopStateFilter = buildStopStateFilter(filters);
+  const shelterFilter = buildShelterFilter(filters);
+  const electricityFilter = buildElectricityFilter(filters);
+  const infoSpotsFilter = buildInfoSpotsFilter(filters);
 
   return {
     _and: [
@@ -101,6 +228,11 @@ export function buildSearchStopsGqlQueryVariables(
       municipalityFilter,
       observationDateFilter,
       priorityFilter,
+      transportationModeFilter,
+      stopStateFilter,
+      shelterFilter,
+      electricityFilter,
+      infoSpotsFilter,
     ].filter((filter) => !isEmpty(filter)),
   };
 }
