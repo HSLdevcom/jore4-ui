5bd7966c010ca78dd926ed0710269be1c3117465
Jesse Jaara <jesse.jaara@vincit.fi>
2025-09-18 17:35:37 +0300

    TDBM: Add support for typed Hasura Queries
    * Add types to `hasuraApi` function calls.
    * No actual types for `response.data` or input variables added to old code.
    * Add support for throwing on results containing errors.
    * Throw on invalid query string.

--- test-db-manager/src/builders/mutations/utils.ts
+++ test-db-manager/src/builders/mutations/utils.ts
@@ -1,5 +1,13 @@
 import { DocumentNode } from 'graphql';
 
-export const getGqlString = (doc: DocumentNode) => {
-  return doc.loc && doc.loc.source.body;
-};
+export function getGqlString(doc: DocumentNode): string {
+  const body = doc.loc?.source.body;
+
+  if (!body) {
+    throw new Error(
+      `Unable to locate GraphGL query! Document:\n${JSON.stringify(doc, null, 2)}`,
+    );
+  }
+
+  return body;
+}
