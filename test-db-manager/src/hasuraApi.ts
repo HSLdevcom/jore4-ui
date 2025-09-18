5d4d0d365bbf229143d1175b36bc6fd4634b5350
Jesse Jaara <jesse.jaara@vincit.fi>
2025-09-18 17:35:37 +0300

    TDBM: Add support for typed Hasura Queries
    * Add types to `hasuraApi` function calls.
    * No actual types for `response.data` or input variables added to old code.
    * Add support for throwing on results containing errors.
    * Throw on invalid query string.

--- test-db-manager/src/hasuraApi.ts
+++ test-db-manager/src/hasuraApi.ts
@@ -1,4 +1,6 @@
 import fetch from 'cross-fetch';
+import { DocumentNode } from 'graphql';
+import { getGqlString } from './builders/mutations/utils';
 
 enum HasuraURL {
   Dev = 'http://127.0.0.1:3201/v1/graphql',
@@ -32,14 +34,113 @@ const getHasuraAuthenticationHeaders = (): { [key: string]: string } => {
   };
 };
 
-export const hasuraApi = async (jsonPayload: unknown): Promise<unknown> => {
+export type HasuraStringQueryBody<
+  Variables extends Readonly<Record<string, unknown>>,
+> = {
+  readonly operationName?: string;
+  readonly query: string;
+  readonly variables?: Variables;
+};
+
+export type HasuraDocumentNodeQueryBody<
+  Variables extends Readonly<Record<string, unknown>>,
+> = {
+  readonly query: DocumentNode;
+  readonly variables?: Variables;
+};
+
+export type HasuraQueryBody<
+  Variables extends Readonly<Record<string, unknown>>,
+> = HasuraStringQueryBody<Variables> | HasuraDocumentNodeQueryBody<Variables>;
+
+export type HasuraErrorLocation = {
+  readonly line: number;
+  readonly column: number;
+};
+
+export type HasuraError = {
+  readonly message: string;
+  readonly locations?: ReadonlyArray<HasuraErrorLocation>;
+  readonly path?: ReadonlyArray<string | number>;
+};
+
+export type HasuraResponseBody<Data> = {
+  readonly errors?: ReadonlyArray<HasuraError>;
+  readonly data?: Data;
+};
+
+const contentTypeHeader = 'content-type';
+const contentTypeJson = 'application/json; charset=utf-8';
+
+class HasuraException extends Error {
+  readonly response: HasuraResponseBody<unknown>;
+
+  constructor(response: HasuraResponseBody<unknown>) {
+    super(
+      `Hasura response contains errors! Response: ${JSON.stringify(response, null, 2)}`,
+    );
+    this.response = response;
+  }
+}
+
+function processHasuraRequestBody<
+  Variables extends Readonly<Record<string, unknown>>,
+>(body: HasuraQueryBody<Variables>): HasuraStringQueryBody<Variables> {
+  if (typeof body.query === 'string') {
+    return body as HasuraStringQueryBody<Variables>;
+  }
+
+  return {
+    operationName: body.query.definitions.find(
+      (def) => def.kind === 'OperationDefinition',
+    )?.name?.value,
+    query: getGqlString(body.query),
+    variables: body.variables,
+  };
+}
+
+export async function hasuraApi<
+  Data = unknown,
+  Variables extends Readonly<Record<string, unknown>> = Readonly<
+    Record<string, unknown>
+  >,
+>(
+  requestBody: HasuraQueryBody<Variables>,
+  expectNoErrors = false,
+): Promise<HasuraResponseBody<Data>> {
   const req = {
     method: 'POST',
-    body: JSON.stringify(jsonPayload),
+    body: JSON.stringify(processHasuraRequestBody(requestBody)),
     headers: {
-      'Content-type': 'application/json; charset=UTF-8',
+      [contentTypeHeader]: contentTypeJson,
       ...getHasuraAuthenticationHeaders(),
     },
   };
-  return fetch(getHasuraURL(), req).then((response) => response.json());
-};
+
+  const response = await fetch(getHasuraURL(), req);
+
+  if (
+    !response.ok ||
+    response.headers.get(contentTypeHeader) !== contentTypeJson
+  ) {
+    const responseJson = JSON.stringify(
+      {
+        status: response.status,
+        headers: Object.fromEntries(response.headers.entries()),
+        body: await response.text(),
+      },
+      null,
+      2,
+    );
+
+    throw new Error(`Hasura request failed! Response:  ${responseJson}`);
+  }
+
+  const body: HasuraResponseBody<Data> = await response.json();
+
+  if (expectNoErrors && body.errors?.length) {
+    throw new HasuraException(body);
+  }
+
+  return body;
+}
