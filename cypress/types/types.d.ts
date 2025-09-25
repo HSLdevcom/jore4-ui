15a8b653cca50732e70c1b3b2f7884da42742369
Suvi Kankare <suvi.kankare@vincit.fi>
2025-09-25 13:23:06 +0300

    Create function clickAtCoordinates to map

--- cypress/types/types.d.ts
+++ cypress/types/types.d.ts
@@ -1,1 +1,11 @@
 export type UUID = string;
+
+// Global window interface extension for Cypress testing utilities
+declare global {
+  interface Window {
+    coordinatesToOnScreenPixels?: (
+      longitude: number,
+      latitude: number,
+    ) => { x: number; y: number };
+  }
+}
