f02ee5264c7a65bf998dfd406839e530f4dccc8a
Jesse Jaara <jesse.jaara@vincit.fi>
2025-09-26 15:47:00 +0300

    SubstituteDaySettingsPage: Improve query typings
    * Use actual generated types, instead of force casting the result into
      the full potential type. Not all fields were present in reality →
      Potential for NPEs.
    * Reduce code duplication → Single base hook implementation, with just
      proper query params given.
    * Inline query params into the query itself, instead of generation
      raw-where conditions → Makes the query more readable in Network tab,
      and Apollo's cache.

--- ui/src/components/timetables/substitute-day-settings/hooks/useGetSubstituteOperatingPeriod.ts
+++ ui/src/components/timetables/substitute-day-settings/hooks/useGetSubstituteOperatingPeriod.ts
@@ -1,100 +1,80 @@
 import { gql } from '@apollo/client';
+import compact from 'lodash/compact';
 import { DateTime } from 'luxon';
+import { useMemo } from 'react';
 import {
-  GetSubstituteOperatingPeriodsQuery,
-  TimetablesServiceCalendarSubstituteOperatingPeriod,
+  GetSubstituteOperatingPeriodsQueryVariables,
   useGetSubstituteOperatingPeriodsQuery,
 } from '../../../../generated/graphql';
-import {
-  buildActiveDateRangeGqlFilterForSubstituteOperatingPeriods,
-  buildIsPresetSubstituteOperatingPeriodFilter,
-} from '../../../../utils';
 
 const GQL_GET_SUBSTITUTE_OPERATING_PERIODS = gql`
   query GetSubstituteOperatingPeriods(
-    $periodFilters: timetables_service_calendar_substitute_operating_period_bool_exp
+    $startDate: date!
+    $endDate: date!
+    $isPreset: Boolean!
   ) {
     timetables {
       timetables_service_calendar_substitute_operating_period(
-        where: $periodFilters
-      ) {
-        period_name
-        is_preset
-        substitute_operating_period_id
-        substitute_operating_day_by_line_types {
-          begin_time
-          end_time
-          substitute_day_of_week
-          substitute_operating_day_by_line_type_id
-          superseded_date
-          type_of_line
+        where: {
+          substitute_operating_day_by_line_types: {
+            _and: [
+              { superseded_date: { _gte: $startDate } }
+              { superseded_date: { _lte: $endDate } }
+            ]
+          }
+          is_preset: { _eq: $isPreset }
         }
+      ) {
+        ...SubstituteOperatingPeriodSettingsInfo
       }
+    }
+  }
+
+  fragment SubstituteOperatingPeriodSettingsInfo on timetables_service_calendar_substitute_operating_period {
+    period_name
+    is_preset
+    substitute_operating_period_id
+    substitute_operating_day_by_line_types {
+      begin_time
+      end_time
+      substitute_day_of_week
+      substitute_operating_day_by_line_type_id
+      superseded_date
+      type_of_line
     }
   }
 `;
 
-const mapSubstituteOperatingPeriodsResult = (
-  result?: GetSubstituteOperatingPeriodsQuery,
-) => {
-  return result?.timetables
-    ?.timetables_service_calendar_substitute_operating_period as ReadonlyArray<TimetablesServiceCalendarSubstituteOperatingPeriod>;
+type DateRange = {
+  readonly startDate: DateTime;
+  readonly endDate: DateTime;
 };
 
-export const useGetCommonSubstituteOperatingPeriods = ({
-  startDate,
-  endDate,
-}: {
-  startDate: DateTime;
-  endDate: DateTime;
-}) => {
-  const periodDateRangeFilter = {
-    ...buildActiveDateRangeGqlFilterForSubstituteOperatingPeriods(
-      startDate,
-      endDate,
-    ),
-  };
-
-  const commonPeriodFilter = {
-    ...periodDateRangeFilter,
-    ...buildIsPresetSubstituteOperatingPeriodFilter(true),
-  };
-
+function useGetSubstituteOperatingPeriods(
+  variables: GetSubstituteOperatingPeriodsQueryVariables,
+) {
   const { data, ...rest } = useGetSubstituteOperatingPeriodsQuery({
-    variables: { periodFilters: commonPeriodFilter },
+    variables,
   });
 
-  const commonSubstituteOperatingPeriods =
-    mapSubstituteOperatingPeriodsResult(data);
+  const substitutePeriods = useMemo(
+    () =>
+      compact(
+        data?.timetables
+          ?.timetables_service_calendar_substitute_operating_period,
+      ),
+    [data],
+  );
 
-  return { ...rest, commonSubstituteOperatingPeriods };
-};
+  return { ...rest, substitutePeriods };
+}
 
-export const useGetOccasionalSubstituteOperatingPeriods = ({
-  startDate,
-  endDate,
-}: {
-  startDate: DateTime;
-  endDate: DateTime;
-}) => {
-  const periodDateRangeFilter = {
-    ...buildActiveDateRangeGqlFilterForSubstituteOperatingPeriods(
-      startDate,
-      endDate,
-    ),
-  };
+export function useGetCommonSubstituteOperatingPeriods(dateRange: DateRange) {
+  return useGetSubstituteOperatingPeriods({ ...dateRange, isPreset: true });
+}
 
-  const occasionalPeriodFilter = {
-    ...periodDateRangeFilter,
-    ...buildIsPresetSubstituteOperatingPeriodFilter(false),
-  };
-
-  const { data, ...rest } = useGetSubstituteOperatingPeriodsQuery({
-    variables: { periodFilters: occasionalPeriodFilter },
-  });
-
-  const occasionalSubstituteOperatingPeriods =
-    mapSubstituteOperatingPeriodsResult(data);
-
-  return { ...rest, occasionalSubstituteOperatingPeriods };
-};
+export function useGetOccasionalSubstituteOperatingPeriods(
+  dateRange: DateRange,
+) {
+  return useGetSubstituteOperatingPeriods({ ...dateRange, isPreset: false });
+}
