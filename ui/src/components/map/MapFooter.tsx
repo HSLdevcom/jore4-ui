54ec6145c35ae63872d184a51b27597febb1850e
Suvi Kankare <suvi.kankare@vincit.fi>
2025-10-02 13:01:05 +0300

    Update map footer functionality

--- ui/src/components/map/MapFooter.tsx
+++ ui/src/components/map/MapFooter.tsx
@@ -2,16 +2,18 @@ import some from 'lodash/some';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { MdDelete } from 'react-icons/md';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { Row, Visible } from '../../layoutComponents';
 import {
   MapEntityEditorViewState,
   Mode,
   isEditorOpen,
+  resetRouteCreatingAction,
   selectHasChangesInProgress,
   selectHasDraftRouteGeometry,
   selectIsInViewMode,
   selectMapRouteEditor,
+  setRouteMetadataFormOpenAction,
 } from '../../redux';
 import { SimpleButton } from '../../uiComponents';
 import { MapFooterActionsDropdown } from './MapFooterActionsDropdown';
@@ -41,6 +43,7 @@ export const MapFooter: FC<MapFooterProps> = ({
   onSave,
 }) => {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
 
   const {
     drawingMode,
@@ -71,6 +74,72 @@ export const MapFooter: FC<MapFooterProps> = ({
     mapViewState,
     (state) => state !== MapEntityEditorViewState.NONE,
   );
+
+  const someItemIsInPlaceMode = some(
+    mapViewState,
+    (state) => state === MapEntityEditorViewState.PLACE,
+  );
+
+  const isInDrawingMode =
+    drawingMode !== undefined || isRouteMetadataFormOpen || creatingNewRoute;
+
+  const onCancelAddMode = () => {
+    setMapViewState({
+      stops: MapEntityEditorViewState.NONE,
+      stopAreas: MapEntityEditorViewState.NONE,
+      terminals: MapEntityEditorViewState.NONE,
+    });
+  };
+
+  const onCancelDrawMode = () => {
+    if (isRouteMetadataFormOpen) {
+      // Close the metadata form
+      dispatch(setRouteMetadataFormOpenAction(false));
+      // Cancel route creation entirely since form wasn't completed
+      dispatch(resetRouteCreatingAction());
+    } else {
+      // Use the regular cancel for drawing mode
+      onCancel();
+    }
+  };
+
+  const getAddModeText = () => {
+    if (isInDrawingMode) {
+      return t('map.drawRoute');
+    }
+    if (mapViewState.stops === MapEntityEditorViewState.PLACE) {
+      return t('map.addStop');
+    }
+    if (mapViewState.stopAreas === MapEntityEditorViewState.PLACE) {
+      return t('map.createNewStopArea');
+    }
+    if (mapViewState.terminals === MapEntityEditorViewState.PLACE) {
+      return t('map.createNewTerminal');
+    }
+    return '';
+  };
+
+  const getCancelHandler = () => {
+    if (isInDrawingMode) {
+      return onCancelDrawMode;
+    }
+    return onCancelAddMode;
+  };
+
+  if (someItemIsInPlaceMode || isInDrawingMode) {
+    return (
+      <div className="absolute bottom-0 left-1/2 z-[60] w-80 -translate-x-1/2 px-10 py-5">
+        <SimpleButton
+          onClick={getCancelHandler()}
+          containerClassName="w-full"
+          className="hover:border-6 relative flex w-full items-center justify-center rounded-md border-2 border-blue-500 bg-white text-brand shadow-lg hover:bg-opacity-100"
+        >
+          <span className="px-8">{getAddModeText()}</span>
+          <span className="absolute right-3 text-lg font-bold">×</span>
+        </SimpleButton>
+      </div>
+    );
+  }
 
   return (
     <Row className="space-x-4 bg-white px-10 py-5">
