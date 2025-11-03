a4e4d83bec855fc0817e02662fb4ebc8279a19f2
Suvi Kankare <suvi.kankare@vincit.fi>
2025-11-03 14:13:57 +0200

    update infraLinks.sql to Digiroad 2025-02 routes and move to Azure

--- scripts/seed-infrastructure-links.sh
+++ scripts/seed-infrastructure-links.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <docker_container_name>"
+  exit 1
+fi
+
 cd "$(dirname "$0")"/..
+
+if [ ! -f infraLinks.sql ]; then
+  ./scripts/development.sh infralinks:download
+  if [ ! -f infraLinks.sql ]; then
+    echo "Failed to download infrastructure links data."
+    exit 1
+  fi
+fi
 
 DB_CONNECTION_STRING=postgresql://dbadmin:adminpassword@localhost:5432/jore4e2e
 
@@ -11,7 +24,7 @@ while ! $SUCCESS; do
   echo "$1: Checking if infrastructure link schema exists..."
   if [[ $(docker exec $1 psql $DB_CONNECTION_STRING -AXqtc "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'infrastructure_network' AND tablename = 'infrastructure_link');") = "t" ]]; then
     echo "$1: Schema found! Seeding infrastructure links..."
-      docker exec -i $1 psql $DB_CONNECTION_STRING < test-db-manager/src/dumps/infraLinks/infraLinks.sql;
+      docker exec -i $1 psql $DB_CONNECTION_STRING < infraLinks.sql;
       SUCCESS=true
   fi
   sleep 2
