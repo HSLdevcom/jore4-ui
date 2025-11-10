0266d255677ff662013cceda5454815335dfb52d
Suvi Kankare <suvi.kankare@vincit.fi>
2025-11-10 15:55:09 +0200

    Add custom infospot purposes to dropdown

---
+++ ui/src/components/stop-registry/stops/stop-details/info-spots/queries/useGetInfoSpotPurposes.ts
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+import compact from 'lodash/compact';
+import uniq from 'lodash/uniq';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useGetInfoSpotPurposesQuery } from '../../../../../../generated/graphql';
+import { stringToInfoSpotPurposeEnum } from '../utils/infoSpotPurposeUtils';
+
+const GQL_GET_INFO_SPOT_PURPOSES = gql`
+  query GetInfoSpotPurposes {
+    stopsDb: stops_database {
+      latestInfoSpots: stops_database_info_spot(
+        distinct_on: netex_id
+        order_by: [{ netex_id: asc }, { version: desc }]
+      ) {
+        purpose
+      }
+    }
+  }
+`;
+
+export function useGetInfoSpotPurposes() {
+  const { data, loading } = useGetInfoSpotPurposesQuery();
+  const {
+    i18n: { language },
+  } = useTranslation();
+
+  const collator = useMemo(() => new Intl.Collator(language), [language]);
+
+  const customPurposes = useMemo(() => {
+    if (!data || loading) {
+      return [];
+    }
+
+    // Collect purposes from latest version of each info spot,
+    // so that unused purposes are not included in the dropdown
+    const inDB = compact(
+      data.stopsDb?.latestInfoSpots?.map((it) => it.purpose),
+    );
+    const withoutEnums = inDB.filter(
+      (purpose) => !stringToInfoSpotPurposeEnum(purpose),
+    );
+
+    return uniq(withoutEnums).sort(collator.compare.bind(collator));
+  }, [data, loading, collator]);
+
+  return { customPurposes, loading };
+}
