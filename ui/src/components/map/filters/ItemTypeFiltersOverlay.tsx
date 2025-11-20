bbd94317cd9902f4bd034bc92d17f766356c14bc
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-11-20 10:23:08 +0200

    Add close icon to the map filter panel

--- ui/src/components/map/filters/ItemTypeFiltersOverlay.tsx
+++ ui/src/components/map/filters/ItemTypeFiltersOverlay.tsx
@@ -1,18 +1,32 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { twMerge } from 'tailwind-merge';
+import { useAppDispatch } from '../../../hooks';
+import { setShowMapEntityTypeFilterOverlayAction } from '../../../redux';
+import { CloseIconButton } from '../../../uiComponents';
 import { MapOverlay, MapOverlayHeader } from '../MapOverlay';
 import { MapEntityTypeFilters } from './MapEntityTypeFilters';
 import { TimeAndPriorityFilters } from './TimeAndPriorityFilters';
 import { ClassNameProps } from './types';
 
+const testIds = {
+  closeButton: 'FilterPanel::closeButton',
+};
+
 export const ItemTypeFiltersOverlay: FC<ClassNameProps> = ({ className }) => {
   const { t } = useTranslation();
+
+  const dispatch = useAppDispatch();
+
+  const onCloseClick = () => {
+    dispatch(setShowMapEntityTypeFilterOverlayAction(false));
+  };
 
   return (
     <MapOverlay className={twMerge(`w-[430px] rounded-b`, className)}>
       <MapOverlayHeader>
         <h4>{t('filters.title')}</h4>
+        <CloseIconButton onClick={onCloseClick} testId={testIds.closeButton} />
       </MapOverlayHeader>
       <div className="flex gap-4">
         <MapEntityTypeFilters className="p-4" />
