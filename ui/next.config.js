6a2311cf1a04e576b7709b03535ca5a0553f439a
Jesse Jaara <jesse.jaara@vincit.fi>
2026-01-12 13:09:55 +0200

    Update most of the deps
    These would get automatically upgraded in yarn.lock when we later
    update HeadlessUI and Tailwind. So mark them as updated in
    package.json files as well.

--- ui/next.config.js
+++ ui/next.config.js
@@ -9,13 +9,6 @@ module.exports = {
       },
     ];
   },
-  eslint: {
-    // Don't run eslint as part of builds.
-    // We have separate CI job to run it, so it is not useful to run it
-    // as part of builds. (It also seems like `next lint` command does not
-    // respect our eslint config defined in `.eslintrc.js`.)
-    ignoreDuringBuilds: true,
-  },
   output: 'export',
   productionBrowserSourceMaps: true,
 
