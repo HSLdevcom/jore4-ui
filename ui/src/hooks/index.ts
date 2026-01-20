975d5012a38f0d82a378f003063e89f35adc2738
Suvi Kankare <suvi.kankare@vincit.fi>
2026-01-20 13:08:43 +0200

    Get usernames from login_audit table

--- ui/src/hooks/index.ts
+++ ui/src/hooks/index.ts
@@ -3,5 +3,6 @@ export * from './redux';
 export * from './urlQuery';
 export * from './useCallbackOnKeyEscape';
 export * from './useDebouncedString';
+export * from './useGetUserNames';
 export * from './useRequiredParams';
 export * from './useSafeBackNavigation';
