2478e7bd9ae23e1b1dac451a3f51368b400266f2
Jesse Jaara <jesse.jaara@vincit.fi>
2026-02-04 15:48:52 +0200

    Apollo Cache: Proper keyFields for `..._stop_place_quays`

--- ui/src/graphql/client.ts
+++ ui/src/graphql/client.ts
@@ -303,6 +303,9 @@ const buildCacheDefinition = () => {
       stops_database_stop_place_children: {
         keyFields: ['stop_place_id', 'children_id'],
       },
+      stops_database_stop_place_quays: {
+        keyFields: ['stop_place_id', 'quays_id'],
+      },
       stops_database_quay_newest_version: {
         keyFields: ['netex_id'],
       },
