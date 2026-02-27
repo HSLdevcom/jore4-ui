a34f2c9c2e7d72f4c94dc9cf276f306451ac62a9
Suvi Kankare <suvi.kankare@vincit.fi>
2026-02-27 13:29:55 +0200

    Show latest changes in stop details page

--- ui/src/components/stop-registry/stops/change-history/queries/index.ts
+++ ui/src/components/stop-registry/stops/change-history/queries/index.ts
@@ -1,3 +1,4 @@
 export * from './useGetHistoricalStopPlace';
+export * from './useGetLatestStopChangeHistoryItems';
 export * from './useGetStopChangeHistoryItems';
 export * from './useGetTodaysNameForQuay';
