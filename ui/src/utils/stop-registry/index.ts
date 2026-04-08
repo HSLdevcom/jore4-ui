5bbaa5e4c3937ac8f3f7dc14f8f03d476edb85d8
Pasi Vuohijoki <pasi.vuohijoki@vincit.fi>
2026-04-08 13:58:20 +0300

    add mirrors/mirroredBy key-value support for multi-transport-mode stops  - User Story 35370

--- ui/src/utils/stop-registry/index.ts
+++ ui/src/utils/stop-registry/index.ts
@@ -1,4 +1,4 @@
-export * from './stopPlace';
 export * from './alternativeNames';
 export * from './buildSearchStopByLabelOrNameFilter';
+export * from './stopPlace';
 export * from './transportMode';
