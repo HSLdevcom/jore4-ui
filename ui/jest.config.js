d4cfc97076a2e3fa25eef7d277eb6afaf07d3789
Jesse Jaara <jesse.jaara@vincit.fi>
2026-04-28 23:03:11 +0300

    Update base dependencies
    Not updated:
    - Eslint: Held back at V9 due to single plugin developers insisting on
      maintaining backwards compatibility indefinitely (Node v4 | Eslint V2):
      * `eslint-plugin-import`: 505 open issues, 87 open prs....
        Will be replaced with `eslint-plugin-import-x` which is well
        maintained and established fork of the og plugin targeting only
        modern platforms, in the next commit.
      * `eslint-plugin-react`: 276 open issues and 72 prs. No proper well
        established drop-in replacement exists, there is `eslint-plugin-react-x`
        but it's commit history and version release history are questionable.
        We don't need most of the rules, but this needs further investigation.
      * `eslint-plugin-jsx-a11y`: 426 open issues and 17 prs. Some migration
        path might exist with `axe-core` project, but further investigation
        has to be done.
    - Apollo: Will be done in a separate commit
    - React & friends Sticking to V18
    - Hookform & Zod: Ticket exists

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,9 +1,16 @@
+const { createDefaultPreset } = require('ts-jest');
+
 module.exports = {
-  preset: 'ts-jest',
+  ...createDefaultPreset({
+    tsconfig: 'tsconfig.test.json',
+    diagnostics: {
+      // Needed to suppress an error with TS6.
+      // See https://github.com/kulshekhar/ts-jest/pull/5273 and related,
+      // for further steps, once a proper fix gets added.
+      ignoreCodes: [5107],
+    },
+  }),
   testEnvironment: 'jsdom',
-  transform: {
-    '.(ts|tsx)': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
-  },
   setupFilesAfterEnv: ['./jest.setup.ts'],
   moduleDirectories: ['node_modules', 'src/utils'],
   testPathIgnorePatterns: ['node_modules', './cypress'],
