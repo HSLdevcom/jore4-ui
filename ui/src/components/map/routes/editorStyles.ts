788030ccaf39d032730245cdd0b29adcce809ef7
patrickangasmaki <patric.kangasmaki@vincit.fi>
2026-05-13 15:30:10 +0300

    Adjust map route editor styles
    This commit aims to make the route editor visuals better.
    - Less clutter on the map
    - Changes in the edit layer functionality

--- ui/src/components/map/routes/editorStyles.ts
+++ ui/src/components/map/routes/editorStyles.ts
@@ -1,5 +1,4 @@
-const FEATURE_INACTIVE_COLOR = '#696969'; // Dark grey
-const FEATURE_ACTIVE_COLOR = FEATURE_INACTIVE_COLOR;
+import { theme } from '../../../generated/theme';
 
 export const ACTIVE_LINE_STROKE_ID = 'active-line-stroke';
 
@@ -13,57 +12,31 @@ export const styles = [
       'line-join': 'round',
     },
     paint: {
-      'line-color': FEATURE_INACTIVE_COLOR,
-      'line-dasharray': [1.2, 0.8],
-      'line-width': 8,
+      'line-color': theme.colors.hslDark80,
+      'line-dasharray': [1, 0.5],
+      'line-width': 2,
     },
   },
   {
     id: 'gl-draw-polygon-midpoint',
-    type: 'circle',
+    type: 'symbol',
     filter: ['all', ['==', '$type', 'Point'], ['==', 'meta', 'midpoint']],
-    paint: {
-      'circle-radius': 8,
-      'circle-color': FEATURE_INACTIVE_COLOR,
-    },
-  },
-  {
-    id: 'gl-draw-line-vertex-halo-inactive',
-    type: 'circle',
-    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point']],
-    paint: {
-      'circle-radius': 12,
-      'circle-color': FEATURE_INACTIVE_COLOR,
+    layout: {
+      'icon-image': 'route_vector_icon',
+      'icon-size': ['interpolate', ['linear'], ['zoom'], 8, 0.11, 16, 0.2],
+      'icon-allow-overlap': true,
+      'icon-ignore-placement': true,
     },
   },
   {
     id: 'gl-draw-line-vertex-inactive',
-    type: 'circle',
-    filter: [
-      'all',
-      ['==', 'meta', 'vertex'],
-      ['==', '$type', 'Point'],
-      ['!=', 'mode', 'static'],
-      ['!=', 'mode', 'simple_select'],
-    ],
-    paint: {
-      'circle-radius': 9,
-      'circle-color': 'white',
-    },
-  },
-  {
-    id: 'gl-draw-line-vertex-active',
-    type: 'circle',
-    filter: [
-      'all',
-      ['==', 'meta', 'vertex'],
-      ['==', '$type', 'Point'],
-      ['!=', 'mode', 'static'],
-      ['==', 'active', 'true'],
-    ],
-    paint: {
-      'circle-radius': 4,
-      'circle-color': FEATURE_ACTIVE_COLOR,
+    type: 'symbol',
+    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point']],
+    layout: {
+      'icon-image': 'route_vector_icon',
+      'icon-size': ['interpolate', ['linear'], ['zoom'], 8, 0.17, 16, 0.27],
+      'icon-allow-overlap': true,
+      'icon-ignore-placement': true,
     },
   },
 ];
