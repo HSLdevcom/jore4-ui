5c28707e4e5036e2a9cda2958086cedb225cae6b
Jesse Jaara <jesse.jaara@vincit.fi>
2026-05-27 18:11:54 +0300

    Map: Mark route stops as TrunkLine stops on edit

--- ui/src/components/map/routes/RouteEditor.tsx
+++ ui/src/components/map/routes/RouteEditor.tsx
@@ -8,7 +8,6 @@ import { useTranslation } from 'react-i18next';
 import { useMap } from 'react-map-gl/maplibre';
 import {
   RouteDefaultFieldsFragment,
-  RouteRoute,
   useGetRouteDetailsByIdLazyQuery,
 } from '../../../generated/graphql';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
@@ -31,10 +30,10 @@ import {
   stopRouteEditingAction,
 } from '../../../redux';
 import { isDateInRange } from '../../../time';
-import { RequiredKeys } from '../../../types';
 import { Priority } from '../../../types/enums';
 import { ConfirmationDialog } from '../../../uiComponents';
 import {
+  showDangerToastWithError,
   showSuccessToast,
   showWarningToast,
   stopInJourneyPatternFieldsToRemove,
@@ -46,8 +45,17 @@ import {
   ConflictResolverModal,
   mapRouteToCommonConflictItem,
 } from '../../routes-and-lines/common/ConflictResolverModal';
+import {
+  SaveBlockers,
+  getBlockers,
+  hasBlockers,
+} from '../../routes-and-lines/common/SaveBlockers';
+import { StopsNeedingUpdateModal } from '../../routes-and-lines/common/StopsNeedingUpdateModal';
+import { StopMetaTypeUpdateInfo } from '../../routes-and-lines/common/useUpdateStopRegistryStopMetatype';
 import { useMapUrlStateContext } from '../utils/mapUrlState';
 import {
+  CreateChanges,
+  EditChanges,
   mapRouteToFormState,
   useCreateRoute,
   useDeleteRoute,
@@ -55,26 +63,21 @@ import {
 } from './hooks';
 import { SNAPPING_LINE_LAYER_ID } from './utils';
 
-const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
-  _,
-  externalRef,
-) => {
+// default handler that can be used to show error messages as toast
+// in case an exception is thrown
+function useDefaultErrorHandler() {
   const { t } = useTranslation();
-  const dispatch = useAppDispatch();
-  const { current: map } = useMap();
 
-  const { creatingNewRoute } = useAppSelector(selectMapRouteEditor);
-  const drawingMode = useAppSelector(selectDrawingMode);
-  const selectedRouteId = useAppSelector(selectSelectedRouteId);
+  return (err: unknown) =>
+    showDangerToastWithError(
+      t(($) => $.errors.saveFailed),
+      err,
+    );
+}
 
-  const {
-    id: editedRouteId,
-    infraLinks,
-    stopsEligibleForJourneyPattern,
-    includedStopLabels,
-    journeyPattern,
-    metaData: routeDetails,
-  } = useAppSelector(selectEditedRouteData);
+function useCreateRouteHelper() {
+  const { t } = useTranslation();
+  const defaultErrorHandler = useDefaultErrorHandler();
 
   const {
     state: {
@@ -82,75 +85,42 @@ const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
     },
     setDisplayedRoute,
     setFlatUrlState,
-    resetUrlState,
   } = useMapUrlStateContext();
 
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [conflicts, setConflicts] = useState<
-    ReadonlyArray<RouteDefaultFieldsFragment>
-  >([]);
-
-  const [getRouteDetailsById] = useGetRouteDetailsByIdLazyQuery();
+  const dispatch = useAppDispatch();
 
   const {
-    prepareCreate,
-    mapCreateChangesToVariables,
-    insertRouteMutation,
-    defaultErrorHandler: defaultInsertRouteErrorHandler,
-  } = useCreateRoute();
+    editedRouteData: { lineInfo },
+  } = useAppSelector(selectMapRouteEditor);
 
   const {
-    prepareEditGeometry,
-    mapEditGeometryChangesToVariables,
-    editRouteGeometryMutation,
-  } = useEditRouteGeometry();
-
-  const { deleteRoute, defaultErrorHandler: defaultDeleteErrorHandler } =
-    useDeleteRoute();
-
-  const editRoute = async (routeId: UUID) => {
-    const changes = await prepareEditGeometry({
-      routeId,
-      stopsEligibleForJourneyPattern,
-      includedStopLabels,
-      journeyPatternStops: journeyPattern.stops,
-      infraLinksAlongRoute: infraLinks ?? [],
-      // journeyPattern id is defined when editing route
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      journeyPatternId: journeyPattern.id!,
-    });
-
-    const variables = mapEditGeometryChangesToVariables(changes);
-
-    await editRouteGeometryMutation(variables);
-  };
+    infraLinks,
+    stopsEligibleForJourneyPattern,
+    includedStopLabels,
+    journeyPattern,
+    metaData: routeDetails,
+  } = useAppSelector(selectEditedRouteData);
 
   const { setIsLoading } = useLoader(Operation.SaveRoute);
 
-  const createRoute = async () => {
-    const changes = await prepareCreate({
-      // At the point of saving a route, the form has been validated
-      // and it contains all required values
-      form: routeDetails as RouteFormState,
-      stopsEligibleForJourneyPattern,
-      includedStopLabels,
-      journeyPatternStops: journeyPattern.stops,
-      infraLinksAlongRoute: infraLinks ?? [],
-    });
-    if (changes.conflicts?.length) {
-      setConflicts(changes.conflicts);
-      return undefined;
+  const [pendingChanges, setPendingChanges] = useState<CreateChanges | null>(
+    null,
+  );
+
+  const { prepareCreate, insertRouteMutation } = useCreateRoute();
+
+  const onCommitCreateChanges = async (changes: CreateChanges) => {
+    const result = await insertRouteMutation(changes);
+    const newRoute = result.data?.insert_route_route_one;
+
+    // Should never happen as insertRouteMutation itself will throw if the mutation failed.
+    // But typings are not good.
+    if (!newRoute) {
+      throw new Error('Failed to insert route!');
     }
-    const variables = mapCreateChangesToVariables(changes);
 
-    const response = await insertRouteMutation(variables);
+    showSuccessToast(t(($) => $.routes.saveSuccess));
 
-    return response.data?.insert_route_route_one;
-  };
-
-  const onRouteCreated = (
-    newRoute: RequiredKeys<Partial<RouteRoute>, 'route_id'>,
-  ) => {
     // Select created route
     dispatch(setSelectedRouteIdAction(newRoute.route_id));
 
@@ -184,6 +154,223 @@ const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
     dispatch(resetDraftRouteGeometryAction());
     dispatch(resetRouteCreatingAction());
   };
+
+  const onDoCreate = async () => {
+    try {
+      setIsLoading(true);
+      const changes = await prepareCreate({
+        // At the point of saving a route, the form has been validated
+        // and it contains all required values
+        form: routeDetails as RouteFormState,
+        stopsEligibleForJourneyPattern,
+        includedStopLabels,
+        journeyPatternStops: journeyPattern.stops,
+        infraLinksAlongRoute: infraLinks ?? [],
+        lineType: lineInfo?.type_of_line ?? null,
+      });
+
+      if (hasBlockers(changes)) {
+        setPendingChanges(changes);
+      } else {
+        await onCommitCreateChanges(changes);
+      }
+    } catch (e) {
+      defaultErrorHandler(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    pendingCreateChanges: pendingChanges,
+    onCancelCreate: () => setPendingChanges(null),
+    onConfirmCreate: pendingChanges
+      ? () => {
+          setIsLoading(true);
+          setPendingChanges(null);
+
+          return onCommitCreateChanges(pendingChanges).finally(() =>
+            setIsLoading(false),
+          );
+        }
+      : null,
+    onDoCreate,
+  };
+}
+
+function useEditRouteHelper() {
+  const { t } = useTranslation();
+  const defaultErrorHandler = useDefaultErrorHandler();
+  const dispatch = useAppDispatch();
+
+  const {
+    id,
+    infraLinks,
+    stopsEligibleForJourneyPattern,
+    includedStopLabels,
+    journeyPattern,
+    metaData: routeDetails,
+    lineInfo,
+  } = useAppSelector(selectEditedRouteData);
+
+  const { setIsLoading } = useLoader(Operation.SaveRoute);
+
+  const [pendingChanges, setPendingChanges] = useState<EditChanges | null>(
+    null,
+  );
+
+  const { prepareEditGeometry, editRouteGeometryMutation } =
+    useEditRouteGeometry();
+
+  const onCommitEditChanges = async (changes: EditChanges) => {
+    await editRouteGeometryMutation(changes);
+
+    showSuccessToast(t(($) => $.routes.saveSuccess));
+    dispatch(stopRouteEditingAction());
+  };
+
+  const onDoEdit = async () => {
+    if (!infraLinks || !journeyPattern.id || !routeDetails || !lineInfo) {
+      const fields = JSON.stringify(
+        {
+          infraLinks,
+          'journeyPattern.id': journeyPattern.id,
+          routeDetails,
+          lineInfo,
+        },
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        (v) => (v === undefined ? '!!undefined¡¡' : v),
+        0,
+      );
+      throw new Error(
+        `Illegal state! Expected all fields (${fields}) to be valid!`,
+      );
+    }
+
+    try {
+      const changes = await prepareEditGeometry({
+        routeId: id ?? '',
+        stopsEligibleForJourneyPattern,
+        includedStopLabels,
+        journeyPatternStops: journeyPattern.stops,
+        infraLinksAlongRoute: infraLinks,
+        journeyPatternId: journeyPattern.id,
+        routePriority: routeDetails.priority,
+        lineType: lineInfo.type_of_line,
+      });
+
+      if (hasBlockers(changes)) {
+        setPendingChanges(changes);
+      } else {
+        await onCommitEditChanges(changes);
+      }
+    } catch (e) {
+      defaultErrorHandler(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    pendingEditChanges: pendingChanges,
+    onCancelEdit: () => setPendingChanges(null),
+    onConfirmEdit: pendingChanges
+      ? () => {
+          setIsLoading(true);
+          setPendingChanges(null);
+
+          return onCommitEditChanges(pendingChanges).finally(() =>
+            setIsLoading(false),
+          );
+        }
+      : null,
+    onDoEdit,
+  };
+}
+
+type PendingChanges = {
+  readonly conflicts: ReadonlyArray<RouteDefaultFieldsFragment>;
+  readonly stopsNeedingUpdate: ReadonlyArray<StopMetaTypeUpdateInfo>;
+};
+
+type SaveHelpersConfirm = {
+  readonly pendingChanges: PendingChanges;
+  readonly saveBlockers: SaveBlockers;
+  readonly onCancelSave: () => void;
+  readonly onConfirmSave: () => Promise<unknown>;
+  readonly onDoCreate?: never;
+  readonly onDoEdit?: never;
+};
+
+type SaveHelpersDo = {
+  readonly pendingChanges: null;
+  readonly saveBlockers: null;
+  readonly onCancelSave?: never;
+  readonly onConfirmSave?: never;
+  readonly onDoCreate: () => Promise<unknown>;
+  readonly onDoEdit: () => Promise<unknown>;
+};
+
+function useSaveHelpers(): SaveHelpersConfirm | SaveHelpersDo {
+  const { pendingCreateChanges, onCancelCreate, onConfirmCreate, onDoCreate } =
+    useCreateRouteHelper();
+  const { pendingEditChanges, onCancelEdit, onConfirmEdit, onDoEdit } =
+    useEditRouteHelper();
+
+  if (pendingCreateChanges && onConfirmCreate) {
+    return {
+      pendingChanges: pendingCreateChanges,
+      saveBlockers: getBlockers(pendingCreateChanges),
+      onCancelSave: onCancelCreate,
+      onConfirmSave: onConfirmCreate,
+    };
+  }
+
+  if (pendingEditChanges && onConfirmEdit) {
+    return {
+      pendingChanges: pendingEditChanges,
+      saveBlockers: getBlockers(pendingEditChanges),
+      onCancelSave: onCancelEdit,
+      onConfirmSave: onConfirmEdit,
+    };
+  }
+
+  return { pendingChanges: null, saveBlockers: null, onDoCreate, onDoEdit };
+}
+
+const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
+  _,
+  externalRef,
+) => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const { current: map } = useMap();
+
+  const { creatingNewRoute } = useAppSelector(selectMapRouteEditor);
+  const drawingMode = useAppSelector(selectDrawingMode);
+  const selectedRouteId = useAppSelector(selectSelectedRouteId);
+
+  const { id: editedRouteId, lineInfo } = useAppSelector(selectEditedRouteData);
+
+  const { resetUrlState } = useMapUrlStateContext();
+
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const {
+    pendingChanges,
+    saveBlockers,
+    onCancelSave,
+    onConfirmSave,
+    onDoCreate,
+    onDoEdit,
+  } = useSaveHelpers();
+
+  const [getRouteDetailsById] = useGetRouteDetailsByIdLazyQuery();
+
+  const { deleteRoute, defaultErrorHandler: defaultDeleteErrorHandler } =
+    useDeleteRoute();
+
+  const { setIsLoading } = useLoader(Operation.SaveRoute);
 
   // The "Draw Route" button has been clicked/toggled -> start drawing a new route OR cancel existing drawing
   const onDrawRoute = () => {
@@ -266,25 +453,13 @@ const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
   };
 
   const onSave = async () => {
-    try {
-      setIsLoading(true);
-
+    if (!saveBlockers) {
       if (editedRouteId) {
-        await editRoute(editedRouteId);
-        showSuccessToast(t(($) => $.routes.saveSuccess));
-        dispatch(stopRouteEditingAction());
+        await onDoEdit();
       } else {
-        const createdRoute = await createRoute();
-
-        if (createdRoute) {
-          showSuccessToast(t(($) => $.routes.saveSuccess));
-          onRouteCreated(createdRoute);
-        }
+        await onDoCreate();
       }
-    } catch (err) {
-      defaultInsertRouteErrorHandler(err);
     }
-    setIsLoading(false);
   };
 
   const onDeleteConfirm = async () => {
@@ -338,10 +513,25 @@ const RouteEditorComponent: ForwardRefRenderFunction<ExplicitAny> = (
         confirmText={t(($) => $.confirmDeleteRouteDialog.confirmText)}
         cancelText={t(($) => $.cancel)}
       />
-      <ConflictResolverModal
-        onClose={() => setConflicts([])}
-        conflicts={conflicts.map(mapRouteToCommonConflictItem)}
-      />
+
+      {saveBlockers && (
+        <>
+          <ConflictResolverModal
+            isOpen={saveBlockers.hasConflicts}
+            onClose={onCancelSave}
+            conflicts={
+              pendingChanges?.conflicts.map(mapRouteToCommonConflictItem) ?? []
+            }
+          />
+          <StopsNeedingUpdateModal
+            isOpen={saveBlockers.hasStopsNeedingUpdate}
+            onCancel={onCancelSave}
+            onConfirm={onConfirmSave}
+            stops={pendingChanges?.stopsNeedingUpdate ?? []}
+            typeOfLine={lineInfo?.type_of_line}
+          />
+        </>
+      )}
     </>
   );
 };
