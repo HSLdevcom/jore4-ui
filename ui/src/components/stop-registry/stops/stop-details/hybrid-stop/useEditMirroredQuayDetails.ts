2f117abf080f263f96903f3eaed80a38d365cb18
Pasi Vuohijoki <pasi.vuohijoki@vincit.fi>
2026-06-10 13:02:12 +0300

    Add edit mode into "mirrored" hybrid stop.  - Implement "pysäkin tila" edit  - Move remove from hybrid use -button into edit mode  - User Story 72811

---
+++ ui/src/components/stop-registry/stops/stop-details/hybrid-stop/useEditMirroredQuayDetails.ts
@@ -0,0 +1,70 @@
+import { useTranslation } from 'react-i18next';
+import {
+  StopRegistryStopPlaceInput,
+  useUpdateStopPlaceMutation,
+} from '../../../../../generated/graphql';
+import { EnrichedQuay, EnrichedStopPlace } from '../../../../../types';
+import {
+  KnownValueKey,
+  patchKeyValues,
+  showDangerToast,
+} from '../../../../../utils';
+import { MirroredQuayFormState } from './mirrored-quay-form/schema';
+
+type SaveMirroredQuayParams = {
+  readonly state: MirroredQuayFormState;
+  readonly quay: EnrichedQuay;
+  readonly stopPlace: EnrichedStopPlace;
+};
+
+export function useEditMirroredQuayDetails() {
+  const { t } = useTranslation();
+  const [updateStopPlaceMutation] = useUpdateStopPlaceMutation();
+
+  const buildMutationInput = ({
+    state,
+    quay,
+    stopPlace,
+  }: SaveMirroredQuayParams): StopRegistryStopPlaceInput => {
+    const quayId = quay.id;
+    const otherQuayIds = (stopPlace.quays ?? [])
+      .map((q) => q?.id)
+      .filter((id): id is string => !!id && id !== quayId)
+      .map((id) => ({ id }));
+
+    return {
+      id: stopPlace.id,
+      quays: [
+        ...otherQuayIds,
+        {
+          id: quayId,
+          keyValues: patchKeyValues(quay, [
+            {
+              key: KnownValueKey.StopState,
+              values: state.stopState ? [state.stopState] : [],
+            },
+          ]),
+          versionComment: state.reasonForChange,
+        },
+      ],
+    };
+  };
+
+  const saveMirroredQuayDetails = async (params: SaveMirroredQuayParams) => {
+    const input = buildMutationInput(params);
+    await updateStopPlaceMutation({
+      variables: { input },
+      refetchQueries: [
+        'GetStopDetails',
+        'GetLatestQuayChange',
+        'GetStopChangeHistory',
+      ],
+    });
+  };
+
+  const defaultErrorHandler = (err: Error) => {
+    showDangerToast(`${t(($) => $.errors.saveFailed)}, ${err}`);
+  };
+
+  return { saveMirroredQuayDetails, defaultErrorHandler };
+}
