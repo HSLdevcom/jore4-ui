9f4e8e033d5539cbb12a3797a5311bbafe78f521
patrickangasmaki <patric.kangasmaki@vincit.fi>
2026-06-29 15:06:34 +0300

    Improve contrast in simple button hover
    Hovered state of blue buttons has insufficient contrast.
    The white text has 2.1:1 contrast ratio against the light blue background.
    
    It should be at least 4.5:1.

--- ui/src/components/common/Buttons/SimpleButton.tsx
+++ ui/src/components/common/Buttons/SimpleButton.tsx
@@ -64,7 +64,7 @@ export function getHoverStyles(inverted: boolean, disabled: boolean) {
     return 'hover:outline-brand hover:border-brand';
   }
 
-  return 'hover:outline-tweaked-brand hover:border-tweaked-brand hover:bg-brand/50';
+  return 'hover:bg-tweaked-brand hover:border-tweaked-brand-darker30';
 }
 
 function getCommonClassNames(
