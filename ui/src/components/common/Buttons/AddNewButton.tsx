ad8ee8d36de16876887eb4b6bc85258797f3103f
patrickangasmaki <patric.kangasmaki@vincit.fi>
2026-07-07 10:55:40 +0300

    Adjust add new button gap
    Adjust the gap between the label and icon to match figma design

--- ui/src/components/common/Buttons/AddNewButton.tsx
+++ ui/src/components/common/Buttons/AddNewButton.tsx
@@ -19,7 +19,7 @@ export const AddNewButton: FC<AddNewButtonProps> = ({
   return (
     <TextAndIconButton
       className={twJoin(
-        'gap-4 font-bold text-brand hover:text-black',
+        'gap-1.5 font-bold text-brand hover:text-black',
         className,
       )}
       data-testid={testId}
