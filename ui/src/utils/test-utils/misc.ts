2024eb24927850b01d8aa619ca3a00daf1241794
Jesse Jaara <jesse.jaara@vincit.fi>
2026-08-21 22:06:43 +0300

    Cleanup: Mock lodash/debounce in setup, not at random import

--- ui/src/utils/test-utils/misc.ts
+++ ui/src/utils/test-utils/misc.ts
@@ -9,9 +9,6 @@ export function sleep<T>(timeout: number) {
 // mock the current date to be static
 Date.now = jest.fn(() => 1487076708000);
 
-// mock the lodash debounce module to call the function immediately
-jest.mock('lodash/debounce', () => jest.fn((fn) => fn));
-
 export function getAssertedElement<T>(obj: T | null | undefined): T {
   expect(obj).toBeDefined();
   return obj as T;
