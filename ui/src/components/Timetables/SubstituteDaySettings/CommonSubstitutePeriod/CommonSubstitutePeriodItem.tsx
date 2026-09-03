f25cc343a9a98c4fd44c20356102d6546802449d
Jesse Jaara <jesse.jaara@vincit.fi>
2026-09-03 16:13:42 +0300

    Cleanup|Forms: Move Timetable forms into TimeTables/

--- ui/src/components/Timetables/SubstituteDaySettings/CommonSubstitutePeriod/CommonSubstitutePeriodItem.tsx
+++ ui/src/components/Timetables/SubstituteDaySettings/CommonSubstitutePeriod/CommonSubstitutePeriodItem.tsx
@@ -3,8 +3,10 @@ import { FieldArrayWithId, useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { mapToShortDate } from '../../../../time';
 import { InputField } from '../../../common/Inputs';
-import { LineTypeMultiSelectDropdown } from '../../../forms/timetables/LineTypeMultiSelectDropdown';
-import { SubstituteDayOfWeekDropdown } from '../../../forms/timetables/SubstituteDayOfWeekDropdown';
+import {
+  LineTypeMultiSelectDropdown,
+  SubstituteDayOfWeekDropdown,
+} from '../Components';
 import {
   CommonDayType,
   FormState,
